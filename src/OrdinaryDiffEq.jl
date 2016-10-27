4a55493e95513d159b9be75928e67f2e2f20e614
ChrisRackauckas <me@chrisrackauckas.com>
2016-10-26 20:42:25 -0700

    update requires and recipes

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -3,10 +3,9 @@ module OrdinaryDiffEq
 using DiffEqBase
 import DiffEqBase: solve
 using IterativeSolvers, Parameters, GenericSVD, ForwardDiff, InplaceOps,
-      Sundials, ParameterizedFunctions, Ranges, NLsolve
+      Sundials, ParameterizedFunctions, Ranges, NLsolve, RecipesBase
 
 import Base: linspace
-import Plots: plot
 import ForwardDiff.Dual
 
 macro def(name, definition)
