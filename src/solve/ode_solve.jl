f9d5b082e9ea8090cad1911a41b6a0ca2b68e389
ChrisRackauckas <me@chrisrackauckas.com>
2016-11-08 16:01:50 -0800

    split interface

--- src/solve/ode_solve.jl
+++ src/solve/ode_solve.jl
@@ -203,15 +203,12 @@ function solve{uType,tType,isinplace,T<:OrdinaryDiffEqAlgorithm}(prob::AbstractO
   end
 end
 
-"""
-`solve(prob::ODEProblem,tspan)`
 
-Solves the ODE defined by prob on the interval tspan. If not given, tspan defaults to [0;1.0].
 
-Please see the solver documentation.
-"""
-function solve{uType,tType,isinplace}(prob::AbstractODEProblem{uType,tType,Val{isinplace}},algType=DefaultODEAlgorithm(),timeseries=[],ts=[],ks=[];kwargs...)
-  alg = algType()
+function solve{uType,tType,isinplace,T<:ODEInterfaceAlgorithm}(prob::AbstractODEProblem{uType,tType,Val{isinplace}},
+    alg::Type{T},timeseries=[],ts=[],ks=[];
+    saveat=[],callback=()->nothing,
+    kwargs...)
   tspan = prob.tspan
 
   if tspan[end]-tspan[1]<tType(0)
@@ -219,10 +216,17 @@ function solve{uType,tType,isinplace}(prob::AbstractODEProblem{uType,tType,Val{i
   end
   atomloaded = isdefined(Main,:Atom)
   o = KW(kwargs)
-  o[:t] = tspan[1]
-  o[:Ts] = tspan[2:end]
+  t = tspan[1]
+  Ts = tspan[2:end]
   u0 = prob.u0
-  uEltype = eltype(u0)
+
+  if typeof(u0) <: Number
+    u = [u0]
+  else
+    u = deepcopy(u0)
+  end
+
+  sizeu = size(u)
 
   command_opts = copy(DIFFERENTIALEQUATIONSJL_DEFAULT_OPTIONS)
   for (k,v) in o
@@ -231,84 +235,110 @@ function solve{uType,tType,isinplace}(prob::AbstractODEProblem{uType,tType,Val{i
   # Get the control variables
   @unpack save_timeseries, progressbar = command_opts
 
-  if command_opts[:callback] == nothing
-    callback = ODE_DEFAULT_CALLBACK
-    custom_callback = false
+  ks = Vector{uType}(0)
+
+  saveat = [float(x) for x in command_opts[:saveat]]
+  if !isinplace && typeof(u)<:AbstractArray
+    f! = (t,u,du) -> (du[:] = vec(prob.f(t,reshape(u,sizeu))); nothing)
   else
-    callback = command_opts[:callback]
-    custom_callback = true
+    f! = (t,u,du) -> (prob.f(t,reshape(u,sizeu),reshape(du,sizeu)); u = vec(u); du=vec(du); nothing)
+  end
+  initialize_backend(:ODEInterface)
+  o[:RHS_CALLMODE] = ODEInterface.RHS_CALL_INSITU
+  dict = buildOptions(o,ODEINTERFACE_OPTION_LIST,ODEINTERFACE_ALIASES,ODEINTERFACE_ALIASES_REVERSED)
+  opts = ODEInterface.OptionsODE([Pair(ODEINTERFACE_STRINGS[k],v) for (k,v) in dict]...) #Convert to the strings
+  du = similar(u)
+  if alg <: dopri5
+    ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.dopri5,f!,[t;Ts],vec(u),opts)
+  elseif alg <: dop853
+    ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.dop853,f!,[t;Ts],vec(u),opts)
+  elseif alg <: odex
+    ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.odex,f!,[t;Ts],vec(u),opts)
+  elseif alg <: seulex
+    ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.seulex,f!,[t;Ts],vec(u),opts)
+  elseif alg <: radau
+    ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.radau,f!,[t;Ts],vec(u),opts)
+  elseif alg <: radau5
+    ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.radau5,f!,[t;Ts],vec(u),opts)
+  end
+  if retcode < 0
+    if retcode == -1
+      warn("Input is not consistent.")
+    elseif retcode == -2
+      warn("Interrupted. Larger maxiters is needed.")
+    elseif retcode == -3
+      warn("Step size went too small.")
+    elseif retcode == -4
+      warn("Interrupted. Problem is probably stiff.")
+    end
+  end
+  t = ts[end]
+  if typeof(u0)<:AbstractArray
+    timeseries = Vector{uType}(0)
+    for i=1:size(vectimeseries,1)
+      push!(timeseries,reshape(view(vectimeseries,i,:,)',sizeu))
+    end
+  else
+    timeseries = vec(vectimeseries)
   end
 
-  if uEltype<:Number
-    u = copy(u0)
+  u = timeseries[end]
+
+  if typeof(prob) <: ODETestProblem
+    timeseries_analytic = Vector{uType}(0)
+    for i in 1:size(timeseries,1)
+      push!(timeseries_analytic,prob.analytic(ts[i],u0))
+    end
+    return(ODESolution(ts,timeseries,prob,alg,
+    u_analytic=timeseries_analytic,
+    k=ks,saveat=saveat,
+    timeseries_errors = command_opts[:timeseries_errors],
+    dense_errors = command_opts[:dense_errors]))
+  else
+    return(ODESolution(ts,timeseries,prob,alg,k=ks,saveat=saveat))
+  end
+end
+
+"""
+`solve(prob::ODEProblem,tspan)`
+
+Solves the ODE defined by prob on the interval tspan. If not given, tspan defaults to [0;1.0].
+
+Please see the solver documentation.
+"""
+function solve{uType,tType,isinplace,algType}(prob::AbstractODEProblem{uType,tType,Val{isinplace}},
+    alg::Type{algType}=DefaultODEAlgorithm(),timeseries=[],ts=[],ks=[];
+    saveat=[],callback=()->nothing,
+    kwargs...)
+  tspan = prob.tspan
+
+  if tspan[end]-tspan[1]<tType(0)
+    error("final time must be greater than starting time. Aborting.")
+  end
+  atomloaded = isdefined(Main,:Atom)
+  o = KW(kwargs)
+  t = tspan[1]
+  Ts = tspan[2:end]
+  u0 = prob.u0
+
+  if typeof(u0) <: Number
+    u = [u0]
   else
     u = deepcopy(u0)
   end
 
+  sizeu = size(u)
+
+  command_opts = copy(DIFFERENTIALEQUATIONSJL_DEFAULT_OPTIONS)
+  for (k,v) in o
+    command_opts[k]=v
+  end
   ks = Vector{uType}(0)
 
-  if typeof(alg) <: ODEInterfaceAlgorithm
-    sizeu = size(u)
-    if typeof(u) <: Number
-      u = [u]
-    end
-    o[:Ts] = float(o[:Ts])
-    o[:t] = float(o[:t])
-    t = o[:t]; Ts = o[:Ts]
-    saveat = [float(x) for x in command_opts[:saveat]]
-    if !isinplace && typeof(u)<:AbstractArray
-      f! = (t,u,du) -> (du[:] = vec(prob.f(t,reshape(u,sizeu))); nothing)
-    else
-      f! = (t,u,du) -> (prob.f(t,reshape(u,sizeu),reshape(du,sizeu)); u = vec(u); du=vec(du); nothing)
-    end
-    initialize_backend(:ODEInterface)
-    o[:RHS_CALLMODE] = ODEInterface.RHS_CALL_INSITU
-    dict = buildOptions(o,ODEINTERFACE_OPTION_LIST,ODEINTERFACE_ALIASES,ODEINTERFACE_ALIASES_REVERSED)
-    opts = ODEInterface.OptionsODE([Pair(ODEINTERFACE_STRINGS[k],v) for (k,v) in dict]...) #Convert to the strings
-    du = similar(u)
-    if typeof(alg) <: dopri5
-      ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.dopri5,f!,[t;Ts],vec(u),opts)
-    elseif typeof(alg) <: dop853
-      ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.dop853,f!,[t;Ts],vec(u),opts)
-    elseif typeof(alg) <: odex
-      ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.odex,f!,[t;Ts],vec(u),opts)
-    elseif typeof(alg) <: seulex
-      ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.seulex,f!,[t;Ts],vec(u),opts)
-    elseif typeof(alg) <: radau
-      ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.radau,f!,[t;Ts],vec(u),opts)
-    elseif typeof(alg) <: radau5
-      ts,vectimeseries,retcode,stats = ODEInterface.odecall(ODEInterface.radau5,f!,[t;Ts],vec(u),opts)
-    end
-    if retcode < 0
-      if retcode == -1
-        warn("Input is not consistent.")
-      elseif retcode == -2
-        warn("Interrupted. Larger maxiters is needed.")
-      elseif retcode == -3
-        warn("Step size went too small.")
-      elseif retcode == -4
-        warn("Interrupted. Problem is probably stiff.")
-      end
-    end
-    t = ts[end]
-    if typeof(u0)<:AbstractArray
-      timeseries = Vector{uType}(0)
-      for i=1:size(vectimeseries,1)
-        push!(timeseries,reshape(view(vectimeseries,i,:,)',sizeu))
-      end
-    else
-      timeseries = vec(vectimeseries)
-    end
-    u = timeseries[end]
-  elseif typeof(alg) <: ODEIterAlgorithm
-    if typeof(u) <: Number
-      u = [u]
-    end
+  if alg <: ODEIterAlgorithm
+
     # Needs robustness
-    o[:Ts] = float(o[:Ts])
-    o[:t] = float(o[:t])
-    t = o[:t]; Ts = o[:Ts]
-    o[:T] = Ts[end]
+    T = Ts[end]
     saveat = [float(x) for x in command_opts[:saveat]]
     initialize_backend(:ODEJL)
     opts = buildOptions(o,ODEJL_OPTION_LIST,ODEJL_ALIASES,ODEJL_ALIASES_REVERSED)
@@ -320,23 +350,23 @@ function solve{uType,tType,isinplace}(prob::AbstractODEProblem{uType,tType,Val{i
     ode  = ODE.ExplicitODE(t,u,f!)
     # adaptive==true ? FoA=:adaptive : FoA=:fixed #Currently limied to only adaptive
     FoA = :adaptive
-    if typeof(alg) <: ode23
+    if alg <: ode23
       solver = ODE.RKIntegrator{FoA,:rk23}
-    elseif typeof(alg) <: ode45
+    elseif alg <: ode45
       solver = ODE.RKIntegrator{FoA,:dopri5}
-    elseif typeof(alg) <: ode78
+    elseif alg <: ode78
       solver = ODE.RKIntegrator{FoA,:feh78}
-    elseif typeof(alg) <: ode23s
+    elseif alg <: ode23s
       solver = ODE.ModifiedRosenbrockIntegrator
-    elseif typeof(alg) <: ode1
+    elseif alg <: ode1
       solver = ODE.RKIntegratorFixed{:feuler}
-    elseif typeof(alg) <: ode2_midpoint
+    elseif alg <: ode2_midpoint
       solver = ODE.RKIntegratorFixed{:midpoint}
-    elseif typeof(alg) <: ode2_heun
+    elseif alg <: ode2_heun
       solver = ODE.RKIntegratorFixed{:heun}
-    elseif typeof(alg) <: ode4
+    elseif alg <: ode4
       solver = ODE.RKIntegratorFixed{:rk4}
-    elseif typeof(alg) <: ode45_fe
+    elseif alg <: ode45_fe
       solver = ODE.RKIntegrator{FoA,:rk45}
     end
     out = ODE.solve(ode;solver=solver,opts...)
@@ -355,22 +385,14 @@ function solve{uType,tType,isinplace}(prob::AbstractODEProblem{uType,tType,Val{i
     end
     t = ts[end]
     u = timeseries[end]
-  elseif typeof(alg) <: SundialsAlgorithm
-    if typeof(alg) == cvode_BDF
+  elseif alg <: SundialsAlgorithm
+    if alg == cvode_BDF
       integrator = :BDF
-    elseif typeof(alg) ==  cvode_Adams
+    elseif alg ==  cvode_Adams
       integrator = :Adams
     end
 
-    sizeu = size(u)
-    if typeof(u) <: Number
-      u = [u]
-    end
-    u = map(Float64,u) # Needs Float64
     # Needs robustness
-    o[:Ts] = map(Float64,o[:Ts])
-    o[:t] = map(Float64,o[:t])
-    t = o[:t]; Ts = o[:Ts];
     saveat = [float(x) for x in command_opts[:saveat]]
     if !isinplace && typeof(u)<:AbstractArray
       f! = (t,u,du) -> (du[:] = vec(prob.f(t,reshape(u,sizeu))); 0)
@@ -413,8 +435,6 @@ function solve{uType,tType,isinplace}(prob::AbstractODEProblem{uType,tType,Val{i
     t = ts[end]
     u = timeseries[end]
   end
-
-  (atomloaded && progressbar) ? Main.Atom.progress(1) : nothing #Use Atom's progressbar if loaded
 
   if typeof(prob) <: ODETestProblem
     timeseries_analytic = Vector{uType}(0)
