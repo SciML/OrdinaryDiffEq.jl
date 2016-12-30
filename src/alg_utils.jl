9f9102a3442600d86a7dec656fce088fa4148ca3
ChrisRackauckas <me@chrisrackauckas.com>
2016-12-30 09:40:55 -0800

    alg funcs

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -22,3 +22,64 @@ function isimplicit(alg::OrdinaryDiffEqAlgorithm)
     return false
   end
 end
+
+qmin_default(alg::OrdinaryDiffEqAlgorithm) = 0.2
+qmin_default(alg::DP8) = 0.333
+
+qmax_default(alg::OrdinaryDiffEqAlgorithm) = 10.0
+qmax_default(alg::DP8) = 6.0
+
+alg_order(alg::OrdinaryDiffEqAlgorithm) = error("Order is not defined for this algorithm")
+alg_adaptive_order(alg::OrdinaryDiffEqAdaptiveAlgorithm) = error("Algorithm is adaptive with no order")
+
+alg_order(alg::Euler) = 1
+alg_order(alg::Midpoint) = 2
+alg_order(alg::RK4) = 4
+alg_order(alg::ExplicitRK) = 4
+alg_order(alg::BS3) = 3
+alg_order(alg::BS5) = 5
+alg_order(alg::DP5) = 5
+alg_order(alg::DP5Threaded) = 5
+alg_order(alg::Tsit5) = 5
+alg_order(alg::DP8) = 8
+alg_order(alg::Vern6) = 6
+alg_order(alg::Vern7) = 7
+alg_order(alg::Vern8) = 8
+alg_order(alg::Vern9) = 9
+alg_order(alg::TanYam7) = 7
+alg_order(alg::TsitPap8) = 8
+alg_order(alg::ImplicitEuler) = 1
+alg_order(alg::Trapezoid) = 2
+alg_order(alg::Rosenbrock23) = 2
+alg_order(alg::Rosenbrock32) = 3
+alg_order(alg::Feagin10) = 10
+alg_order(alg::Feagin12) = 12
+alg_order(alg::Feagin14) = 14
+
+alg_adaptive_order(alg::ExplicitRK) = 3
+alg_adaptive_order(alg::BS3) = 2
+alg_adaptive_order(alg::BS5) = 4
+alg_adaptive_order(alg::DP5) = 4
+alg_adaptive_order(alg::DP5Threaded) = 4
+alg_adaptive_order(alg::Tsit5) = 4
+alg_adaptive_order(alg::DP8) = 6
+alg_adaptive_order(alg::Vern6) = 5
+alg_adaptive_order(alg::Vern7) = 6
+alg_adaptive_order(alg::Vern8) = 7
+alg_adaptive_order(alg::Vern9) = 8
+alg_adaptive_order(alg::TanYam7) = 6
+alg_adaptive_order(alg::TsitPap8) = 7
+alg_adaptive_order(alg::Rosenbrock23) = 3
+alg_adaptive_order(alg::Rosenbrock32) = 2
+alg_adaptive_order(alg::Feagin10) = 8
+alg_adaptive_order(alg::Feagin12) = 10
+alg_adaptive_order(alg::Feagin14) = 12
+
+beta2_default(alg::OrdinaryDiffEqAlgorithm) = 0.4/alg_order(alg)
+beta2_default(alg::DP8) = 0.0
+beta2_default(alg::DP5) = 0.04
+beta2_default(alg::DP5Threaded) = 0.04
+
+beta1_default(alg::OrdinaryDiffEqAlgorithm,beta2) = 0.7/alg_order(alg)
+beta1_default(alg::DP8,beta2) = 1/alg_order(alg) - 0.2beta2
+beta1_default(alg::DP5,beta2) = 1/alg_order(alg) - 0.75beta2
