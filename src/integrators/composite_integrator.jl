aee88680254dc6cd7f2238293cbcf39fe5e22fcb
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-05 11:16:46 -0800

    composite benchmarks well, reorganize

--- src/integrators/composite_integrator.jl
+++ src/integrators/composite_integrator.jl
@@ -7,3 +7,36 @@ end
 @inline function perform_step!(integrator::ODEIntegrator,cache::CompositeCache)
   perform_step!(integrator,cache.caches[cache.current])
 end
+
+"""
+If no user default, then this will change the default to the defaults
+for the second algorithm.
+"""
+@inline function reset_alg_dependent_opts!(integrator,alg1,alg2)
+  integrator.dtchangeable = isdtchangeable(alg2)
+  if integrator.opts.adaptive == isadaptive(alg1)
+    integrator.opts.adaptive = isadaptive(alg2)
+  end
+  if integrator.opts.qmin == qmin_default(alg1)
+    integrator.opts.qmin = qmin_default(alg2)
+  end
+  if integrator.opts.qmax == qmax_default(alg1)
+    integrator.opts.qmax == qmax_default(alg2)
+  end
+  if integrator.opts.beta2 == beta2_default(alg1)
+    integrator.opts.beta2 = beta2_default(alg2)
+  end
+  if integrator.opts.beta1 == beta1_default(alg1,integrator.opts.beta2)
+    integrator.opts.beta1 = beta1_default(alg2,integrator.opts.beta2)
+  end
+end
+
+@inline choose_algorithm!(integrator,cache::OrdinaryDiffEqCache) = nothing
+@inline function choose_algorithm!(integrator,cache::CompositeCache)
+  new_current = cache.choice_function(integrator)
+  if new_current != cache.current
+    initialize!(integrator,cache.caches[new_current])
+    reset_alg_dependent_opts!(integrator,integrator.alg.algs[cache.current],integrator.alg.algs[new_current])
+    cache.current = new_current
+  end
+end
--- src/integrators/integrator_utils.jl
+++ src/integrators/integrator_utils.jl
@@ -18,16 +18,6 @@ initialize{uType}(integrator,cache::OrdinaryDiffEqCache,::Type{uType}) =
   choose_algorithm!(integrator,integrator.cache)
 end
 
-@inline choose_algorithm!(integrator,cache::OrdinaryDiffEqCache) = nothing
-@inline function choose_algorithm!(integrator,cache::CompositeCache)
-  new_current = cache.choice_function(integrator)
-  if new_current != cache.current
-    initialize!(integrator,cache.caches[new_current])
-    reset_alg_dependent_opts!(integrator,integrator.alg.algs[cache.current],integrator.alg.algs[new_current])
-    cache.current = new_current
-  end
-end
-
 @inline function modify_dt_for_tstops!(integrator)
   tstops = integrator.opts.tstops
   if !isempty(tstops)
@@ -257,29 +247,6 @@ end
         error("Something went wrong. Integrator stepped past tstops but the algorithm was dtchangeable. Please report this error.")
       end
     end
-  end
-end
-
-"""
-If no user default, then this will change the default to the defaults
-for the second algorithm.
-"""
-@inline function reset_alg_dependent_opts!(integrator,alg1,alg2)
-  integrator.dtchangeable = isdtchangeable(alg2)
-  if integrator.opts.adaptive == isadaptive(alg1)
-    integrator.opts.adaptive = isadaptive(alg2)
-  end
-  if integrator.opts.qmin == qmin_default(alg1)
-    integrator.opts.qmin = qmin_default(alg2)
-  end
-  if integrator.opts.qmax == qmax_default(alg1)
-    integrator.opts.qmax == qmax_default(alg2)
-  end
-  if integrator.opts.beta2 == beta2_default(alg1)
-    integrator.opts.beta2 = beta2_default(alg2)
-  end
-  if integrator.opts.beta1 == beta1_default(alg1,integrator.opts.beta2)
-    integrator.opts.beta1 = beta1_default(alg2,integrator.opts.beta2)
   end
 end
 
