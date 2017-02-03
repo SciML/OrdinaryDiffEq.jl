e71ae2acf24f9c26aa21fba8ae579a1e87b2026c
ChrisRackauckas <me@chrisrackauckas.com>
2017-02-02 22:24:53 -0800

    muladd to Base

--- src/misc_utils.jl
+++ src/misc_utils.jl
@@ -50,41 +50,5 @@ end
 #Base.getindex(m::MutableReference)=m.val
 #Base.setindex!(m::MutableReference{T}, v::{T},I...) = m.val=v
 
-macro muladd(ex)
-  esc(to_muladd(ex))
-end
-
-function to_muladd(ex)
-  is_add_operation(ex) || return ex
-
-  all_operands = ex.args[2:end]
-  mul_operands = filter(is_mul_operation, all_operands)
-  odd_operands = filter(x->!is_mul_operation(x), all_operands)
-
-  muladd_operands = collect(zip(
-    to_muladd.((x->x.args[2]).(mul_operands)),
-    to_muladd.((x->x.args[3]).(mul_operands))))
-
-  if isempty(odd_operands)
-    to_be_muladded = muladd_operands[1:end-1]
-    last_operation = :($(muladd_operands[end][1]) * $(muladd_operands[end][2]))
-  else
-    to_be_muladded = muladd_operands
-    last_operation = make_addition(odd_operands)
-  end
-
-  foldr(last_operation, to_be_muladded) do xs, r
-    :($(Base.muladd)($(xs[1]), $(xs[2]), $r))
-  end
-end
-
-is_operation(ex::Expr, op::Symbol) = ex.head == :call && !isempty(ex.args) && ex.args[1] == op
-is_operation(ex, op::Symbol) = false
-
-is_add_operation(ex) = is_operation(ex, :+)
-is_mul_operation(ex) = is_operation(ex, :*)
-
-make_addition(args) = length(args) == 1 ? args[1] : Expr(:call, :+, args...)
-
 realtype{T}(::Type{T}) = T
 realtype{T}(::Type{Complex{T}}) = T
