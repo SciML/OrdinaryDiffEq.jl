34aea9314c5ea9f5c4d219765406957dac835c80
ChrisRackauckas <me@chrisrackauckas.com>
2017-03-31 07:29:55 -0700

    composite solution retcodes

--- src/composite_solution.jl
+++ src/composite_solution.jl
@@ -10,6 +10,7 @@ type ODECompositeSolution{uType,tType,rateType,P,A,IType} <: AbstractODESolution
   alg_choice::Vector{Int}
   dense::Bool
   tslocation::Int
+  retcode::Symbol
 end
 (sol::ODECompositeSolution)(t,deriv::Type=Val{0};idxs=nothing) = sol.interp(t,idxs,deriv)
 (sol::ODECompositeSolution)(v,t,deriv::Type=Val{0};idxs=nothing) = sol.interp(v,t,idxs,deriv)
@@ -33,8 +34,8 @@ end
 function build_solution{uType,tType,isinplace}(
         prob::AbstractODEProblem{uType,tType,isinplace},
         alg::OrdinaryDiffEqCompositeAlgorithm,t,u;dense=false,alg_choice=[1],
-        k=[],interp = (tvals) -> nothing,kwargs...)
-  ODECompositeSolution(u,t,k,prob,alg,interp,alg_choice,dense,0)
+        k=[],interp = (tvals) -> nothing, retcode = :Default, kwargs...)
+  ODECompositeSolution(u,t,k,prob,alg,interp,alg_choice,dense,0,retcode)
 end
 
 function build_solution{uType,tType,isinplace}(
@@ -42,10 +43,10 @@ function build_solution{uType,tType,isinplace}(
         alg::OrdinaryDiffEqCompositeAlgorithm,t,u,;dense=false,
         k=[],interp = (tvals) -> nothing,alg_choice=[1],
         timeseries_errors=true,dense_errors=true,
-        calculate_error = true,kwargs...)
+        calculate_error = true,retcode = :Default, kwargs...)
   u_analytic = Vector{uType}(0)
   errors = Dict{Symbol,eltype(u[1])}()
-  sol = ODECompositeTestSolution(u,u_analytic,errors,t,k,prob,alg,interp,alg_choice,dense,0)
+  sol = ODECompositeTestSolution(u,u_analytic,errors,t,k,prob,alg,interp,alg_choice,dense,0,retcode)
   if calculate_error
     calculate_solution_errors!(sol;timeseries_errors=timeseries_errors,dense_errors=dense_errors)
   end
