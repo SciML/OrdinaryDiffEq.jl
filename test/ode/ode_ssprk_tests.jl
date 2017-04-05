5a9b831996d090057c31e9a5a96cda4c54ccc131
Hendrik Ranocha <h.ranocha@tu-bs.de>
2017-04-05 16:26:33 +0200

    added tests for SSPRK22 as simple integrator

--- test/ode/ode_ssprk_tests.jl
+++ test/ode/ode_ssprk_tests.jl
@@ -3,7 +3,13 @@ using OrdinaryDiffEq, DiffEqProblemLibrary, DiffEqDevTools, Base.Test
 dts = 1.//2.^(8:-1:4)
 testTol = 0.2
 
-for prob in [prob_ode_linear, prob_ode_2Dlinear, prob_ode_bigfloat2Dlinear]
+prob_ode_sin = ODETestProblem((t,u)->cos(t), 0., (t,u0)->sin(t))
+prob_ode_sin_inplace = ODETestProblem((t,u,du)->du[1]=cos(t), [0.], (t,u0)->[sin(t)])
+
+test_problems = [prob_ode_sin, prob_ode_sin_inplace,
+                 prob_ode_linear, prob_ode_2Dlinear, prob_ode_bigfloat2Dlinear]
+
+for prob in test_problems
   for alg in [SSPRK22()]
     sim = test_convergence(dts, prob, alg)
     @test abs(sim.𝒪est[:final]-OrdinaryDiffEq.alg_order(alg)) < testTol
