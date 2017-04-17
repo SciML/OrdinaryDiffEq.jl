5bf154dcd63036ba8348dc239205292e8c693b99
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-17 06:35:52 -0700

    partitioned euler

--- test/partitioned_methods_tests.jl
+++ test/partitioned_methods_tests.jl
@@ -1,4 +1,4 @@
-using DiffEqBase, OrdinaryDiffEq, Base.Test
+using DiffEqBase, OrdinaryDiffEq, Base.Test, RecursiveArrayTools
 
 u0 = 3ones(4)
 v0 = ones(4)
@@ -21,3 +21,14 @@ sol2 = solve(prob,SymplecticEuler(),dt=1/100)
 
 @test sol[end][1] == sol2[end][1]
 @test sol[end][5] == sol2[end][5]
+
+f = function (t,u,du)
+  du.x[1] .= u.x[2]
+  du.x[2] .= -2u.x[1]
+end
+
+u = ArrayPartition((u0,v0))
+
+prob = ODEProblem(f,u,(0.0,5.0))
+
+sol = solve(prob,Euler(),dt=1/100)
