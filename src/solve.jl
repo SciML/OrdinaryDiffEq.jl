188053407cf502b5f764844d244df79b8fdef80f
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-25 07:22:01 -0700

    allow wrong sign dt input.

--- src/solve.jl
+++ src/solve.jl
@@ -136,13 +136,19 @@ function init{algType<:OrdinaryDiffEqAlgorithm,recompile_flag}(
     reltol_internal = reltol
   end
 
+  dtmax > 0 && tdir < 0 && (dtmax *= tdir) # Allow positive dtmax, but auto-convert
+  # dtmin is all abs => does not care about sign already.
   if dt == zero(dt) && adaptive
     dt = tType(ode_determine_initdt(u,t,tdir,dtmax,abstol_internal,reltol_internal,internalnorm,prob,order))
+    if sign(dt)!=tdir && dt!=tType(0)
+      error("Automatic dt setting has the wrong sign. Exiting. Please report this error.")
+    end
+  elseif adaptive && dt > zero(dt) && tdir < 0
+    dt *= tdir # Allow positive dt, but auto-convert
   end
 
-  if sign(dt)!=tdir && dt!=tType(0)
-    error("dt has the wrong sign. Exiting")
-  end
+  dt = tdir*min(abs(dtmax),abs(dt))
+  dt = tdir*max(abs(dt),abs(dtmin))
 
   if typeof(u) <: Union{AbstractArray,Tuple}
     rate_prototype = similar(u/zero(t),indices(u)) # rate doesn't need type info
