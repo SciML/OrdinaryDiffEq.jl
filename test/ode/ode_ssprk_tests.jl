4185df431855dc86f304cefbdc232cb8cc53f250
Hendrik Ranocha <h.ranocha@tu-bs.de>
2017-06-22 16:32:01 +0200

    recreate prob_ode_bigfloat2Dlinear in order to avoid problems of BigFloats in the precompiled module DiffEqProblemLibrary

--- test/ode/ode_ssprk_tests.jl
+++ test/ode/ode_ssprk_tests.jl
@@ -19,6 +19,16 @@ f = (t,u,du)->du[1]=sin(u[1])
 (p::typeof(f))(::Type{Val{:analytic}},t,u0) = [2*acot(exp(-t)*cot(0.5))]
 prob_ode_nonlinear_inplace = ODEProblem(f,[1.],(0.,0.5))
 
+const linear_bigα = parse(BigFloat,"1.01")
+f_2dlinearbig = (t,u,du) -> begin
+  for i in 1:length(u)
+    du[i] = linear_bigα*u[i]
+  end
+end
+(f::typeof(f_2dlinearbig))(::Type{Val{:analytic}},t,u0) = u0*exp.(1.01*t)
+prob_ode_bigfloat2Dlinear = ODEProblem(f_2dlinearbig,map(BigFloat,rand(4,2)).*ones(4,2)/2,(0.0,1.0))
+
+
 test_problems_only_time = [prob_ode_sin, prob_ode_sin_inplace]
 test_problems_linear = [prob_ode_linear, prob_ode_2Dlinear, prob_ode_bigfloat2Dlinear]
 test_problems_nonlinear = [prob_ode_nonlinear, prob_ode_nonlinear_inplace]
