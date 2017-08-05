d91da02c05e56e6dc5cea31bae938d35c21794f2
David Widmann <dev+git@devmotion.de>
2017-08-05 20:11:47 +0200

    Clean and optimize initdt

--- src/initdt.jl
+++ src/initdt.jl
@@ -1,19 +1,19 @@
 @muladd function ode_determine_initdt{tType,uType}(u0,t::tType,tdir,dtmax,abstol,reltol,internalnorm,prob::AbstractODEProblem{uType,tType,true},order)
   f = prob.f
-  f₀ = zeros(u0./t); f₁ = zeros(u0./t); u₁ = zeros(u0); sk = zeros(u0);
-  # Hack to  make a generic u0 with no units, https://github.com/JuliaLang/julia/issues/22216
-  typeof(u0[1]) <: AbstractArray ? tmp = zeros(u0,typeof(ones(u0[1]))) : tmp = zeros(u0,typeof(one(u0[1])))
+  oneunit_tType = oneunit(tType)
+  dtmax_tdir = tdir*dtmax
 
-  @. sk = abstol+abs(u0)*reltol
-  @. tmp = u0/sk
+  sk = @. abstol+abs(u0)*reltol
+  tmp = @. u0/sk
   d₀ = internalnorm(tmp)
 
+  f₀ = zeros(u0./t)
   f(t,u0,f₀)
   if any((isnan(x) for x in f₀))
     warn("First function call produced NaNs. Exiting.")
   end
 
-  @. tmp = f₀/sk*oneunit(tType)
+  @. tmp = f₀/sk*oneunit_tType
   d₁ = internalnorm(tmp)
 
   if d₀ < 1//10^(5) || d₁ < 1//10^(5)
@@ -21,25 +21,30 @@
   else
     dt₀ = tType((d₀/d₁)/100)
   end
-  dt₀ = min(dt₀,tdir*dtmax)
+  dt₀ = min(dt₀,dtmax_tdir)
+  dt₀_tdir = tdir*dt₀
 
-  @. u₁ = u0 + tdir*dt₀*f₀
-  f(t+tdir*dt₀,u₁,f₁)
+  u₁ = @. u0 + dt₀_tdir*f₀
+  f₁ = similar(f₀)
+  f(t+dt₀_tdir,u₁,f₁)
 
-  @. tmp = (f₁-f₀)/(abstol+abs(u0)*reltol)*oneunit(tType)
-  d₂ = internalnorm(tmp)/dt₀*oneunit(tType)
+  @. tmp = (f₁-f₀)/sk*oneunit_tType
+  d₂ = internalnorm(tmp)/dt₀*oneunit_tType
   # Hairer has d₂ = sqrt(sum(abs2,tmp))/dt₀, note the lack of norm correction
 
-  if max(d₁,d₂) <= 1//Int64(10)^(15)
+  max_d₁d₂ = max(d₁,d₂)
+  if max_d₁d₂ <= 1//Int64(10)^(15)
     dt₁ = max(tType(1//10^(6)),dt₀*1//10^(3))
   else
-    dt₁ = tType(10.0^(-(2+log10(max(d₁,d₂)))/order))
+    dt₁ = tType(10.0^(-(2+log10(max_d₁d₂))/order))
   end
-  dt = tdir*min(100dt₀,dt₁,tdir*dtmax)
+  dt = tdir*min(100dt₀,dt₁,dtmax_tdir)
 end
 
 @muladd function ode_determine_initdt{uType,tType}(u0::uType,t,tdir,dtmax,abstol,reltol,internalnorm,prob::AbstractODEProblem{uType,tType,false},order)
   f = prob.f
+  oneunit_tType = oneunit(tType)
+  dtmax_tdir = tdir*dtmax
 
   sk = @. abstol+abs(u0)*reltol
   d₀ = internalnorm(@. u0/sk)
@@ -49,24 +54,26 @@ end
     error("First function call produced NaNs. Exiting.")
   end
 
-  d₁ = internalnorm(@. f₀/sk*oneunit(tType))
+  d₁ = internalnorm(@. f₀/sk*oneunit_tType)
 
   if d₀ < 1//10^(5) || d₁ < 1//10^(5)
     dt₀ = tType(1//10^(6))
   else
     dt₀ = tType((d₀/d₁)/100)
   end
-  dt₀ = min(dt₀,tdir*dtmax)
+  dt₀ = min(dt₀,dtmax_tdir)
+  dt₀_tdir = tdir*dt₀
 
-  u₁ = @. u0 + tdir*dt₀*f₀
-  f₁ = f(t+tdir*dt₀,u₁)
+  u₁ = @. u0 + dt₀_tdir*f₀
+  f₁ = f(t+dt₀_tdir,u₁)
 
-  d₂ = internalnorm(@. (f₁-f₀)/(abstol+abs(u0)*reltol)*oneunit(tType))/dt₀*oneunit(tType)
+  d₂ = internalnorm(@. (f₁-f₀)/sk*oneunit_tType)/dt₀*oneunit_tType
 
-  if max(d₁,d₂) <= 1//Int64(10)^(15)
+  max_d₁d₂ = max(d₁, d₂)
+  if max_d₁d₂ <= 1//Int64(10)^(15)
     dt₁ = max(tType(1//10^(6)),dt₀*1//10^(3))
   else
-    dt₁ = tType(10.0^(-(2+log10(max(d₁,d₂)))/order))
+    dt₁ = tType(10.0^(-(2+log10(max_d₁d₂))/order))
   end
-  dt = tdir*min(100dt₀,dt₁,tdir*dtmax)
+  dt = tdir*min(100dt₀,dt₁,dtmax_tdir)
 end
