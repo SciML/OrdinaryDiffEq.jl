4417935500dca08a0ad564897fa8f775bc1a98a9
ChrisRackauckas <accounts@chrisrackauckas.com>
2017-08-07 09:17:54 -0700

    remove dopri5 from test

--- test/ode/ode_backwards_test.jl
+++ test/ode/ode_backwards_test.jl
@@ -2,13 +2,6 @@ using OrdinaryDiffEq, DiffEqProblemLibrary, Base.Test
 prob = deepcopy(prob_ode_2Dlinear)
 prob.tspan = (1.0,0.0)
 
-sol = solve(prob,DP5(),dt=-1/4)
-
-if !is_windows()
-  sol2 = solve(prob,dopri5(),dt=-1/4)
-  @test sol.t ≈ sol2.t
-end
-
 sol = solve(prob,DP5(),dt=-1/4,tstops=[0.5])
 @test sol.t == [1.0,.75,.5,0]
 
