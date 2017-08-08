ba74de08d4ab73fe346b928ae47251b0681ea6de
ChrisRackauckas <accounts@chrisrackauckas.com>
2017-08-08 02:36:19 -0700

    owrenzen interpolants

--- test/owrenzen_tests.jl
+++ test/owrenzen_tests.jl
@@ -15,12 +15,15 @@ sol = solve(prob,OwrenZen4())
 sol = solve(prob,OwrenZen5())
 @test length(sol) < 20
 
-sim = test_convergence(dts,prob,OwrenZen3())
+sim = test_convergence(dts,prob,OwrenZen3(),dense_errors=true)
 @test abs(sim.𝒪est[:final]-3) < testTol
-sim = test_convergence(dts,prob,OwrenZen4())
+@test abs(sim.𝒪est[:L2]-3) < testTol
+sim = test_convergence(dts,prob,OwrenZen4(),dense_errors=true)
 @test abs(sim.𝒪est[:final]-4) < testTol
-sim = test_convergence(dts,prob,OwrenZen5())
+@test abs(sim.𝒪est[:L2]-4) < testTol
+sim = test_convergence(dts,prob,OwrenZen5(),dense_errors=true)
 @test abs(sim.𝒪est[:final]-5) < testTol
+@test abs(sim.𝒪est[:L2]-5) < testTol
 
 prob = prob_ode_2Dlinear
 sol = solve(prob,OwrenZen3())
@@ -30,9 +33,12 @@ sol = solve(prob,OwrenZen4())
 sol = solve(prob,OwrenZen5())
 @test length(sol) < 20
 
-sim = test_convergence(dts,prob,OwrenZen3())
+sim = test_convergence(dts,prob,OwrenZen3(),dense_errors=true)
 @test abs(sim.𝒪est[:final]-3) < testTol
-sim = test_convergence(dts,prob,OwrenZen4())
+@test abs(sim.𝒪est[:L2]-3) < testTol
+sim = test_convergence(dts,prob,OwrenZen4(),dense_errors=true)
 @test abs(sim.𝒪est[:final]-4) < testTol
-sim = test_convergence(dts,prob,OwrenZen5())
+@test abs(sim.𝒪est[:L2]-4) < testTol
+sim = test_convergence(dts,prob,OwrenZen5(),dense_errors=true)
 @test abs(sim.𝒪est[:final]-5) < testTol
+@test abs(sim.𝒪est[:L2]-5) < testTol
