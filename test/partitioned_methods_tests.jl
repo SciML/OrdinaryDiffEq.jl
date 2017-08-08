f757313af6b4b49b081d07d8c28d321b7590abd3
YingboMa <mayingbo5@gmail.com>
2017-08-08 19:00:43 -0400

    Add convergence test for IRKN4 algorithm

--- test/partitioned_methods_tests.jl
+++ test/partitioned_methods_tests.jl
@@ -112,6 +112,9 @@ sim = test_convergence(dts,prob,Nystrom4(),dense_errors=true)
 sim = test_convergence(dts,prob,Nystrom4VelocityIndependent(),dense_errors=true)
 @test sim.𝒪est[:l2] ≈ 4 rtol = 1e-1
 @test sim.𝒪est[:L2] ≈ 4 rtol = 1e-1
+sim = test_convergence(dts,prob,IRKN4(),dense_errors=true)
+@test sim.𝒪est[:l2] ≈ 4 rtol = 1e-1
+@test sim.𝒪est[:L2] ≈ 4 rtol = 1e-1
 dts = 1.0./2.0.^(5:-1:0)
 sim = test_convergence(dts,prob,Nystrom5VelocityIndependent(),dense_errors=true)
 @test sim.𝒪est[:l2] ≈ 5 rtol = 1e-1
