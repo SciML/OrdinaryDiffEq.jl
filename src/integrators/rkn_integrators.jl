8ab4b1dccebee261a2da34f8b5a98174913546c7
YingboMa <mayingbo5@gmail.com>
2017-08-15 20:54:01 -0400

    Add DPRKN8 related utilities

--- src/integrators/rkn_integrators.jl
+++ src/integrators/rkn_integrators.jl
@@ -8,7 +8,7 @@ const NystromDefaultInitialization = Union{Nystrom4Cache,
                                            Nystrom4VelocityIndependentCache,
                                            Nystrom5VelocityIndependentCache,
                                            IRKN3Cache, IRKN4Cache,
-                                           DPRKN6Cache}
+                                           DPRKN6Cache, DPRKN6Cache}
 
 function initialize!(integrator,cache::NystromDefaultInitialization,f=integrator.f)
   @unpack fsalfirst,k = cache
