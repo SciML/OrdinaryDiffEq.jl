f6c2b743cd5a623f2fa4e66b9b36f6f69d5cb95b
David Widmann <dev+git@devmotion.de>
2017-08-27 17:26:31 +0200

    Update Rosenbrock interpolants

--- src/dense/rosenbrock_interpolants.jl
+++ src/dense/rosenbrock_interpolants.jl
@@ -1,81 +1,65 @@
 """
 From MATLAB ODE Suite by Shampine
 """
-@inline function ode_interpolant(Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23ConstantCache,Rosenbrock32ConstantCache},idxs,T::Type{Val{0}})
+@muladd @inline function ode_interpolant(Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23ConstantCache,Rosenbrock32ConstantCache},idxs,T::Type{Val{0}})
   d = cache.d
   c1 = Θ*(1-Θ)/(1-2d)
   c2 = Θ*(Θ-2d)/(1-2d)
-  #@. y₀ + dt*(c1*k[1] + c2*k[2])
-  y₀ + dt*(c1*k[1] + c2*k[2])
+  @. y₀ + dt*(c1*k[1] + c2*k[2])
 end
 
 # First Derivative of the dense output
-@inline function ode_interpolant(Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23ConstantCache,Rosenbrock32ConstantCache},idxs,T::Type{Val{1}})
+@muladd @inline function ode_interpolant(Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23ConstantCache,Rosenbrock32ConstantCache},idxs,T::Type{Val{1}})
   d = cache.d
   c1diff = (1-2*Θ)/(1-2*d)
   c2diff = (2*Θ-2*d)/(1-2*d)
-  #@. c1diff*k[1] + c2diff*k[2]
-  c1diff*k[1] + c2diff*k[2]
+  @. c1diff*k[1] + c2diff*k[2]
 end
 
 """
 From MATLAB ODE Suite by Shampine
 """
-@inline function ode_interpolant!(out,Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23Cache,Rosenbrock32Cache},idxs,T::Type{Val{0}})
+@muladd @inline function ode_interpolant!(out,Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23Cache,Rosenbrock32Cache},idxs,T::Type{Val{0}})
   d = cache.tab.d
   c1 = Θ*(1-Θ)/(1-2d)
   c2 = Θ*(Θ-2d)/(1-2d)
   if out == nothing
-    return y₀[idxs] + dt*(c1*k[1][idxs] + c2*k[2][idxs])
+    return @. y₀[idxs] + dt*(c1*k[1][idxs] + c2*k[2][idxs])
   elseif idxs == nothing
-    #@. out = y₀ + dt*(c1*k[1] + c2*k[2])
-    @inbounds for i in eachindex(out)
-      out[i] = y₀[i] + dt*(c1*k[1][i] + c2*k[2][i])
-    end
+    @. out = y₀ + dt*(c1*k[1] + c2*k[2])
   else
-    #@views @. out = y₀[idxs] + dt*(c1*k[1][idxs] + c2*k[2][idxs])
-    @inbounds for (j,i) in enumerate(idxs)
-      out[j] = y₀[i] + dt*(c1*k[1][i] + c2*k[2][i])
-    end
+    @views @. out = y₀[idxs] + dt*(c1*k[1][idxs] + c2*k[2][idxs])
   end
 end
 
-@inline function ode_interpolant!(out,Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23Cache,Rosenbrock32Cache},idxs,T::Type{Val{1}})
+@muladd @inline function ode_interpolant!(out,Θ,dt,y₀,y₁,k,cache::Union{Rosenbrock23Cache,Rosenbrock32Cache},idxs,T::Type{Val{1}})
   d = cache.tab.d
   c1diff = (1-2*Θ)/(1-2*d)
   c2diff = (2*Θ-2*d)/(1-2*d)
   if out == nothing
-    return c1diff*k[1][idxs] + c2diff*k[2][idxs]
+    return @. c1diff*k[1][idxs] + c2diff*k[2][idxs]
   elseif idxs == nothing
-    #@. out = c1diff*k[1] + c2diff*k[2]
-    @inbounds for i in eachindex(out)
-      out[i] = c1diff*k[1][i] + c2diff*k[2][i]
-    end
+    @. out = c1diff*k[1] + c2diff*k[2]
   else
-    #@views @. out = c1diff*k[1][idxs] + c2diff*k[2][idxs]
-    @inbounds for (j,i) in enumerate(idxs)
-      out[j] = c1diff*k[1][i] + c2diff*k[2][i]
-    end
+    @views @. out = c1diff*k[1][idxs] + c2diff*k[2][idxs]
   end
 end
 
-@inline function ode_interpolant(Θ,dt,y₀,y₁,k,cache::Rodas4ConstantCache,idxs,T::Type{Val{0}})
-  y₀*(1-Θ)+Θ*(y₁+(1-Θ)*(k[1] + Θ*k[2]))
+@muladd @inline function ode_interpolant(Θ,dt,y₀,y₁,k,cache::Rodas4ConstantCache,idxs,T::Type{Val{0}})
+  Θ1 = 1 - Θ
+  @. Θ1*y₀ + Θ*(y₁ + Θ1*(k[1] + Θ*k[2]))
 end
 
 """
 From MATLAB ODE Suite by Shampine
 """
-@inline function ode_interpolant!(out,Θ,dt,y₀,y₁,k,cache::Rodas4Cache,idxs,T::Type{Val{0}})
+@muladd @inline function ode_interpolant!(out,Θ,dt,y₀,y₁,k,cache::Rodas4Cache,idxs,T::Type{Val{0}})
+  Θ1 = 1 - Θ
   if out == nothing
-    return y₀[idxs]*(1-Θ)+Θ*(y₁[idxs]+(1-Θ)*(k[1][idxs] + Θ*k[2][idxs]))
+    return @. Θ1*y₀[idxs] + Θ*(y₁[idxs] + Θ1*(k[1][idxs] + Θ*k[2][idxs]))
   elseif idxs == nothing
-    @inbounds for i in eachindex(out)
-      out[i] = y₀[i]*(1-Θ)+Θ*(y₁[i]+(1-Θ)*(k[1][i] + Θ*k[2][i]))
-    end
+    @. out = Θ1*y₀ + Θ*(y₁ + Θ1*(k[1] + Θ*k[2]))
   else
-    @inbounds for (j,i) in enumerate(idxs)
-      out[j] = y₀[i]*(1-Θ)+Θ*(y₁[i]+(1-Θ)*(k[1][i] + Θ*k[2][i]))
-    end
+    @views @. out = Θ1*y₀[idxs] + Θ*(y₁[idxs] + Θ1*(k[1][idxs] + Θ*k[2][idxs]))
   end
 end
