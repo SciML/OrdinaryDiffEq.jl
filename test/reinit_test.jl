d39e712316c0bbd1d3bd89816b78c9863d7ee11b
Sohan Rudra <sohanrudra@gmail.com>
2018-01-23 00:46:53 +0530

    Updated tests

--- test/reinit_test.jl
+++ test/reinit_test.jl
@@ -46,27 +46,22 @@ solve!(integrator)
 @test t == integrator.sol.t
 
 #callback test
-f(t,u) = 2.0*t-2.0
+g(t,u) = 2.0*t-2.0
 u0=0.0
 tspan = (0.0,4.0)
-prob = ODEProblem(f,u0,tspan)
-function condition(t,u,integrator) # Event when event_f(t,u) == 0
-  u
-end
-function affect!(integrator)
-  integrator.u = 0.7
-end
-cb = ContinuousCallback(condition,affect!)
+prob = ODEProblem(g,u0,tspan)
+saved_values = SavedValues(Float64, Float64)
+cb = SavingCallback((t,u,integrator)->u, saved_values)
 integrator = init(prob,Tsit5();dt=1//2^(4),callback = cb)
 dt = integrator.dt
 solve!(integrator)
 
-u = copy(integrator.sol.u)
-t = copy(integrator.sol.t)
-
+u = saved_values.saveval
+t = saved_values.t
+saved_values = SavedValues(Float64, Float64)
 reinit!(integrator)
 integrator.dt = dt
 solve!(integrator)
 
-@test u == integrator.sol.u
-@test t == integrator.sol.t
+@test u == saved_values.saveval
+@test t == saved_values.t
