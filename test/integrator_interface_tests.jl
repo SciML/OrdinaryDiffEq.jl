df997a4ec34ca293889c311221405dce74e841ce
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-19 20:59:15 -0800

    Add inplace version of set_*! tests

--- test/integrator_interface_tests.jl
+++ test/integrator_interface_tests.jl
@@ -2,10 +2,16 @@ using DiffEqBase: set_t!, set_u!, set_ut!
 using OrdinaryDiffEq
 
 # set_X!(integrator, integrator.X) should not change the result.
-@testset "Trivial $setter ($alg)" for alg in [RK4, Trapezoid],
-                                   setter in [set_t!, set_u!, set_ut!]
-  f = (u,p,t) -> (2u)
-  prob = ODEProblem(f,1/2,(0.0,2.0))
+@testset "Trivial $setter ($alg, inplace=$iip)" for alg in [RK4, Trapezoid],
+    setter in [set_t!, set_u!, set_ut!],
+    iip in [false, true]
+  if iip
+    f = (du,u,p,t) -> (du .= 2u)
+    prob = ODEProblem{iip}(f,[1/2],(0.0,2.0))
+  else
+    f = (u,p,t) -> (2u)
+    prob = ODEProblem{iip}(f,1/2,(0.0,2.0))
+  end
   t_half = 1.0
 
   integrator1 = init(prob, alg(); save_everystep=false)
@@ -33,11 +39,19 @@ using OrdinaryDiffEq
   end
 end
 
-@testset "Resolve with $setter ($alg)" for alg in [RK4, Trapezoid],
-                                        setter in [set_t!, set_ut!]
-  f = (u,p,t) -> (2u * cos(2π * t))
-  integrator1 = init(ODEProblem(f,1/2,(0.0,1.0)), alg(); save_everystep=false)
-  integrator2 = init(ODEProblem(f,1/2,(0.0,2.0)), alg(); save_everystep=false)
+@testset "Resolve with $setter ($alg, inplace=$iip)" for alg in [RK4, Trapezoid],
+    setter in [set_t!, set_ut!],
+    iip in [false, true]
+  if iip
+    f = (du,u,p,t) -> (du .= 2u * cos(2π * t))
+    prob1 = ODEProblem{iip}(f,[1/2],(0.0,1.0))
+  else
+    f = (u,p,t) -> (2u * cos(2π * t))
+    prob1 = ODEProblem{iip}(f,1/2,(0.0,1.0))
+  end
+  prob2 = remake(prob1; tspan=(0.0,2.0))
+  integrator1 = init(prob1, alg(); save_everystep=false)
+  integrator2 = init(prob2, alg(); save_everystep=false)
 
   solve!(integrator1)
   if setter === set_t!
