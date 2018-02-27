2d6fb48eeee7c35c77299281ed85d749ab8beac0
Xingjian Guo <xg703@nyu.edu>
2018-02-27 16:43:00 -0500

    Fix test script to avoid global name clash

--- test/linear_nonlinear_Krylov_tests.jl
+++ test/linear_nonlinear_Krylov_tests.jl
@@ -13,25 +13,24 @@ Base.A_mul_B!(Y, op::MatrixOperator, B) = A_mul_B!(Y, op.A, B)
 (op::MatrixOperator)(u,p,t) = op.A * u
 (op::MatrixOperator)(du,u,p,t) = A_mul_B!(du, op.A, u)
 
-const N = 100
-const b = 0.2
+N = 100
+b = 0.2
+tspan = (0.0, 1.0)
+dts = 1./2.^(7:-1:4) #14->7 good plot
 #########################################
 # Test problem 1 (heat equation with radiative loss, u' = Au - bu)
 dd = -2 * ones(N); du = ones(N - 1)
 A = spdiagm((du,dd,du), (-1,0,1))
 f1 = MatrixOperator(A)
-f2(u,p,t) = -b * u
-f2!(du,u,p,t) = du .= -b * u
+f2 = (u,p,t) -> -b * u
 
 srand(0); u0 = rand(N)
-tspan = (0.0, 1.0)
 prob = SplitODEProblem(f1,f2,u0,tspan)
 function (::typeof(prob.f))(::Type{Val{:analytic}},u0,p,t)
     tmp = (A - b*I)*t
     return expm(full(tmp)) * u0
 end
 
-dts = 1./2.^(7:-1:4) #14->7 good plot
 sim = test_convergence(dts,prob,LawsonEulerKrylov())
 @test abs(sim.𝒪est[:l2]-1) < 0.1
 sim = test_convergence(dts,prob,ExpEulerKrylov())
@@ -40,9 +39,9 @@ sim = test_convergence(dts,prob,ExpEulerKrylov())
 # Test problem 2 (Schrodinger Equation, iu' = -Au - bu)
 # Uses in-place functions
 f1 = MatrixOperator(1im * A)
-f2!(du,u,p,t) = du .= (1im*b) * u
+f2 = (du,u,p,t) -> du .= (1im*b) * u
 u0 = complex(u0)
-prob = SplitODEProblem(f1,f2!,u0,tspan)
+prob = SplitODEProblem(f1,f2,u0,tspan)
 function (::typeof(prob.f))(::Type{Val{:analytic}},u0,p,t)
     tmp = (A + b*I) * (1im*t)
     return expm(full(tmp)) * u0
