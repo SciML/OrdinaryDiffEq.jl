fd887d994ad0a9e1cb4c55e6ff7730f954eab251
sipah00 <shubhamsipah00@gmail.com>
2018-03-04 11:22:29 +0530

    reduced unnecessary copy of k2

--- src/perform_step/adams_bashforth_moulton_perform_step.jl
+++ src/perform_step/adams_bashforth_moulton_perform_step.jl
@@ -129,7 +129,11 @@ end
     @. u = uprev + (dt/4)*(k1 + 3*ralk2)       #Ralston Method
     cache.k2 .= k1
   else
-    perform_step!(integrator, AB3Cache(u,uprev,fsalfirst,copy(k2),k3,ralk2,k,tmp))  #Here passing copy of k2, otherwise it will change in AB3()
+    if cnt == 2
+      perform_step!(integrator, AB3Cache(u,uprev,fsalfirst,copy(k2),k3,ralk2,k,tmp))  #Here passing copy of k2, otherwise it will change in AB3()
+    else
+      perform_step!(integrator, AB3Cache(u,uprev,fsalfirst,k2,k3,ralk2,k,tmp))
+    end
     k = integrator.fsallast
     @. u = uprev + (dt/12)*(5*k + 8*k1 - k2)
     cache.k2,cache.k3 = k3,k2
