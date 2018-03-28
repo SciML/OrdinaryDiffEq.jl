6969d90dcf5937288cee482c6d78a8db7db6522d
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-03-28 16:09:26 -0700

    fix test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,7 +38,7 @@ if group == "All" || group == "Interface"
     @time @testset "Integrator Interface Tests" begin include("integrator_interface_tests.jl") end
     @time @testset "Complex Tests" begin include("complex_tests.jl") end
     @time @testset "Stiffness Detection Tests" begin include("stiffness_detection_test.jl") end
-    @time @testset "Add Steps Tests" begin include("ode\ode_add_steps_tests.jl") end
+    @time @testset "Add Steps Tests" begin include("ode/ode_add_steps_tests.jl") end
 end
 
 if group == "All" || group == "Regression"
