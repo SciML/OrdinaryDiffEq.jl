82b41b457f2a048b47864a78ee965ab0bc75cd92
YingboMa <mayingbo5@gmail.com>
2018-03-30 14:53:02 -0700

    Add a strategy to switch from `Rodas5` to `DP5` or `Tsit5`

--- test/stiffness_detection_test.jl
+++ test/stiffness_detection_test.jl
@@ -1,10 +1,14 @@
 using OrdinaryDiffEq, DiffEqProblemLibrary, Base.Test
 
-prob = DiffEqProblemLibrary.prob_ode_vanstiff
+prob = ODEProblem(DiffEqProblemLibrary.van,[0;sqrt(3)],(0.0,3.0),1e6)
 
-sol = solve(prob, AutoRodas5(Tsit5(); maxstiffstep=15), dt=1e-5, reltol=1e-5, abstol=1e-5)
-@test length(sol.t) < 40
-@test length(unique(sol.alg_choice)) == 2
-sol = solve(prob, AutoRodas5(DP5(); maxstiffstep=15), dt=1e-5, reltol=1e-5, abstol=1e-5)
-@test length(sol.t) < 42
-@test length(unique(sol.alg_choice)) == 2
+# Test if switching back and forth
+is_switching_fb(sol) = maximum(diff(find(x->x==2, sol.alg_choice))) > 5
+sol = solve(prob, AutoRodas5(Tsit5(); maxstiffstep=15, maxnonstiffstep=8, tol=11//10),
+            dt=1e-5, reltol=1e-5, abstol=1e-5)
+@test length(sol.t) < 90
+@test is_switching_fb(sol)
+sol = solve(prob, AutoRodas5(DP5(); maxstiffstep=15, maxnonstiffstep=8, tol=11//10),
+            dt=1e-5, reltol=1e-5, abstol=1e-5)
+@test length(sol.t) < 150
+@test is_switching_fb(sol)
