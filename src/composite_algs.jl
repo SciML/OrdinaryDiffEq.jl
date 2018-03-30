cc1796cfe6acb7add3c16ce6aefc2d34ff3dd340
YingboMa <mayingbo5@gmail.com>
2018-03-29 17:00:43 -0700

    Addressing the code review comments

--- src/composite_algs.jl
+++ src/composite_algs.jl
@@ -1,11 +1,14 @@
-mutable struct AutoSwitch{Alg}
-  count::UInt8
-  alg::Alg
-  algnum::UInt8
+mutable struct AutoSwitch{nAlg,sAlg}
+  # TODO: Is it better to use UInt8?
+  count::Int
+  nonstiffalg::nAlg
+  stiffalg::sAlg
+  is_stiffalg::Bool
   is_successive::Bool
+  maxstiffstep::Int
+  maxnonstiffstep::Int
 end
-AutoSwitch(alg::Alg) where Alg = AutoSwitch(zero(UInt8), alg,
-                                            one(UInt8), false)
+AutoSwitch(nonstiffalg::nAlg, stiffalg::sAlg; maxstiffstep=15, maxnonstiffstep=15) where {nAlg,sAlg} = AutoSwitch(0, nonstiffalg, stiffalg, false, false, maxstiffstep, maxnonstiffstep)
 
 function is_stiff(eigen_est, dt, alg)
   stiffness = eigen_est*dt/alg_stability_size(alg())
@@ -14,13 +17,13 @@ end
 
 function (AS::AutoSwitch)(integrator)
   eigen_est, dt = integrator.eigen_est, integrator.dt
-  # Here we assume that the first algorithm is always for non-stiff problems
-  if (AS.count > 15 && AS.is_successive || AS.algnum == 2)
+  # Switching from a stiff solver to a non-stiff solver is not yet implemented
+  if (AS.count > AS.maxstiffstep && AS.is_successive || AS.is_stiffalg)
     AS.count = 0
-    AS.algnum= 2
+    AS.is_stiffalg = true
     return 2
   end
-  if is_stiff(eigen_est, dt, AS.alg)
+  if is_stiff(eigen_est, dt, AS.nonstiffalg)
     AS.count += 1
     if !(AS.is_successive)
       AS.is_successive = true
@@ -31,4 +34,4 @@ function (AS::AutoSwitch)(integrator)
   return 1
 end
 
-AutoRodas5(;alg=Tsit5) = CompositeAlgorithm((alg(), Rodas5()), AutoSwitch(alg))
+AutoRodas5(alg=Tsit5(); kwargs...) = CompositeAlgorithm((alg, Rodas5()), AutoSwitch(typeof(alg), Rodas5; kwargs...))
--- test/stiffness_detection_test.jl
+++ test/stiffness_detection_test.jl
@@ -1,8 +1,10 @@
 using OrdinaryDiffEq, DiffEqProblemLibrary, Base.Test
 
-prob = prob_ode_vanderpol
+prob = DiffEqProblemLibrary.prob_ode_vanstiff
 
-sol = solve(prob, AutoRodas5(alg=Tsit5), dt=1e-5, reltol=1e-5, abstol=1e-5)
-@test length(sol.t) < 15
-sol = solve(prob, AutoRodas5(alg=DP5), dt=1e-5, reltol=1e-5, abstol=1e-5)
-@test length(sol.t) < 15
+sol = solve(prob, AutoRodas5(Tsit5(); maxstiffstep=15), dt=1e-5, reltol=1e-5, abstol=1e-5)
+@test length(sol.t) < 40
+@test length(unique(sol.alg_choice)) == 2
+sol = solve(prob, AutoRodas5(DP5(); maxstiffstep=15), dt=1e-5, reltol=1e-5, abstol=1e-5)
+@test length(sol.t) < 42
+@test length(unique(sol.alg_choice)) == 2
