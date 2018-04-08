f3f509eda2d338d2fd4e2fbfbf5575eee0e141ee
YingboMa <mayingbo5@gmail.com>
2018-04-08 12:40:34 -0700

    Add in-place `ABDF2`

--- src/caches/bdf_caches.jl
+++ src/caches/bdf_caches.jl
@@ -1,4 +1,4 @@
-mutable struct ABDF2ConstantCache{F,uToltype,dtType} <: OrdinaryDiffEqConstantCache
+mutable struct ABDF2ConstantCache{F,uToltype,dtType,rate_prototype} <: OrdinaryDiffEqConstantCache
   uf::F
   ηold::uToltype
   κ::uToltype
@@ -6,6 +6,7 @@ mutable struct ABDF2ConstantCache{F,uToltype,dtType} <: OrdinaryDiffEqConstantCa
   newton_iters::Int
   eulercache::ImplicitEulerConstantCache
   dtₙ₊₁::dtType
+  fsalfirstprev::rate_prototype
 end
 
 function alg_cache(alg::ABDF2,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,
@@ -28,17 +29,19 @@ function alg_cache(alg::ABDF2,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUni
   eulercache = ImplicitEulerConstantCache(uf,ηold,κ,tol,100000)
 
   dtₙ₊₁ = one(dt)
+  fsalfirstprev = zero(rate_prototype)
 
-  ABDF2ConstantCache(uf, ηold, κ, tol, 10000, eulercache, dtₙ₊₁)
+  ABDF2ConstantCache(uf, ηold, κ, tol, 10000, eulercache, dtₙ₊₁, fsalfirstprev)
 end
 
-#=
 mutable struct ABDF2Cache{uType,rateType,uNoUnitsType,J,UF,JC,uToltype,F,dtType} <: OrdinaryDiffEqMutableCache
+  uₙ₊₂::uType
+  uₙ₊₁::uType
+  uₙ::uType
   du1::rateType
   fsalfirst::rateType
-  f2::rateType
-  zprev::uType
-  zᵧ::uType
+  fsalfirstprev::rateType
+  k::rateType
   z::uType
   dz::uType
   b::uType
@@ -53,15 +56,15 @@ mutable struct ABDF2Cache{uType,rateType,uNoUnitsType,J,UF,JC,uToltype,F,dtType}
   κ::uToltype
   tol::uToltype
   newton_iters::Int
-  dtprev::dtType
+  eulercache::ImplicitEulerCache
+  dtₙ₊₁::dtType
 end
 
-u_cache(c::ABDF2Cache)    = (c.zprev,c.zᵧ,c.z,c.dz)
-du_cache(c::ABDF2Cache)   = (c.f2,c.fsalfirst)
+u_cache(c::ABDF2Cache)    = (c.z,c.dz)
+du_cache(c::ABDF2Cache)   = (c.k,c.fsalfirst)
 
 function alg_cache(alg::ABDF2,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,
                    tTypeNoUnits,uprev,uprev2,f,t,dt,reltol,p,calck,::Type{Val{true}})
-
   du1 = zeros(rate_prototype)
   J = zeros(uEltypeNoUnits,length(u),length(u)) # uEltype?
   W = similar(J)
@@ -69,15 +72,16 @@ function alg_cache(alg::ABDF2,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUni
   zᵧ = similar(u,indices(u)); z = similar(u,indices(u))
   dz = similar(u,indices(u))
   fsalfirst = zeros(rate_prototype)
-  f2 = zeros(rate_prototype)
+  fsalfirstprev = zeros(rate_prototype)
+  k = zeros(rate_prototype)
   tmp = similar(u); b = similar(u,indices(u))
   atmp = similar(u,uEltypeNoUnits,indices(u))
 
   uf = DiffEqDiffTools.UJacobianWrapper(f,t,p)
   linsolve = alg.linsolve(Val{:init},uf,u)
   jac_config = build_jac_config(alg,f,uf,du1,uprev,u,tmp,dz)
-
   uToltype = real(uBottomEltypeNoUnits)
+
   if alg.κ != nothing
     κ = uToltype(alg.κ)
   else
@@ -91,11 +95,9 @@ function alg_cache(alg::ABDF2,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUni
 
   ηold = one(uToltype)
 
-  dtprev = zero(dt)
+  eulercache = ImplicitEulerCache(u,uprev,uprev2,du1,fsalfirst,k,z,dz,b,tmp,atmp,J,W,uf,jac_config,linsolve,ηold,κ,tol,10000)
 
-  ABDF2Cache{typeof(u),typeof(rate_prototype),typeof(atmp),typeof(J),typeof(uf),
-              typeof(jac_config),uToltype,typeof(tab),typeof(linsolve)}(
-              du1,fsalfirst,f2,zprev,zᵧ,z,dz,b,tmp,atmp,J,
-              W,uf,jac_config,linsolve,ηold,κ,tol,10000,dtprev)
+  dtₙ₊₁ = one(dt)
+  ABDF2Cache(u,uprev,uprev2,du1,fsalfirst,fsalfirstprev,k,z,dz,b,tmp,atmp,J,
+              W,uf,jac_config,linsolve,ηold,κ,tol,10000,eulercache,dtₙ₊₁)
 end
-=#
