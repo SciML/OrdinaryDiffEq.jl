d84c1a716ef4b2d6c9f9a77bc6d3d1ee852d2931
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-12 22:51:57 -0700

    Use DiffEqBase.check_error!
    https://github.com/JuliaDiffEq/DifferentialEquations.jl/issues/276

--- src/integrators/integrator_utils.jl
+++ src/integrators/integrator_utils.jl
@@ -25,37 +25,11 @@ function loopheader!(integrator)
   choose_algorithm!(integrator,integrator.cache)
 end
 
+last_step_failed(integrator::ODEIntegrator) =
+  integrator.last_stepfail && !integrator.opts.adaptive
+
 @def ode_exit_conditions begin
-  if integrator.iter > integrator.opts.maxiters
-    if integrator.opts.verbose
-      warn("Interrupted. Larger maxiters is needed.")
-    end
-    postamble!(integrator)
-    integrator.sol = solution_new_retcode(integrator.sol,:MaxIters)
-    return integrator.sol
-  end
-  if !integrator.opts.force_dtmin && integrator.opts.adaptive && abs(integrator.dt) <= abs(integrator.opts.dtmin)
-    if integrator.opts.verbose
-      warn("dt <= dtmin. Aborting. If you would like to force continuation with dt=dtmin, set force_dtmin=true")
-    end
-    postamble!(integrator)
-    integrator.sol = solution_new_retcode(integrator.sol,:DtLessThanMin)
-    return integrator.sol
-  end
-  if integrator.opts.unstable_check(integrator.dt,integrator.u,integrator.p,integrator.t)
-    if integrator.opts.verbose
-      warn("Instability detected. Aborting")
-    end
-    postamble!(integrator)
-    integrator.sol = solution_new_retcode(integrator.sol,:Unstable)
-    return integrator.sol
-  end
-  if integrator.last_stepfail && !integrator.opts.adaptive
-    if integrator.opts.verbose
-      warn("Newton steps could not converge and algorithm is not adaptive. Use a lower dt.")
-    end
-    postamble!(integrator)
-    integrator.sol = solution_new_retcode(integrator.sol,:ConvergenceFailure)
+  if check_error!(integrator) != :Success
     return integrator.sol
   end
 end
--- src/iterator_interface.jl
+++ src/iterator_interface.jl
@@ -1,51 +1,3 @@
-function start(integrator::ODEIntegrator)
-  0
-end
-
-@inline function next(integrator::ODEIntegrator,state)
-  state += 1
-  step!(integrator) # Iter updated in the step! header
-  # Next is callbacks -> iterator  -> top
-  integrator,state
-end
-
-done(integrator::ODEIntegrator) = done(integrator,integrator.iter)
-
-@inline function done(integrator::ODEIntegrator,state)
-  if integrator.iter > integrator.opts.maxiters
-    if integrator.opts.verbose
-      warn("Interrupted. Larger maxiters is needed.")
-    end
-    postamble!(integrator)
-    return true
-  end
-  if integrator.opts.unstable_check(integrator.dt,integrator.u,integrator.p,integrator.t)
-    if integrator.opts.verbose
-      warn("Instability detected. Aborting")
-    end
-    postamble!(integrator)
-    return true
-  end
-  if !integrator.opts.force_dtmin && integrator.opts.adaptive && abs(integrator.dt) <= abs(integrator.opts.dtmin)
-    if integrator.opts.verbose
-      warn("dt <= dtmin. Aborting. If you would like to force continuation with dt=dtmin, set force_dtmin=true")
-    end
-    postamble!(integrator)
-    return true
-  end
-  if isempty(integrator.opts.tstops)
-    postamble!(integrator)
-    return true
-  elseif integrator.just_hit_tstop
-    integrator.just_hit_tstop = false
-    if integrator.opts.stop_at_next_tstop
-      postamble!(integrator)
-      return true
-    end
-  end
-  false
-end
-
 @inline function step!(integrator::ODEIntegrator)
   if integrator.opts.advance_to_tstop
     @inbounds while integrator.tdir*integrator.t < integrator.tdir*top(integrator.opts.tstops)
@@ -65,5 +17,3 @@ end
   end
   @inbounds handle_tstop!(integrator)
 end
-
-eltype(integrator::ODEIntegrator) = typeof(integrator)
