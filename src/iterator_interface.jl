2b2755d5bd5e0eadc7fafc1528d22e55bbf325bb
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-13 19:04:59 -0700

    Return nothing from step! always

--- src/iterator_interface.jl
+++ src/iterator_interface.jl
@@ -2,13 +2,13 @@
   if integrator.opts.advance_to_tstop
     @inbounds while integrator.tdir*integrator.t < integrator.tdir*top(integrator.opts.tstops)
       loopheader!(integrator)
-      @ode_exit_conditions
+      check_error!(integrator) != :Success && return
       perform_step!(integrator,integrator.cache)
       loopfooter!(integrator)
     end
   else
     @inbounds loopheader!(integrator)
-    @ode_exit_conditions
+    check_error!(integrator) != :Success && return
     @inbounds perform_step!(integrator,integrator.cache)
     @inbounds loopfooter!(integrator)
     @inbounds while !integrator.accept_step
