8dff23102f0ef8a201396297da5a1e99986e9797
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-13 18:14:21 -0700

    Call @ode_exit_conditions in step!
    closes #318

--- src/iterator_interface.jl
+++ src/iterator_interface.jl
@@ -2,11 +2,13 @@
   if integrator.opts.advance_to_tstop
     @inbounds while integrator.tdir*integrator.t < integrator.tdir*top(integrator.opts.tstops)
       loopheader!(integrator)
+      @ode_exit_conditions
       perform_step!(integrator,integrator.cache)
       loopfooter!(integrator)
     end
   else
     @inbounds loopheader!(integrator)
+    @ode_exit_conditions
     @inbounds perform_step!(integrator,integrator.cache)
     @inbounds loopfooter!(integrator)
     @inbounds while !integrator.accept_step
