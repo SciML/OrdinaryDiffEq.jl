e803df5fb448fbaa0c3bfbcd78051d96a557c604
YingboMa <mayingbo5@gmail.com>
2018-04-30 15:15:26 -0700

    Fix stiffness detection test failure

--- test/stiffness_detection_test.jl
+++ test/stiffness_detection_test.jl
@@ -14,25 +14,25 @@ is_switching_fb(sol) = maximum(diff(find(x->x==2, sol.alg_choice))) > 5
 for prob in probArr
   alg = AutoTsit5(Rodas5(); maxstiffstep=5, maxnonstiffstep=5, stiffalgfirst=true)
   sol = solve(prob, alg)
-  @test length(sol.t) < 335
+  @test length(sol.t) < 280
   @test typeof(alg.algs[sol.alg_choice[1]]) <: Rodas5
   @test is_switching_fb(sol)
   sol = solve(prob, AutoDP5(Rodas5(); maxstiffstep=2, maxnonstiffstep=2,
                             stifftol=11//10, nonstifftol=9//10),
                             reltol=1e-5, abstol=1e-5)
-  @test length(sol.t) < 620
+  @test length(sol.t) < 625
   @test is_switching_fb(sol)
 
   sol = solve(prob,AutoVern6(Kvaerno3(); maxstiffstep=4, maxnonstiffstep=4))
-  @test length(sol.t) < 810
+  @test length(sol.t) < 690
   @test is_switching_fb(sol)
   sol = solve(prob,AutoVern7(Hairer42(); maxstiffstep=4, maxnonstiffstep=4))
-  @test length(sol.t) < 635
+  @test length(sol.t) < 535
   @test is_switching_fb(sol)
   sol = solve(prob,AutoVern8(Rosenbrock23(); maxstiffstep=4, maxnonstiffstep=4))
-  @test length(sol.t) < 570
+  @test length(sol.t) < 910
   @test is_switching_fb(sol)
   sol = solve(prob,AutoVern9(KenCarp3(); maxstiffstep=4, maxnonstiffstep=4))
-  @test length(sol.t) < 705
+  @test length(sol.t) < 470
   @test is_switching_fb(sol)
 end
