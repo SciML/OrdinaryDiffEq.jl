97269d387989502d1c992fa1c62aad30e07124cc
Christopher Rackauckas <accounts@chrisrackauckas.com>
2018-05-10 06:30:59 -0700

    Revert "Update callbacks.jl"
    This reverts commit 381ef94da69ef63ac6ed4f4e8fa1bb875a8882f8.

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -188,8 +188,7 @@ end
 @inline function apply_discrete_callback!(integrator,callback::DiscreteCallback)
   saved_in_cb = false
   if callback.condition(integrator.u,integrator.t,integrator)
-    # Pre-callback save, but don't double up if unnecessary
-    @inbounds if callback.save_positions[1] && integrator.t != sol.t[end]
+    @inbounds if callback.save_positions[1]
       savevalues!(integrator,true)
       saved_in_cb = true
     end
