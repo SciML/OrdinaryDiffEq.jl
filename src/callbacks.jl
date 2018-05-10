381ef94da69ef63ac6ed4f4e8fa1bb875a8882f8
Christopher Rackauckas <accounts@chrisrackauckas.com>
2018-05-10 06:30:59 -0700

    Update callbacks.jl

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -188,7 +188,8 @@ end
 @inline function apply_discrete_callback!(integrator,callback::DiscreteCallback)
   saved_in_cb = false
   if callback.condition(integrator.u,integrator.t,integrator)
-    @inbounds if callback.save_positions[1]
+    # Pre-callback save, but don't double up if unnecessary
+    @inbounds if callback.save_positions[1] && integrator.t != sol.t[end]
       savevalues!(integrator,true)
       saved_in_cb = true
     end
