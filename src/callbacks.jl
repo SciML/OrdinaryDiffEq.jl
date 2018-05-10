ec42b20229e65602ef90757b191576961b5da495
Christopher Rackauckas <accounts@chrisrackauckas.com>
2018-05-10 06:26:21 -0700

    Revert "Update callbacks.jl"
    This reverts commit c96026b99f09c78f1b41bbfcae336b2f70fd4200.

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -151,8 +151,7 @@ function apply_callback!(integrator,callback::ContinuousCallback,cb_time,prev_si
   end
   saved_in_cb = false
 
-  # Pre-callback save, but don't double up if unnecessary
-  @inbounds if callback.save_positions[1] && integrator.t != sol.t[end]
+  @inbounds if callback.save_positions[1]
     savevalues!(integrator,true)
     saved_in_cb = true
   end
