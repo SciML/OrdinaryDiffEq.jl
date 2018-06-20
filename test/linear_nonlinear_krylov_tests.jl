27dc43ab90d8b49a7d3e85056fde8567ccc9d424
Xingjian Guo <xg703@nyu.edu>
2018-06-20 14:52:54 -0400

    Reduce Krylov test time

--- test/linear_nonlinear_krylov_tests.jl
+++ test/linear_nonlinear_krylov_tests.jl
@@ -1,6 +1,6 @@
 using OrdinaryDiffEq, Base.Test, DiffEqOperators
-N = 100
-dx = 1.0; dt=0.01
+N = 20
+dx = 1.0; dt=0.1
 srand(0); u0 = rand(N)
 reltol = 1e-4
 L = DerivativeOperator{Float64}(2,2,dx,N,:Dirichlet0,:Dirichlet0)
@@ -17,10 +17,12 @@ Algs = [LawsonEuler,NorsettEuler,ETDRK2,ETDRK3,ETDRK4]
 for Alg in Algs
     gc()
     sol = solve(prob, Alg(); dt=dt, internalnorm=Base.norm)
-    sol_krylov = solve(prob, Alg(krylov=true); dt=dt, reltol=reltol, internalnorm=Base.norm)
+    sol_krylov = solve(prob, Alg(krylov=true, m=10); dt=dt, reltol=reltol, internalnorm=Base.norm)
     @test isapprox(sol.u,sol_krylov.u; rtol=reltol)
 
     sol_ip = solve(prob_inplace, Alg(); dt=dt, internalnorm=Base.norm)
-    sol_ip_krylov = solve(prob_inplace, Alg(krylov=true); dt=dt, reltol=reltol, internalnorm=Base.norm)
+    sol_ip_krylov = solve(prob_inplace, Alg(krylov=true, m=10); dt=dt, reltol=reltol, internalnorm=Base.norm)
     @test isapprox(sol.u,sol_krylov.u; rtol=reltol)
+
+    println(Alg) # prevent Travis hanging
 end
