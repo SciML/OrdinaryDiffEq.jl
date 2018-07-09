e7844fa29aee4c446695e5b7f0cf1043aee8abfe
YingboMa <mayingbo5@gmail.com>
2018-07-08 19:31:55 -0700

    Reduce Vern9's `nth_der` check to 3

--- test/ode/ode_dense_tests.jl
+++ test/ode/ode_dense_tests.jl
@@ -234,8 +234,8 @@ regression_test(Vern8(), 3e-8, 5e-8; test_diff1 = true)
 regression_test(Vern8(lazy=false), 3e-8, 5e-8; test_diff1 = true)
 
 # Vern9
-regression_test(Vern9(), 1e-9, 2e-9; test_diff1 = true, nth_der=4, dertol=1e-2)
-regression_test(Vern9(lazy=false), 1e-9, 2e-9; test_diff1 = true, nth_der=4, dertol=1e-2)
+regression_test(Vern9(), 1e-9, 2e-9; test_diff1 = true, nth_der=3, dertol=1e-2)
+regression_test(Vern9(lazy=false), 1e-9, 2e-9; test_diff1 = true, nth_der=3, dertol=1e-2)
 
 println("Rosenbrocks")
 
