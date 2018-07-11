b5b4bcd3914b038d688e11458a38da6ff9764310
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-10 21:22:33 -0700

    eps scaling in time cutoffs

--- src/integrators/integrator_utils.jl
+++ src/integrators/integrator_utils.jl
@@ -270,7 +270,7 @@ function loopfooter!(integrator)
       # integrator.EEst has unitless type of integrator.t
       if typeof(integrator.EEst)<: AbstractFloat && !isempty(integrator.opts.tstops)
         tstop = top(integrator.opts.tstops)
-        abs(ttmp - tstop) < 10eps(typeof(integrator.t))*oneunit(integrator.t) ?
+        abs(ttmp - tstop) < 10eps(integrator.t/oneunit(integrator.t))*oneunit(integrator.t) ?
                                   (integrator.t = tstop) : (integrator.t = ttmp)
       else
         integrator.t = ttmp
@@ -283,7 +283,7 @@ function loopfooter!(integrator)
     # integrator.EEst has unitless type of integrator.t
     if typeof(integrator.EEst)<: AbstractFloat && !isempty(integrator.opts.tstops)
       tstop = top(integrator.opts.tstops)
-      abs(ttmp - tstop) < 10eps(typeof(integrator.t))*oneunit(integrator.t) ?
+      abs(ttmp - tstop) < 10eps(integrator.t/oneunit(integrator.t))*oneunit(integrator.t) ?
                                   (integrator.t = tstop) : (integrator.t = ttmp)
     else
       integrator.t = ttmp
