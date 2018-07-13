29aeafce652f9f1df8f62f41d5b12f254cec52c2
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-12 19:58:17 -0700

    update saveat tests and fix saveat behavior

--- test/ode/ode_saveat_tests.jl
+++ test/ode/ode_saveat_tests.jl
@@ -3,65 +3,67 @@ using OrdinaryDiffEq, DiffEqProblemLibrary, Base.Test
 @testset "saveat Tests" begin
   prob = prob_ode_linear
 
-  sol =solve(prob,DP5(),dt=1//2^(2),save_everystep=false,dense=false)
-  sol2=solve(prob,DP5(),dt=1//2^(2),save_everystep=false,dense=false,saveat=[1/2])
+  sol =solve(prob,DP5(),dt=1//2^(2),save_everystep=false)
+  sol2=solve(prob,DP5(),dt=1//2^(2),save_everystep=false,saveat=[1/2])
+
+  @test sort!(symdiff(sol.t,sol2.t)) == [0.0,1/2,1.0]
+
+  sol2=solve(prob,DP5(),dt=1//2^(2),save_everystep=false,saveat=[0.0,1/2,1.0])
 
   @test symdiff(sol.t,sol2.t) == [1/2]
 
-  sol2=solve(prob,DP5(),dt=1//2^(2),save_everystep=false,dense=false,saveat=1/2)
+  sol2=solve(prob,DP5(),dt=1//2^(2),save_everystep=false,saveat=1/2)
 
-  @test symdiff(sol.t,sol2.t) == [1/2]
+  @test sort!(symdiff(sol.t,sol2.t)) == [1/2]
 
-  sol3=solve(prob,DP5(),dt=1//2^(2),save_everystep=false,dense=false,saveat=[1/2],tstops=[1/2])
+  sol3=solve(prob,DP5(),dt=1//2^(2),save_everystep=false,saveat=[1/2],tstops=[1/2])
 
-  @test sol3.t == [0.0,0.5,1.00]
+  @test sol3.t == [1/2]
 
-  sol3=solve(prob,DP5(),dt=1//2^(2),saveat=[1/2],tstops=[1/2])
+  sol3=solve(prob,DP5(),dt=1//2^(2),saveat=[0.0,1/2,1.0],tstops=[1/2])
 
-  @test sol3.t == [0.0,0.5,1.00]
+  @test sol3.t == [0.0,1/2,1.0]
 
   sol3=solve(prob,DP5(),dt=1//2^(2),saveat=1/10,tstops=[1/2])
 
   @test sol3.t == collect(0.0:0.1:1.00)
 
-  #plot(0:1//2^(4):1,interpd)
-
-  sol =solve(prob,RK4(),dt=1/2^(2),save_everystep=true,dense=false)
-  sol2=solve(prob,RK4(),dt=1/2^(2),save_everystep=true,dense=false,saveat=[.125,.6,.61,.8])
+  sol =solve(prob,RK4(),dt=1/2^(2),save_everystep=true,adaptive=false)
+  sol2=solve(prob,RK4(),dt=1/2^(2),save_everystep=true,adaptive=false,saveat=[.125,.6,.61,.8])
 
   @test symdiff(sol.t,sol2.t) == [.125,.6,.61,.8]
 
-  sol =solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true,dense=false)
-  sol2=solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true,dense=false,saveat=[.125,.6,.61,.8])
+  sol =solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true)
+  sol2=solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true,saveat=[.125,.6,.61,.8])
 
   @test symdiff(sol.t,sol2.t) == [.125,.6,.61,.8]
 
-  sol =solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=true,dense=false)
-  sol2=solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=true,dense=false,saveat=[.125,.6,.61,.8])
+  sol =solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=true)
+  sol2=solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=true,saveat=[.125,.6,.61,.8])
 
   @test symdiff(sol.t,sol2.t) == [.125,.6,.61,.8]
 
   prob = prob_ode_2Dlinear
 
-  sol =solve(prob,DP5(),dt=1//2^(2),save_everystep=true,dense=false)
-  sol2=solve(prob,DP5(),dt=1//2^(2),save_everystep=true,dense=false,saveat=[1/2])
+  sol =solve(prob,DP5(),dt=1//2^(2),save_everystep=true)
+  sol2=solve(prob,DP5(),dt=1//2^(2),save_everystep=true,saveat=[0.0,1/2,1.0])
 
   @test symdiff(sol.t,sol2.t) == [1/2]
 
-  sol =solve(prob,RK4(),dt=1/2^(2),save_everystep=true,dense=false)
-  sol2=solve(prob,RK4(),dt=1/2^(2),save_everystep=true,dense=false,saveat=[.125,.6,.61,.8])
+  sol =solve(prob,RK4(),dt=1/2^(2),save_everystep=true,adaptive=false)
+  sol2=solve(prob,RK4(),dt=1/2^(2),save_everystep=true,adaptive=false,saveat=[0.0,.125,.6,.61,.8,1.0])
 
   @test symdiff(sol.t,sol2.t) == [.125,.6,.61,.8]
 
-  sol =solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true,dense=false)
-  sol2=solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true,dense=false,saveat=[.125,.6,.61,.8])
+  sol =solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true)
+  sol2=solve(prob,Rosenbrock32(),dt=1/2^(2),save_everystep=true,saveat=[.125,.6,.61,.8])
 
   @test symdiff(sol.t,sol2.t) == [.125,.6,.61,.8]
 
-  sol =solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=true,dense=false)
-  sol2=solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=true,dense=false,saveat=[.125,.6,.61,.8])
+  sol =solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=false)
+  sol2=solve(prob,GenericTrapezoid(),dt=1/2^(2),saveat=[.125,.6,.61,.8])
 
-  @test symdiff(sol.t,sol2.t) == [.125,.6,.61,.8]
+  @test sort!(symdiff(sol.t,sol2.t)) == [0.0,.125,.6,.61,.8,1.0]
 
   sol=solve(prob,GenericTrapezoid(),dt=1/2^(2),save_everystep=true,dense=false,saveat=[0,.125,.6,.61,.8])
 
