69dc293f794fdb1684270cd57bdf49cb0cb83f88
YingboMa <mayingbo5@gmail.com>
2018-07-20 13:45:24 -0700

    Add docstrings for `diffeq_nlsolve!`

--- src/nlsolve_utils.jl
+++ src/nlsolve_utils.jl
@@ -23,11 +23,27 @@ struct NLsolveMutableCache{rateType,uType,J,uToltype,cType,gType} <: AbstractNLs
   k::rateType
   new_W::Bool
 end
+
+"""
+  nlsolve_cache(alg, cache::OrdinaryDiffEqConstantCache, z, tmp, W, γ, c, new_W)
+  -> NLsolveConstantCache
+
+Return a wrapper `NLsolveConstantCache` for `cache::OrdinaryDiffEqConstantCache`,
+so that `diffeq_nlsolve!` does not need to assume fieldnames of `cache`.
+"""
 function nlsolve_cache(alg::Union{OrdinaryDiffEqNewtonAdaptiveAlgorithm,
                                   OrdinaryDiffEqNewtonAlgorithm},
                        cache::OrdinaryDiffEqConstantCache, z, tmp, W, γ, c, new_W)
   NLsolveConstantCache(z, tmp, W, cache.κ, cache.tol, γ, c)
 end
+
+"""
+  nlsolve_cache(alg, cache::OrdinaryDiffEqMutableCache, z, tmp, W, γ, c, new_W)
+  -> NLsolveMutableCache
+
+Return a wrapper `NLsolveMutableCache` for `cache::OrdinaryDiffEqMutableCache`,
+so that `diffeq_nlsolve!` does not need to assume fieldnames of `cache`.
+"""
 function nlsolve_cache(alg::Union{OrdinaryDiffEqNewtonAdaptiveAlgorithm,
                                   OrdinaryDiffEqNewtonAlgorithm},
                        cache::OrdinaryDiffEqMutableCache, z, tmp, γ, c, new_W)
@@ -35,7 +51,39 @@ function nlsolve_cache(alg::Union{OrdinaryDiffEqNewtonAdaptiveAlgorithm,
                       cache.W, cache.κ, cache.tol, γ, c, cache.k, new_W)
 end
 
-# return `fail_convergence`
+"""
+  diffeq_nlsolve!(integrator, nlcache, cache, ::Type{Val{:newton}}) -> (z, η,
+  iter, retcode)
+
+Perform numerically stable modified Newton iteration that is specialized for
+implicit methods (see [^HS96] and [^HW96]), where `z` is the solution, `η` is
+used to measure the iteration error (see [^HW96]), `iter` is the number of
+iteration, and `retcode` reports whether the algorithm succeed. It solves
+
+```math
+G(z) = dt⋅f(tmp + γ⋅z, p, t+c⋅h) - z = 0⃗
+```
+
+by iterating
+
+```math
+W Δᵏ = dt⋅f(tmp + γ⋅zᵏ, p, t+c⋅h) - zᵏ
+zᵏ⁺¹ = zᵏ + Δᵏ
+```
+
+where `W=M-dt⋅γJ`, `M` is the mass matrix, `dt` is the step size, `γ` is a
+constant, and `J` is the Jacobian matrix.
+
+[^HS96]: M.E.Hoseaa and L.F.Shampine, "Analysis and implementation of TR-BDF2",
+Applied Numerical Mathematics, Volume 20, Issues 1–2, February 1996, Pages
+21-37.
+[doi:10.1016/0168-9274(95)00115-8](https://doi.org/10.1016/0168-9274(95)00115-8)
+
+[^HW96]: Ernst Hairer and Gerhard Wanner, "Solving Ordinary Differential
+Equations II, Springer Series in Computational Mathematics. ISBN
+978-3-642-05221-7. Section IV.8.
+[doi:10.1007/978-3-642-05221-7](https://doi.org/10.1007/978-3-642-05221-7)
+"""
 function diffeq_nlsolve!(integrator,
                          nlcache::NLsolveConstantCache,
                          cache::OrdinaryDiffEqConstantCache,
