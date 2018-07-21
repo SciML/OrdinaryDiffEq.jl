5d04810cb794a1a19edfae3f07344380e8a6e40e
YingboMa <mayingbo5@gmail.com>
2018-07-20 20:08:16 -0700

    Add in-place functional iteration

--- src/nlsolve_utils.jl
+++ src/nlsolve_utils.jl
@@ -292,3 +292,70 @@ function diffeq_nlsolve!(integrator,
 
   return (z, η, iter, false)
 end
+
+function diffeq_nlsolve!(integrator,
+                         nlcache::NLsolveMutableCache,
+                         cache::OrdinaryDiffEqMutableCache,
+                         ::Type{Val{:functional}})
+  @unpack t,dt,uprev,u,f,p = integrator
+  @unpack z,dz,tmp,κ,tol,b,k,c,γ = nlcache
+  z₊ = b
+  mass_matrix = integrator.sol.prob.mass_matrix
+  alg = unwrap_alg(integrator, true)
+  if typeof(integrator.f) <: SplitFunction
+    f = integrator.f.f1
+  else
+    f = integrator.f
+  end
+  # precalculations
+  κtol = κ*tol
+  # initial step of functional iteration
+  iter = 1
+  tstep = t + c*dt
+  @. u = tmp + γ*z
+  f(k, u, p, tstep)
+  if mass_matrix == I
+    @. z₊ = dt*k
+  else
+    lmul!(dt, k) # TODO: check unit
+    mul!(z₊, mass_matrix, k)
+  end
+  @. dz = z₊ - z
+  @. z = z₊
+  ndz = integrator.opts.internalnorm(dz)
+
+  η = max(cache.ηold,eps(eltype(integrator.opts.reltol)))^(0.8)
+  do_functional = integrator.success_iter == 0 || η*ndz > κtol
+
+  # Functional iteration
+  fail_convergence = false
+  while (do_functional || iter < alg.min_newton_iter) && iter < alg.max_newton_iter
+    iter += 1
+    @. u = tmp + γ*z
+    f(k, u, p, tstep)
+    if mass_matrix == I
+      @. z₊ = dt*k
+    else
+      lmul!(dt, k) # TODO: check unit
+      mul!(z₊, mass_matrix, k)
+    end
+    @. dz = z₊ - z
+    ndzprev = ndz
+    ndz = integrator.opts.internalnorm(dz)
+    θ = ndz/ndzprev
+    if θ > 1 || ndz*(θ^(alg.max_newton_iter - iter)/(1-θ)) > κtol
+      fail_convergence = true
+      break
+    end
+    η = θ/(1-θ)
+    do_functional = (η*ndz > κtol)
+    @. z = z₊
+  end
+
+  if (iter >= alg.max_newton_iter && do_functional) || fail_convergence
+    integrator.force_stepfail = true
+    return (z, η, iter, true)
+  end
+
+  return (z, η, iter, false)
+end
