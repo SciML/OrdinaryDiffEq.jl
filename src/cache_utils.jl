a393f1fdb44de092e3414e7a3fa2278e30f019e7
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-08-15 19:12:20 +0200

    fix callbacks

--- src/cache_utils.jl
+++ src/cache_utils.jl
@@ -1,2 +1,3 @@
 is_constant_cache(cache::OrdinaryDiffEqConstantCache) = true
+is_constant_cache(cache::OrdinaryDiffEqCache) = false
 is_constant_cache(cache::CompositeCache) = is_constant_cache(cache.caches[1])
