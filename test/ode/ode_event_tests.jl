a0c3e0ad9418981d12f68eadebf5ceb2838be722
Yingbo Ma <mayingbo5@gmail.com>
2018-09-26 09:34:43 -0400

    Add a `saveat` test with a callback [broken]

--- test/ode/ode_event_tests.jl
+++ test/ode/ode_event_tests.jl
@@ -78,6 +78,9 @@ tspan = (0.0,15.0)
 prob = ODEProblem(f,u0,tspan)
 
 sol = solve(prob,Tsit5(),callback=callback_single,adaptive=false,dt=1/4)
+sol = solve(prob,Tsit5(),callback=callback_single,saveat=3.192754284070482)
+@test_broken count(x->x==3.192754284070482, sol.t) == 2
+
 @test sol(sol.t[findfirst(x->abs(x[1])<1e-12, sol.u)],continuity=:right)[2] > 0
 @test sol(sol.t[findfirst(x->abs(x[1])<1e-12, sol.u)],continuity=:left)[2] < 0
 
