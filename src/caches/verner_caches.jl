5ed9e08a49ee3a6baaca95ec29c517ef3095ab0c
kanav99 <kanav0610@gmail.com>
2019-02-06 01:52:03 +0530

    Vern7 cache reduction

--- src/caches/verner_caches.jl
+++ src/caches/verner_caches.jl
@@ -49,9 +49,9 @@ end
 
 function alg_cache(alg::Vern7,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,uprev2,f,t,dt,reltol,p,calck,::Type{Val{true}})
   tab = Vern7ConstantCache(real(uBottomEltypeNoUnits),real(tTypeNoUnits))
-  k1 = zero(rate_prototype); k2 = zero(rate_prototype); k3 = zero(rate_prototype); k4 = zero(rate_prototype);
+  k1 = zero(rate_prototype); k2 = zero(rate_prototype); k3 = k2                  ; k4 = zero(rate_prototype);
   k5 = zero(rate_prototype); k6 = zero(rate_prototype); k7 = zero(rate_prototype); k8 = zero(rate_prototype);
-  k9 = zero(rate_prototype); k10 = zero(rate_prototype); utilde = similar(u)
+  k9 = zero(rate_prototype); k10 = k2                 ; utilde = similar(u)
   tmp = similar(u); atmp = similar(u,uEltypeNoUnits)
   Vern7Cache(u,uprev,k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,utilde,tmp,atmp,tab)
 end
