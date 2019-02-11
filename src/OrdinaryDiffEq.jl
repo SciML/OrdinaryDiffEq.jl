d2343db56136acadb4d474ea45fd432191b93dd6
Hendrik Ranocha <h.ranocha@tu-bs.de>
2019-02-11 18:48:24 +0100

    ParsaniKetchesonDeconinck3S173

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -168,6 +168,7 @@ module OrdinaryDiffEq
 
   export ORK256, CarpenterKennedy2N54, HSLDDRK64, DGLDDRK73_C, DGLDDRK84_C, DGLDDRK84_F, NDBLSRK124, NDBLSRK134, NDBLSRK144,
          CFRLDDRK64, TSLDDRK74,
+         ParsaniKetchesonDeconinck3S173,
          ParsaniKetchesonDeconinck3S94, ParsaniKetchesonDeconinck3S184,
          ParsaniKetchesonDeconinck3S105, ParsaniKetchesonDeconinck3S205
 
