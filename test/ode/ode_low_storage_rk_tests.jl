4a0d82042941fb38aad8f5a538dfd39c6bc2caba
deeepeshthakur <deeepeshthakur@gmail.com>
2019-02-19 01:34:47 +0530

    Fix Coefficients

--- test/ode/ode_low_storage_rk_tests.jl
+++ test/ode/ode_low_storage_rk_tests.jl
@@ -370,7 +370,7 @@ alg = CKLLSRK95_4S()
 dts = 1 ./ 2 .^(6:-1:2)
 for prob in test_problems_only_time
   sim = test_convergence(dts, prob, alg)
-  @test sim.𝒪est[:final] ≈ 1 atol=testTol
+  @test sim.𝒪est[:final] ≈ OrdinaryDiffEq.alg_order(alg) atol=testTol
 end
 for prob in test_problems_linear
   sim = test_convergence(dts, prob, alg)
