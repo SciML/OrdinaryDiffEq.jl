c941ff75f2a0d3d8c5f47def7daed0ef1c988a60
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-02-19 11:35:31 -0500

    just recursivecopy in the basic rate_prototype construction case

--- src/solve.jl
+++ src/solve.jl
@@ -138,7 +138,7 @@ function DiffEqBase.__init(
 
   if isinplace(prob) && typeof(u) <: AbstractArray && eltype(u) <: Number && uBottomEltypeNoUnits == uBottomEltype # Could this be more efficient for other arrays?
     if !(typeof(u) <: ArrayPartition)
-      rate_prototype = similar(u,typeof(oneunit(uBottomEltype)/oneunit(tType)))
+      rate_prototype = recursivecopy(u)
     else
       rate_prototype = similar(u, typeof.(oneunit.(recursive_bottom_eltype.(u.x))./oneunit(tType))...)
     end
