3e054df91affbf66710eaf02357b8dfb6340df3e
Yingbo Ma <mayingbo5@gmail.com>
2019-03-18 16:49:54 -0400

    Fix forward difference cache

--- src/derivative_wrappers.jl
+++ src/derivative_wrappers.jl
@@ -62,20 +62,21 @@ jacobian_finitediff(f, x::AbstractArray, diff_type) =
     DiffEqDiffTools.finite_difference_jacobian(f, x, diff_type, eltype(x), Val{false})
 
 function jacobian!(J::AbstractMatrix{<:Number}, f, x::AbstractArray{<:Number}, fx::AbstractArray{<:Number}, integrator::DiffEqBase.DEIntegrator, jac_config)
-    local count
     if get_current_alg_autodiff(integrator.alg, integrator.cache)
       ForwardDiff.jacobian!(J, f, fx, x, jac_config)
-      count = 1
+      integrator.destats.nf += 1
     else
       isforward = integrator.alg.diff_type === Val{:forward}
-      isforward ? (du = get_du(integrator); f(du, x); DiffEqDiffTools.finite_difference_jacobian!(J, f, x, jac_config, du)) : DiffEqDiffTools.finite_difference_jacobian!(J, f, x, jac_config)
-      if (integrator.alg.diff_type==Val{:complex} && eltype(x)<:Real) || isforward
-        count = length(x) + isforward
-      else
-        count = 2*length(x)
+      if isforward
+        forwardcache = get_tmp_cache(integrator)[2]
+        f(forwardcache, x)
+        integrator.destats.nf += 1
+        DiffEqDiffTools.finite_difference_jacobian!(J, f, x, jac_config, forwardcache)
+      else # not forward difference
+        DiffEqDiffTools.finite_difference_jacobian!(J, f, x, jac_config)
       end
+      integrator.destats.nf += (integrator.alg.diff_type==Val{:complex} && eltype(x)<:Real || isforward) ? length(x) : 2length(x)
     end
-    integrator.destats.nf += count
     nothing
 end
 
