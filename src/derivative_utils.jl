3adc552de2c1d78ec736c21d9e6195a8cfeb288b
Yingbo Ma <mayingbo5@gmail.com>
2019-03-20 18:04:09 -0400

    Fix KenCarp's error on operators

--- src/derivative_utils.jl
+++ src/derivative_utils.jl
@@ -162,7 +162,9 @@ function Base.convert(::Type{AbstractMatrix}, W::WOperator)
       rmul!(copyto!(W._concrete_form, W.mass_matrix), -1/W.gamma)
       axpy!(one(W.gamma), convert(AbstractMatrix,W.J), W._concrete_form)
     else
-      @. W._concrete_form = -W.mass_matrix
+      @inbounds for j in axes(W._concrete_form, 2), i in axes(W._concrete_form, 1)
+        W._concrete_form[i, j] = -W.mass_matrix[i, j]
+      end
       axpy!(W.gamma, convert(AbstractMatrix,W.J), W._concrete_form)
     end
   end
