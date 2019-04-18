36203d15866def738e587be66129ef479656c3d8
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-04-18 17:54:41 -0400

    generic dense zero'd Jacobians

--- src/nlsolve/utils.jl
+++ src/nlsolve/utils.jl
@@ -117,7 +117,7 @@ DiffEqBase.@def iipnlsolve begin
         W = WOperator(f, dt, true)
         J = nothing # is J = W.J better?
       else
-        J = zero(u .* u')
+        J = false .* u .* u'
         W = similar(J)
       end
     end
