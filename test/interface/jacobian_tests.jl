a9e67c40b4560b631b123a8fc44042e2ff6ad981
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-04 10:34:43 -0400

    Restructure tests

---
+++ test/interface/jacobian_tests.jl
@@ -0,0 +1,37 @@
+using OrdinaryDiffEq, Test, ParameterizedFunctions
+
+d_alembert = @ode_def DAlembert begin
+    dx = a - b*x + c*t
+end a b c
+function d_alembert_analytic(u0,p,t::Number)
+    a,b,c = p
+    ebt = exp(b*t)
+    @. exp(-b*t)*(-a*b + c + ebt*(a*b + c*(b*t - 1)) + b^2 * u0)/(b^2)
+end
+
+p = (1., 2., 3.)
+u0 = [1.0]
+tspan = (0.0,10.0)
+prob = ODEProblem(ODEFunction(d_alembert.f,
+                              jac = d_alembert.jac,
+                              analytic=d_alembert_analytic),
+                              u0,tspan,p)
+
+sol = solve(prob,Tsit5(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,Rosenbrock23(),abstol=1e-8,reltol=1e-8)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,Rodas4(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,Veldd4(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,Rodas5(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,TRBDF2(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,Trapezoid(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,KenCarp3(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
+sol = solve(prob,KenCarp4(),abstol=1e-10,reltol=1e-10)
+@test sol.errors[:l2] < 1e-7
---
+++ test/regression/time_derivative_test.jl
@@ -0,0 +1,44 @@
+using OrdinaryDiffEq, Test
+
+function time_derivative(du,u,p,t)
+  du[1] = -t
+end
+function time_derivative_analytic(u0,p,t)
+  u0 .- t.^2 ./ 2
+end
+
+ff_time_derivative = ODEFunction(time_derivative,
+                                 analytic=time_derivative_analytic)
+u0 = [1.0]
+tspan = (0.0,1.0)
+prob = ODEProblem(ff_time_derivative,u0,tspan)
+
+sol = solve(prob,Rosenbrock32(),reltol=1e-9,abstol=1e-9)
+@test sol.errors[:final] < 1e-5
+sol = solve(prob,Rosenbrock23())
+@test sol.errors[:final] < 1e-10
+sol = solve(prob,Rodas4())
+@test sol.errors[:final] < 1e-10
+sol = solve(prob,Rodas5())
+@test sol.errors[:final] < 1e-10
+sol = solve(prob,Veldd4())
+@test sol.errors[:final] < 1e-10
+sol = solve(prob,KenCarp4())
+@test sol.errors[:final] < 1e-10
+sol = solve(prob,TRBDF2())
+@test sol.errors[:final] < 1e-10
+
+
+sol = solve(prob,GenericImplicitEuler(),dt=1/10)
+@test sol.errors[:final] < 1e-1
+sol = solve(prob,GenericTrapezoid(),dt=1/10)
+@test sol.errors[:final] < 1e-12
+sol = solve(prob,Euler(),dt=1/100)
+@test sol.errors[:final] < 6e-3
+
+for alg in CACHE_TEST_ALGS
+  sol = solve(prob,alg,dt=1/10)
+  if !(typeof(alg) <: Euler)
+    @test sol.errors[:final] < 4e-14
+  end
+end
