2f751af160e857d3b3f1bae85ccab7039bcfe7bf
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-30 15:17:32 -0400

    more AD coverage

--- test/interface/ad_tests.jl
+++ test/interface/ad_tests.jl
@@ -40,10 +40,48 @@ function myobj(θ)
   f(u,p,t) = -θ[1]*u
   u0, _ = promote(10.0, θ[1])
   prob = ODEProblem(f, u0, (0.0, 1.0))
-  sol = solve(prob, Tsit5(), dt = 0.1)
+  sol = solve(prob, Tsit5())
   diff = sol.u - 10*exp.(-sol.t)
   return diff'diff
 end
 
 ForwardDiff.gradient(myobj, [1.0])
 ForwardDiff.hessian(myobj, [1.0])
+
+function myobj2(θ)
+  f(du,u,p,t) = (du[1]=-θ[1]*u[1])
+  u0, _ = promote(10.0, θ[1])
+  prob = ODEProblem(f, [u0], (0.0, 1.0))
+  sol = solve(prob, Tsit5())
+  diff = sol[:,1] .- 10 .*exp.(-sol.t)
+  return diff'diff
+end
+
+ForwardDiff.gradient(myobj2, [1.0])
+ForwardDiff.hessian(myobj2, [1.0])
+
+function myobj3(θ)
+  f(u,p,t) = -θ[1]*u
+  u0, _ = promote(10.0, θ[1])
+  tspan_end, _ = promote(1.0, θ[1])
+  prob = ODEProblem(f, u0, (0.0, tspan_end))
+  sol = solve(prob, Tsit5())
+  diff = sol.u - 10*exp.(-sol.t)
+  return diff'diff
+end
+
+ForwardDiff.gradient(myobj3, [1.0])
+ForwardDiff.hessian(myobj3, [1.0])
+
+function myobj4(θ)
+  f(du,u,p,t) = (du[1] = -θ[1]*u[1])
+  u0, _ = promote(10.0, θ[1])
+  tspan_end, _ = promote(1.0, θ[1])
+  prob = ODEProblem(f, [u0], (0.0, tspan_end))
+  sol = solve(prob, Tsit5())
+  diff = sol[:,1] .- 10 .* exp.(-sol.t)
+  return diff'diff
+end
+
+ForwardDiff.gradient(myobj4, [1.0])
+ForwardDiff.hessian(myobj4, [1.0])
