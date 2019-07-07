122b99c77024102d2b63839514c91bc6e4e71392
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-07 17:02:08 -0400

    remove invW trait

--- test/interface/differentiation_traits_tests.jl
+++ test/interface/differentiation_traits_tests.jl
@@ -43,21 +43,3 @@ sol = solve(prob2,Rosenbrock23(autodiff=true,chunk_size=1))
 
 sol = solve(prob2,Rosenbrock23(autodiff=false))
 @test ≈(good_sol[end],sol[end],rtol=1e-2)
-
-invW_called = Ref(false)
-
-function Lotka_invW(iW,u,p,γ,t)
-  invW_called.x = true
-  iW[1,1] = (1 - (1 * u[1] * u[2] * γ ^ 2) / (((1 - (-3 + u[1]) * γ) + (1 * u[1] * u[2] * γ ^ 2) / (1 - (1 - 1 * u[2]) * γ)) * (1 - (1 - 1 * u[2]) * γ))) / (1 - (1 - 1 * u[2]) * γ)
-  iW[1,2] = (-(1) * u[1] * γ) / (((1 - (-3 + u[1]) * γ) + (1 * u[1] * u[2] * γ ^ 2) / (1 - (1 - 1 * u[2]) * γ)) * (1 - (1 - 1 * u[2]) * γ))
-  iW[2,1] = (u[2] * γ) / (((1 - (-3 + u[1]) * γ) + (1 * u[1] * u[2] * γ ^ 2) / (1 - (1 - 1 * u[2]) * γ)) * (1 - (1 - 1 * u[2]) * γ))
-  iW[2,2] = ((1 - (-3 + u[1]) * γ) + (1 * u[1] * u[2] * γ ^ 2) / (1 - (1 - 1 * u[2]) * γ)) ^ -1
-  nothing
-end
-
-Lotka_f2 = ODEFunction(Lotka,jac=Lotka_jac,tgrad=Lotka_tgrad,invW = Lotka_invW)
-prob3 = ODEProblem(Lotka_f2,ones(2),(0.0,10.0))
-inv_sol = solve(prob3,Rosenbrock23())
-
-@test invW_called[]
-@test ≈(good_sol[end],inv_sol[end],rtol=1e-2)
