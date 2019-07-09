61bf4dcb8d34d7206dad69536abbdfcd3621b267
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-08 23:51:05 -0400

    psos passes

--- test/runtests.jl
+++ test/runtests.jl
@@ -57,7 +57,7 @@ if !is_APPVEYOR && (GROUP == "All" || GROUP == "Regression")
   @time @safetestset "Dense Tests" begin include("regression/ode_dense_tests.jl") end
   @time @safetestset "Inplace Tests" begin include("regression/ode_inplace_tests.jl") end
   @time @safetestset "Adaptive Tests" begin include("regression/ode_adaptive_tests.jl") end
-  #@time @safetestset "PSOS Energy Conservation Tests" begin include("regression/psos_and_energy_conservation.jl") end
+  @time @safetestset "PSOS Energy Conservation Tests" begin include("regression/psos_and_energy_conservation.jl") end
   @time @safetestset "Unrolled Tests" begin include("regression/ode_unrolled_comparison_tests.jl") end
   @time @safetestset "Time derivative Tests" begin include("regression/time_derivative_test.jl") end
 end
