a10521366885b4146ff4a6f81b14e8b2935e70c3
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-13 12:40:45 -0400

    update GPU tests

--- test/gpu/simple_gpu.jl
+++ test/gpu/simple_gpu.jl
@@ -13,14 +13,23 @@ prob = ODEProblem(ff,u0,tspan)
 
 CuArrays.allowscalar(false)
 sol = solve(prob,Tsit5())
-@test_broken sol = solve(prob,Rosenbrock23())
+@test_broken solve(prob,Rosenbrock23()).retcode == :Success
+solve(prob,Rosenbrock23(autodiff=false))
 
 prob_nojac = ODEProblem(f,u0,tspan)
-@test_broken sol = solve(prob_nojac,Rosenbrock23())
+@test_broken solve(prob_nojac,Rosenbrock23()).retcode == :Success
+@test_broken solve(prob_nojac,Rosenbrock23(autodiff=false)).retcode == :Success
 
 # Test auto-offload
-A = -rand(3,3)
+_A = -rand(3,3)
+function f2(du,u,p,t)
+    mul!(du,_A,u)
+end
+function jac2(J,u,p,t)
+    J .= A
+end
+ff2 = ODEFunction(f2,jac=jac2)
 u0 = [1.0;0.0;0.0]
 tspan = (0.0,100.0)
-prob_nojac2 = ODEProblem(f,u0,tspan)
-sol = solve(prob,Rosenbrock23(linsolve=LinSolveGPUFactorize()))
+prob_num = ODEProblem(ff2,u0,tspan)
+sol = solve(prob_num,Rosenbrock23(linsolve=LinSolveGPUFactorize()))
