ab26c8b0ebc82cbb5e2e7bb42c741eba0f38d5c0
Ranjan Anantharaman <ranjan.anantharaman@gmail.com>
2019-07-13 20:00:27 -0400

    Add failing GPU test with mass matrix
    For the Rosenbrock23 method

--- test/gpu/simple_gpu.jl
+++ test/gpu/simple_gpu.jl
@@ -33,3 +33,17 @@ u0 = [1.0;0.0;0.0]
 tspan = (0.0,100.0)
 prob_num = ODEProblem(ff2,u0,tspan)
 sol = solve(prob_num,Rosenbrock23(linsolve=LinSolveGPUFactorize()))
+
+# Mass matrix example 
+CuArrays.allowscalar(false)
+function g(du, u, p, t)
+    du[1] = -p[1]*u[1] + p[2]*u[2]*u[3]
+    du[2] = p[1]*u[1] - p[2]*u[2]*u[3] - p[3]*u[2]*u[2]
+    du[3] = u[1] + u[2] + u[3] - 1.
+end
+M = [1 0 0; 0 1 0; 0 0 0.]
+p = [0.04, 10^4, 3e7] |> cu
+u0 = [1.,0.,0.] |> cu
+tspan = (0., 1e6)
+prob = ODEProblem(ODEFunction(g, mass_matrix = M), u0, tspan, p)
+@test_broken solve(prob, Rosenbrock23()).retcode == :Success
