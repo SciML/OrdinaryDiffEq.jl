278a2a8be82363ae0a128eb5a83496725ecce0b1
Yingbo Ma <mayingbo5@gmail.com>
2019-07-30 19:51:21 -0400

    Add tests

--- test/interface/jacobian_tests.jl
+++ test/interface/jacobian_tests.jl
@@ -35,3 +35,21 @@ sol = solve(prob,KenCarp3(),abstol=1e-10,reltol=1e-10)
 @test sol.errors[:l2] < 8e-4
 sol = solve(prob,KenCarp4(),abstol=1e-10,reltol=1e-10)
 @test sol.errors[:l2] < 1e-7
+
+using ModelingToolkit
+function lotka(du,u,p,t)
+  x = u[1]
+  y = u[2]
+  du[1] = p[1]*x - p[2]*x*y
+  du[2] = -p[3]*y + p[4]*x*y
+end
+
+prob = ODEProblem(lotka,[1.0,1.0],(0.0,1.0),[1.5,1.0,3.0,1.0])
+de = ModelingToolkit.modelingtoolkitize(prob)
+prob2 = remake(prob, f=ODEFunction(de..., Val(false), Wfact=true))
+
+sol = solve(prob, TRBDF2())
+
+for Alg in [Rodas5, Rosenbrock23, TRBDF2, KenCarp4]
+  @test Array( solve(prob2, Alg(), tstops=sol.t, adaptive=false) ) ≈ Array( solve(prob, Alg(), tstops=sol.t, adaptive=false) )
+end
