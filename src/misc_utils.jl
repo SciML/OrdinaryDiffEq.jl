4b5a99d65e7b85aa84369055a621d5fb8c32e308
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-08-28 11:20:07 -0400

    Remove generic implicit methods
    Now that we are making our implicit setup generic and documented, we shouldn't have specific methods which call directly into NLsolve. Although this removes the IIF methods, we need a more direct implementation anyways to get their full advantages.

--- src/misc_utils.jl
+++ src/misc_utils.jl
@@ -1,21 +1,3 @@
-struct DiffEqNLSolveTag end
-
-struct DiffCache{T<:AbstractArray, S<:AbstractArray}
-    du::T
-    dual_du::S
-end
-
-Base.@pure function DiffCache(T, size, ::Type{Val{chunk_size}}) where chunk_size
-  DiffCache(fill(zero(T), size...), fill(zero(Dual{typeof(ForwardDiff.Tag(DiffEqNLSolveTag(),T)),T,chunk_size}), size...))
-end
-
-Base.@pure DiffCache(u::AbstractArray) = DiffCache(eltype(u),size(u),Val{ForwardDiff.pickchunksize(length(u))})
-Base.@pure DiffCache(u::AbstractArray,nlsolve) = DiffCache(eltype(u),size(u),Val{get_chunksize(nlsolve)})
-Base.@pure DiffCache(u::AbstractArray,T::Type{Val{CS}}) where {CS} = DiffCache(eltype(u),size(u),T)
-
-get_du(dc::DiffCache, ::Type{T}) where {T<:Dual} = dc.dual_du
-get_du(dc::DiffCache, T) = dc.du
-
 # Default nlsolve behavior, should move to DiffEqDiffTools.jl
 
 Base.@pure determine_chunksize(u,alg::DiffEqBase.DEAlgorithm) = determine_chunksize(u,get_chunksize(alg))
@@ -26,18 +8,6 @@ Base.@pure function determine_chunksize(u,CS)
     return ForwardDiff.pickchunksize(length(u))
   end
 end
-
-struct NLSOLVEJL_SETUP{CS,AD} end
-Base.@pure NLSOLVEJL_SETUP(;chunk_size=0,autodiff=true) = NLSOLVEJL_SETUP{chunk_size,autodiff}()
-(::NLSOLVEJL_SETUP)(f,u0; kwargs...) = (res=NLsolve.nlsolve(f,u0; kwargs...); res.zero)
-function (p::NLSOLVEJL_SETUP{CS,AD})(::Type{Val{:init}},f,u0_prototype) where {CS,AD}
-  AD ? autodiff = :forward : autodiff = :central
-  OnceDifferentiable(f, u0_prototype, u0_prototype, autodiff,
-                     ForwardDiff.Chunk(determine_chunksize(u0_prototype,CS)))
-end
-
-get_chunksize(x) = 0
-get_chunksize(x::NLSOLVEJL_SETUP{CS,AD}) where {CS,AD} = CS
 
 macro swap!(x,y)
   quote
