8214e1e469ee0676e0b321f3646aba6866a47fec
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-08-28 14:19:30 -0400

    fully remove NLsolve

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -20,7 +20,7 @@ module OrdinaryDiffEq
   import RecursiveArrayTools: chain, recursivecopy!
 
   using Parameters, GenericSVD, ForwardDiff, RecursiveArrayTools,
-        NLsolve, DataStructures, DiffEqDiffTools, ArrayInterface
+        DataStructures, DiffEqDiffTools, ArrayInterface
 
   import ForwardDiff.Dual
 
