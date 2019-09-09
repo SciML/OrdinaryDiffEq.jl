badc00f45c019093383fcd6af3f295b79432252d
Kanav Gupta <kanav0610@gmail.com>
2019-09-09 21:30:04 +0530

    Change Orego Diagrams

--- diagrams/orego.jl
+++ diagrams/orego.jl
@@ -1,6 +1,5 @@
-using OrdinaryDiffEq, ParameterizedFunctions, Plots, DiffEqDevTools
+using OrdinaryDiffEq, ParameterizedFunctions, Plots, DiffEqDevTools, Sundials, LSODA
 gr() #gr(fmt=:png)
-DIAGRAMS = Dict()
 f = @ode_def Orego begin
   dy1 = p1*(y2+y1*(1-p2*y1-y2))
   dy2 = (y3-(1+y1)*y2)/p1
@@ -16,39 +15,78 @@ test_sol = TestSolution(sol)
 abstols = 1 ./ 10 .^ (5:8)
 reltols = 1 ./ 10 .^ (1:4);
 setups = [Dict(:alg=>Rosenbrock23()),
-          Dict(:alg=>Rodas3()),
-          Dict(:alg=>TRBDF2())]
+          Dict(:alg=>TRBDF2()),
+          Dict(:alg=>CVODE_BDF()),
+          Dict(:alg=>lsoda())]
 wp = WorkPrecisionSet(prob,abstols,reltols,setups;
                       save_everystep=false,appxsol=test_sol,maxiters=Int(1e5))
-DIAGRAMS["OregoHighTol1"] = plot(wp)
+DIAGRAMS["Orego-HighTol-Group1-1"] = plot(wp)
 
 
 wp = WorkPrecisionSet(prob,abstols,reltols,setups;dense = false,verbose=false,
                       appxsol=test_sol,maxiters=Int(1e5),error_estimate=:l2)
-DIAGRAMS["OregoHighTol2"] = plot(wp)
+DIAGRAMS["Orego-HighTol-Group1-2"] = plot(wp)
 
 wp = WorkPrecisionSet(prob,abstols,reltols,setups;
                       appxsol=test_sol,maxiters=Int(1e5),error_estimate=:L2)
-DIAGRAMS["OregoHighTol3"] = plot(wp)
+DIAGRAMS["Orego-HighTol-Group1-3"] = plot(wp)
 
 # Low Tolerance
 
 abstols = 1 ./ 10 .^ (7:13)
 reltols = 1 ./ 10 .^ (4:10)
 
-setups = [Dict(:alg=>GRK4A()),
-          Dict(:alg=>Rodas4P()),
-          Dict(:alg=>Rodas4())
-]
 wp = WorkPrecisionSet(prob,abstols,reltols,setups;
                       save_everystep=false,appxsol=test_sol,maxiters=Int(1e5))
-DIAGRAMS["OregoLowTol1"] = plot(wp)
+DIAGRAMS["Orego-LowTol-Group1-1"] = plot(wp)
 
 wp = WorkPrecisionSet(prob,abstols,reltols,setups;verbose=false,
                       dense=false,appxsol=test_sol,maxiters=Int(1e5),error_estimate=:l2)
-DIAGRAMS["OregoLowTol2"] = plot(wp)
+DIAGRAMS["Orego-LowTol-Group1-2"] = plot(wp)
 
 wp = WorkPrecisionSet(prob,abstols,reltols,setups;
                       appxsol=test_sol,maxiters=Int(1e5),error_estimate=:L2)
-DIAGRAMS["OregoLowTol3"] = plot(wp)
+DIAGRAMS["Orego-LowTol-Group1-3"] = plot(wp)
+
+
+# group 2
+
+# High Tolerance
+abstols = 1 ./ 10 .^ (5:8)
+reltols = 1 ./ 10 .^ (1:4)
+
+setups = [Dict(:alg=>Rodas4()),
+          Dict(:alg=>Rodas5()),
+          Dict(:alg=>KenCarp4()),
+          Dict(:alg=>RadauIIA5()),
+          Dict(:alg=>CVODE_BDF()),
+          Dict(:alg=>lsoda())]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;
+                      save_everystep=false,appxsol=test_sol,maxiters=Int(1e5))
+DIAGRAMS["Orego-HighTol-Group2-1"] = plot(wp)
+
+
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;dense = false,verbose=false,
+                      appxsol=test_sol,maxiters=Int(1e5),error_estimate=:l2)
+DIAGRAMS["Orego-HighTol-Group2-2"] = plot(wp)
+
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;
+                      appxsol=test_sol,maxiters=Int(1e5),error_estimate=:L2)
+DIAGRAMS["Orego-HighTol-Group2-3"] = plot(wp)
+
+# Low Tolerance
+abstols = 1 ./ 10 .^ (7:13)
+reltols = 1 ./ 10 .^ (4:10)
+
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;
+                      save_everystep=false,appxsol=test_sol,maxiters=Int(1e5))
+DIAGRAMS["Orego-LowTol-Group2-1"] = plot(wp)
+
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;verbose=false,
+                      dense=false,appxsol=test_sol,maxiters=Int(1e5),error_estimate=:l2)
+DIAGRAMS["Orego-LowTol-Group2-2"] = plot(wp)
+
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;
+                      appxsol=test_sol,maxiters=Int(1e5),error_estimate=:L2)
+DIAGRAMS["Orego-LowTol-Group2-3"] = plot(wp)
 
