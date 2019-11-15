ad05e1202800524ad0f469d4e9b8d71a5da9930b
Yingbo Ma <mayingbo5@gmail.com>
2019-11-15 18:16:45 -0500

    WIP

---
+++ test/interface/linear_solver_test.jl
@@ -0,0 +1,15 @@
+using Test, OrdinaryDiffEq
+
+prob = ODEProblem(function (du,u,p,t)
+                    du[1] = 10.0(u[2]-u[1])
+                    du[2] = u[1]*(28.0-u[3]) - u[2]
+                    du[3] = u[1]*u[2] - (8/3)*u[3]
+                  end,
+                  [1.0;0.0;0.0], (0.0, 100.0))
+
+function buildW(...)::NTuple{3}
+  , nothing #= left preconditioner =#, nothing #= right preconditioner =#
+end
+
+nlsolve = NLNewton(buildW, )
+sol = solve(prob, TRBDF2(nlsolve=nlsolve))
