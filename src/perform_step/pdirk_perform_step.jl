819ce2ba82b05c7a3a9bf16227c9a2789ebfd274
Yingbo Ma <mayingbo5@gmail.com>
2019-11-27 14:47:49 -0500

    Fix out-of-place pdirk

--- src/perform_step/pdirk_perform_step.jl
+++ src/perform_step/pdirk_perform_step.jl
@@ -14,10 +14,10 @@ function initialize!(integrator, cache::PDIRK44ConstantCache) end
       Threads.@threads for i in 1:2
         nlsolver[i].z = zero(u)
         nlsolver[i].tmp = uprev
-        update_W!(nlsolver[i], integrator, cache, γs[i]*dt, repeat_step)
         nlsolver[i].γ = γs[i]
         nlsolver[i].c = cs[i]
-        k1[i] = nlsolve!(nlsolver[i], integrator)
+        markfirststage!(nlsolver[i])
+        k1[i] = nlsolve!(nlsolver[i], integrator, cache, repeat_step)
       end
     end
     nlsolvefail(nlsolver[1]) && return
@@ -28,7 +28,7 @@ function initialize!(integrator, cache::PDIRK44ConstantCache) end
         nlsolver[i].c = cs[2+i]
         nlsolver[i].z = zero(u)
         nlsolver[i].tmp = uprev + α1[i] * k1[1] + α2[i] * k1[2]
-        k2[i] = nlsolve!(nlsolver[i], integrator)
+        k2[i] = nlsolve!(nlsolver[i], integrator, cache, repeat_step)
       end
     end
     nlsolvefail(nlsolver[1]) && return
@@ -37,32 +37,32 @@ function initialize!(integrator, cache::PDIRK44ConstantCache) end
   else
     _nlsolver = nlsolver[1]
     _nlsolver.z = zero(u)
-    update_W!(_nlsolver, integrator, cache, γs[1]*dt, repeat_step)
     _nlsolver.tmp = uprev
     _nlsolver.γ = γs[1]
     _nlsolver.c = cs[1]
-    k11 = nlsolve!(_nlsolver, integrator)
+    markfirststage!(_nlsolver)
+    k11 = nlsolve!(_nlsolver, integrator, γs[1]*dt, repeat_step)
     nlsolvefail(_nlsolver) && return
     _nlsolver.z = zero(u)
-    update_W!(_nlsolver, integrator, cache, γs[2]*dt, repeat_step)
     _nlsolver.tmp = uprev
     _nlsolver.γ = γs[2]
     _nlsolver.c = cs[2]
-    k12 = nlsolve!(_nlsolver, integrator)
+    markfirststage!(_nlsolver)
+    k12 = nlsolve!(_nlsolver, integrator, γs[2]*dt, repeat_step)
     nlsolvefail(_nlsolver) && return
     _nlsolver.z = zero(u)
-    update_W!(_nlsolver, integrator, cache, γs[1]*dt, repeat_step)
     _nlsolver.tmp = uprev + α1[1] * k11 + α2[1] * k12
     _nlsolver.γ = γs[1]
     _nlsolver.c = cs[3]
-    k21 = nlsolve!(_nlsolver, integrator)
+    markfirststage!(_nlsolver)
+    k21 = nlsolve!(_nlsolver, integrator, γs[1]*dt, repeat_step)
     nlsolvefail(_nlsolver) && return
     _nlsolver.z = zero(u)
-    update_W!(_nlsolver, integrator, cache, γs[2]*dt, repeat_step)
     _nlsolver.tmp = uprev + α1[2] * k11 + α2[2] * k12
     _nlsolver.γ = γs[2]
     _nlsolver.c = cs[4]
-    k22 = nlsolve!(_nlsolver, integrator)
+    markfirststage!(_nlsolver)
+    k22 = nlsolve!(_nlsolver, integrator, γs[2]*dt, repeat_step)
     nlsolvefail(_nlsolver) && return
     integrator.u = uprev + b1 * k11 + b2 * k21 + b3 * k12 + b4 * k22
   end
@@ -119,12 +119,14 @@ function initialize!(integrator, cache::PDIRK44Cache) end
     @.. _nlsolver.tmp .= uprev + α1[1] * k1[1] + α2[1] * k1[2]
     _nlsolver.γ = γs[1]
     _nlsolver.c = cs[3]
+    markfirststage!(_nlsolver)
     k2[1] .= nlsolve!(_nlsolver, integrator, cache, repeat_step)
     nlsolvefail(_nlsolver) && return
     _nlsolver.z .= zero(eltype(u))
     @.. _nlsolver.tmp = uprev + α1[2] * k1[1] + α2[2] * k1[2]
     _nlsolver.γ = γs[2]
     _nlsolver.c = cs[4]
+    markfirststage!(_nlsolver)
     k2[2] .= nlsolve!(_nlsolver, integrator, cache, repeat_step)
     nlsolvefail(_nlsolver) && return
   end
