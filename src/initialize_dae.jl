7d163b130eb94a585e936743cd842877e8758e79
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-02-18 10:36:06 -0500

    fix convergence tests and allow for out of place to work on number

--- src/initialize_dae.jl
+++ src/initialize_dae.jl
@@ -1,9 +1,21 @@
 abstract type DAEInitializationAlgorithm end
 
-struct BrownFullBasicInit <: DAEInitializationAlgorithm end
+struct BrownFullBasicInit{T} <: DAEInitializationAlgorithm
+	abstol::T
+end
+BrownFullBasicInit() = BrownFullBasicInit(1e-10)
 
 function initialize_dae!(integrator, u, du, differential_vars, alg::BrownFullBasicInit, ::Val{true})
 	@unpack p, t, f = integrator
+
+	tmp = get_tmp_cache(integrator)[1]
+	f(tmp, du, u, p, t)
+
+	if integrator.opts.internalnorm(tmp,t) <= alg.abstol
+		return
+	elseif differential_vars === nothing
+		error("differential_vars must be set for DAE initialization to occur. Either set consistent initial conditions or differential_vars")
+	end
 
 	nlequation = (out, x) -> begin
 		for i in 1:length(x)
@@ -25,10 +37,27 @@ function initialize_dae!(integrator, u, du, differential_vars, alg::BrownFullBas
 			u[i] = r.zero[i]
 		end
 	end
+
+	return
 end
 
-function initialize_dae!(integrator, u, du, differential_vars, alg::BrownFullBasicInit, ::Val{false})
+function initialize_dae!(integrator, _u, _du, differential_vars, alg::BrownFullBasicInit, ::Val{false})
 	@unpack p, t, f = integrator
+
+	if integrator.opts.internalnorm(f(_du, _u, p, t),t) <= alg.abstol
+		return
+	elseif differential_vars === nothing
+		error("differential_vars must be set for DAE initialization to occur. Either set consistent initial conditions or differential_vars")
+	end
+
+	if _u isa Number && _du isa Number
+		# This doesn't fix static arrays!
+		u = [_u]
+		du = [_du]
+	else
+		u = _u
+		du = _du
+	end
 
 	nlequation = (out,x) -> begin
 		for i in 1:length(x)
@@ -50,4 +79,15 @@ function initialize_dae!(integrator, u, du, differential_vars, alg::BrownFullBas
 			u[i] = r.zero[i]
 		end
 	end
+
+	if _u isa Number && _du isa Number
+		# This doesn't fix static arrays!
+		integrator.u = first(u)
+		integrator.du = first(du)
+	else
+		integrator.u = u
+		integrator.du = du
+	end
+
+	return
 end
