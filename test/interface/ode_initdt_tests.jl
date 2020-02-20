d656c21a5df703f2ad8062cf427d74b9e54605ce
Yingbo Ma <mayingbo5@gmail.com>
2020-02-20 17:04:35 -0500

    Add Float32 Rober test

--- test/interface/ode_initdt_tests.jl
+++ test/interface/ode_initdt_tests.jl
@@ -29,3 +29,17 @@ prob = remake(prob, u0=u0, tspan=tspan)
 tspan = T.((2000,2100))
 prob = remake(prob, tspan=tspan)
 @test_throws ArgumentError solve(prob, Euler(); dt=T(0.0001)) # Loops forever
+
+function rober(du,u,p,t)
+  y₁,y₂,y₃ = u
+  k₁,k₂,k₃ = p
+  du[1] = -k₁*y₁+k₃*y₂*y₃
+  du[2] =  k₁*y₁-k₂*y₂^2-k₃*y₂*y₃
+  du[3] =  k₂*y₂^2
+  nothing
+end
+u0 = Float32[1.0,0.0,0.0]
+tspan = (0f0, 1f5)
+params = (4f-2,3f7,1f4)
+prob = ODEProblem(rober,u0,tspan,params)
+sol = solve(prob, Rosenbrock23())
