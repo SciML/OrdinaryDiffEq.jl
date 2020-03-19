3bb1ea93bddfdc9cd16407f8d41fa4ed8afe90e9
Diogo Netto <diogonetto.dcn@gmail.com>
2020-03-18 21:29:24 -0400

    lddrk

--- src/perform_step/low_storage_rk_perform_step.jl
+++ src/perform_step/low_storage_rk_perform_step.jl
@@ -639,10 +639,10 @@ end
   γ = α3*γ + dt*f(u, p, t+c3*dt)
 	u   = u + β3*dt*γ
 
-  γ = α2*γ + dt*f(u, p, t+c4*dt)
+  γ = α4*γ + dt*f(u, p, t+c4*dt)
 	u   = u + β4*dt*γ
 
-  γ = α4*γ + dt*f(u, p, t+c5*dt)
+  γ = α5*γ + dt*f(u, p, t+c5*dt)
 	u   = u + β5*dt*γ
 
 	integrator.fsallast = f(u, p, t+dt)
