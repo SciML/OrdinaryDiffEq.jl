9b12228204808101c1d32af88439b6fc5092ea4c
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-13 02:52:16 -0400

    always color
    ```julia
    using DifferentialEquations
    const N = 32
    const xyd_brusselator = range(0,stop=1,length=N)
    brusselator_f(x, y, t) = (((x-0.3)^2 + (y-0.6)^2) <= 0.1^2) * (t >= 1.1) * 5.
    limit(a, N) = a == N+1 ? 1 : a == 0 ? N : a
    function brusselator_2d_loop(du, u, p, t)
      A, B, alpha, dx = p
      alpha = alpha/dx^2
      @inbounds for I in CartesianIndices((N, N))
        i, j = Tuple(I)
        x, y = xyd_brusselator[I[1]], xyd_brusselator[I[2]]
        ip1, im1, jp1, jm1 = limit(i+1, N), limit(i-1, N), limit(j+1, N), limit(j-1, N)
        du[i,j,1] = alpha*(u[im1,j,1] + u[ip1,j,1] + u[i,jp1,1] + u[i,jm1,1] - 4u[i,j,1]) +
                    B + u[i,j,1]^2*u[i,j,2] - (A + 1)*u[i,j,1] + brusselator_f(x, y, t)
        du[i,j,2] = alpha*(u[im1,j,2] + u[ip1,j,2] + u[i,jp1,2] + u[i,jm1,2] - 4u[i,j,2]) +
                    A*u[i,j,1] - u[i,j,1]^2*u[i,j,2]
        end
    end
    p = (3.4, 1., 10., step(xyd_brusselator))
    
    using SparsityDetection, SparseArrays
    input = rand(32,32,2)
    output = similar(input)
    sparsity_pattern = jacobian_sparsity(brusselator_2d_loop,output,input,p,0.0)
    jac_sparsity = Float64.(sparse(sparsity_pattern))
    f = ODEFunction(brusselator_2d_loop;jac_prototype=jac_sparsity)
    
    function init_brusselator_2d(xyd)
      N = length(xyd)
      u = zeros(N, N, 2)
      for I in CartesianIndices((N, N))
        x = xyd[I[1]]
        y = xyd[I[2]]
        u[I,1] = 22*(y*(1-y))^(3/2)
        u[I,2] = 27*(x*(1-x))^(3/2)
      end
      u
    end
    u0 = init_brusselator_2d(xyd_brusselator)
    prob_ode_brusselator_2d = ODEProblem(brusselator_2d_loop,
                                         u0,(0.,11.5),p)
    
    prob_ode_brusselator_2d_sparse = ODEProblem(f,
                                         u0,(0.,11.5),p)
    
    @btime solve(prob_ode_brusselator_2d_sparse,save_everystep=false)
    
    using SparseDiffTools, BenchmarkTools
    @time colorvec = matrix_colors(jac_sparsity)
    
    f = ODEFunction(brusselator_2d_loop;jac_prototype=jac_sparsity,
                                        colorvec=colorvec)
    prob_ode_brusselator_2d_sparse = ODEProblem(f,
                                         init_brusselator_2d(xyd_brusselator),
                                         (0.,11.5),p)
    @btime solve(prob_ode_brusselator_2d_sparse,save_everystep=false)
    ```
    
    3.230 s (18691 allocations: 881.07 MiB)
    10.766 s (19042 allocations: 881.07 MiB

--- src/derivative_wrappers.jl
+++ src/derivative_wrappers.jl
@@ -40,9 +40,9 @@ end
 
 jacobian_autodiff(f, x, odefun, alg) = (ForwardDiff.derivative(f,x),1, alg)
 function jacobian_autodiff(f, x::AbstractArray, odefun, alg)
-  colorvec = DiffEqBase.has_colorvec(odefun) ? odefun.colorvec : 1:length(x)
   sparsity = odefun.sparsity
   jac_prototype = odefun.jac_prototype
+  colorvec = DiffEqBase.has_colorvec(odefun) ? odefun.colorvec : matrix_colors(sparsity)
   maxcolor = maximum(colorvec)
   chunk_size = get_chunksize(alg)==0 ? nothing : get_chunksize(alg) # SparseDiffEq uses different convection...
   num_of_chunks = chunk_size==nothing ? Int(ceil(maxcolor / getsize(default_chunk_size(maxcolor)))) :
@@ -74,9 +74,9 @@ function jacobian(f, x, integrator)
     if alg_autodiff(alg)
       J, tmp = jacobian_autodiff(f, x, integrator.f, alg)
     else
-      colorvec = DiffEqBase.has_colorvec(integrator.f) ? integrator.f.colorvec : 1:length(x)
       sparsity = integrator.f.sparsity
       jac_prototype = integrator.f.jac_prototype
+      colorvec = DiffEqBase.has_colorvec(integrator.f) ? integrator.f.colorvec : matrix_colors(sparsity)
       dir = diffdir(integrator)
       J, tmp = jacobian_finitediff(f, x, alg.diff_type, dir, colorvec, sparsity, jac_prototype)
     end
@@ -84,12 +84,12 @@ function jacobian(f, x, integrator)
     J
 end
 
-jacobian_finitediff_forward!(J,f,x,jac_config,forwardcache,integrator,colorvec)=
+jacobian_finitediff_forward!(J,f,x,jac_config,forwardcache,integrator)=
   (FiniteDiff.finite_difference_jacobian!(J,f,x,jac_config,forwardcache,
-    dir=diffdir(integrator),colorvec=colorvec,sparsity=integrator.f.sparsity);maximum(colorvec))
+    dir=diffdir(integrator));maximum(jac_config.colorvec))
 jacobian_finitediff!(J,f,x,jac_config,integrator,colorvec)=
   (FiniteDiff.finite_difference_jacobian!(J,f,x,jac_config,
-    dir=diffdir(integrator),colorvec=colorvec,sparsity=integrator.f.sparsity);2*maximum(colorvec))
+    dir=diffdir(integrator));2*maximum(jac_config.colorvec))
 
 function jacobian!(J::AbstractMatrix{<:Number}, f, x::AbstractArray{<:Number}, fx::AbstractArray{<:Number}, integrator::DiffEqBase.DEIntegrator, jac_config)
     alg = unwrap_alg(integrator, true)
@@ -97,15 +97,14 @@ function jacobian!(J::AbstractMatrix{<:Number}, f, x::AbstractArray{<:Number}, f
       forwarddiff_color_jacobian!(J,f,x,jac_config)
       integrator.destats.nf += 1
     else
-      colorvec = DiffEqBase.has_colorvec(integrator.f) ? integrator.f.colorvec : 1:length(x)
       isforward = alg.diff_type === Val{:forward}
       if isforward
         forwardcache = get_tmp_cache(integrator, alg, unwrap_cache(integrator, true))[2]
         f(forwardcache, x)
         integrator.destats.nf += 1
-        tmp=jacobian_finitediff_forward!(J, f, x, jac_config, forwardcache, integrator, colorvec)
+        tmp=jacobian_finitediff_forward!(J, f, x, jac_config, forwardcache, integrator)
       else # not forward difference
-        tmp=jacobian_finitediff!(J, f, x, jac_config, integrator, colorvec)
+        tmp=jacobian_finitediff!(J, f, x, jac_config, integrator)
       end
       integrator.destats.nf += tmp
     end
@@ -115,9 +114,9 @@ end
 function DiffEqBase.build_jac_config(alg::Union{OrdinaryDiffEqAlgorithm,DAEAlgorithm},f,uf,du1,uprev,u,tmp,du2,::Val{transform}=Val(true)) where transform
   if !DiffEqBase.has_jac(f) && ((!transform && !DiffEqBase.has_Wfact(f)) || (transform && !DiffEqBase.has_Wfact_t(f)))
     if alg_autodiff(alg)
-      colorvec = DiffEqBase.has_colorvec(f) ? f.colorvec : 1:length(uprev)
       sparsity = f.sparsity
       jac_prototype = f.jac_prototype
+      colorvec = DiffEqBase.has_colorvec(f) ? f.colorvec : matrix_colors(sparsity)
       _chunksize = get_chunksize(alg)==0 ? nothing : get_chunksize(alg) # SparseDiffEq uses different convection...
       jac_config = ForwardColorJacCache(uf,uprev,_chunksize;colorvec=colorvec,sparsity=sparsity)
     else
