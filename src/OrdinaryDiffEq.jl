bc35eea2ddcc7f89934beb98aa36ea7048d6cb69
Utkarsh <rajpututkarsh530@gmail.com>
2020-06-11 18:24:43 +0530

    Add KenCarp47 in algorithms

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -199,7 +199,7 @@ module OrdinaryDiffEq
 
   export ImplicitEuler, ImplicitMidpoint, Trapezoid, TRBDF2, SDIRK2, SDIRK22,
          Kvaerno3, KenCarp3, Cash4, Hairer4, Hairer42, SSPSDIRK2, Kvaerno4,
-         Kvaerno5, KenCarp4, KenCarp5, ESDIRK54I8L2SA, SFSDIRK4, SFSDIRK5, CFNLIRK3, SFSDIRK6, SFSDIRK7, SFSDIRK8
+         Kvaerno5, KenCarp4, KenCarp47, KenCarp5, ESDIRK54I8L2SA, SFSDIRK4, SFSDIRK5, CFNLIRK3, SFSDIRK6, SFSDIRK7, SFSDIRK8
 
   export MagnusMidpoint, LinearExponential, MagnusLeapfrog, LieEuler, CayleyEuler, MagnusGauss4
 
