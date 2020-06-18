97fc5383fbbc4169faaa8066d28c5cdd57254003
Biswajit Ghosh <biswajitghosh98@iitkgp.ac.in>
2020-06-18 20:39:14 +0530

    modified test/algconvergence/linear_method_tests.jl

--- test/algconvergence/linear_method_tests.jl
+++ test/algconvergence/linear_method_tests.jl
@@ -91,14 +91,13 @@ sim = analyticless_test_convergence(dts,prob,MagnusGL6(krylov=true),test_setup)
 @test sim.𝒪est[:l2] ≈ 6 atol=0.2
 
 A = DiffEqArrayOperator(ones(2,2),update_func=update_func)
-prob = ODEProblem(A, ones(2), (1.0, 6.))
-sol = solve(prob,MagnusGL8(),dt=1/10)
-dts = 1 ./2 .^(4:-1:1)
+prob = ODEProblem(A, ones(2), (0.0, 100.))
+dts = 1.775 .^(5:-1:0)
 test_setup = Dict(:alg=>Vern9(),:reltol=>1e-14,:abstol=>1e-14)
 sim = analyticless_test_convergence(dts,prob,MagnusGL8(),test_setup)
-@test sim.𝒪est[:l2] ≈ 6 atol=0.2
+@test sim.𝒪est[:l2] ≈ 8 atol=0.2
 sim = analyticless_test_convergence(dts,prob,MagnusGL8(krylov=true),test_setup)
-@test sim.𝒪est[:l2] ≈ 6 atol=0.2
+@test sim.𝒪est[:l2] ≈ 8 atol=0.2
 
 function B(y::AbstractMatrix)
     b = similar(y)
