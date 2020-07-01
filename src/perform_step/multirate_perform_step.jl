754878aa03b00b54e5ff31265f3df69a2e9a9960
Kanav Gupta <kanav0610@gmail.com>
2020-07-01 20:21:27 +0530

    (feat) Rice3 IIP

--- src/perform_step/multirate_perform_step.jl
+++ src/perform_step/multirate_perform_step.jl
@@ -64,12 +64,11 @@ end
   integrator.u = ArrayPartition(x, y)
 end
 
-#= This is just copied BS3
 function initialize!(integrator, cache::Rice3Cache)
   integrator.kshortsize = 2
   resize!(integrator.k, integrator.kshortsize)
   integrator.fsalfirst = cache.fsalfirst  # done by pointers, no copying
-  integrator.fsallast = cache.k4
+  integrator.fsallast = ArrayPartition(cache.k4, cache.h4)
   integrator.k[1] = integrator.fsalfirst
   integrator.k[2] = integrator.fsallast
   integrator.f(integrator.fsalfirst, integrator.uprev, integrator.p, integrator.t) # Pre-start fsal
@@ -77,24 +76,65 @@ function initialize!(integrator, cache::Rice3Cache)
 end
 
 @muladd function perform_step!(integrator, cache::Rice3Cache, repeat_step=false)
-  @unpack t,dt,uprev,u,f,p = integrator
-  @unpack k2,k3,k4,utilde,tmp,atmp = cache
+  @unpack t,dt,uprev,u,p = integrator
+  f, g = integrator.f.f1, integrator.f.f2
   @unpack a21,a32,a41,a42,a43,c1,c2,btilde1,btilde2,btilde3,btilde4 = cache.tab
-  # k1 = cache.fsalfirst
-  k1 = integrator.fsalfirst
-  a1 = dt*a21
-  @.. tmp = uprev+a1*k1
-  f(k2, tmp, p, t+c1*dt)
-  a2 = dt*a32
-  @.. tmp = uprev+a2*k2
-  f(k3, tmp, p, t+c2*dt)
-  @.. u = uprev+dt*(a41*k1+a42*k2+a43*k3)
-  f(k4, u, p, t+dt)
-  integrator.destats.nf += 3
+  @unpack k2,k3,k4,h2,h3,h4,d1,d2,d3,utilde,tmp,tmp2,atmp = cache
+  K = integrator.alg.K
+
+  xtmp, ytmp = tmp.x[1], tmp.x[2]
+  xtmp2, ytmp2 = tmp2.x[1], tmp2.x[2]
+  xprev, yprev = uprev.x[1], uprev.x[2]
+  x, y = u.x[1], u.x[2]
+
+  # fast time stepping
+  k1, h1 = integrator.fsalfirst.x[1], integrator.fsalfirst.x[2]
+  @.. xtmp = xprev + dt * a21 * k1
+  @.. ytmp = yprev + dt * a21 * h1
+  f(k2, xtmp, ytmp, p, t + c1 * dt)
+  g(h2, xtmp, ytmp, p, t + c1 * dt)
+
+  @.. xtmp = xprev + dt * a32 * k2
+  @.. ytmp = yprev + dt * a32 * h2
+  f(k3, xtmp, ytmp, p, t + c2 * dt)
+  g(h3, xtmp, ytmp, p, t + c2 * dt)
+
+  @.. x = xprev + dt * (a41 * k1 + a42 * k2 + a43 * k3)
+
+  # slow time stepping
+  h = dt/K
+  xj = xtmp
+  xj .= xprev
+  yj = ytmp
+  yj .= yprev
+  for j in 1:K
+    tj = t + j * h
+    λ1 = j / K
+    λ5 = 2 * (1 + 3j) / (9 * K * K * a21)
+    λ4 = 2 / (3K) - λ5
+
+    g(d1, xj, yj, p, tj)
+
+    @.. xtmp2 = xj + λ4 * k1 + λ5 * k2
+    @.. ytmp2 = yj + (2//3) * h * d1
+    g(d2, xtmp2, ytmp2, p, tj + (2//3) * h)
+
+    @.. ytmp2 = yj + h * d2 - h * d1
+    g(d3, xj, ytmp2, p, tj)
+
+    @.. yj = yj + (3//4) * h * d2 + (1//4) * h * d3
+    @.. xj = xprev + λ1 * k1
+  end
+  y .= yj
+
+  f(k4, x, y, p, t + dt)
+  g(h4, x, y, p, t + dt)
+
   if integrator.opts.adaptive
-    @.. utilde = dt*(btilde1*k1 + btilde2*k2 + btilde3*k3 + btilde4*k4)
-    calculate_residuals!(atmp, utilde, uprev, u, integrator.opts.abstol, integrator.opts.reltol,integrator.opts.internalnorm,t)
+    xtilde, ytilde = utilde.x[1], utilde.x[2]
+    @.. xtilde = dt*(btilde1*k1 + btilde2*k2 + btilde3*k3 + btilde4*k4)
+    @.. ytilde = dt*(btilde1*h1 + btilde2*h2 + btilde3*h3 + btilde4*h4)
+    calculate_residuals!(atmp, ArrayPartition(xtilde, ytilde), uprev, u, integrator.opts.abstol, integrator.opts.reltol,integrator.opts.internalnorm,t)
     integrator.EEst = integrator.opts.internalnorm(atmp,t)
   end
 end
-=#
