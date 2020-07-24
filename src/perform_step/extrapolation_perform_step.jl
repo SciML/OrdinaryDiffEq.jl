6789b8519b5d60ac99dc32cb8059e6825b5c4c4c
Utkarsh <rajpututkarsh530@gmail.com>
2020-07-25 00:13:00 +0530

    Update threading in ImplDeufEx OOP

--- src/perform_step/extrapolation_perform_step.jl
+++ src/perform_step/extrapolation_perform_step.jl
@@ -1156,29 +1156,113 @@ function perform_step!(integrator,cache::ImplicitDeuflhardExtrapolationConstantC
 
   # Compute the internal discretisations
   J = calc_J(integrator,cache) # Store the calculated jac as it won't change in internal discretisation
-  for i in 0:n_curr
-    j_int = 4 * subdividing_sequence[i+1]
-    dt_int = dt / j_int # Stepsize of the ith internal discretisation
-    W = dt_int*J - integrator.f.mass_matrix
-    integrator.destats.nw += 1
-    u_temp2 = uprev
-    u_temp1 = u_temp2 + _reshape(W\-_vec(dt_int*integrator.fsalfirst), axes(uprev)) # Euler starting step
-    diff1 = u_temp1 - u_temp2
-    for j in 2:j_int
-      T[i+1] = 2*u_temp1 - u_temp2 + 2 * _reshape(W\-_vec(dt_int*f(u_temp1, p, t + (j-1) * dt_int) - (u_temp1 - u_temp2)),axes(uprev))
-      integrator.destats.nf += 1
-      u_temp2 = u_temp1
-      u_temp1 = T[i+1]
-      if(i<=1)
-        # Deuflhard Stability check for initial two sequences 
-        diff2 = u_temp1 - u_temp2
-        if(integrator.opts.internalnorm(diff1,t)<integrator.opts.internalnorm(0.5*(diff2 - diff1),t))
-          # Divergence of iteration, overflow is possible. Force fail and start with smaller step
-          integrator.force_stepfail = true
-          return
+  if !integrator.alg.threading
+    for i in 0:n_curr
+      j_int = 4 * subdividing_sequence[i+1]
+      dt_int = dt / j_int # Stepsize of the ith internal discretisation
+      W = dt_int*J - integrator.f.mass_matrix
+      integrator.destats.nw += 1
+      u_temp2 = uprev
+      u_temp1 = u_temp2 + _reshape(W\-_vec(dt_int*integrator.fsalfirst), axes(uprev)) # Euler starting step
+      diff1 = u_temp1 - u_temp2
+      for j in 2:j_int
+        T[i+1] = 2*u_temp1 - u_temp2 + 2 * _reshape(W\-_vec(dt_int*f(u_temp1, p, t + (j-1) * dt_int) - (u_temp1 - u_temp2)),axes(uprev))
+        integrator.destats.nf += 1
+        u_temp2 = u_temp1
+        u_temp1 = T[i+1]
+        if(i<=1)
+          # Deuflhard Stability check for initial two sequences 
+          diff2 = u_temp1 - u_temp2
+          if(integrator.opts.internalnorm(diff1,t)<integrator.opts.internalnorm(0.5*(diff2 - diff1),t))
+            # Divergence of iteration, overflow is possible. Force fail and start with smaller step
+            integrator.force_stepfail = true
+            return
+          end
         end
       end
     end
+  else
+    if integrator.alg.sequence == :romberg
+      # Compute solution by using maximum two threads for romberg sequence
+      # One thread will fill T matrix till second last element and another thread will
+      # fill last element of T matrix.
+      # Romberg sequence --> 1, 2, 4, 8, ..., 2^(i)
+      # 1 + 2 + 4 + ... + 2^(i-1) = 2^(i) - 1
+      let n_curr=n_curr,subdividing_sequence=subdividing_sequence,uprev=uprev,dt=dt,u_temp2=u_temp2,
+          u_temp2=u_temp2,p=p,t=t,T=T
+        Threads.@threads for i = 1 : 2
+          startIndex = (i == 1) ? 0 : n_curr
+          endIndex = (i == 1) ? n_curr - 1 : n_curr
+
+          for index in startIndex:endIndex
+            j_int = 4 * subdividing_sequence[index+1]
+            dt_int = dt / j_int # Stepsize of the ith internal discretisation
+            W = dt_int*J - integrator.f.mass_matrix
+            integrator.destats.nw += 1
+            u_temp4 = uprev
+            u_temp3 = u_temp4 + _reshape(W\-_vec(dt_int*integrator.fsalfirst), axes(uprev)) # Euler starting step
+            diff1 = u_temp3 - u_temp4
+            for j in 2:j_int + 1
+              T[index+1] = 2*u_temp3 - u_temp4 + 2*_reshape(W\-_vec(dt_int * f(u_temp3, p, t + (j-1) * dt_int) - (u_temp3 - u_temp4)),axes(uprev))
+              integrator.destats.nf += 1
+              if(j == j_int + 1)
+                T[index + 1] = 0.5(T[index + 1] + u_temp4)
+              end
+              u_temp4 = u_temp3
+              u_temp3 = T[index+1]
+              if(index<=1)
+                # Deuflhard Stability check for initial two sequences 
+                diff2 = u_temp3 - u_temp4
+                if(integrator.opts.internalnorm(diff1,t)<integrator.opts.internalnorm(0.5*(diff2 - diff1),t))
+                  # Divergence of iteration, overflow is possible. Force fail and start with smaller step
+                  integrator.force_stepfail = true
+                  return
+                end
+              end
+              diff1 = u_temp3 - u_temp4
+            end
+          end
+          integrator.force_stepfail ? break : continue
+        end
+      end
+    else
+      let n_curr=n_curr,subdividing_sequence=subdividing_sequence,uprev=uprev,dt=dt,
+        integrator=integrator,p=p,t=t,T=T
+        Threads.@threads for i in 0:(n_curr ÷ 2)
+          indices = i != n_curr - i ? (i, n_curr - i) : (n_curr-i) #Avoid duplicate entry in tuple
+          for index in indices
+            j_int = 4 * subdividing_sequence[index+1]
+            dt_int = dt / j_int # Stepsize of the ith internal discretisation
+            W = dt_int*J - integrator.f.mass_matrix
+            integrator.destats.nw += 1
+            u_temp4 = uprev
+            u_temp3 = u_temp4 + _reshape(W\-_vec(dt_int*integrator.fsalfirst), axes(uprev)) # Euler starting step
+            diff1 = u_temp3 - u_temp4
+            for j in 2:j_int
+              T[index+1] = 2*u_temp3 - u_temp4 + 2*_reshape(W\-_vec(dt_int * f(u_temp3, p, t + (j-1) * dt_int) - (u_temp3 - u_temp4)),axes(uprev))
+              integrator.destats.nf += 1
+              u_temp4 = u_temp3
+              u_temp3 = T[index+1]
+              if(index<=1)
+                # Deuflhard Stability check for initial two sequences 
+                diff2 = u_temp3 - u_temp4
+                if(integrator.opts.internalnorm(diff1,t)<integrator.opts.internalnorm(0.5*(diff2 - diff1),t))
+                  # Divergence of iteration, overflow is possible. Force fail and start with smaller step
+                  integrator.force_stepfail = true
+                  return
+                end
+              end
+              diff1 = u_temp3 - u_temp4
+            end
+          end
+          integrator.force_stepfail ? break : continue
+        end
+      end
+    end    
+  end
+
+  if integrator.force_stepfail
+    return
   end
 
 
@@ -1757,6 +1841,10 @@ function perform_step!(integrator, cache::ImplicitHairerWannerExtrapolationConst
         end
       end
     end
+  end
+  
+  if integrator.force_stepfail
+    return
   end
 
   if integrator.opts.adaptive
