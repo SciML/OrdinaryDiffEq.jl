2a8ddabacd8ca3ba8189121f9096f779962e2060
Biswajit Ghosh <biswajitghosh98@iitkgp.ac.in>
2020-07-26 21:32:03 +0530

    init

--- src/caches/linear_caches.jl
+++ src/caches/linear_caches.jl
@@ -23,6 +23,31 @@ function alg_cache(alg::MagnusMidpoint,u,rate_prototype,uEltypeNoUnits,
   MagnusMidpointConstantCache()
 end
 
+@cache struct RKMK2Cache{uType,rateType,WType} <: OrdinaryDiffEqMutableCache
+  u::uType
+  uprev::uType
+  uprev2::uType
+  tmp::uType
+  fsalfirst::rateType
+  W::WType
+  k::rateType
+end
+
+function alg_cache(alg::RKMK2,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,
+                   tTypeNoUnits,uprev,uprev2,f,t,dt,reltol,p,calck,::Val{true})
+  W = false .* vec(rate_prototype) .* vec(rate_prototype)' # uEltype?
+  k = zero(rate_prototype); fsalfirst = zero(rate_prototype)
+  RKMK2Cache(u,uprev,uprev2,similar(u),fsalfirst,W,k)
+end
+
+struct RKMK2ConstantCache <: OrdinaryDiffEqConstantCache
+end
+
+function alg_cache(alg::RKMK2,u,rate_prototype,uEltypeNoUnits,
+                   tTypeNoUnits,uprev,uprev2,f,t,dt,reltol,p,calck,::Val{false})
+  RKMK2ConstantCache()
+end
+
 @cache struct MagnusAdapt4Cache{uType,rateType,WType,uNoUnitsType} <: OrdinaryDiffEqMutableCache
   u::uType
   uprev::uType
