2377f187c10fabfa39bdf8066fcb0c3b63224aa1
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-10-14 09:49:27 -0400

    don't use a non-constant global

--- test/downstream/delaydiffeq.jl
+++ test/downstream/delaydiffeq.jl
@@ -32,11 +32,10 @@ DDEProblemLibrary.importddeproblems()
     end
 end
 
-τ = 1.0
 function lotka_volterra!(du,u,h,p,t)
     🐰, 🐺 = u
     🕥🐰 = h(p,t-τ;idxs=1)
-    α,β,γ,δ = p
+    α,β,γ,δ,τ = p
     du[1] = d🐰 = α*🕥🐰 - β*🐺*🐰
     du[2] = d🐺 = γ*🐺*🐰 -δ*🐺
     nothing
@@ -45,6 +44,6 @@ uₒ = [1.0,1.0]
 tspan = (0.0, 10.0)
 h(p,t) = [1.0,1.0]
 h(p,t;idxs = 1) = 1.0
-p = [1.5,1.0,3.0,1.0]
+p = [1.5,1.0,3.0,1.0,1.0]
 prob = DDEProblem(lotka_volterra!,uₒ,h,tspan,p,constant_lag = [τ] )
 sol = solve(prob,MethodOfSteps(AutoTsit5(Rosenbrock23(autodiff=false))))
