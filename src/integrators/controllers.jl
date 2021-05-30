765020100b0bd20f5e2802b40d946ccedcd708b0
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2021-05-30 14:39:39 +0530

    Use DiffEqBase.value on arguments to fastpow call in stepsize_controller

--- src/integrators/controllers.jl
+++ src/integrators/controllers.jl
@@ -132,8 +132,8 @@ end
   if iszero(EEst)
     q = inv(qmax)
   else
-    q11 = DiffEqBase.fastpow(EEst, beta1)
-    q = DiffEqBase.value(q11 / DiffEqBase.fastpow(qold, beta2))
+    q11 = DiffEqBase.fastpow(DiffEqBase.value(EEst), DiffEqBase.value(beta1))
+    q = DiffEqBase.value(q11 / DiffEqBase.fastpow(DiffEqBase.value(qold), DiffEqBase.value(beta2)))
     integrator.q11 = q11
     @fastmath q = max(inv(qmax), min(inv(qmin), q / gamma))
   end
