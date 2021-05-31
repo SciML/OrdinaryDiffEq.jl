a9775f2988186217e8be79b55d82112b710bcbf0
Frank Schaefer <frank.schaefer@unibas.ch>
2021-05-31 16:21:59 +0200

    resolve index swap confusion

--- test/integrators/autodiff_events.jl
+++ test/integrators/autodiff_events.jl
@@ -23,9 +23,9 @@ prob = ODEProblem(f,eltype(p).([1.0,0.0]),eltype(p).((0.0,1.0)),copy(p))
 
 solve(prob,Tsit5(),abstol=1e-14,reltol=1e-14,callback=cb,save_everystep=true)[end]
 
-function test_f(p, sensealg=ForwardDiffSensitivity())
+function test_f(p)
   _prob = remake(prob, p=p)
-  solve(_prob,Tsit5(),sensealg=sensealg,abstol=1e-14,reltol=1e-14,callback=cb,save_everystep=false)[end]
+  solve(_prob,Tsit5(),abstol=1e-14,reltol=1e-14,callback=cb,save_everystep=false)[end]
 end
 findiff = Calculus.finite_difference_jacobian(test_f,p)
 findiff
@@ -36,8 +36,16 @@ ad
 
 @test ad ≈ findiff
 
-g1 = Zygote.gradient(θ->test_f(θ,ForwardDiffSensitivity())[1], p)
-g2 = Zygote.gradient(θ->test_f(θ,ReverseDiffAdjoint())[1], p)
+function test_f2(p, sensealg=ForwardDiffSensitivity())
+  _prob = remake(prob, p=p)
+  u = Array(solve(_prob,Tsit5(),sensealg=sensealg,abstol=1e-14,reltol=1e-14,callback=cb,save_everystep=false))
+  u[end]
+end
 
-@test g1[1] ≈ findiff[1,1:2]
+@test test_f2(p) == test_f(p)[end]
+
+g1 = Zygote.gradient(θ->test_f2(θ,ForwardDiffSensitivity()), p)
+g2 = Zygote.gradient(θ->test_f2(θ,ReverseDiffAdjoint()), p)
+
+@test g1[1] ≈ findiff[2,1:2]
 @test g2[1] ≈ findiff[2,1:2]
