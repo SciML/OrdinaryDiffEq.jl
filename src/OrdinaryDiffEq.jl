363b655c265ea88d27ade7a84548a71cdd475802
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-07-31 11:23:42 -0400

    help tsit5 precompile

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -170,6 +170,15 @@ using DocStringExtensions
   include("interp_func.jl")
   include("composite_algs.jl")
 
+  function lorenz(du,u,p,t)
+    du[1] = 10.0(u[2]-u[1])
+    du[2] = u[1]*(28.0-u[3]) - u[2]
+    du[3] = u[1]*u[2] - (8/3)*u[3]
+  end
+  lorenzprob = ODEProblem(lorenz,[1.0;0.0;0.0],(0.0,100.0))
+  alg = Tsit5()
+  solve(lorenzprob,alg)
+  
   #General Functions
   export solve, solve!, init, step!
 
--- src/caches/low_order_rk_caches.jl
+++ src/caches/low_order_rk_caches.jl
@@ -344,7 +344,7 @@ function alg_cache(alg::RK46NL,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUn
   RK46NLConstantCache(constvalue(uBottomEltypeNoUnits), constvalue(tTypeNoUnits))
 end
 
-function alg_cache(alg::Tsit5,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,uprev2,f,t,dt,reltol,p,calck,::Val{true})
+function alg_cache(alg::Tsit5,u,rate_prototype,::Type{uEltypeNoUnits},::Type{uBottomEltypeNoUnits},::Type{tTypeNoUnits},uprev,uprev2,f,t,dt,reltol,p,calck,::Val{true}) where {uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits}
   tab = Tsit5ConstantCache(constvalue(uBottomEltypeNoUnits),constvalue(tTypeNoUnits))
   k1 = zero(rate_prototype)
   k2 = zero(rate_prototype)
