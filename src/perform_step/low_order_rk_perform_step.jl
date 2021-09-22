c02198707c79630d287770e8d225b57e8a65e844
Arpit-Babbar <arpitbabbar@gmail.com>
2021-09-22 13:17:25 +0530

    Add limiter to Tsit5()

--- src/perform_step/low_order_rk_perform_step.jl
+++ src/perform_step/low_order_rk_perform_step.jl
@@ -627,21 +627,28 @@ end
 @muladd function perform_step!(integrator, cache::Tsit5Cache, repeat_step=false)
   @unpack t,dt,uprev,u,f,p = integrator
   @unpack c1,c2,c3,c4,c5,c6,a21,a31,a32,a41,a42,a43,a51,a52,a53,a54,a61,a62,a63,a64,a65,a71,a72,a73,a74,a75,a76,btilde1,btilde2,btilde3,btilde4,btilde5,btilde6,btilde7 = cache.tab
-  @unpack k1,k2,k3,k4,k5,k6,k7,utilde,tmp,atmp = cache
+  @unpack k1,k2,k3,k4,k5,k6,k7,utilde,tmp,atmp,stage_limiter!,step_limiter! = cache
   a = dt*a21
   @.. tmp = uprev+a*k1
+  stage_limiter!(tmp, f, p, t+c1*dt)
   f(k2, tmp, p, t+c1*dt)
   @.. tmp = uprev+dt*(a31*k1+a32*k2)
+  stage_limiter!(tmp, f, p, t+c2*dt)
   f(k3, tmp, p, t+c2*dt)
   @.. tmp = uprev+dt*(a41*k1+a42*k2+a43*k3)
+  stage_limiter!(tmp, f, p, t+c3*dt)
   f(k4, tmp, p, t+c3*dt)
   @.. tmp = uprev+dt*(a51*k1+a52*k2+a53*k3+a54*k4)
+  stage_limiter!(tmp, f, p, t+c4*dt)
   f(k5, tmp, p, t+c4*dt)
   @.. tmp = uprev+dt*(a61*k1+a62*k2+a63*k3+a64*k4+a65*k5)
+  stage_limiter!(tmp, f, p, t+dt)
   f(k6, tmp, p, t+dt)
   @.. u = uprev+dt*(a71*k1+a72*k2+a73*k3+a74*k4+a75*k5+a76*k6)
+  stage_limiter!(u, f, p, t+dt)
   f(k7, u, p, t+dt)
   integrator.destats.nf += 6
+  step_limiter!(u, f, p, t+dt)
   if integrator.alg isa CompositeAlgorithm
     g7 = u
     g6 = tmp
@@ -664,33 +671,40 @@ end
   @unpack t,dt,uprev,u,f,p = integrator
   uidx = eachindex(integrator.uprev)
   @unpack c1,c2,c3,c4,c5,c6,a21,a31,a32,a41,a42,a43,a51,a52,a53,a54,a61,a62,a63,a64,a65,a71,a72,a73,a74,a75,a76,btilde1,btilde2,btilde3,btilde4,btilde5,btilde6,btilde7 = cache.tab
-  @unpack k1,k2,k3,k4,k5,k6,k7,utilde,tmp,atmp = cache
+  @unpack k1,k2,k3,k4,k5,k6,k7,utilde,tmp,atmp,stage_limiter!,step_limiter! = cache
   a = dt*a21
   @inbounds @simd ivdep for i in uidx
     tmp[i] = uprev[i]+a*k1[i]
   end
+  stage_limiter!(tmp, f, p, t+c1*dt)
   f(k2, tmp, p, t+c1*dt)
   @inbounds @simd ivdep for i in uidx
     tmp[i] = uprev[i]+dt*(a31*k1[i]+a32*k2[i])
   end
+  stage_limiter!(tmp, f, p, t+c2*dt)
   f(k3, tmp, p, t+c2*dt)
   @inbounds @simd ivdep for i in uidx
     tmp[i] = uprev[i]+dt*(a41*k1[i]+a42*k2[i]+a43*k3[i])
   end
+  stage_limiter!(tmp, f, p, t+c3*dt)
   f(k4, tmp, p, t+c3*dt)
   @inbounds @simd ivdep for i in uidx
     tmp[i] = uprev[i]+dt*(a51*k1[i]+a52*k2[i]+a53*k3[i]+a54*k4[i])
   end
+  stage_limiter!(tmp, f, p, t+c4*dt)
   f(k5, tmp, p, t+c4*dt)
   @inbounds @simd ivdep for i in uidx
     tmp[i] = uprev[i]+dt*(a61*k1[i]+a62*k2[i]+a63*k3[i]+a64*k4[i]+a65*k5[i])
   end
+  stage_limiter!(tmp, f, p, t+dt)
   f(k6, tmp, p, t+dt)
   @inbounds @simd ivdep for i in uidx
     u[i] = uprev[i]+dt*(a71*k1[i]+a72*k2[i]+a73*k3[i]+a74*k4[i]+a75*k5[i]+a76*k6[i])
   end
+  stage_limiter!(u, f, p, t+dt)
   f(k7, u, p, t+dt)
   integrator.destats.nf += 6
+  step_limiter!(u, f, p, t+dt)
   if typeof(integrator.alg) <: CompositeAlgorithm
     g7 = u
     g6 = tmp
