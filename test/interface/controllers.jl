88e4953482c5bb6550198a8172e00cf3d2e131aa
Hendrik Ranocha <ranocha@users.noreply.github.com>
2022-02-03 14:42:50 +0100

    add ICOntroller test

--- test/interface/controllers.jl
+++ test/interface/controllers.jl
@@ -4,6 +4,9 @@ f(u,p,t) = zero(u)
 prob = ODEProblem(f, 1.0, (0.0, 10.0))
 
 # Shouldn't error
+sol = solve(prob, Tsit5(), controller=IController())
+@test sol.retcode === :Success
+
 sol = solve(prob, Tsit5(), controller=PIController(7//50, 2//25))
 @test sol.retcode === :Success
 
