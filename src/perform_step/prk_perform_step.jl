118e05c31808dabab7e69991d5e9a12712100264
Daniel Wennberg <daniel.wennberg@gmail.com>
2022-03-20 01:50:13 -0700

    Correct fsallast assignment in KuttaPRK2p5
    Fixes #1623

--- src/perform_step/prk_perform_step.jl
+++ src/perform_step/prk_perform_step.jl
@@ -23,7 +23,7 @@ end
 
   if !isthreaded(integrator.alg.threading)
     k5_6[1] = f(uprev + dt*(α5_6[1,1]*k1 + α5_6[1,2]*k2 + α5_6[1,3]*k3 + α5_6[1,4]*k4), p, t + c5_6[1]*dt)
-    k5_6[2] = f(uprev + dt*(α5_6[2,1]*k1 + α5_6[2,2]*k2 + α5_6[2,3]*k3 + α5_6[2,4]*k4), p, t + c5_6[2]*dt)  
+    k5_6[2] = f(uprev + dt*(α5_6[2,1]*k1 + α5_6[2,2]*k2 + α5_6[2,3]*k3 + α5_6[2,4]*k4), p, t + c5_6[2]*dt)
   else
     let
       @threaded integrator.alg.threading for i in [1,2]
@@ -37,7 +37,7 @@ end
 
   integrator.fsallast = k # For interpolation, then FSAL'd
   integrator.k[1] = integrator.fsalfirst
-  integrator.k[1] = integrator.fsallast
+  integrator.k[2] = integrator.fsallast
   integrator.u = u
 end
 
