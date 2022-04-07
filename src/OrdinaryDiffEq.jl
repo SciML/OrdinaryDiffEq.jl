a37b119dbc5bf40ed4d97b8ceb028e333bbf74f8
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-04-07 07:25:03 -0400

    Update src/OrdinaryDiffEq.jl

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -229,7 +229,7 @@ using DocStringExtensions
 
   export FunctionMap, Euler, Heun, Ralston, Midpoint, RK4, ExplicitRK, OwrenZen3, OwrenZen4, OwrenZen5,
          BS3, BS5, RK46NL, DP5, Tsit5, DP8, Vern6, Vern7, Vern8, TanYam7, TsitPap8,
-         Vern9, Feagin10, Feagin12, Feagin14, CompositeAlgorithm, Anas5, RKO65, FRK65, PFRK87, RKM , MSRK5, MSRK6
+         Vern9, Feagin10, Feagin12, Feagin14, CompositeAlgorithm, Anas5, RKO65, FRK65, PFRK87, RKM , MSRK5
 
   export SSPRK22, SSPRK33, KYKSSPRK42, SSPRK53, SSPRK53_2N1, SSPRK53_2N2, SSPRK53_H, SSPRK63, SSPRK73, SSPRK83, SSPRK43, SSPRK432,
          SSPRKMSVS32, SSPRKMSVS43, SSPRK932, SSPRK54, SSPRK104
