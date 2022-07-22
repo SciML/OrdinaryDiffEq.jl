0ff522fa7d9c05eef94e99f76a32e1f848b11b3c
Hendrik Ranocha <mail@ranocha.de>
2022-07-22 08:38:11 +0200

    add failing test

--- test/interface/controllers.jl
+++ test/interface/controllers.jl
@@ -12,3 +12,10 @@ sol = solve(prob, Tsit5(), controller=PIController(7//50, 2//25))
 
 sol = solve(prob, Tsit5(), controller=PIDController(0.7, -0.4))
 @test sol.retcode === :Success
+
+
+# OrdinaryDiffEq.jl#1703
+# https://github.com/SciML/OrdinaryDiffEq.jl/issues/1703
+prob = ODEProblem((du, u, p, t) -> du[1] = 1, [0.0], (0.0, 5.0))
+sol = solve(prob, RDPK3SpFSAL49())
+@test sol.retcode === :Success
