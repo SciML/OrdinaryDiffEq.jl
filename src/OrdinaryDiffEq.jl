3da66056ecf152e8652cdfc890be45bf3f56583d
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-20 07:37:39 -0400

    trim the precompile list

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -205,35 +205,35 @@ using DocStringExtensions
         Rosenbrock23(), Rosenbrock23(autodiff=false),
         Rosenbrock23(chunk_size = 1), Rosenbrock23(chunk_size = Val{1}()),
 
-        Rodas4(), Rodas4(autodiff=false), Rodas4(chunk_size = 1),
-        Rodas4(chunk_size = Val{1}()),
+        Rodas4(), Rodas4(autodiff=false),
+        #Rodas4(chunk_size = 1), Rodas4(chunk_size = Val{1}()),
 
-        Rodas5(), Rodas5(autodiff=false), Rodas5(chunk_size = 1),
-        Rodas5(chunk_size = Val{1}()),
+        Rodas5(), Rodas5(autodiff=false),
+        #Rodas5(chunk_size = 1), Rodas5(chunk_size = Val{1}()),
 
-        Rodas5P(), Rodas5P(autodiff=false), Rodas5P(chunk_size = 1),
-        Rodas5P(chunk_size = Val{1}()),
+        Rodas5P(), Rodas5P(autodiff=false),
+        Rodas5P(chunk_size = 1), Rodas5P(chunk_size = Val{1}()),
 
-        TRBDF2(), TRBDF2(autodiff=false), TRBDF2(chunk_size = 1),
-        TRBDF2(chunk_size = Val{1}()),
+        TRBDF2(), TRBDF2(autodiff=false),
+        #TRBDF2(chunk_size = 1), TRBDF2(chunk_size = Val{1}()),
 
-        KenCarp4(), KenCarp4(autodiff=false), KenCarp4(chunk_size = 1),
-        KenCarp4(chunk_size = Val{1}()),
+        KenCarp4(), KenCarp4(autodiff=false),
+        #KenCarp4(chunk_size = 1), KenCarp4(chunk_size = Val{1}()),
 
-        QNDF(), QNDF(autodiff=false), QNDF(chunk_size = 1),
-        QNDF(chunk_size = Val{1}()),
+        QNDF(), QNDF(autodiff=false),
+        #QNDF(chunk_size = 1), QNDF(chunk_size = Val{1}()),
 
         AutoTsit5(Rosenbrock23()), AutoTsit5(Rosenbrock23(autodiff=false)),
         AutoTsit5(Rosenbrock23(chunk_size = 1)),
         AutoTsit5(Rosenbrock23(chunk_size = Val{1}())),
 
         AutoTsit5(TRBDF2()), AutoTsit5(TRBDF2(autodiff=false)),
-        AutoTsit5(TRBDF2(chunk_size = 1)),
-        AutoTsit5(TRBDF2(chunk_size = Val{1}())),
+        #AutoTsit5(TRBDF2(chunk_size = 1)),
+        #AutoTsit5(TRBDF2(chunk_size = Val{1}())),
 
         AutoVern9(KenCarp47()), AutoVern9(KenCarp47(autodiff=false)),
-        AutoVern9(KenCarp47(chunk_size = 1)),
-        AutoVern9(KenCarp47(chunk_size = Val{1}())),
+        #AutoVern9(KenCarp47(chunk_size = 1)),
+        #AutoVern9(KenCarp47(chunk_size = Val{1}())),
 
         AutoVern9(Rodas5()), AutoVern9(Rodas5(autodiff=false)),
         AutoVern9(Rodas5(chunk_size = 1)),
@@ -244,16 +244,16 @@ using DocStringExtensions
         AutoVern9(Rodas5P(chunk_size = Val{1}())),
 
         AutoVern7(Rodas4()), AutoVern7(Rodas4(autodiff=false)),
-        AutoVern7(Rodas4(chunk_size = 1)),
-        AutoVern7(Rodas4(chunk_size = Val{1}())),
+        #AutoVern7(Rodas4(chunk_size = 1)),
+        #AutoVern7(Rodas4(chunk_size = Val{1}())),
 
-        AutoVern7(Rodas5P()), AutoVern7(Rodas5P(autodiff=false)),
-        AutoVern7(Rodas5P(chunk_size = 1)),
-        AutoVern7(Rodas5P(chunk_size = Val{1}())),
+        #AutoVern7(Rodas5P()), AutoVern7(Rodas5P(autodiff=false)),
+        #AutoVern7(Rodas5P(chunk_size = 1)),
+        #AutoVern7(Rodas5P(chunk_size = Val{1}())),
 
         AutoVern7(TRBDF2()), AutoVern7(TRBDF2(autodiff=false)),
-        AutoVern7(TRBDF2(chunk_size = 1)),
-        AutoVern7(TRBDF2(chunk_size = Val{1}())),
+        #AutoVern7(TRBDF2(chunk_size = 1)),
+        #AutoVern7(TRBDF2(chunk_size = Val{1}())),
       ]
 
       prob_list = [
