0653a5457ddc1dde9e8d0dde35ad75fe4e3f94aa
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2022-09-15 09:00:34 +0530

    Fix the formatting of `set_snoop_precompile`

--- src/preferences.jl
+++ src/preferences.jl
@@ -6,14 +6,14 @@ By default it is enabled aka `true`.
 These preferences are stored in a LocalPrefernces.toml.
 Instead, to store it in (OrdinaryDiffEq)Project.toml, set `export_prefs = true`
 """
-function set_snoop_precompile!(
-    include_snoop_precompile::Bool = true;
-    export_prefs::Bool = false,
-    )
+function set_snoop_precompile!(include_snoop_precompile::Bool = true;
+                               export_prefs::Bool = false)
     set_preferences!(@__MODULE__,
-        "include_snoop_precompile" => include_snoop_precompile;
-        export_prefs,
-        force = true)
+                     "include_snoop_precompile" => include_snoop_precompile;
+                     export_prefs,
+                     force = true)
 end
 
-const INCLUDE_SNOOP_PRECOMPILE = load_preference(@__MODULE__, "include_snoop_precompile", true)
+const INCLUDE_SNOOP_PRECOMPILE = load_preference(@__MODULE__,
+                                                 "include_snoop_precompile",
+                                                 true)
