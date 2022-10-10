5ad4223b695647c3e01b7d540839a8b3acf2bbfe
Oscar Smith <oscar.smith@juliacomputing.com>
2022-10-10 15:50:21 -0500

    Make `jac_prototype` use `undefmatrix` instead of `zeromatrix`
    https://github.com/JuliaArrays/ArrayInterface.jl/pull/353 adds an `undefmatrix`which is significantly faster than `zeromatrix` since it doesn't have to initialize values. This matters for autostiff solvers on non-stiff equations since in these cases filling the jacobian matrix with zeros can be as expensive as the entire ODE solve.

--- src/derivative_utils.jl
+++ src/derivative_utils.jl
@@ -870,7 +870,7 @@ function build_J_W(alg, u, uprev, p, t, dt, f::F, ::Type{uEltypeNoUnits},
         # Thus setup JacVec and a concrete J, using sparsity when possible
         _f = islin ? (isode ? f.f : f.f1.f) : f
         J = if f.jac_prototype === nothing
-            ArrayInterface.zeromatrix(u)
+            ArrayInterface.undefmatrix(u)
         else
             deepcopy(f.jac_prototype)
         end
@@ -886,7 +886,7 @@ function build_J_W(alg, u, uprev, p, t, dt, f::F, ::Type{uEltypeNoUnits},
         W = WOperator{IIP}(f.mass_matrix, dt, J, u)
     else
         J = if f.jac_prototype === nothing
-            ArrayInterface.zeromatrix(u)
+            ArrayInterface.undefmatrix(u)
         else
             deepcopy(f.jac_prototype)
         end
