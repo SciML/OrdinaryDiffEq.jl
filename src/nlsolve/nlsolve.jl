21228ab4b5df71cdedfca7cd772ccbb46e787d27
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-12-24 08:26:20 -0500

    Update src/nlsolve/nlsolve.jl

--- src/nlsolve/nlsolve.jl
+++ src/nlsolve/nlsolve.jl
@@ -90,7 +90,6 @@ function nlsolve!(nlsolver::AbstractNLSolver, integrator::DiffEqBase.DEIntegrato
         end
     end
 
-
     if isnewton(nlsolver) && nlsolver.status == Divergence && !isJcurrent(nlsolver, integrator)
         nlsolver.status = TryAgain
         nlsolver.nfails += 1
