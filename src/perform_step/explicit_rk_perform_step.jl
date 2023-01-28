18c7b960b72dffe18d4de6533ecf040512a5be28
Yingbo Ma <mayingbo5@gmail.com>
2023-01-28 04:40:07 -0500

    Unroll ExplicitRK

--- src/perform_step/explicit_rk_perform_step.jl
+++ src/perform_step/explicit_rk_perform_step.jl
@@ -77,12 +77,22 @@ function initialize!(integrator, cache::ExplicitRKCache)
     integrator.destats.nf += 1
 end
 
-@muladd function perform_step!(integrator, cache::ExplicitRKCache, repeat_step = false)
-    @unpack t, dt, uprev, u, f, p = integrator
-    alg = unwrap_alg(integrator, nothing)
-    @unpack A, c, α, αEEst, stages = cache.tab
-    @unpack kk, utilde, tmp, atmp = cache
+@generated function accumulate_explicit_stages!(out, A, uprev, kk, dt, ::Val{s}) where s
+    s <= 1 && error("$s must be > 1")
+    # Note that `A` is transposed
+    if s == 2
+        return :(@muladd @.. broadcast=false out = uprev + dt * (A[1, $s] * kk[1]))
+    else
+        expr = :(@muladd @.. broadcast=false out = uprev + dt * (A[1, $s] * kk[1] + A[2, $s] * kk[2]))
+        acc = expr.args[end].args[end].args[end].args[end].args[end].args
+        for i in 3:s-1
+            push!(acc, :(A[$i, $s] * kk[$i]))
+        end
+        return expr
+    end
+end
 
+@muladd function compute_stages!(f::F, A, c, utilde, u, tmp, uprev, kk, p, t, dt, stages::Integer) where {F}
     # Middle
     for i in 2:(stages - 1)
         @.. broadcast=false utilde=zero(kk[1][1])
@@ -91,7 +101,6 @@ end
         end
         @.. broadcast=false tmp=uprev + dt * utilde
         f(kk[i], tmp, p, t + c[i] * dt)
-        integrator.destats.nf += 1
     end
 
     #Last
@@ -101,7 +110,33 @@ end
     end
     @.. broadcast=false u=uprev + dt * utilde
     f(kk[end], u, p, t + c[end] * dt) #fsallast is tmp even if not fsal
-    integrator.destats.nf += 1
+    return nothing
+end
+
+@generated function compute_stages!(f::F, A, c, u, tmp, uprev, kk, p, t, dt, ::Val{s}) where {F, s}
+    quote
+        Base.@nexprs $(s - 2) i′ -> begin
+            i = i′ + 1
+            accumulate_explicit_stages!(tmp, A, uprev, kk, dt, Val(i))
+            f(kk[i], tmp, p, t + c[i] * dt)
+        end
+        accumulate_explicit_stages!(u, A, uprev, kk, dt, Val(s))
+        f(kk[s], u, p, t + c[end] * dt)
+    end
+end
+
+function runtime_split_stages!(f::F, A, c, utilde, u, tmp, uprev, kk, p, t, dt, stages::Integer) where {F}
+    Base.@nif 16 (s -> (s == stages)) (s -> compute_stages!(f, A, c, u, tmp, uprev, kk, p, t, dt, Val(s))) (s -> compute_stages!(f, A, c, utilde, u, tmp, uprev, kk, p, t, dt, stages))
+end
+
+@muladd function perform_step!(integrator, cache::ExplicitRKCache, repeat_step = false)
+    @unpack t, dt, uprev, u, f, p = integrator
+    alg = unwrap_alg(integrator, nothing)
+    @unpack A, c, α, αEEst, stages = cache.tab
+    @unpack kk, utilde, tmp, atmp = cache
+
+    runtime_split_stages!(f, A, c, utilde, u, tmp, uprev, kk, p, t, dt, stages)
+    integrator.destats.nf += stages - 1
 
     #Accumulate
     if !isfsal(alg.tableau)
