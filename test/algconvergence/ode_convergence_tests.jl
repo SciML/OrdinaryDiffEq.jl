974af72e1d52424250c69b1bbc3207e5c617fa20
ErikQQY <2283984853@qq.com>
2023-01-29 17:40:40 +0800

    Add MSRK54 method
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/algconvergence/ode_convergence_tests.jl
+++ test/algconvergence/ode_convergence_tests.jl
@@ -49,6 +49,9 @@ testTol = 0.2
     sim_ms5 = test_convergence(dts2, prob, MSRK5())
     @test sim_ms5.𝒪est[:l∞]≈5 atol=testTol
 
+    sim_ms54 = test_convergence(dts, prob, MSRK54())
+    @test sim_ms54.𝒪est[:l∞]≈4 atol=testTol
+
     sim4 = test_convergence(dts, prob, BS3())
     @test sim4.𝒪est[:l2]≈3 atol=testTol
 
