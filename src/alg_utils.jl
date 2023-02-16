eefc2ccbd690e52fd9b27d0838a5e42aafc3674b
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-02-16 10:51:40 -0500

    use the tab stability size

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -920,6 +920,7 @@ ssp_coefficient(alg::KYK2014DGSSPRK_3S2) = 0.8417
 ssp_coefficient(alg::SSPSDIRK2) = 4
 
 # stability regions
+alg_stability_size(alg::ExplicitRK) = alg.tab.stability_size
 alg_stability_size(alg::DP5) = 3.3066
 alg_stability_size(alg::Tsit5) = 3.5068
 alg_stability_size(alg::Vern6) = 4.8553
