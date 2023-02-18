769966975f40e0e03d8a50353e1af91de0bb6c01
ErikQQY <2283984853@qq.com>
2023-02-18 15:28:31 +0800

    Remove redundant lorenz definition
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/regression/ode_adaptive_tests.jl
+++ test/regression/ode_adaptive_tests.jl
@@ -100,13 +100,6 @@ integrator = init(ode, Tsit5())
 
 prob_linear = prob_ode_linear
 
-function lorenz(du, u, p, t)
-    du[1] = 10.0(u[2] - u[1])
-    du[2] = u[1] * (28.0 - u[3]) - u[2]
-    du[3] = u[1] * u[2] - (8 / 3) * u[3]
-end
-u0 = [1.0; 0.0; 0.0]
-tspan = (0.0, 100.0)
 prob_lorenz = ODEProblem{true}(lorenz, u0, tspan)
 
 # ESDIRK436L2SA2
