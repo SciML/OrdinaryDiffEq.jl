dfd108d101c32030732b9a49e1e9360938f1b764
Henry Langner <128313572+HenryLangner@users.noreply.github.com>
2023-04-20 12:10:22 +0200

    Update src/algorithms.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -5234,7 +5234,8 @@ struct IRKN3 <: OrdinaryDiffEqPartitionedAlgorithm end
 """
 A 4th order explicit Runge-Kutta-Nyström method which can be applied directly on second order ODEs. Can only be used with fixed time steps.
 
-In case the ODE Problem is not dependent on the first derivative consider using Nystrom4VelocityIndependent to increase performance.
+In case the ODE Problem is not dependent on the first derivative consider using 
+[`Nystrom4VelocityIndependent`](@ref) to increase performance.
 
 ## References
 E. Hairer, S.P. Norsett, G. Wanner, (1993) Solving Ordinary Differential Equations I.
