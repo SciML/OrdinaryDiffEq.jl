c8d3b26a5475ef7bfdf84e6247efea11a8a5dabc
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-04-21 10:46:06 +0100

    fix usings

--- test/interface/newton_tests.jl
+++ test/interface/newton_tests.jl
@@ -6,8 +6,6 @@ using Test
 
 import ODEProblemLibrary: prob_ode_orego, prob_ode_rober, prob_ode_lorenz
 
-using BenchmarkTools
-
 for prob in (prob_ode_lorenz, prob_ode_orego)
     sol1 = solve(prob, Trapezoid(), reltol=1e-12, abstol=1e-12)
     @test sol1.retcode == DiffEqBase.ReturnCode.Success
