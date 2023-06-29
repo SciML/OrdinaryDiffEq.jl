f159eacbb4f4239bea03f0ea613102f8d19c8fb0
Saravan Kumar <42614670+sar1kumar@users.noreply.github.com>
2023-06-30 00:08:50 +0100

    Update low_order_rk_caches.jl

--- src/caches/low_order_rk_caches.jl
+++ src/caches/low_order_rk_caches.jl
@@ -448,6 +448,28 @@ if isdefined(Base, :Experimental) && isdefined(Base.Experimental, :silence!)
     Base.Experimental.silence!(Tsit5Cache)
 end
 
+@cache struct Tsit5TestCache{uType, rateType, uNoUnitsType, StageLimiter, StepLimiter,
+                        Thread} <: OrdinaryDiffEqMutableCache
+    u::uType
+    uprev::uType
+    k1::rateType
+    k2::rateType
+    k3::rateType
+    k4::rateType
+    k5::rateType
+    k6::rateType
+    k7::rateType
+    utilde::uType
+    tmp::uType
+    atmp::uNoUnitsType
+    stage_limiter!::StageLimiter
+    step_limiter!::StepLimiter
+    thread::Thread
+end
+if isdefined(Base, :Experimental) && isdefined(Base.Experimental, :silence!)
+    Base.Experimental.silence!(Tsit5TestCache)
+end
+
 @cache struct RK46NLCache{uType, rateType, TabType, StageLimiter, StepLimiter, Thread} <:
               OrdinaryDiffEqMutableCache
     u::uType
@@ -545,6 +567,35 @@ function alg_cache(alg::Tsit5, u, rate_prototype, ::Type{uEltypeNoUnits},
     dt, reltol, p, calck,
     ::Val{false}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
     Tsit5ConstantCache()
+end
+
+
+function alg_cache(alg::Tsit5Test, u, rate_prototype, ::Type{uEltypeNoUnits},
+                   ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+                   dt, reltol, p, calck,
+                   ::Val{true}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    k1 = zero(rate_prototype)
+    k2 = zero(rate_prototype)
+    k3 = zero(rate_prototype)
+    k4 = zero(rate_prototype)
+    k5 = zero(rate_prototype)
+    k6 = zero(rate_prototype)
+    k7 = zero(rate_prototype)
+    utilde = zero(u)
+    atmp = similar(u, uEltypeNoUnits)
+    recursivefill!(atmp, false)
+    tmp = zero(u)
+    Tsit5TestCache(u, uprev, k1, k2, k3, k4, k5, k6, k7, utilde, tmp, atmp,
+    alg.stage_limiter!, alg.step_limiter!, alg.thread)
+end
+
+TruncatedStacktraces.@truncate_stacktrace Tsit5TestCache 1
+
+function alg_cache(alg::Tsit5Test, u, rate_prototype, ::Type{uEltypeNoUnits},
+                  ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+                  dt, reltol, p, calck,
+                  ::Val{false}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    Tsit5TestConstantCache()
 end
 
 @cache struct DP5Cache{uType, rateType, uNoUnitsType, StageLimiter, StepLimiter,
