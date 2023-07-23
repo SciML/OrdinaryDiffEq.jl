e476b24726ffa639d61696178eeab0f5e8622171
Gaurav Arya <gauravarya272@gmail.com>
2023-07-23 01:54:02 -0400

    Fix wprototype test

--- test/interface/wprototype_tests.jl
+++ test/interface/wprototype_tests.jl
@@ -19,20 +19,20 @@ for prob in (prob_ode_vanderpol_stiff,)
     W_op = -(I - gamma_op * J_op) * transform_op
 
     # Make problem with custom MatrixOperator jac_prototype
-    f_J = ODEFunction(prob.f; jac_prototype = J_op)
+    f_J = ODEFunction(prob.f.f; jac_prototype = J_op)
     prob_J = remake(prob; f = f_J)
 
     # Test that the custom jacobian is used
     integrator = init(prob_J, Rosenbrock23())
-    @test integrator.cache.J === J_op
+    @test integrator.cache.J isa typeof(J_op)
 
     # Make problem with custom SciMLOperator W_prototype 
-    f_W = ODEFunction(prob.f; jac_prototype = J_op, W_prototype = W_op)
+    f_W = ODEFunction(prob.f.f; jac_prototype = J_op, W_prototype = W_op)
     prob_W = remake(prob; f = f_W)
 
     # Test that the custom W operator is used
     integrator = init(prob_W, Rosenbrock23())
-    @test integrator.cache.W === W_op
+    @test integrator.cache.W isa typeof(W_op)
 
     # Run solves. Ensure all solutions use the same linear solve for fair comparison.
     # TODO: in future, ensure and test that polyalg chooses the best linear solve for all. 
