0491642cf49d5a58affcab008957c06c49341b23
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-10-20 17:00:16 -0400

    Update src/initialize_dae.jl

--- src/initialize_dae.jl
+++ src/initialize_dae.jl
@@ -23,7 +23,7 @@ BrownFullBasicInit(abstol) = BrownFullBasicInit(; abstol = abstol, nlsolve = not
 using SciMLNLSolve
 default_nlsolve(alg, isinplace, u, autodiff = false) = alg
 function default_nlsolve(::Nothing, isinplace, u, autodiff = false)
-    RobustMultiNewton(autodiff = autodiff)
+    TrustRegion(autodiff = autodiff)
 end
 function default_nlsolve(::Nothing, isinplace::Val{false}, u::StaticArray, autodiff = false)
     SimpleNewtonRaphson(autodiff = autodiff)
