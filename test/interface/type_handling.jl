e746338a52432d8869bdf5c807b8b4120dbb80f9
oscarddssmith <oscar.smith@juliacomputing.com>
2023-11-03 11:24:13 -0400

    add test

--- test/interface/type_handling.jl
+++ test/interface/type_handling.jl
@@ -8,3 +8,9 @@ f(u, p, t) = u
 u0(p, t0) = ones(2)
 prob = ODEProblem(f, u0, (0.0, 1.0))
 sol = solve(prob, Tsit5())
+
+# test different u and t type
+ode_f(du, u, p, t) = du[1] = -u[1]
+# autospecialize broken for the next 35 minutes
+prob = ODEProblem(ODEFunction{true, SciMLBase.FullSpecialize()}(ode_f), [1f0], (0.0,1.0))
+solve(prob, Rodas5P())
