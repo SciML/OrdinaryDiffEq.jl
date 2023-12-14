3df1150db24eb4f3a1cc88885a18f1bf17fb758c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-12-13 22:44:07 -0500

    finish

--- test/regression/hard_dae.jl
+++ test/regression/hard_dae.jl
@@ -216,14 +216,13 @@ cb = DiscreteCallback(condition, affect!)
 
 prob = ODEProblem(f, deepcopy(res.zero), (0, 20.0), deepcopy(p_inv))
 refsol = solve(prob, Rodas4(), saveat = 0.1, callback = cb, tstops = [1.0], reltol = 1e-12,
-    abstol = 1e-18)
+    abstol = 1e-17)
 
 for solver in (Rodas4, Rodas4P, Rodas5, Rodas5P, FBDF, QNDF, Rosenbrock23)
     @show solver
     prob = ODEProblem(f, deepcopy(res.zero), (0, 20.0), deepcopy(p_inv))
-    maxiters = solver isa Rosenbrock23 ? 1e8 : 1e5
-    sol = solve(prob, solver(), saveat = 0.1, callback = cb, tstops = [1.0], reltol = 1e-12,
-        abstol = 1e-16, maxiters = maxiters)
+    sol = solve(prob, solver(), saveat = 0.1, callback = cb, tstops = [1.0], reltol = 1e-14,
+        abstol = 1e-14)
     @test sol.retcode == ReturnCode.Success
     @test sol.t[end] == 20.0
     @test maximum(sol - refsol) < 1e-11
