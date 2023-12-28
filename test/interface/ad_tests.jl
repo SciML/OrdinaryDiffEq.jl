e692db7c7986f7f5e95ce7d86d2389b76accc36b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-12-28 07:03:33 -0500

    Test master

--- test/interface/ad_tests.jl
+++ test/interface/ad_tests.jl
@@ -328,11 +328,11 @@ fn(x, solver) = objfun(x, prob, data, solver, reltol, abstol)
 @test norm(ForwardDiff.gradient(x -> fn(x, AutoVern9(Rodas4())), p0)) < 1e-9
 
 # test AD with LinearExponential()
-function f(x)
+function f2(x)
     K = MatrixOperator(x)
     u0 = eltype(x).([1.0, 0.0])
     prob = ODEProblem(K, u0, (0.0, 10.0))
     sol = solve(prob, LinearExponential(), tstops = [0.0, 10.0])[2, :]
 end
 K_ = [-1.0 0.0; 1.0 -1.0]
-@test isapprox(ForwardDiff.jacobian(f, K_)[2], 0.00226999, atol = 1e-6)
+@test isapprox(ForwardDiff.jacobian(f2, K_)[2], 0.00226999, atol = 1e-6)
