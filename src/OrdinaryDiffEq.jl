817d2507a19892b2ed192d1e5a980db2e34a8969
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-01-01 15:02:34 -0500

    Redesign default ODE solver to be fully type-grounded
    This accomplishes a few things:
    
    * Faster precompile times by precompiling less
    * Full inference of results when using the automatic algorithm
    * Hopefully faster load times by also precompiling less
    
    This is done the same way as
    
    * linearsolve https://github.com/SciML/LinearSolve.jl/pull/307
    * nonlinearsolve https://github.com/SciML/NonlinearSolve.jl/pull/238
    
    and is thus the more modern SciML way of doing it. It avoids dispatch by having a single algorithm that always generates the full cache and instead of dispatching between algorithms always branches for the choice.
    
    It turns out, the mechanism already existed for this in OrdinaryDiffEq... it's CompositeAlgorithm, the same bones as AutoSwitch! As such, this reuses quite a bit of code from the auto-switch algorithms but instead of just having two choices it (currently) has 6 that it chooses between. This means that it has stiffness detection and switching behavior, but also in a size-dependent way.
    
    There are still some optimizations to do though. Like LinearSolve.jl, it would be more efficient to have a way to initialize the caches to size zero and then have a way to re-initialize them to the correct size. Right now, it'll generate the same Jacobian N times and it shouldn't need to do that.
    
    fix typo / test
    
    fix precompilation choices
    
    Update src/composite_algs.jl
    
    Co-authored-by: Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
    
    Update src/composite_algs.jl
    
    switch CompositeCache away from tuple so it can start undef
    
    Default Cache
    
    fix precompile
    
    remove fallbacks
    
    remove fallbacks

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -26,6 +26,8 @@ using LinearSolve, SimpleNonlinearSolve
 
 using LineSearches
 
+import EnumX
+
 import FillArrays: Trues
 
 # Interfaces
@@ -139,6 +141,7 @@ include("nlsolve/functional.jl")
 include("nlsolve/newton.jl")
 
 include("generic_rosenbrock.jl")
+include("composite_algs.jl")
 
 include("caches/basic_caches.jl")
 include("caches/low_order_rk_caches.jl")
@@ -232,7 +235,6 @@ include("constants.jl")
 include("solve.jl")
 include("initdt.jl")
 include("interp_func.jl")
-include("composite_algs.jl")
 
 import PrecompileTools
 
@@ -251,9 +253,14 @@ PrecompileTools.@compile_workload begin
         Tsit5(), Vern7()
     ]
 
-    stiff = [Rosenbrock23(), Rosenbrock23(autodiff = false),
-        Rodas5P(), Rodas5P(autodiff = false),
-        FBDF(), FBDF(autodiff = false)
+    stiff = [Rosenbrock23(),
+        Rodas5P(),
+        FBDF()
+    ]
+
+    default_ode = [
+        DefaultODEAlgorithm(autodiff=false),
+        DefaultODEAlgorithm()
     ]
 
     autoswitch = [
@@ -282,7 +289,11 @@ PrecompileTools.@compile_workload begin
         append!(solver_list, stiff)
     end
 
-    if Preferences.@load_preference("PrecompileAutoSwitch", true)
+    if Preferences.@load_preference("PrecompileDefault", true)
+        append!(solver_list, default_ode)
+    end
+
+    if Preferences.@load_preference("PrecompileAutoSwitch", false)
         append!(solver_list, autoswitch)
     end
 
