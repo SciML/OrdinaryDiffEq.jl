2fbc0b2e2fc43bf4e269842a0b7fcfa4dfecd17c
Collin Wittenstein <126870995+cwittens@users.noreply.github.com>
2024-01-16 15:48:39 +0100

    Update test/algconvergence/ode_rosenbrock_tests.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- test/algconvergence/ode_rosenbrock_tests.jl
+++ test/algconvergence/ode_rosenbrock_tests.jl
@@ -277,7 +277,7 @@ import LinearSolve
     prob = prob_ode_linear
 
     sim = test_convergence(dts, prob, ROS34PRw())
-    @test sim.𝒪est[:final]≈4 atol=testTol
+    @test sim.𝒪est[:final]≈3 atol=testTol
 
     sol = solve(prob, ROS34PRw())
     @test length(sol) < 20
