971fdf6479531eff37fa575a9fd4a49df3345b5b
oscarddssmith <oscar.smith@juliacomputing.com>
2024-05-15 15:30:19 -0400

    better test

--- test/interface/default_solver_tests.jl
+++ test/interface/default_solver_tests.jl
@@ -7,8 +7,8 @@ sol = solve(prob_ode_2Dlinear)
 
 tsitsol = solve(prob_ode_2Dlinear, Tsit5())
 # test that default isn't much worse than Tsit5 (we expect it to use Tsit5 for this).
-@test sol.stats.naccept < tsitsol.stats.naccept + 2
-@test sol.stats.nf < tsitsol.stats.nf + 20
+@test sol.stats.naccept == tsitsol.stats.naccept
+@test sol.stats.nf == tsitsol.stats.nf
 
 sol = solve(prob_ode_2Dlinear, reltol=1e-10)
 vernsol = solve(prob_ode_2Dlinear, Vern7(), reltol=1e-10)
