7b4181343155ddb1d1a8c492ba4bcb6b37bf8848
termi-official <termi-official@users.noreply.github.com>
2024-05-15 23:03:59 +0200

    oopsie.

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1292,27 +1292,6 @@ function NewmarkBeta(; β=nothing, γ=nothing, chunk_size = Val{0}(), autodiff =
         precs)
 end
 
-# struct CNAB2{CS, AD, F, F2, P, FDT, ST, CJ} <:
-#     OrdinaryDiffEqNewtonAlgorithm{CS, AD, FDT, ST, CJ}
-#  linsolve::F
-#  nlsolve::F2
-#  precs::P
-#  extrapolant::Symbol
-# end
-
-# function CNAB2(; chunk_size = Val{0}(), autodiff = Val{true}(), standardtag = Val{true}(),
-#      concrete_jac = nothing, diff_type = Val{:forward},
-#      linsolve = nothing, precs = DEFAULT_PRECS, nlsolve = NLNewton(),
-#      extrapolant = :linear)
-#  CNAB2{
-#      _unwrap_val(chunk_size), _unwrap_val(autodiff), typeof(linsolve), typeof(nlsolve),
-#      typeof(precs), diff_type, _unwrap_val(standardtag), _unwrap_val(concrete_jac)}(
-#      linsolve,
-#      nlsolve,
-#      precs,
-#      extrapolant)
-# end
-
 ################################################################################
 
 # Adams Bashforth and Adams moulton methods
