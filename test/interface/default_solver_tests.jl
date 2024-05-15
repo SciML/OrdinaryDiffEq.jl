217fb5302e3e813750c3ec8cdfd89b8b79b030f4
oscarddssmith <oscar.smith@juliacomputing.com>
2024-05-15 17:19:27 -0400

    better tests

--- test/interface/default_solver_tests.jl
+++ test/interface/default_solver_tests.jl
@@ -9,14 +9,14 @@ tsitsol = solve(prob_ode_2Dlinear, Tsit5())
 # test that default isn't much worse than Tsit5 (we expect it to use Tsit5 for this).
 @test sol.stats.naccept == tsitsol.stats.naccept
 @test sol.stats.nf == tsitsol.stats.nf
-@test unique(sol.alg_choice) = [1]
+@test all(isequal(1), sol.alg_choice)
 
 sol = solve(prob_ode_2Dlinear, reltol=1e-10)
 vernsol = solve(prob_ode_2Dlinear, Vern7(), reltol=1e-10)
 # test that default isn't much worse than Tsit5 (we expect it to use Tsit5 for this).
 @test sol.stats.naccept < tsitsol.stats.naccept + 2
 @test sol.stats.nf < tsitsol.stats.nf + 20
-@test unique(sol.alg_choice) = [2]
+@test all(isequal(2), sol.alg_choice)
 
 function rober(u, p, t)
     y₁, y₂, y₃ = u
@@ -31,8 +31,9 @@ rosensol = solve(prob_rober, AutoTsit5(Rosenbrock23()))
 # test that default has the same performance as AutoTsit5(Rosenbrock23()) (which we expect it to use for this).
 @test sol.stats.naccept == rosensol.stats.naccept
 @test sol.stats.nf == rosensol.stats.nf
-@test sol.alg_choice[end] == 3
 @test unique(sol.alg_choice) == [1,3]
+@test sol.alg_choice[1] == 1
+@test sol.alg_choice[end] == 3
 
 sol = solve(prob_rober, reltol=1e-7, abstol=1e-7)
 rosensol = solve(prob_rober, AutoVern7(Rodas5P(), lazy=false), reltol=1e-7, abstol=1e-7)
@@ -40,3 +41,5 @@ rosensol = solve(prob_rober, AutoVern7(Rodas5P(), lazy=false), reltol=1e-7, abst
 @test sol.stats.naccept == rosensol.stats.naccept
 @test sol.stats.nf == rosensol.stats.nf
 @test unique(sol.alg_choice) == [2,4]
+@test sol.alg_choice[1] == 2
+@test sol.alg_choice[end] == 4
