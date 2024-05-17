f46750aea4f77dedff69b7638c73ee095ea82cba
Alex Jones <alex.lemony@gmail.com>
2024-05-17 16:34:41 +0100

    test

---
+++ test/gpu/hermite_test.jl
@@ -0,0 +1,15 @@
+using ComponentArrays, CUDA, Adapt, RecursiveArrayTools, FastBroadcast, FillArrays, OrdinaryDiffEq, Test
+
+a = ComponentArray((a=rand(Float32, 5,5), b=rand(Float32, 5, 5)))
+a = adapt(CuArray, a)
+pa = ArrayPartition(a)
+pb = deepcopy(pa)
+pc = deepcopy(pa)
+pd = deepcopy(pa)
+pe = deepcopy(pa)
+k = [pd, pe]
+t = FillArrays.Trues(length(pa))
+
+OrdinaryDiffEq.hermite_interpolant!(pa, 0.1, 0.2, pb, pc, k, nothing, Val{0}, t) # if this doesnt error we're good
+
+@test pa.a != pb.a
