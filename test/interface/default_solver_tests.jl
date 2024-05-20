fc7600622d4340374ff4f8e659ecd2fd3f2752c9
oscarddssmith <oscar.smith@juliacomputing.com>
2024-05-20 13:53:23 -0400

    add inferred test

--- test/interface/default_solver_tests.jl
+++ test/interface/default_solver_tests.jl
@@ -3,7 +3,7 @@ using OrdinaryDiffEq, Test, LinearSolve, LinearAlgebra, SparseArrays
 f_2dlinear = (du, u, p, t) -> (@. du = p * u)
 
 prob_ode_2Dlinear = ODEProblem(f_2dlinear, rand(4, 2), (0.0, 1.0), 1.01)
-sol = solve(prob_ode_2Dlinear)
+sol = @inferred solve(prob_ode_2Dlinear)
 
 tsitsol = solve(prob_ode_2Dlinear, Tsit5())
 # test that default isn't much worse than Tsit5 (we expect it to use Tsit5 for this).
