f8f8307cf3717ae985c6ef165ef2bc8d60947bfc
termi-official <termi-official@users.noreply.github.com>
2024-05-24 16:26:25 +0200

    Update Rosenbrock method docs.

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -3118,10 +3118,15 @@ function GeneralRosenbrock(; chunk_size = Val{0}(), autodiff = true,
         _unwrap_val(standardtag), _unwrap_val(concrete_jac), typeof(tableau)}(tableau,
         factorization)
 end
+
+@doc rosenbrock_wanner_docstring(
 """
-RosenbrockW6S4OS: Rosenbrock-W Method
 A 4th order L-stable Rosenbrock-W method (fixed step only).
-"""
+""",
+"RosenbrockW6S4OS",
+references = """
+https://doi.org/10.1016/j.cam.2009.09.017
+""")
 struct RosenbrockW6S4OS{CS, AD, F, P, FDT, ST, CJ} <:
        OrdinaryDiffEqRosenbrockAlgorithm{CS, AD, FDT, ST, CJ}
     linsolve::F
