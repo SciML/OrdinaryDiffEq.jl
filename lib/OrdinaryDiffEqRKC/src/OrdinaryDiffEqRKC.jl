c7ae68de061a4389e5cef7437f8adff99ed99e21
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-19 15:00:03 +0530

    Fixes

--- lib/OrdinaryDiffEqRKC/src/OrdinaryDiffEqRKC.jl
+++ lib/OrdinaryDiffEqRKC/src/OrdinaryDiffEqRKC.jl
@@ -5,15 +5,16 @@ import OrdinaryDiffEq: alg_order, alg_maximum_order, get_current_adaptive_order,
                        default_controller, beta2_default, beta1_default, gamma_default,
                        initialize!, perform_step!, @unpack, unwrap_alg, isthreaded,
                        step_accept_controller!, calculate_residuals,
+                       OrdinaryDiffEqMutableCache, OrdinaryDiffEqConstantCache,
                        reset_alg_dependent_opts!, AbstractController,
                        step_accept_controller!, step_reject_controller!,
+                       OrdinaryDiffEqAdaptiveAlgorithm, OrdinaryDiffEqAdaptiveImplicitAlgorithm,
                        alg_cache, CompiledFloats, @threaded, stepsize_controller!, DEFAULT_PRECS,
                        constvalue, PolyesterThreads, Sequential, BaseThreads,
                        _digest_beta1_beta2, timedepentdtmin, _unwrap_val,
                        TimeDerivativeWrapper, UDerivativeWrapper, calc_J, _reshape, _vec,
                        WOperator, TimeGradientWrapper, UJacobianWrapper, build_grad_config,
-                       build_jac_config, calc_J!, jacobian2W!, dolinsolve
-                       
+                       build_jac_config, calc_J!, jacobian2W!, dolinsolve 
 using DiffEqBase, FastBroadcast, Polyester, MuladdMacro, RecursiveArrayTools, LinearSolve
 import OrdinaryDiffEq: ROCK2, ROCK4, RKC, ESERK4, ESERK5, SERK2, IRKC
 
