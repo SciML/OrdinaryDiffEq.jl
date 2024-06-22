e852657e027e707ec116ed3ce2d4dc65323a9dde
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-22 10:21:04 -0400

    Update rkc_tests.jl

--- lib/OrdinaryDiffEqStabilizedRK/test/rkc_tests.jl
+++ lib/OrdinaryDiffEqStabilizedRK/test/rkc_tests.jl
@@ -1,5 +1,5 @@
 using OrdinaryDiffEq, DiffEqDevTools, Test, LinearAlgebra, Random
-using OrdinaryDiffEq: maxeig!
+using OrdinaryDiffEq.OrdinaryDiffEqStabilizedRK: maxeig!
 import ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinear
 probArr = Vector{ODEProblem}(undef, 2)
 probArr[1] = prob_ode_linear
