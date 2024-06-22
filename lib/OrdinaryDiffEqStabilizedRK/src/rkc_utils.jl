7bb31ee5b04974b9ffbd42ea895180b21bd17182
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-22 11:03:24 -0400

    Update rkc_utils.jl

--- lib/OrdinaryDiffEqStabilizedRK/src/rkc_utils.jl
+++ lib/OrdinaryDiffEqStabilizedRK/src/rkc_utils.jl
@@ -2,28 +2,6 @@
 # (absolute value wise) by power iteration.
 const RKCAlgs = Union{RKC, IRKC, ESERK4, ESERK5, SERK2, ROCK2, ROCK4}
 
-alg_order(alg::ROCK2) = 2
-alg_order(alg::ROCK4) = 4
-
-alg_order(alg::ESERK4) = 4
-alg_order(alg::ESERK5) = 5
-alg_order(alg::SERK2) = 2
-
-alg_order(alg::IRKC) = 2
-alg_order(alg::RKC) = 2
-
-ispredictive(alg::Union{SERK2}) = alg.controller === :Predictive
-ispredictive(alg::Union{RKC}) = true
-
-alg_adaptive_order(alg::RKC) = 2
-alg_adaptive_order(alg::IRKC) = 1
-
-gamma_default(alg::RKC) = 8 // 10
-gamma_default(alg::IRKC) = 8 // 10
-
-alg_can_repeat_jac(alg::IRKC) = false
-
-
 function maxeig!(integrator, cache::OrdinaryDiffEqConstantCache)
     isfirst = integrator.iter == 1 || integrator.u_modified
     @unpack t, dt, uprev, u, f, p, fsalfirst = integrator
