acc104cd045828b3454da5d554b50eda3ea3fbc4
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-22 20:27:49 +0530

    Update rkc_caches.jl

--- lib/OrdinaryDiffEqStabilizedRK/src/rkc_caches.jl
+++ lib/OrdinaryDiffEqStabilizedRK/src/rkc_caches.jl
@@ -23,6 +23,16 @@ end
     constantcache::C
 end
 
+function build_nlsolver(alg, u, uprev, p, t, dt, f::F, rate_prototype,
+    ::Type{uEltypeNoUnits},
+    ::Type{uBottomEltypeNoUnits},
+    ::Type{tTypeNoUnits}, γ, c,
+    iip) where {F, uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+build_nlsolver(alg, u, uprev, p, t, dt, f, rate_prototype, uEltypeNoUnits,
+    uBottomEltypeNoUnits,
+    tTypeNoUnits, γ, c, 1, iip)
+end
+
 function alg_cache(alg::ROCK2, u, rate_prototype, ::Type{uEltypeNoUnits},
         ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
         dt, reltol, p, calck,
