7a54a84c7b82b982373c1ce37c791b7bb7317062
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-24 18:36:17 +0530

    Fixes

--- src/doc_utils.jl
+++ src/doc_utils.jl
@@ -85,6 +85,27 @@ function rosenbrock_docstring(description::String,
     )
 end
 
+function explicit_rk_docstring(description::String,
+    name::String;
+    references::String = "",
+    extra_keyword_description::String = "",
+    extra_keyword_default::String = "")
+keyword_default = """
+stage_limiter! = OrdinaryDiffEq.trivial_limiter!,
+step_limiter! = OrdinaryDiffEq.trivial_limiter!,
+""" * extra_keyword_default
+
+keyword_default_description = """
+- `stage_limiter!`: function of the form `limiter!(u, integrator, p, t)`
+- `step_limiter!`: function of the form `limiter!(u, integrator, p, t)`
+""" * extra_keyword_description
+
+generic_solver_docstring(
+    description, name, "Explicit Runge-Kutta Method. ", references,
+    keyword_default_description, keyword_default
+)
+end
+
 function rosenbrock_wanner_docstring(description::String,
         name::String;
         references::String = "",
