9e4257363f98138a0debf181a442ac29dd42f49e
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-24 09:34:40 +0530

    SSPRK

--- lib/OrdinaryDiffEqLowStorageRK/src/alg_utils.jl
+++ lib/OrdinaryDiffEqLowStorageRK/src/alg_utils.jl
@@ -1,19 +1,16 @@
-using OrdinaryDiffEq: ORK256, SSPRK53_2N1, SSPRK53_2N2, NDBLSRK124, CarpenterKennedy2N54, NDBLSRK134, NDBLSRK144,
+using OrdinaryDiffEq: ORK256, NDBLSRK124, CarpenterKennedy2N54, NDBLSRK134, NDBLSRK144,
                       CFRLDDRK64, TSLDDRK74, DGLDDRK73_C, DGLDDRK84_C, DGLDDRK84_F, SHLDDRK64, RK46NL, ParsaniKetchesonDeconinck3S32,
                       ParsaniKetchesonDeconinck3S82, ParsaniKetchesonDeconinck3S53, ParsaniKetchesonDeconinck3S173, ParsaniKetchesonDeconinck3S94,
                       ParsaniKetchesonDeconinck3S184, ParsaniKetchesonDeconinck3S105, ParsaniKetchesonDeconinck3S205, CKLLSRK43_2,
                       CKLLSRK54_3C, CKLLSRK95_4S, CKLLSRK95_4C, CKLLSRK95_4M, CKLLSRK54_3C_3R, CKLLSRK54_3M_3R, CKLLSRK54_3N_3R,
                       CKLLSRK85_4C_3R, CKLLSRK85_4M_3R, CKLLSRK85_4P_3R, CKLLSRK54_3N_4R, CKLLSRK54_3M_4R, CKLLSRK65_4M_4R,
                       CKLLSRK85_4FM_4R, CKLLSRK75_4M_5R, RDPK3Sp35, RDPK3SpFSAL35, RDPK3Sp49, RDPK3SpFSAL49, RDPK3Sp510,
-                      RDPK3SpFSAL510, SSPRK104, SSPRK54, SSPRK932, SSPRKMSVS32, SSPRKMSVS43, SSPRK432, SSPRK43, SSPRK83,
-                      SSPRK73, SSPRK63, SSPRK53_H, KYKSSPRK42, SSPRK53, SSPRK33, SSPRK22, KYK2014DGSSPRK_3S2, NDBLSRK134, 
+                      RDPK3SpFSAL510, KYKSSPRK42, KYK2014DGSSPRK_3S2, NDBLSRK134, 
                       SLDDRK64, SHLDDRK_2N, SHLDDRK52
 
 alg_order(alg::KYK2014DGSSPRK_3S2) = 2
 alg_order(alg::ORK256) = 2
 alg_order(alg::CarpenterKennedy2N54) = 4
-alg_order(alg::SSPRK53_2N1) = 3
-alg_order(alg::SSPRK53_2N2) = 3
 alg_order(alg::NDBLSRK124) = 4
 alg_order(alg::NDBLSRK134) = 4
 alg_order(alg::NDBLSRK144) = 4
@@ -55,28 +52,12 @@ alg_order(alg::CKLLSRK75_4M_5R) = 5
 alg_order(alg::CKLLSRK54_3M_4R) = 4
 alg_order(alg::CKLLSRK65_4M_4R) = 5
 
-alg_order(alg::SSPRK22) = 2
-alg_order(alg::SSPRKMSVS32) = 2
-alg_order(alg::SSPRK33) = 3
 alg_order(alg::KYKSSPRK42) = 2
-alg_order(alg::SSPRK53) = 3
-alg_order(alg::SSPRK53_H) = 3
-alg_order(alg::SSPRK63) = 3
-alg_order(alg::SSPRK73) = 3
-alg_order(alg::SSPRK83) = 3
-alg_order(alg::SSPRK43) = 3
-alg_order(alg::SSPRK432) = 3
-alg_order(alg::SSPRKMSVS43) = 3
-alg_order(alg::SSPRK932) = 3
-alg_order(alg::SSPRK54) = 4
-alg_order(alg::SSPRK104) = 4
 alg_order(alg::SHLDDRK52) = 2
 alg_order(alg::SHLDDRK_2N) = 4
 
 isfsal(alg::ORK256) = false
 isfsal(alg::CarpenterKennedy2N54) = false
-isfsal(alg::SSPRK53_2N1) = false
-isfsal(alg::SSPRK53_2N2) = false
 isfsal(alg::DGLDDRK84_F) = false
 isfsal(alg::DGLDDRK73_C) = false
 isfsal(alg::DGLDDRK84_C) = false
@@ -87,19 +68,6 @@ isfsal(alg::SHLDDRK64) = false
 isfsal(alg::NDBLSRK134) = false
 isfsal(alg::NDBLSRK124) = false
 isfsal(alg::NDBLSRK144) = false
-
-isfsal(alg::SSPRK22) = false
-isfsal(alg::SSPRK33) = false
-isfsal(alg::SSPRK53) = false
-isfsal(alg::SSPRK53_H) = false
-isfsal(alg::SSPRK63) = false
-isfsal(alg::SSPRK73) = false
-isfsal(alg::SSPRK83) = false
-isfsal(alg::SSPRK43) = false
-isfsal(alg::SSPRK432) = false
-isfsal(alg::SSPRK932) = false
-isfsal(alg::SSPRK54) = false
-isfsal(alg::SSPRK104) = false
 
 uses_uprev(alg::ORK256, adaptive::Bool) = false
 uses_uprev(alg::SHLDDRK64, adaptive::Bool) = false
@@ -144,24 +112,8 @@ julia> ssp_coefficient(SSPRK104())
 ```
 """
 
-ssp_coefficient(alg::SSPRK53_2N1) = 2.18
-ssp_coefficient(alg::SSPRK53_2N2) = 2.148
-ssp_coefficient(alg::SSPRK53) = 2.65
-ssp_coefficient(alg::SSPRK53_H) = 2.65
-ssp_coefficient(alg::SSPRK63) = 3.518
-ssp_coefficient(alg::SSPRK73) = 4.2879
-ssp_coefficient(alg::SSPRK83) = 5.107
-ssp_coefficient(alg::SSPRK43) = 2
-ssp_coefficient(alg::SSPRK432) = 2
-ssp_coefficient(alg::SSPRKMSVS32) = 0.5
 ssp_coefficient(alg::KYKSSPRK42) = 2.459
-ssp_coefficient(alg::SSPRKMSVS43) = 0.33
-ssp_coefficient(alg::SSPRK932) = 6
-ssp_coefficient(alg::SSPRK54) = 1.508
-ssp_coefficient(alg::SSPRK104) = 6
 ssp_coefficient(alg::KYK2014DGSSPRK_3S2) = 0.8417
-ssp_coefficient(alg::SSPRK33) = 1
-ssp_coefficient(alg::SSPRK22) = 1
 
 function default_controller(alg::RDPK3Sp35, cache, qoldinit, args...)
     QT = typeof(qoldinit)
