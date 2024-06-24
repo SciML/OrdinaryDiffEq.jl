b636cb4d3a4be5e81fbf47962744013c758cb10c
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-24 09:20:01 +0530

    RK46NL

--- lib/OrdinaryDiffEqLowStorageRK/src/low_storage_rk_caches.jl
+++ lib/OrdinaryDiffEqLowStorageRK/src/low_storage_rk_caches.jl
@@ -118,6 +118,83 @@ struct RK46NLConstantCache{T, T2} <: OrdinaryDiffEqConstantCache
     end
 end
 
+@cache struct KYK2014DGSSPRK_3S2_Cache{uType, rateType, TabType, StageLimiter, StepLimiter,
+    Thread} <:
+              OrdinaryDiffEqMutableCache
+    u::uType
+    uprev::uType
+    k::rateType
+    fsalfirst::rateType
+    tab::TabType
+    #temporary values for Shu-Osher
+    u_1::uType
+    u_2::uType
+    kk_1::rateType
+    kk_2::rateType
+    stage_limiter!::StageLimiter
+    step_limiter!::StepLimiter
+    thread::Thread
+end
+
+struct KYK2014DGSSPRK_3S2_ConstantCache{T, T2} <: OrdinaryDiffEqConstantCache
+    #These are not α and β for RK but for Shu-Osher
+    #see top of page 317 in
+    #Optimal Strong-Stability-Preserving Runge–Kutta Time Discretizations for
+    #Discontinuous Garlekin Methods, Kubatko, Yaeger, Ketcheson 2014
+    α_10::T
+    α_20::T
+    α_21::T
+    α_30::T
+    α_32::T
+    β_10::T
+    β_21::T
+    β_30::T
+    β_32::T
+    #Shu-Osher is normally stated for autonomous systems, the times
+    #are calculated by hand for this scheme
+    c_1::T
+    c_2::T
+
+    function KYK2014DGSSPRK_3S2_ConstantCache(T, T2)
+        α_10 = T(1.0)
+        α_20 = T(0.087353119859156)
+        α_21 = T(0.912646880140844)
+        α_30 = T(0.344956917166841)
+        α_32 = T(0.655043082833159)
+        β_10 = T(0.528005024856522)
+        β_21 = T(0.481882138633993)
+        β_30 = T(0.022826837460491)
+        β_32 = T(0.345866039233415)
+        c_1 = β_10
+        c_2 = α_21 * β_10 + β_21 # ==0.96376427726
+        new{T, T2}(α_10, α_20, α_21, α_30, α_32, β_10, β_21, β_30, β_32, c_1, c_2)
+    end
+end
+
+function alg_cache(alg::KYK2014DGSSPRK_3S2, u, rate_prototype, ::Type{uEltypeNoUnits},
+    ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+    dt, reltol, p, calck,
+    ::Val{true}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+u_1 = zero(u)
+u_2 = zero(u)
+kk_1 = zero(rate_prototype)
+kk_2 = zero(rate_prototype)
+k = zero(rate_prototype)
+fsalfirst = zero(rate_prototype)
+tab = KYK2014DGSSPRK_3S2_ConstantCache(constvalue(uBottomEltypeNoUnits),
+    constvalue(tTypeNoUnits))
+KYK2014DGSSPRK_3S2_Cache(u, uprev, k, fsalfirst, tab, u_1, u_2, kk_1, kk_2,
+    alg.stage_limiter!, alg.step_limiter!, alg.thread)
+end
+
+function alg_cache(alg::KYK2014DGSSPRK_3S2, u, rate_prototype, ::Type{uEltypeNoUnits},
+    ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+    dt, reltol, p, calck,
+    ::Val{false}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+KYK2014DGSSPRK_3S2_ConstantCache(constvalue(uBottomEltypeNoUnits),
+    constvalue(tTypeNoUnits))
+end
+
 function alg_cache(alg::RK46NL, u, rate_prototype, ::Type{uEltypeNoUnits},
     ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
     dt, reltol, p, calck,
