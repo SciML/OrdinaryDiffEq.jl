86dd0652100d5b608a9d8ff06f4ba8742345c412
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-06-24 16:11:34 +0530

    Update lib/OrdinaryDiffEqLowStorageRK/src/OrdinaryDiffEqLowStorageRK.jl
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- lib/OrdinaryDiffEqLowStorageRK/src/OrdinaryDiffEqLowStorageRK.jl
+++ lib/OrdinaryDiffEqLowStorageRK/src/OrdinaryDiffEqLowStorageRK.jl
@@ -12,25 +12,6 @@ import OrdinaryDiffEq: alg_order, alg_adaptive_order, calculate_residuals!,
 using DiffEqBase, FastBroadcast, Polyester, MuladdMacro, RecursiveArrayTools, LinearSolve
 import StaticArrays: SArray, MVector, SVector, @SVector, StaticArray, MMatrix, SA
 
-macro cache(expr)
-    name = expr.args[2].args[1].args[1]
-    fields = [x for x in expr.args[3].args if typeof(x) != LineNumberNode]
-    cache_vars = Expr[]
-    jac_vars = Pair{Symbol, Expr}[]
-    for x in fields
-        if x.args[2] == :uType || x.args[2] == :rateType ||
-           x.args[2] == :kType || x.args[2] == :uNoUnitsType
-            push!(cache_vars, :(c.$(x.args[1])))
-        elseif x.args[2] == :DiffCacheType
-            push!(cache_vars, :(c.$(x.args[1]).du))
-            push!(cache_vars, :(c.$(x.args[1]).dual_du))
-        end
-    end
-    quote
-        $(esc(expr))
-        $(esc(:full_cache))(c::$name) = tuple($(cache_vars...))
-    end
-end
 
 include("low_storage_rk_caches.jl")
 include("low_storage_rk_perform_step.jl")
