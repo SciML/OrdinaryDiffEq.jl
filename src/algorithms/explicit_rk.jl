ee12ea4577a3c5c91b4610838c36abd70eaada39
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-27 17:55:20 +0530

    Feagin

--- src/algorithms/explicit_rk.jl
+++ src/algorithms/explicit_rk.jl
@@ -406,47 +406,6 @@ function TsitPap8(stage_limiter!, step_limiter! = trivial_limiter!)
     TsitPap8(stage_limiter!, step_limiter!, False())
 end
 
-"""
-@article{feagin2012high,
-title={High-order explicit Runge-Kutta methods using m-symmetry},
-author={Feagin, Terry},
-year={2012},
-publisher={Neural, Parallel \\& Scientific Computations}
-}
-
-Feagin10: Explicit Runge-Kutta Method
-Feagin's 10th-order Runge-Kutta method.
-"""
-Base.@kwdef struct Feagin10{StepLimiter} <: OrdinaryDiffEqAdaptiveAlgorithm
-    step_limiter!::StepLimiter = trivial_limiter!
-end
-
-"""
-@article{feagin2012high,
-title={High-order explicit Runge-Kutta methods using m-symmetry},
-author={Feagin, Terry},
-year={2012},
-publisher={Neural, Parallel \\& Scientific Computations}
-}
-
-Feagin12: Explicit Runge-Kutta Method
-Feagin's 12th-order Runge-Kutta method.
-"""
-Base.@kwdef struct Feagin12{StepLimiter} <: OrdinaryDiffEqAdaptiveAlgorithm
-    step_limiter!::StepLimiter = trivial_limiter!
-end
-
-"""
-Feagin, T., “An Explicit Runge-Kutta Method of Order Fourteen,” Numerical
-Algorithms, 2009
-
-Feagin14: Explicit Runge-Kutta Method
-Feagin's 14th-order Runge-Kutta method.
-"""
-Base.@kwdef struct Feagin14{StepLimiter} <: OrdinaryDiffEqAdaptiveAlgorithm
-    step_limiter!::StepLimiter = trivial_limiter!
-end
-
 @doc explicit_rk_docstring("Zero Dissipation Runge-Kutta of 6th order.", "FRK65",
     extra_keyword_description = """- `omega`: a periodicity phase estimate,
                                    when accurate this method results in zero numerical dissipation.
--- test/runtests.jl
+++ test/runtests.jl
@@ -53,6 +53,12 @@ function activate_ssprk()
     Pkg.instantiate()
 end
 
+function activate_feagin()
+    Pkg.activate("../lib/OrdinaryDiffEqFeagin")
+    Pkg.develop(PackageSpec(path = dirname(@__DIR__)))
+    Pkg.instantiate()
+end
+
 #Start Test Script
 
 @time begin
@@ -187,13 +193,16 @@ end
         @time @safetestset "FIRK Tests" include("algconvergence/ode_firk_tests.jl")
         @time @safetestset "Linear-Nonlinear Methods Tests" include("algconvergence/linear_nonlinear_convergence_tests.jl")
         @time @safetestset "Linear-Nonlinear Krylov Methods Tests" include("algconvergence/linear_nonlinear_krylov_tests.jl")
-        @time @safetestset "Feagin Tests" include("algconvergence/ode_feagin_tests.jl")
         @time @safetestset "Symplectic Tests" include("algconvergence/symplectic_tests.jl")
         @time @safetestset "Quadruple precision Runge-Kutta Tests" include("algconvergence/ode_quadruple_precision_tests.jl")
     end
 
     if !is_APPVEYOR && GROUP == "Extrapolation"
         @time @safetestset "Extrapolation Tests" include("../lib/OrdinaryDiffEqExtrapolation/test/runtests.jl")
+    end
+
+    if !is_APPVEYOR && GROUP == "Feagin"
+        @time @safetestset "Feagin Tests" include("../lib/OrdinaryDiffEqFeagin/test/ode_feagin_tests.jl")
     end
 
     if !is_APPVEYOR && GROUP == "StabilizedRK"
