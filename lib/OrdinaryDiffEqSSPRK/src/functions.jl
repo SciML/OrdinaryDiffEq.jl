4422396ae3bb7bd438b5d3b935231057de089c3b
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-28 20:07:37 +0530

    Changes

--- lib/OrdinaryDiffEqSSPRK/src/functions.jl
+++ lib/OrdinaryDiffEqSSPRK/src/functions.jl
@@ -47,3 +47,11 @@ end
     cache::OrdinaryDiffEqConstantCache)
     nothing
 end
+
+@inline function DiffEqBase.get_tmp_cache(integrator,
+    alg::Union{SSPRK22, SSPRK33, SSPRK53_2N1,
+        SSPRK53_2N2, SSPRK43, SSPRK432,
+        SSPRK932},
+    cache::OrdinaryDiffEqMutableCache)
+(cache.k,)
+end
--- src/integrators/integrator_interface.jl
+++ src/integrators/integrator_interface.jl
@@ -142,13 +142,7 @@ end
         cache::OrdinaryDiffEqMutableCache)
     (cache.tmp, cache.linsolve_tmp)
 end
-@inline function DiffEqBase.get_tmp_cache(integrator,
-        alg::Union{SSPRK22, SSPRK33, SSPRK53_2N1,
-            SSPRK53_2N2, SSPRK43, SSPRK432,
-            SSPRK932},
-        cache::OrdinaryDiffEqMutableCache)
-    (cache.k,)
-end
+
 @inline function DiffEqBase.get_tmp_cache(integrator,
         alg::OrdinaryDiffEqAdaptiveExponentialAlgorithm,
         cache::OrdinaryDiffEqMutableCache)
