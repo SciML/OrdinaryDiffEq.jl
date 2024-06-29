067f29f858c37d974d8f4c0bd6c1a2393b93e9b9
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-30 03:26:27 +0800

    Update src/OrdinaryDiffEq.jl

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -419,7 +419,7 @@ export Rosenbrock23, Rosenbrock32, RosShamp4, Veldd4, Velds4, GRK4T, GRK4A,
        Ros4LStab, ROS3P, Rodas3, Rodas23W, Rodas3P, Rodas4, Rodas42, Rodas4P, Rodas4P2,
        Rodas5, Rodas5P, Rodas5Pe, Rodas5Pr,
        RosenbrockW6S4OS, ROS34PW1a, ROS34PW1b, ROS34PW2, ROS34PW3, ROS34PRw, ROS3PRL,
-       ROS3PRL2, ROK4a
+       ROS3PRL2, ROK4a,
        ROS2, ROS2PR, ROS2S, ROS3, ROS3PR, Scholz4_7
 
 export LawsonEuler, NorsettEuler, ETD1, ETDRK2, ETDRK3, ETDRK4, HochOst4, Exp4, EPIRK4s3A,
