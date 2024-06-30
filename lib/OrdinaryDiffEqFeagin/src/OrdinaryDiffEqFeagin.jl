1cde4b36b55e1cd7a3838cf8e0e4cc1b649c6b9b
ParamThakkar123 <paramthakkar864@gmail.com>
2024-06-30 09:54:42 +0530

    Feagin10ConstantCache

--- lib/OrdinaryDiffEqFeagin/src/OrdinaryDiffEqFeagin.jl
+++ lib/OrdinaryDiffEqFeagin/src/OrdinaryDiffEqFeagin.jl
@@ -19,9 +19,9 @@ using Static: False
 
 include("algorithms.jl")
 include("alg_utils.jl")
+include("feagin_tableaus.jl")
 include("feagin_caches.jl")
 include("feagin_rk_perform_step.jl")
-include("feagin_tableaus.jl")
 
 export Feagin10, Feagin12, Feagin14
 
