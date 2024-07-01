9b849e5a82b4ea1bd22cbd5a7ae95769a7708a7f
ParamThakkar123 <paramthakkar864@gmail.com>
2024-07-01 07:06:17 +0530

    Symplectic2ConstantCache

--- lib/OrdinaryDiffEqSymplecticRK/src/OrdinaryDiffEqSymplecticRK.jl
+++ lib/OrdinaryDiffEqSymplecticRK/src/OrdinaryDiffEqSymplecticRK.jl
@@ -16,8 +16,8 @@ using DiffEqBase, FastBroadcast, Polyester, MuladdMacro, RecursiveArrayTools
 include("algorithms.jl")
 include("alg_utils.jl")
 include("symplectic_caches.jl")
-include("symplectic_perform_step.jl")
 include("symplectic_tableaus.jl")
+include("symplectic_perform_step.jl")
 
 export SymplecticEuler, VelocityVerlet, VerletLeapfrog, PseudoVerletLeapfrog,
        McAte2, Ruth3, McAte3, CandyRoz4, McAte4, McAte42, McAte5,
