10534f88001909e75c07608d8d6775038678a0c4
ParamThakkar123 <paramthakkar864@gmail.com>
2024-07-01 06:58:57 +0530

    Changes

--- lib/OrdinaryDiffEqSymplecticRK/src/OrdinaryDiffEqSymplecticRK.jl
+++ lib/OrdinaryDiffEqSymplecticRK/src/OrdinaryDiffEqSymplecticRK.jl
@@ -1,8 +1,16 @@
 module OrdinaryDiffEqSymplecticRK
 
-import OrdinaryDiffEq: OrdinaryDiffEqPartitionedAlgorithm, OrdinaryDiffEqMutableCache, OrdinaryDiffEqConstantCache,
-                       @cache, @unpack
-import MuladdMacro: @muladd
+import OrdinaryDiffEq: alg_order, calculate_residuals!,
+                       initialize!, perform_step!, @unpack, unwrap_alg,
+                       calculate_residuals,
+                       OrdinaryDiffEqAlgorithm,
+                       OrdinaryDiffEqMutableCache, OrdinaryDiffEqConstantCache,
+                       OrdinaryDiffEqAdaptiveAlgorithm, CompiledFloats, uses_uprev,
+                       alg_cache, _vec, _reshape, @cache, isfsal, full_cache,
+                       constvalue, _unwrap_val, du_alias_or_new,
+                       explicit_rk_docstring, trivial_limiter!,
+                       _ode_interpolant!, _ode_addsteps!
+using DiffEqBase, FastBroadcast, Polyester, MuladdMacro, RecursiveArrayTools
 
 
 include("algorithms.jl")
