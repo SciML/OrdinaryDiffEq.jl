97456ecd05ae9a28efbd7130e6380733484c8e0c
ParamThakkar123 <paramthakkar864@gmail.com>
2024-07-02 11:25:52 +0530

    RKN Methods

---
+++ lib/OrdinaryDiffEqRKN/src/OrdinaryDiffEqRKN.jl
@@ -0,0 +1,26 @@
+module OrdinaryDiffEqRKN
+
+import OrdinaryDiffEq: alg_order, calculate_residuals!,
+                       initialize!, perform_step!, @unpack, unwrap_alg,
+                       calculate_residuals,
+                       OrdinaryDiffEqAlgorithm,
+                       OrdinaryDiffEqMutableCache, OrdinaryDiffEqConstantCache,
+                       OrdinaryDiffEqAdaptivePartitionedAlgorithm,
+                       OrdinaryDiffEqAdaptiveAlgorithm, CompiledFloats, uses_uprev,
+                       alg_cache, _vec, _reshape, @cache, isfsal, full_cache,
+                       constvalue, _unwrap_val, du_alias_or_new,
+                       trivial_limiter!, _ode_interpolant!, _ode_addsteps!
+using DiffEqBase, FastBroadcast, Polyester, MuladdMacro, RecursiveArrayTools
+
+include("algorithms.jl")
+include("alg_utils.jl")
+include("rkn_tableaus.jl")
+include("rkn_caches.jl")
+include("rkn_perform_step.jl")
+
+export Nystrom4, FineRKN4, FineRKN5, Nystrom4VelocityIndependent,
+       Nystrom5VelocityIndependent,
+       IRKN3, IRKN4, DPRKN4, DPRKN5, DPRKN6, DPRKN6FM, DPRKN8, DPRKN12, ERKN4, ERKN5, ERKN7,
+       RKN4
+
+end
