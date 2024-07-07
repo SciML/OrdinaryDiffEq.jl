2018ec12d5d6b0b4e5e24e261d83a922619af858
ParamThakkar123 <paramthakkar864@gmail.com>
2024-07-07 06:50:55 +0530

    AutoSwitch

--- lib/OrdinaryDiffEqDefault/src/OrdinaryDiffEqDefault.jl
+++ lib/OrdinaryDiffEqDefault/src/OrdinaryDiffEqDefault.jl
@@ -3,6 +3,7 @@ module OrdinaryDiffEqDefault
 using OrdinaryDiffEq: Vern7, Vern8, Vern9, Vern6, Tsit5, Rosenbrock23, Rodas5P, FBDF, Krylov, KrylovFBDF
 
 include("composite_algs.jl")
+include("AutoSwitch.jl")
 include("integrators/integrator_interface.jl")
 
 end # module OrdinaryDiffEqDefault
