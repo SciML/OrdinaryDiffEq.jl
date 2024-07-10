49d65ec16da064f5b63c7228b2672c14ce1019dd
ParamThakkar123 <paramthakkar864@gmail.com>
2024-07-10 08:53:53 +0530

    Rosenbrock23

--- lib/OrdinaryDiffEqDefault/src/OrdinaryDiffEqDefault.jl
+++ lib/OrdinaryDiffEqDefault/src/OrdinaryDiffEqDefault.jl
@@ -1,8 +1,9 @@
 module OrdinaryDiffEqDefault
 
-using OrdinaryDiffEq: Vern7, Vern8, Vern9, Vern6, Tsit5, Rosenbrock23, Rodas5P, FBDF,
+using OrdinaryDiffEq: Vern7, Vern8, Vern9, Vern6, Tsit5, Rodas5P, FBDF,
     alg_stability_size, beta2_default, beta1_default, AutoSwitchCache, ODEIntegrator,
     CompositeAlgorithm, OrdinaryDiffEqAlgorithm, OrdinaryDiffEqMutableCache, AutoAlgSwitch
+using OrdinaryDiffEqRosenbrock
 import OrdinaryDiffEq: is_mass_matrix_alg, default_autoswitch
 import LinearSolve
 using LinearAlgebra: I, isdiag
