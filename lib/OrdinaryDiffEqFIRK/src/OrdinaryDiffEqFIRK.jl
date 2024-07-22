b018418bf1ea848df4e549f31c118d8d50f270df
ParamThakkar123 <paramthakkar864@gmail.com>
2024-07-22 11:30:41 +0530

    LinearSolve

--- lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
+++ lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
@@ -13,6 +13,7 @@ import OrdinaryDiffEq: alg_order, calculate_residuals!,
                        NLStatus, qmax_default, alg_adaptive_order, DEFAULT_PRECS,
                        UJacobianWrapper, build_J_W, build_jac_config
 using MuladdMacro, DiffEqBase, RecursiveArrayTools
+import LinearSolve
 import FastBroadcast: @..
 include("algorithms.jl")
 include("alg_utils.jl")
