7856169c327ac21fbdff86b4547b47fb6e7f3e34
ParamThakkar123 <paramthakkar864@gmail.com>
2024-07-22 13:26:36 +0530

    calc_J!

--- lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
+++ lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
@@ -12,7 +12,7 @@ import OrdinaryDiffEq: alg_order, calculate_residuals!,
                        _ode_interpolant!, _ode_addsteps!, AbstractController,
                        NLStatus, qmax_default, alg_adaptive_order, DEFAULT_PRECS,
                        UJacobianWrapper, build_J_W, build_jac_config, UDerivativeWrapper,
-                       Convergence
+                       Convergence, calc_J!
 using MuladdMacro, DiffEqBase, RecursiveArrayTools
 using SciMLOperators: AbstractSciMLOperator
 import LinearSolve
