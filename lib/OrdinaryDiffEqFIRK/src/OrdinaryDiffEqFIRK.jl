d84437bbcf70b3fca592421955b1855d6be653a8
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-07-23 17:03:48 -0400

    Update lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl

--- lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
+++ lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
@@ -16,7 +16,7 @@ import OrdinaryDiffEq: alg_order, calculate_residuals!,
                        stepsize_controller!, islinearfunction
 using MuladdMacro, DiffEqBase, RecursiveArrayTools
 using SciMLOperators: AbstractSciMLOperator
-using LinearAlgebra: I, UniformScaling
+using LinearAlgebra: I, UniformScaling, mul!, lu
 import LinearSolve
 import FastBroadcast: @..
 include("algorithms.jl")
