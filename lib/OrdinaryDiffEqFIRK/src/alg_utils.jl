335bc8b73eead769c23b618e2cd2d2fce7efe445
Shreyas-Ekanathan <142109039+Shreyas-Ekanathan@users.noreply.github.com>
2024-07-26 11:58:10 -0400

    Update alg_utils.jl

--- lib/OrdinaryDiffEqFIRK/src/alg_utils.jl
+++ lib/OrdinaryDiffEqFIRK/src/alg_utils.jl
@@ -2,12 +2,12 @@ qmax_default(alg::Union{RadauIIA3, RadauIIA5, RadauIIA7}) = 8
 
 alg_order(alg::RadauIIA3) = 3
 alg_order(alg::RadauIIA5) = 5
-alg_order(alg::RadauIIA7) = 7
+alg_order(alg::RadauIIA9) = 9
 
 isfirk(alg::RadauIIA3) = true
 isfirk(alg::RadauIIA5) = true
-isfirk(alg::RadauIIA7) = true
+isfirk(alg::RadauIIA9) = true
 
 alg_adaptive_order(alg::RadauIIA3) = 1
 alg_adaptive_order(alg::RadauIIA5) = 3
-alg_adaptive_order(alg::RadauIIA7) = 5
+alg_adaptive_order(alg::RadauIIA9) = 7
