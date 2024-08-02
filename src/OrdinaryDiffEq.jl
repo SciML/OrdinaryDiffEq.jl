61732021c4bb62d98f8f32335ebcfdf9d42a04a3
ParamThakkar123 <paramthakkar864@gmail.com>
2024-08-02 20:20:54 +0530

    Update

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -250,6 +250,15 @@ include("../lib/OrdinaryDiffEqTsit5/src/OrdinaryDiffEqTsit5.jl")
 using ..OrdinaryDiffEqTsit5
 export Tsit5
 
+include("../lib/OrdinaryDiffEqRosenbrock/src/OrdinaryDiffEqRosenbrock.jl")
+using ..OrdinaryDiffEqRosenbrock
+export Rosenbrock23, Rosenbrock32, RosShamp4, Veldd4, Velds4, GRK4T, GRK4A,
+       Ros4LStab, ROS3P, Rodas3, Rodas23W, Rodas3P, Rodas4, Rodas42, Rodas4P, Rodas4P2,
+       Rodas5, Rodas5P, Rodas5Pe, Rodas5Pr,
+       RosenbrockW6S4OS, ROS34PW1a, ROS34PW1b, ROS34PW2, ROS34PW3, ROS34PRw, ROS3PRL,
+       ROS3PRL2, ROK4a,
+       ROS2, ROS2PR, ROS2S, ROS3, ROS3PR, Scholz4_7
+
 include("../lib/OrdinaryDiffEqDefault/src/OrdinaryDiffEqDefault.jl")
 using ..OrdinaryDiffEqDefault
 export DefaultODEAlgorithm
@@ -294,15 +303,6 @@ export AB3, AB4, AB5, ABM32, ABM43, ABM54, VCAB3,
 include("../lib/OrdinaryDiffEqNordsieck/src/OrdinaryDiffEqNordsieck.jl")
 using ..OrdinaryDiffEqNordsieck
 export AN5, JVODE, JVODE_Adams, JVODE_BDF
-
-include("../lib/OrdinaryDiffEqRosenbrock/src/OrdinaryDiffEqRosenbrock.jl")
-using ..OrdinaryDiffEqRosenbrock
-export Rosenbrock23, Rosenbrock32, RosShamp4, Veldd4, Velds4, GRK4T, GRK4A,
-       Ros4LStab, ROS3P, Rodas3, Rodas23W, Rodas3P, Rodas4, Rodas42, Rodas4P, Rodas4P2,
-       Rodas5, Rodas5P, Rodas5Pe, Rodas5Pr,
-       RosenbrockW6S4OS, ROS34PW1a, ROS34PW1b, ROS34PW2, ROS34PW3, ROS34PRw, ROS3PRL,
-       ROS3PRL2, ROK4a,
-       ROS2, ROS2PR, ROS2S, ROS3, ROS3PR, Scholz4_7
 
 include("../lib/OrdinaryDiffEqExplicitRK/src/OrdinaryDiffEqExplicitRK.jl")
 using ..OrdinaryDiffEqExplicitRK
