984bce70764b0d5cb779d91d316e873518bc68a9
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-03 11:01:26 -0400

    Update lib/OrdinaryDiffEqNordsieck/src/OrdinaryDiffEqNordsieck.jl

--- lib/OrdinaryDiffEqNordsieck/src/OrdinaryDiffEqNordsieck.jl
+++ lib/OrdinaryDiffEqNordsieck/src/OrdinaryDiffEqNordsieck.jl
@@ -4,7 +4,7 @@ import OrdinaryDiffEq: alg_order, alg_adaptive_order, qsteady_max_default, get_c
                        AbstractController, OrdinaryDiffEqAdaptiveAlgorithm, OrdinaryDiffEqAdamsVarOrderVarStepAlgorithm,
                        alg_cache, OrdinaryDiffEqMutableCache, OrdinaryDiffEqConstantCache, initialize!, @unpack,
                        initialize!, perform_step!, stepsize_controller!, step_accept_controller!, step_reject_controller!,
-                       calculate_residuals, calculate_residuals!, Tsit5ConstantCache
+                       calculate_residuals, calculate_residuals!, Tsit5ConstantCache, get_current_adaptive_order, ode_interpolant, ode_interpolant!
 using MuladdMacro, FastBroadcast, RecursiveArrayTools
 
 include("algorithms.jl")
