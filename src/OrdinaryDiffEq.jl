4ff49d930982b70926abcf9d4ab3d0c2fb4b1e90
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-03 04:54:03 -0400

    Fix ABM import

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -293,6 +293,7 @@ export Euler, SplitEuler, Heun, Ralston, Midpoint, RK4,
        DP5, Anas5, RKO65, FRK65, RKM, MSRK5, MSRK6,
        PSRK4p7q6, PSRK3p5q4, PSRK3p6q5, Stepanov5, SIR54,
        Alshina2, Alshina3, Alshina6
+using ..OrdinaryDiffEqLowOrderRK: BS3Cache, BS3ConstantCache
 
 include("../lib/OrdinaryDiffEqFunctionMap/src/OrdinaryDiffEqFunctionMap.jl")
 using ..OrdinaryDiffEqFunctionMap
