09ee69760f3978fd1976ca8e328f721bab042647
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-02 22:20:27 -0400

    should be the last one

--- lib/OrdinaryDiffEqRosenbrock/src/OrdinaryDiffEqRosenbrock.jl
+++ lib/OrdinaryDiffEqRosenbrock/src/OrdinaryDiffEqRosenbrock.jl
@@ -8,7 +8,7 @@ import OrdinaryDiffEq: alg_order, alg_adaptive_order, isWmethod, isfsal, _unwrap
                        OrdinaryDiffEqRosenbrockAlgorithm, generic_solver_docstring, namify, initialize!, perform_step!,
                        constvalue, TimeDerivativeWrapper, TimeGradientWrapper, UDerivativeWrapper, UJacobianWrapper,
                        wrapprecs, alg_autodiff, calc_tderivative, build_grad_config, build_jac_config,
-                       issuccess_W
+                       issuccess_W, calculate_residuals
 using TruncatedStacktraces, MuladdMacro, FastBroadcast, DiffEqBase, RecursiveArrayTools
 import MacroTools
 using MacroTools: @capture
