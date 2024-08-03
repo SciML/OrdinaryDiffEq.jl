d5aad61124717c3bc4193368db2024a96cc052fa
ParamThakkar123 <paramthakkar864@gmail.com>
2024-08-03 09:12:37 +0530

    calculate_residuals

--- lib/OrdinaryDiffEqExplicitRK/src/OrdinaryDiffEqExplicitRK.jl
+++ lib/OrdinaryDiffEqExplicitRK/src/OrdinaryDiffEqExplicitRK.jl
@@ -3,7 +3,7 @@ module OrdinaryDiffEqExplicitRK
 import OrdinaryDiffEq: alg_order, alg_adaptive_order, alg_stability_size, OrdinaryDiffEqAdaptiveAlgorithm,
                        @cache, alg_cache, OrdinaryDiffEqConstantCache, @unpack, unwrap_alg, 
                        OrdinaryDiffEqMutableCache, initialize!, perform_step!, isfsal,
-                       CompositeAlgorithm
+                       CompositeAlgorithm, calculate_residuals!, calculate_residuals
 using TruncatedStacktraces, RecursiveArrayTools, FastBroadcast, MuladdMacro, DiffEqBase
 
 include("algorithms.jl")
