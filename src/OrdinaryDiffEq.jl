4f4640a6071878760e5f11ff20223021d46ac1b6
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-06 09:10:20 -0400

    more imports

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -148,7 +148,7 @@ using ..OrdinaryDiffEqDifferentiation: _alg_autodiff, resize_grad_config!
 
 include("../lib/OrdinaryDiffEqNonlinearSolve/src/OrdinaryDiffEqNonlinearSolve.jl")
 using ..OrdinaryDiffEqNonlinearSolve
-using ..OrdinaryDiffEqNonlinearSolve: NLNewton, NLAnderson, NLFunctional, nlsolvefail, initial_η, NonlinearSolveAlg, compute_step!, NLSolver, nlsolve!, resize_jac_config!
+using ..OrdinaryDiffEqNonlinearSolve: NLNewton, NLAnderson, NLFunctional, nlsolvefail, initial_η, NonlinearSolveAlg, compute_step!, NLSolver, nlsolve!, resize_jac_config!, aderson!
 
 include("../lib/OrdinaryDiffEqExtrapolation/src/OrdinaryDiffEqExtrapolation.jl")
 using ..OrdinaryDiffEqExtrapolation
