5327e46764b323c376046c503f0b1d0cc0840755
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-06 04:23:40 -0400

    fix exports for downstream

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -144,7 +144,7 @@ include("interp_func.jl")
 
 include("../lib/OrdinaryDiffEqDifferentiation/src/OrdinaryDiffEqDifferentiation.jl")
 import ..OrdinaryDiffEqDifferentiation
-using ..OrdinaryDiffEqDifferentiation: _alg_autodiff
+using ..OrdinaryDiffEqDifferentiation: _alg_autodiff, resize_grad_config!
 
 include("../lib/OrdinaryDiffEqNonlinearSolve/src/OrdinaryDiffEqNonlinearSolve.jl")
 using ..OrdinaryDiffEqNonlinearSolve
