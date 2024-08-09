586d8b3abaa6e01597981d49e32109d2b0603dcd
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-09 03:20:46 -0400

    more

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -23,7 +23,7 @@ import OrdinaryDiffEqCore: trivial_limiter!, CompositeAlgorithm, alg_order, Sham
         isimplicit, initialize_d_discontinuities, isdtchangeable, _searchsortedfirst,
         @unpack, ismultistep, DEFAULT_PRECS, isautoswitch, get_chunksize_int,
         _unwrap_val, alg_autodiff, concrete_jac, alg_difftype, standardtag,
-        alg_extrapolates
+        alg_extrapolates, alg_maximum_order
 
 export CompositeAlgorithm, ShampineCollocationInit, BrownFullBasicInit, NoInit
        AutoSwitch
