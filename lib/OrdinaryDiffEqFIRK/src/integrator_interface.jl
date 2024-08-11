9fee28b49b2c9e2793237841f495b7228fd171fc
Shreyas-Ekanathan <142109039+Shreyas-Ekanathan@users.noreply.github.com>
2024-08-10 21:50:27 -0400

    Update integrator_interface.jl

--- lib/OrdinaryDiffEqFIRK/src/integrator_interface.jl
+++ lib/OrdinaryDiffEqFIRK/src/integrator_interface.jl
@@ -1,4 +1,4 @@
-@inline function DiffEqBase.get_tmp_cache(integrator, alg::Union{RadauIIA3, RadauIIA5, RadauIIA9, adaptiveRadau},
+@inline function DiffEqBase.get_tmp_cache(integrator, alg::Union{RadauIIA3, RadauIIA5, RadauIIA9, AdaptiveRadau},
         cache::OrdinaryDiffEqMutableCache)
     (cache.tmp, cache.atmp)
 end
