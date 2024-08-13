d1cd0d42bcd4114d922e2e611ca1b939020ce2ad
ParamThakkar123 <paramthakkar864@gmail.com>
2024-08-13 08:08:28 +0530

    interp_func.jl

--- lib/OrdinaryDiffEqRosenbrock/src/interp_func.jl
+++ lib/OrdinaryDiffEqRosenbrock/src/interp_func.jl
@@ -1,18 +1,15 @@
 function DiffEqBase.interp_summary(::Type{cacheType},
         dense::Bool) where {
         cacheType <:
-        Union{Rosenbrock23ConstantCache,
-        Rosenbrock32ConstantCache,
-        Rosenbrock23Cache,
-        Rosenbrock32Cache}}
+        Union{RosenbrockConstantCache,
+        RosenbrockCache}}
     dense ? "specialized 2nd order \"free\" stiffness-aware interpolation" :
     "1st order linear"
 end
 function DiffEqBase.interp_summary(::Type{cacheType},
         dense::Bool) where {
         cacheType <:
-        Union{Rodas4ConstantCache, Rodas23WConstantCache, Rodas3PConstantCache,
-        Rodas4Cache, Rodas23WCache, Rodas3PCache}}
+        Union{RosenbrockCache, RosenbrockConstantCache}}
     dense ? "specialized 3rd order \"free\" stiffness-aware interpolation" :
     "1st order linear"
 end
@@ -20,8 +17,7 @@ end
 function DiffEqBase.interp_summary(::Type{cacheType},
         dense::Bool) where {
         cacheType <:
-        Union{Rosenbrock5ConstantCache,
-        Rosenbrock5Cache}}
+        Union{RosenbrockConstantCache}}
     dense ? "specialized 4rd order \"free\" stiffness-aware interpolation" :
     "1st order linear"
 end
