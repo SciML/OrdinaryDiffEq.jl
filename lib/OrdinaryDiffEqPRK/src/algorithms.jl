4223150a916f6562b59ce0ca7f48acd6611ea7e4
ArnoStrouwen <arno.strouwen@telenet.be>
2024-08-18 12:59:28 +0200

    docs PRK

--- lib/OrdinaryDiffEqPRK/src/algorithms.jl
+++ lib/OrdinaryDiffEqPRK/src/algorithms.jl
@@ -1,10 +1,10 @@
-"""
-KuttaPRK2p5: Parallel Explicit Runge-Kutta Method
-A 5 parallel, 2 processor explicit Runge-Kutta method of 5th order.
-
-These methods utilize multithreading on the f calls to parallelize the problem.
-This requires that simultaneous calls to f are thread-safe.
-"""
+@doc generic_solver_docstring("A 5 parallel, 2 processor method of 5th order.
+        ",
+        "KuttaPRK2p5",
+        "Explicit Runge-Kutta Method",
+        "REFERENCE TBD",
+        "- `threading`: TBD",
+        "threading = true,")
 Base.@kwdef struct KuttaPRK2p5{TO} <: OrdinaryDiffEqAlgorithm
     threading::TO = true
 end
