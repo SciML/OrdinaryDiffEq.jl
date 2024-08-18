a9444e90ec52f64686c872d95634f2fee9138d6f
ParamThakkar123 <paramthakkar864@gmail.com>
2024-08-18 13:07:00 +0530

    Fixes

--- lib/OrdinaryDiffEqRosenbrock/src/rosenbrock_caches.jl
+++ lib/OrdinaryDiffEqRosenbrock/src/rosenbrock_caches.jl
@@ -695,7 +695,6 @@ function alg_cache(alg::Rodas3P, u, rate_prototype, ::Type{uEltypeNoUnits},
 end
 
 ### Rodas4 methods
-
 function alg_cache(alg::Rodas4, u, rate_prototype, ::Type{uEltypeNoUnits},
         ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
         dt, reltol, p, calck,
@@ -703,11 +702,11 @@ function alg_cache(alg::Rodas4, u, rate_prototype, ::Type{uEltypeNoUnits},
     dense = zeros(typeof(rate_prototype), 2)
     dus = zeros(typeof(rate_prototype), 3)
     ks = zeros(typeof(rate_prototype), 6)
-    fsalfirst = zero(rate_prototype)
-    fsallast = zero(rate_prototype)
-    dT = zero(rate_prototype)
+    fsalfirst = zeros(rate_prototype)
+    fsallast = zeros(rate_prototype)
+    dT = zeros(rate_prototype)
     J, W = build_J_W(alg, u, uprev, p, t, dt, f, uEltypeNoUnits, Val(true))
-    tmp = zero(rate_prototype)
+    tmp = zeros(rate_prototype)
     atmp = similar(u, uEltypeNoUnits)
     recursivefill!(atmp, false)
     weight = similar(u, uEltypeNoUnits)
@@ -716,7 +715,7 @@ function alg_cache(alg::Rodas4, u, rate_prototype, ::Type{uEltypeNoUnits},
 
     tf = TimeGradientWrapper(f, uprev, p)
     uf = UJacobianWrapper(f, t, p)
-    linsolve_tmp = zero(rate_prototype)
+    linsolve_tmp = zeros(rate_prototype)
     linprob = LinearProblem(W, _vec(linsolve_tmp); u0 = _vec(tmp))
     Pl, Pr = wrapprecs(
         alg.precs(W, nothing, u, p, t, nothing, nothing, nothing,
