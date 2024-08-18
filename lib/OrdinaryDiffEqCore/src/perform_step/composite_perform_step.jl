99abecd92619bf81f7ad37bf665d31821bbad87c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-18 03:15:28 -0400

    handle all

--- lib/OrdinaryDiffEqCore/src/perform_step/composite_perform_step.jl
+++ lib/OrdinaryDiffEqCore/src/perform_step/composite_perform_step.jl
@@ -78,13 +78,22 @@ function initialize!(integrator, cache::CompositeCache)
     cache.current = cache.choice_function(integrator)
     if cache.current == 1
         initialize!(integrator, @inbounds(cache.caches[1]))
+        fsalfirst, fsallast = get_fsalfirstlast(cache.caches[1])
+        integrator.fsalfirst = fsalfirst
+        integrator.fsallast = fsallast
     elseif cache.current == 2
         initialize!(integrator, @inbounds(cache.caches[2]))
+        fsalfirst, fsallast = get_fsalfirstlast(cache.caches[2])
+        integrator.fsalfirst = fsalfirst
+        integrator.fsallast = fsallast
         # the controller was initialized by default for integrator.alg.algs[1]
         reset_alg_dependent_opts!(integrator.opts.controller, integrator.alg.algs[1],
             integrator.alg.algs[2])
     else
         initialize!(integrator, @inbounds(cache.caches[cache.current]))
+        fsalfirst, fsallast = get_fsalfirstlast(cache.caches[cache.current])
+        integrator.fsalfirst = fsalfirst
+        integrator.fsallast = fsallast
         reset_alg_dependent_opts!(integrator.opts.controller, integrator.alg.algs[1],
             integrator.alg.algs[cache.current])
     end
