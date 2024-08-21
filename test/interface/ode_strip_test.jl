adc61814e9696d86d943cff33d813f82f93668b8
jClugstor <jadonclugston@gmail.com>
2024-08-21 11:50:02 -0400

    add tests for interp and cache stripping

---
+++ test/interface/ode_strip_test.jl
@@ -0,0 +1,16 @@
+using OrdinaryDiffEq, Test
+
+function lorenz!(du, u, p, t)
+    du[1] = 10.0 * (u[2] - u[1])
+    du[2] = u[1] * (28.0 - u[3]) - u[2]
+    du[3] = u[1] * u[2] - (8 / 3) * u[3]
+end
+
+u0 = [1.0; 0.0; 0.0]
+tspan = (0.0, 0.5)
+prob = ODEProblem(lorenz!, u0, tspan)
+
+sol = solve(prob, Rosenbrock23())
+
+@test isnothing(strip_interpolation(sol.interp).f)
+@test isnothing(strip_interpolation(sol.interp).cache.jac_config)
