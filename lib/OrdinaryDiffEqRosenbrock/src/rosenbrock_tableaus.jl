71cc48489b1d347a43742bcd3e2d6077b9617062
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-08-22 16:44:28 +0530

    Update lib/OrdinaryDiffEqRosenbrock/src/rosenbrock_tableaus.jl
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- lib/OrdinaryDiffEqRosenbrock/src/rosenbrock_tableaus.jl
+++ lib/OrdinaryDiffEqRosenbrock/src/rosenbrock_tableaus.jl
@@ -503,7 +503,7 @@ function Rodas5Tableau(T, T2)
     b8 = convert(T,1)
     =#
 
-    Rodas5Tableau(a, C, gamma, d, c,
+    RodasTableau(a, C, gamma, d, c,
     h21, h22, h23, h24, h25, h26, h27, h28, h31, h32, h33, h34, h35, h36, h37,
     h38, h41, h42, h43, h44, h45, h46, h47, h48)
 end
