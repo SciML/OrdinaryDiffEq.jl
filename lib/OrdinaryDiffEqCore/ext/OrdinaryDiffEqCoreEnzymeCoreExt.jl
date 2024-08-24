4dc378b3887f87f04e44f6d642ffcbda22c77ce7
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-24 09:04:29 -0400

    Update OrdinaryDiffEqCoreEnzymeCoreExt.jl

--- lib/OrdinaryDiffEqCore/ext/OrdinaryDiffEqCoreEnzymeCoreExt.jl
+++ lib/OrdinaryDiffEqCore/ext/OrdinaryDiffEqCoreEnzymeCoreExt.jl
@@ -14,10 +14,6 @@ function EnzymeCore.EnzymeRules.inactive(
         ::typeof(OrdinaryDiffEqCore.increment_reject!), args...)
     true
 end
-function EnzymeCore.EnzymeRules.inactive(
-        ::typeof(OrdinaryDiffEqCore.increment_nf_perform_step!), args...)
-    true
-end
 EnzymeCore.EnzymeRules.inactive(::typeof(OrdinaryDiffEqCore.check_error!), args...) = true
 EnzymeCore.EnzymeRules.inactive(::typeof(OrdinaryDiffEqCore.log_step!), args...) = true
 
