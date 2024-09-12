69e9a16bc3d289302049c59692283832aa4c23d0
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-09-12 05:55:47 +0200

    Update basic_caches.jl

--- lib/OrdinaryDiffEqCore/src/caches/basic_caches.jl
+++ lib/OrdinaryDiffEqCore/src/caches/basic_caches.jl
@@ -17,6 +17,7 @@ mutable struct CompositeCache{T, F} <: OrdinaryDiffEqCache
 end
 
 function ismutablecache(cache::CompositeCache{T, F}) where {T, F}
+    @show eltype(T)
     eltype(T) <: OrdinaryDiffEqMutableCache
 end
 
