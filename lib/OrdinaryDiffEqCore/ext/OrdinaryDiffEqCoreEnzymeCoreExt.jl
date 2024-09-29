4f5ae3f8bf65dc6630f96548fdc14dd615c9ce1c
William Moses <gh@wsmoses.com>
2024-09-28 23:26:55 -0500

    EnzymeExt Use inactive_noinl
    This will improve performance

--- lib/OrdinaryDiffEqCore/ext/OrdinaryDiffEqCoreEnzymeCoreExt.jl
+++ lib/OrdinaryDiffEqCore/ext/OrdinaryDiffEqCoreEnzymeCoreExt.jl
@@ -1,20 +1,20 @@
 module OrdinaryDiffEqCoreEnzymeCoreExt
 import OrdinaryDiffEqCore, EnzymeCore
 
-EnzymeCore.EnzymeRules.inactive(::typeof(OrdinaryDiffEqCore.increment_nf!), args...) = true
-function EnzymeCore.EnzymeRules.inactive(
+EnzymeCore.EnzymeRules.inactive_noinl(::typeof(OrdinaryDiffEqCore.increment_nf!), args...) = true
+function EnzymeCore.EnzymeRules.inactive_noinl(
         ::typeof(OrdinaryDiffEqCore.fixed_t_for_floatingpoint_error!), args...)
     true
 end
-function EnzymeCore.EnzymeRules.inactive(
+function EnzymeCore.EnzymeRules.inactive_noinl(
         ::typeof(OrdinaryDiffEqCore.increment_accept!), args...)
     true
 end
-function EnzymeCore.EnzymeRules.inactive(
+function EnzymeCore.EnzymeRules.inactive_noinl(
         ::typeof(OrdinaryDiffEqCore.increment_reject!), args...)
     true
 end
-EnzymeCore.EnzymeRules.inactive(::typeof(OrdinaryDiffEqCore.check_error!), args...) = true
-EnzymeCore.EnzymeRules.inactive(::typeof(OrdinaryDiffEqCore.log_step!), args...) = true
+EnzymeCore.EnzymeRules.inactive_noinl(::typeof(OrdinaryDiffEqCore.check_error!), args...) = true
+EnzymeCore.EnzymeRules.inactive_noinl(::typeof(OrdinaryDiffEqCore.log_step!), args...) = true
 
 end
