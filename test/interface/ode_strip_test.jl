57de663c6a2058e01e82cda8742b036f349b4007
jClugstor <jadonclugston@gmail.com>
2024-10-10 12:07:28 -0400

    more tests

--- test/interface/ode_strip_test.jl
+++ test/interface/ode_strip_test.jl
@@ -14,7 +14,7 @@ prob = ODEProblem(lorenz!, u0, tspan)
 rosenbrock_sol = solve(prob, Rosenbrock23())
 TRBDF_sol = solve(prob, TRBDF2())
 vern_sol = solve(prob, Vern7())
-
+default_sol = solve(prob)
 @testset "Interpolation Stripping" begin
     @test isnothing(SciMLBase.strip_interpolation(rosenbrock_sol.interp).f)
     @test isnothing(SciMLBase.strip_interpolation(rosenbrock_sol.interp).cache.jac_config)
@@ -22,20 +22,28 @@ vern_sol = solve(prob, Vern7())
 end
 
 @testset "Rosenbrock Solution Stripping" begin
-    @test SciMLBase.strip_solution(rosenbrock_sol).prob isa NamedTuple
+    stripped_sol = SciMLBase.strip_solution(rosenbrock_sol)
+    @test stripped_sol.prob isa NamedTuple
     @test isnothing(SciMLBase.strip_solution(rosenbrock_sol, strip_alg = true).alg)
-    @test isnothing(SciMLBase.strip_solution(rosenbrock_sol).interp.f)
-    @test isnothing(SciMLBase.strip_solution(rosenbrock_sol).interp.cache.jac_config)
-    @test isnothing(SciMLBase.strip_solution(rosenbrock_sol).interp.cache.grad_config)
-    @test isnothing(SciMLBase.strip_solution(rosenbrock_sol).interp.cache.uf)
-    @test isnothing(SciMLBase.strip_solution(rosenbrock_sol).interp.cache.tf)
+    @test isnothing(stripped_sol.interp.f)
+    @test isnothing(stripped_sol.interp.cache.jac_config)
+    @test isnothing(stripped_sol.interp.cache.grad_config)
+    @test isnothing(stripped_sol.interp.cache.uf)
+    @test isnothing(stripped_sol.interp.cache.tf)
 end
 
 @testset "TRBDF Solution Stripping" begin
-    @test SciMLBase.strip_solution(TRBDF_sol).prob isa NamedTuple
+    stripped_sol = SciMLBase.strip_solution(TRBDF_sol)
+    @test stripped_sol.prob isa NamedTuple
     @test isnothing(SciMLBase.strip_solution(TRBDF_sol, strip_alg = true).alg)
-    @test isnothing(SciMLBase.strip_solution(TRBDF_sol).interp.f)
-    @test isnothing(SciMLBase.strip_solution(TRBDF_sol).interp.cache.nlsolver)
+    @test isnothing(stripped_sol.interp.f)
+    @test isnothing(stripped_sol.interp.cache.nlsolver)
+end
+
+@testset "Default Solution Stripping" begin
+    stripped_sol = SciMLBase.strip_solution(default_sol)
+    @test isnothing(stripped_sol.interp.cache.args)
+
 end
 
 @test_throws SciMLBase.LazyInterpolationException SciMLBase.strip_solution(vern_sol)
