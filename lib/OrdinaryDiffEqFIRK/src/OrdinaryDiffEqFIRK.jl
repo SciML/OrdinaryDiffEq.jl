3eb41df73c8fab5d99293a0f8b713fce0ddaa3a2
jClugstor <jadonclugston@gmail.com>
2024-10-31 16:43:07 -0400

    import bool_to_ADType

--- lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
+++ lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
@@ -16,8 +16,8 @@ import OrdinaryDiffEqCore: alg_order, calculate_residuals!,
                            PredictiveController, alg_can_repeat_jac, NewtonAlgorithm,
                            fac_default_gamma,
                            get_current_adaptive_order, get_fsalfirstlast,
-                           isfirk, generic_solver_docstring
-using MuladdMacro, DiffEqBase, RecursiveArrayTools, Polyester
+                           isfirk, generic_solver_docstring, bool_to_ADType
+using MuladdMacro, DiffEqBase, RecursiveArrayTools
 using SciMLOperators: AbstractSciMLOperator
 using LinearAlgebra: I, UniformScaling, mul!, lu
 import LinearSolve
