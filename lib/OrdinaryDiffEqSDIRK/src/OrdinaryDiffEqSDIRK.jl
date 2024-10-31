a3fb339341bf05d76e8a890f822f483bcc275cb8
jClugstor <jadonclugston@gmail.com>
2024-10-31 16:43:07 -0400

    import bool_to_ADType

--- lib/OrdinaryDiffEqSDIRK/src/OrdinaryDiffEqSDIRK.jl
+++ lib/OrdinaryDiffEqSDIRK/src/OrdinaryDiffEqSDIRK.jl
@@ -13,7 +13,8 @@ import OrdinaryDiffEqCore: alg_order, calculate_residuals!,
                            constvalue, _unwrap_val, _ode_interpolant,
                            trivial_limiter!, _ode_interpolant!,
                            isesdirk, issplit,
-                           ssp_coefficient, get_fsalfirstlast, generic_solver_docstring
+                           ssp_coefficient, get_fsalfirstlast, generic_solver_docstring,
+                           bool_to_ADType
 using TruncatedStacktraces, MuladdMacro, MacroTools, FastBroadcast, RecursiveArrayTools
 using SciMLBase: SplitFunction
 using LinearAlgebra: mul!, I
