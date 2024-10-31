678b02d63f33df9c3edf201b95d3d1f8eefd19e3
jClugstor <jadonclugston@gmail.com>
2024-10-31 16:46:13 -0400

    missed one

--- lib/OrdinaryDiffEqPDIRK/src/OrdinaryDiffEqPDIRK.jl
+++ lib/OrdinaryDiffEqPDIRK/src/OrdinaryDiffEqPDIRK.jl
@@ -5,7 +5,8 @@ import OrdinaryDiffEqCore: isfsal, alg_order, _unwrap_val,
                            OrdinaryDiffEqMutableCache, constvalue, alg_cache,
                            uses_uprev, @unpack, unwrap_alg, @cache, DEFAULT_PRECS,
                            @threaded, initialize!, perform_step!, isthreaded,
-                           full_cache, get_fsalfirstlast, differentiation_rk_docstring
+                           full_cache, get_fsalfirstlast, differentiation_rk_docstring,
+                           bool_to_ADType
 import StaticArrays: SVector
 import MuladdMacro: @muladd
 import FastBroadcast: @..
