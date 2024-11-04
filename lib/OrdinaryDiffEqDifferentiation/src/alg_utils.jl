e0ffa3b650b2956937ff65d526275fed3ec8db9a
jClugstor <jadonclugston@gmail.com>
2024-11-04 13:36:57 -0500

    use AD.fdtype for derivative, change alg_autodiff to get AD

--- lib/OrdinaryDiffEqDifferentiation/src/alg_utils.jl
+++ lib/OrdinaryDiffEqDifferentiation/src/alg_utils.jl
@@ -2,28 +2,30 @@
 function _alg_autodiff(alg::OrdinaryDiffEqAlgorithm)
     error("This algorithm does not have an autodifferentiation option defined.")
 end
-_alg_autodiff(::OrdinaryDiffEqAdaptiveImplicitAlgorithm{CS, AD}) where {CS, AD} = Val{AD}()
-_alg_autodiff(::DAEAlgorithm{CS, AD}) where {CS, AD} = Val{AD}()
-_alg_autodiff(::OrdinaryDiffEqImplicitAlgorithm{CS, AD}) where {CS, AD} = Val{AD}()
+_alg_autodiff(::OrdinaryDiffEqAdaptiveImplicitAlgorithm{CS, AD}) where {CS, AD} = AD
+_alg_autodiff(::DAEAlgorithm{CS, AD}) where {CS, AD} = AD
+_alg_autodiff(::OrdinaryDiffEqImplicitAlgorithm{CS, AD}) where {CS, AD} = AD
 _alg_autodiff(alg::CompositeAlgorithm) = _alg_autodiff(alg.algs[end])
 function _alg_autodiff(::Union{OrdinaryDiffEqExponentialAlgorithm{CS, AD},
         OrdinaryDiffEqAdaptiveExponentialAlgorithm{CS, AD}
 }) where {
         CS, AD
 }
-    Val{AD}()
+    AD
 end
 
-function alg_autodiff(alg)
-    autodiff = _alg_autodiff(alg)
-    if autodiff == Val(false)
-        return AutoFiniteDiff()
-    elseif autodiff == Val(true)
-        return AutoForwardDiff()
-    else
-        return _unwrap_val(autodiff)
-    end
-end
+#function alg_autodiff(alg)
+#    autodiff = _alg_autodiff(alg)
+#    if autodiff == Val(false)
+#        return AutoFiniteDiff()
+#   elseif autodiff == Val(true)
+#        return AutoForwardDiff()
+#    else
+#        return _unwrap_val(autodiff)
+#    end
+#end
+
+alg_autodiff(alg) = _alg_autodiff(alg)
 
 Base.@pure function determine_chunksize(u, alg::DiffEqBase.DEAlgorithm)
     determine_chunksize(u, get_chunksize(alg))
