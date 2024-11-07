758e6d56769c417003acb48e3e42ba009625cc84
jClugstor <jadonclugston@gmail.com>
2024-11-07 14:48:51 -0500

    no call for diff_type

--- lib/OrdinaryDiffEqCore/src/misc_utils.jl
+++ lib/OrdinaryDiffEqCore/src/misc_utils.jl
@@ -139,7 +139,7 @@ end
 
 function _bool_to_ADType(::Val{false}, chunksize, diff_type)
     Base.depwarn("Using a `Bool` for keyword argument `autodiff` is deprecated. Please use an `ADType` specifier.", :_bool_to_ADType)
-    typeof(AutoFiniteDiff(fdtype = diff_type()))
+    typeof(AutoFiniteDiff(fdtype = diff_type))
 end
 
 # Functions to get ADType type from Bool or ADType object, or ADType type
