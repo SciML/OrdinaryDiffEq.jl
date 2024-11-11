5e24c9338aec2bf84f3b3835ea0a95f3daa369d2
jClugstor <jadonclugston@gmail.com>
2024-11-11 18:07:36 -0500

    another dang nameof

--- lib/OrdinaryDiffEqCore/src/initialize_dae.jl
+++ lib/OrdinaryDiffEqCore/src/initialize_dae.jl
@@ -150,7 +150,7 @@ function _initialize_dae!(integrator, prob::AbstractDEProblem,
     isAD = if iu0 === nothing
         AutoForwardDiff
     elseif has_autodiff(integrator.alg)
-        nameof(alg_autodiff(integrator.alg)) == :AutoForwardDiff
+        alg_autodiff(integrator.alg) isa AutoForwardDiff
     else
         true
     end
