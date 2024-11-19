b6b86de50a9b8eec97f61ee109d1127c34463976
Shreyas Ekanathan <shreyase39@gmail.com>
2024-11-19 07:42:41 -0500

    fix tests

--- lib/OrdinaryDiffEqFIRKGenerator/test/ode_firk_tests.jl
+++ lib/OrdinaryDiffEqFIRKGenerator/test/ode_firk_tests.jl
@@ -6,8 +6,8 @@ testTol = 0.5
 prob_ode_linear_big = remake(prob_ode_linear, u0 = big.(prob_ode_linear.u0), tspan = big.(prob_ode_linear.tspan))
 prob_ode_2Dlinear_big = remake(prob_ode_2Dlinear, u0 = big.(prob_ode_2Dlinear.u0), tspan = big.(prob_ode_2Dlinear.tspan))
 
-for i in [9], prob in [prob_ode_linear_big, prob_ode_2Dlinear_big]
+for i in [17, 21], prob in [prob_ode_linear_big, prob_ode_2Dlinear_big]
     dts = 1 ./ 2 .^ (4.25:-1:0.25)
-    sim21 = test_convergence(dts, prob, AdaptiveRadau(min_stages = i, max_stages = i))
-    @test sim21.𝒪est[:final]≈ (2 * i - 1) atol=testTol
+    sim21 = test_convergence(dts, prob, AdaptiveRadau(min_order = i, max_order = i))
+    @test sim21.𝒪est[:final]≈ i atol=testTol
 end
