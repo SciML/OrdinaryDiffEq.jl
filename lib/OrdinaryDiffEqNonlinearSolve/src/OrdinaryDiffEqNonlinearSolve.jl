a832526243ba0aef50a7e1e5c9924257806a115e
jClugstor <jadonclugston@gmail.com>
2024-11-29 12:55:05 -0500

    import AutoSparse in OrdinaryDiffEqNonlinearSolve

--- lib/OrdinaryDiffEqNonlinearSolve/src/OrdinaryDiffEqNonlinearSolve.jl
+++ lib/OrdinaryDiffEqNonlinearSolve/src/OrdinaryDiffEqNonlinearSolve.jl
@@ -1,6 +1,6 @@
 module OrdinaryDiffEqNonlinearSolve
 
-import ADTypes: AutoFiniteDiff, AutoForwardDiff
+import ADTypes: AutoFiniteDiff, AutoForwardDiff, AutoSparse
 
 import SciMLBase
 import SciMLBase: init, solve, solve!, remake
