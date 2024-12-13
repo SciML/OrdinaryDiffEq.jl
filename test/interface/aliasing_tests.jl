ec1e99fe85e99bfaa91a45909f46a284dda3811c
jClugstor <jadonclugston@gmail.com>
2024-12-13 14:00:24 -0500

    add test

---
+++ test/interface/aliasing_tests.jl
@@ -0,0 +1,11 @@
+using OrdinaryDiffEq, Test
+
+import ODEProblemLibrary: prob_ode_2Dlinear
+import DAEProblemLibrary: prob_dae_transamp
+
+# Test that the old keyword works, and that the new AliasSpecier works.
+u0_old_alias_kwarg_sol = solve(prob_ode_linear, Tsit5(), alias_u0 = true)
+u0_new_alias_kwarg_sol = solve(prob_ode_linear, Tsit5(), alias = ODEAliasSpecifier(alias_u0 = true))
+
+@test u0_old_alias_kwarg_sol == u0_new_alias_kwarg_sol
+
