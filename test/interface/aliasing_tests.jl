4d885f5a64b5acdcf2f24a31826cd417aef4a4c1
jClugstor <jadonclugston@gmail.com>
2024-12-14 10:59:10 -0500

    get rid of DAE problem, unused

--- test/interface/aliasing_tests.jl
+++ test/interface/aliasing_tests.jl
@@ -1,7 +1,6 @@
 using OrdinaryDiffEq, Test
 
 import ODEProblemLibrary: prob_ode_2Dlinear
-import DAEProblemLibrary: prob_dae_transamp
 
 # Test that the old keyword works, and that the new AliasSpecier works.
 u0_old_alias_kwarg_sol = solve(prob_ode_linear, Tsit5(), alias_u0 = true)
