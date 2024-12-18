23c054b86faeda01d9323f10afac8659b0e75a06
Michele Mesiti <michele.mesiti@kit.edu>
2024-12-18 11:55:51 +0100

    Add regression tests for multithreaded ABM algs

--- lib/OrdinaryDiffEqAdamsBashforthMoulton/test/runtests.jl
+++ lib/OrdinaryDiffEqAdamsBashforthMoulton/test/runtests.jl
@@ -2,3 +2,4 @@ using SafeTestsets
 
 @time @safetestset "ABM Convergence Tests" include("abm_convergence_tests.jl")
 @time @safetestset "Adams Variable Coefficients Tests" include("adams_tests.jl")
+@time @safetestset "Regression test for threading versions vs non threading versions"  include("regression_test_threading.jl")
