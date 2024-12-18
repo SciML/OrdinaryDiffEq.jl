e2c99b5f8da919a8695165d5697ea95b938bd6f3
Michele Mesiti <michele.mesiti@kit.edu>
2024-12-18 12:04:29 +0100

    Format ABM subdirectory
    Run the reformatting command
    suggested in CONTRIBUTING.md,
    but keeping only the changes on the ABM directory
    (which my current PR is affecting)

--- lib/OrdinaryDiffEqAdamsBashforthMoulton/test/regression_test_threading.jl
+++ lib/OrdinaryDiffEqAdamsBashforthMoulton/test/regression_test_threading.jl
@@ -2,26 +2,24 @@ using OrdinaryDiffEqAdamsBashforthMoulton, ODEProblemLibrary
 using Test
 using Static
 
-free_timestep_algorithms = [ VCAB3, VCAB4, VCAB5, VCABM3, VCABM4, VCABM5, VCABM]
+free_timestep_algorithms = [VCAB3, VCAB4, VCAB5, VCABM3, VCABM4, VCABM5, VCABM]
 
-fixed_timestep_algorithms = [AB3,AB4, AB5, ABM32, ABM43, ABM54]
+fixed_timestep_algorithms = [AB3, AB4, AB5, ABM32, ABM43, ABM54]
 
 problem = ODEProblemLibrary.prob_ode_linear
 
 function test_alg(ALG; kwargs...)
-   sol_thread = solve(problem, ALG(Static.True()); kwargs...)
-   sol_nothread = solve(problem, ALG(Static.False()); kwargs...)
+    sol_thread = solve(problem, ALG(Static.True()); kwargs...)
+    sol_nothread = solve(problem, ALG(Static.False()); kwargs...)
 
-   @test all(sol_nothread .== sol_thread)
+    @test all(sol_nothread .== sol_thread)
 end
-
 
 @testset "Regression test for threading versions vs non threading versions" begin
     @testset "$ALG" for ALG in fixed_timestep_algorithms
-        test_alg(ALG, dt=1//2^9)
+        test_alg(ALG, dt = 1 // 2^9)
     end
     @testset "$ALG" for ALG in free_timestep_algorithms
         test_alg(ALG)
     end
-
 end
