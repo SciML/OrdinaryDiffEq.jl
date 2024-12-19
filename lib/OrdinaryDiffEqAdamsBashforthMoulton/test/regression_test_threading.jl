b92befb19508915a3c1bdc82112015db441cdfbf
Michele Mesiti <michele.mesiti@kit.edu>
2024-12-19 15:22:28 +0100

    ABM algs: refactor regr/thread tests for clarity

--- lib/OrdinaryDiffEqAdamsBashforthMoulton/test/regression_test_threading.jl
+++ lib/OrdinaryDiffEqAdamsBashforthMoulton/test/regression_test_threading.jl
@@ -1,25 +1,22 @@
 using OrdinaryDiffEqAdamsBashforthMoulton, ODEProblemLibrary
+import OrdinaryDiffEqCore: OrdinaryDiffEqAdaptiveAlgorithm
 using Test
 using Static
 
-free_timestep_algorithms = [VCAB3, VCAB4, VCAB5, VCABM3, VCABM4, VCABM5, VCABM]
-
-fixed_timestep_algorithms = [AB3, AB4, AB5, ABM32, ABM43, ABM54]
+algorithms = [
+    AB3, AB4, AB5, ABM32, ABM43, ABM54, VCAB3, VCAB4, VCAB5, VCABM3, VCABM4, VCABM5, VCABM]
 
 problem = ODEProblemLibrary.prob_ode_linear
 
-function test_alg(ALG; kwargs...)
-    sol_thread = solve(problem, ALG(Static.True()); kwargs...)
-    sol_nothread = solve(problem, ALG(Static.False()); kwargs...)
-
-    @test all(sol_nothread .== sol_thread)
-end
-
 @testset "Regression test for threading versions vs non threading versions" begin
-    @testset "$ALG" for ALG in fixed_timestep_algorithms
-        test_alg(ALG, dt = 1 // 2^9)
-    end
-    @testset "$ALG" for ALG in free_timestep_algorithms
-        test_alg(ALG)
+    @testset "$ALG" for ALG in algorithms
+        if ALG isa OrdinaryDiffEqAdaptiveAlgorithm
+            sol_thread = solve(problem, ALG(Static.True()))
+            sol_nothread = solve(problem, ALG(Static.False()))
+        else
+            sol_thread = solve(problem, ALG(Static.True()), dt = 1 // 2^9)
+            sol_nothread = solve(problem, ALG(Static.False()), dt = 1 // 2^9)
+        end
+        @test all(sol_nothread .== sol_thread)
     end
 end
