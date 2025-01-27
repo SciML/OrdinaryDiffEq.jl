cba08fa4e1e742572df804d352892a436289962f
Shreyas Ekanathan <shreyase39@gmail.com>
2025-01-27 17:32:22 -0500

    fixes

--- lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
+++ lib/OrdinaryDiffEqFIRK/src/OrdinaryDiffEqFIRK.jl
@@ -18,7 +18,8 @@ import OrdinaryDiffEqCore: alg_order, calculate_residuals!,
                            get_current_adaptive_order, get_fsalfirstlast,
                            isfirk, generic_solver_docstring, _bool_to_ADType,
                            _process_AD_choice
-using MuladdMacro, DiffEqBase, RecursiveArrayTools
+using MuladdMacro, DiffEqBase, RecursiveArrayTools, Polyester
+                           isfirk, generic_solver_docstring
 using SciMLOperators: AbstractSciMLOperator
 using LinearAlgebra: I, UniformScaling, mul!, lu
 import LinearSolve
