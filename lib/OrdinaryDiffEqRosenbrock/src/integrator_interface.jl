8999b5d53a59623fa4c4c179a24cc5b125f09df7
jClugstor <jadonclugston@gmail.com>
2025-01-30 14:35:11 -0500

    set up resizing

--- lib/OrdinaryDiffEqRosenbrock/src/integrator_interface.jl
+++ lib/OrdinaryDiffEqRosenbrock/src/integrator_interface.jl
@@ -2,7 +2,19 @@ function resize_non_user_cache!(integrator::ODEIntegrator,
         cache::RosenbrockMutableCache, i)
     cache.J = similar(cache.J, i, i)
     cache.W = similar(cache.W, i, i)
-    resize_jac_config!(cache.jac_config, i)
-    resize_grad_config!(cache.grad_config, i)
+
+    uf = cache.uf
+    uf = SciMLBase.@set uf.f = SciMLBase.unwrapped_f(uf.f)
+
+    cache.jac_config = resize_jac_config!(uf, cache.du1, cache.jac_config, alg_autodiff(integrator.alg), integrator.u)
+
+    if alg_autodiff(integrator.alg) isa AutoSparse
+        ad = ADTypes.dense_ad(alg_autodiff(integrator.alg))
+    else
+        ad = alg_autodiff(integrator.alg)
+    end
+
+    cache.grad_config = resize_grad_config!(cache.tf, cache.du1, cache.grad_config, ad, integrator.t)
+
     nothing
 end
