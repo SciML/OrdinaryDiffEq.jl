86f84a796ac57a8f4f7338b62206b35c3afb1e4e
Oscar Smith <oscar.smith@juliacomputing.com>
2025-02-05 12:50:08 -0500

    return retcode rather than bool

--- lib/OrdinaryDiffEqCore/src/iterator_interface.jl
+++ lib/OrdinaryDiffEqCore/src/iterator_interface.jl
@@ -3,24 +3,24 @@ function step!(integrator::ODEIntegrator)
         while integrator.tdir * integrator.t < first(integrator.opts.tstops)
             loopheader!(integrator)
             (integrator.do_error_check && check_error!(integrator) != ReturnCode.Success) &&
-                return false
+                return integrator.sol.retcode
             perform_step!(integrator, integrator.cache)
             loopfooter!(integrator)
         end
     else
         loopheader!(integrator)
         (integrator.do_error_check && check_error!(integrator) != ReturnCode.Success) &&
-            return false
+            return integrator.sol.retcode
         perform_step!(integrator, integrator.cache)
         loopfooter!(integrator)
         while !integrator.accept_step
             loopheader!(integrator)
             (integrator.do_error_check && check_error!(integrator) != ReturnCode.Success) &&
-                return false
+                return integrator.sol.retcode
             perform_step!(integrator, integrator.cache)
             loopfooter!(integrator)
         end
     end
     handle_tstop!(integrator)
-    return true
+    return integrator.sol.retcode
 end
