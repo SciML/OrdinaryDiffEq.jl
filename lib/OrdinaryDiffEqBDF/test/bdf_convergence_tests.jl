4ace9d035680157c45167d7c8b8e060850947b07
jClugstor <jadonclugston@gmail.com>
2025-02-13 16:25:21 -0500

    using Enzyme for convergence tests

--- lib/OrdinaryDiffEqBDF/test/bdf_convergence_tests.jl
+++ lib/OrdinaryDiffEqBDF/test/bdf_convergence_tests.jl
@@ -1,5 +1,5 @@
 # This definitely needs cleaning
-using OrdinaryDiffEqBDF, ODEProblemLibrary, DiffEqDevTools, ADTypes
+using OrdinaryDiffEqBDF, ODEProblemLibrary, DiffEqDevTools, ADTypes, Enzyme
 using OrdinaryDiffEqNonlinearSolve: NLFunctional, NLAnderson, NonlinearSolveAlg
 using Test, Random
 Random.seed!(100)
