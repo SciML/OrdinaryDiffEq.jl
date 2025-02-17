3807590fe97537c753cfb62223e4ebf50e83751b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-02-17 10:16:28 -0500

    more test fixes

--- test/interface/nojac.jl
+++ test/interface/nojac.jl
@@ -1,4 +1,4 @@
-using OrdinaryDiffEq, LinearSolve, Test, ADTypes
+using OrdinaryDiffEq, RecursiveFactorization, LinearSolve, Test, ADTypes
 
 const N = 32
 const xyd_brusselator = range(0, stop = 1, length = N)
