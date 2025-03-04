6472a315c5043f43f2ae9d234737772535ea9a71
jClugstor <jadonclugston@gmail.com>
2025-03-04 11:47:24 -0500

    fix remake so chunksize is only used if not default ADType

--- lib/OrdinaryDiffEqCore/src/misc_utils.jl
+++ lib/OrdinaryDiffEqCore/src/misc_utils.jl
@@ -158,7 +158,7 @@ end
 function _process_AD_choice(
         ad_alg::AutoForwardDiff{CS}, ::Val{CS2}, ::Val{FD}) where {CS, CS2, FD}
     # Non-default `chunk_size`
-    if CS2 != 0
+    if CS2 != 0 && ad_alg == AutoForwardDiff()
         @warn "The `chunk_size` keyword is deprecated. Please use an `ADType` specifier. For now defaulting to using `AutoForwardDiff` with `chunksize=$(CS2)`."
         return _bool_to_ADType(Val{true}(), Val{CS2}(), Val{FD}()), Val{CS2}(), Val{FD}()
     end
