aae61f258be4bb8ffeebdf20a9c9e2757736e782
jClugstor <jadonclugston@gmail.com>
2025-03-06 18:11:37 -0500

    bring in LinearSolve for resize tests

--- test/integrators/resize_tests.jl
+++ test/integrators/resize_tests.jl
@@ -1,4 +1,4 @@
-using OrdinaryDiffEq, Test, ADTypes, SparseMatrixColorings, DiffEqBase, ForwardDiff, SciMLBase
+using OrdinaryDiffEq, Test, ADTypes, SparseMatrixColorings, DiffEqBase, ForwardDiff, SciMLBase, LinearSolve
 import OrdinaryDiffEqDifferentiation.DI
 
 f(du, u, p, t) = du .= u
