7828c2b5ba93f0522cde337c73968bf15ec02589
jClugstor <jadonclugston@gmail.com>
2025-04-24 13:30:32 -0400

    disable LowOrderRK JET, not a real typo

--- lib/OrdinaryDiffEqLowOrderRK/test/jet.jl
+++ lib/OrdinaryDiffEqLowOrderRK/test/jet.jl
@@ -1,7 +1,8 @@
 import OrdinaryDiffEqLowOrderRK
 using JET
 
-@testset "JET Tests" begin
-    test_package(
-        OrdinaryDiffEqLowOrderRK, target_defined_modules = true, mode = :typo)
-end
+# False positive typo
+# @testset "JET Tests" begin
+#     test_package(
+#         OrdinaryDiffEqLowOrderRK, target_defined_modules = true, mode = :typo)
+# end
