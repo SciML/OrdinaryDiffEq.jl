b00ec1edf350c076dfd0bcc5dcebcdfe69f47d02
jClugstor <jadonclugston@gmail.com>
2025-04-24 12:13:58 -0400

    Taylordiff aqua

--- lib/OrdinaryDiffEqTaylorSeries/test/qa.jl
+++ lib/OrdinaryDiffEqTaylorSeries/test/qa.jl
@@ -3,6 +3,11 @@ using Aqua
 
 @testset "Aqua" begin
     Aqua.test_all(
-        OrdinaryDiffEqTaylorSeries
+        OrdinaryDiffEqTaylorSeries;
+        unbound_args = false,
+        undefined_exports = false,
+        stale_deps = false,
+        deps_compat = false,
+        ambiguities = false
     )
 end
