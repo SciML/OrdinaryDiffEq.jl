0cb3e46a575b09191bace01db901288864f1afb0
Azercoco <20425137+Azercoco@users.noreply.github.com>
2025-06-10 17:05:13 +0200

    Use generic_solver_docstring for the documentation

--- lib/OrdinaryDiffEqRKIP/src/algorithms.jl
+++ lib/OrdinaryDiffEqRKIP/src/algorithms.jl
@@ -1,20 +1,105 @@
 using OrdinaryDiffEqCore: OrdinaryDiffEqCore
 
-mutable struct RKIP_ALG{tableauType<:ExplicitRKTableau,elType,dtType<:AbstractVector{elType}} <: OrdinaryDiffEqAdaptiveAlgorithm
+METHOD_DESCRIPTION = """
+Runge-Kutta in the interaction picture.
+
+This is suited for solving semilinear problem of the form:
+
+```math
+\frac{du}{dt} =  Au + f(u,p,t)
+```
+
+where A is possibly stiff constant time-indepdant linear operator whose scaled exponential exp(Ah) can be calculated efficiently for any h.
+The problem is first transformed in a non-stiff variant (interaction picture)
+
+```math
+u_I(t) = \exp(At) u_0
+\frac{du_I}{dt} =  f_I(u_I,p,t)
+f_I(u_I,p,t) = f(exp(-At)u_I, p, t)
+```
+and is then solved with an explicit (adaptive) Runge-Kutta method.
+
+This solver is only implemented for semilinear problem: `SplitODEProblem` when the first function `f1` is a `AbsractSciMLOperator` A implementing:
+
+```julia
+LinearAlgebra.exp(A, t) # = exp(A*t)
+```
+`A` and the return value of `exp(A, t)` must either also both implement:
+```
+A(du, u, v, p, t) # for in-place problem
+A(u, v p, t) # for out-of-place problem
+```
+
+For performance, the algorithm will cache and reuse the computed operator-exponential for a fixed set of time steps.
+
+# Arguments
+- `dtmin::T`: the smallest step `dt` for which `exp(A*dt)` will be cached. Default is `1e-3`
+- `dtmax::T`: the largest step `dt` for which `exp(A*dt)` will be cached. Default is `1.0`
+
+The fixed steps will follow a geometric progression.
+Time stepping can still happen outside the bonds (for the end step for e.g) but no cache will occur (`exp(A*dt)` getting computed each step) degrading the performances.
+The time step can be forcibly clamped within the cache range through the keywords `clamp_lower_dt` and `clamp_higher_dt`.
+
+The cached operator exponentials are also directly stored in the alorithm such that:
+
+```julia
+rkip = RKIP()
+solve(ode_prob_1, rkip, t1)
+solve(ode_prob_2, rkip, t2)
+````
+
+will reuse the precomputed exponential cached during the first `solve` call.
+This can be useful when needed to solve several time the same problem succesively from a same `A`.
+
+"""
+REFERENCE = """Zhongxi Zhang, Liang Chen, and Xiaoyi Bao, "A fourth-order Runge-Kutta in the interaction picture method for numerically solving the coupled nonlinear Schrödinger equation," Opt. Express 18, 8261-8276 (2010)"""
+
+KEYWORD_DESCRIPTION = """
+- `nb_of_cache_step::Integer`: the number of steps. Default is `100`.
+- `tableau::ExplicitRKTableau`: the Runge-Kutta Tableau to use. Default is `constructDormandPrince6()`.
+- `clamp_lower_dt::Bool`: weither to clamp proposed step to the smallest cached step in order to force the use of cached exponential, improving performance.
+	This may prevent reaching the desired tolerance. Default is `false`.
+- `clamp_higher_dt::Bool`: weither to clamp proposed step to the largest cached step in order to force the use of cached exponential, improving performance.
+	This can cause performance degredation if `integrator.dtmax` is too small. Default is `true`.
+- `use_ldiv::Bool`: weither, to use `ldiv(exp(A, t), v)` instead of caching `exp(A, -t)*v`. Reduce the memory usage but slightly less efficient. `ldiv` must be implemented. Only works for in-place problem. Default is `false`.
+"""
+
+@doc generic_solver_docstring(
+    METHOD_DESCRIPTION, "RKIP", "Adaptative Exponential Runge-Kutta",
+    REFERENCE, KEYWORD_DESCRIPTION, "")
+mutable struct RKIP{
+    tableauType <: ExplicitRKTableau, elType, dtType <: AbstractVector{elType}} <:
+               OrdinaryDiffEqAdaptiveAlgorithm
     tableau::tableauType
     dt_for_expÂ_caching::dtType
     clamp_lower_dt::Bool
     clamp_higher_dt::Bool
     use_ldiv::Bool
-    cache::Union{Nothing,RKIPCache}
+    cache::Union{Nothing, RKIPCache}
 end
 
-alg_order(alg::RKIP_ALG) = alg.tableau.order
-alg_adaptive_order(alg::RKIP_ALG) = alg.tableau.adaptiveorder
+function RKIP(dtmin::T = 1e-3, dtmax::T = 1.0; nb_of_cache_step::Int = 100,
+        tableau = constructDormandPrince6(T), clamp_lower_dt::Bool = false,
+        clamp_higher_dt::Bool = true, use_ldiv = false) where {T}
+    RKIP{
+        typeof(tableau), T, Vector{T}}(
+        tableau,
+        logrange(dtmin, dtmax, nb_of_cache_step),
+        clamp_lower_dt,
+        clamp_higher_dt,
+        use_ldiv,
+        nothing
+    )
+end
 
-has_dtnew_modification(alg::RKIP_ALG) = true
 
-function dtnew_modification(alg::RKIP_ALG{tableauType,elType,dtType}, dtnew) where {tableauType,elType,dtType}
+alg_order(alg::RKIP) = alg.tableau.order
+alg_adaptive_order(alg::RKIP) = alg.tableau.adaptiveorder
+
+has_dtnew_modification(alg::RKIP) = true
+
+function dtnew_modification(alg::RKIP{tableauType, elType, dtType},
+        dtnew) where {tableauType, elType, dtType}
     @unpack dt_for_expÂ_caching = alg
     if first(alg.dt_for_expÂ_caching) > dtnew && alg.clamp_lower_dt
         dtnew = first(alg.dt_for_expÂ_caching)
@@ -26,93 +111,22 @@ function dtnew_modification(alg::RKIP_ALG{tableauType,elType,dtType}, dtnew) whe
     return dtnew
 end
 
-dtnew_modification(_, alg::RKIP_ALG, dtnew) = dtnew_modification(alg, dtnew)
+dtnew_modification(_, alg::RKIP, dtnew) = dtnew_modification(alg, dtnew)
 
 
-"""
-Runge-Kutta in the Interaction Picture (RIKP) method.
-
-This is suited for solving semilinear problem of the form:
-
-```math
-\frac{du}{dt} =  Au + f(u,p,t)
-```
-where A is possibly stiff constant linear operator whose scaled exponential exp(Ah) can be calculated efficiently for any h.
-
-The problem is first transformed into the Interaction Picture.
-
-```math
-u_I(t) = \exp(At) u_0
-\frac{du_I}{dt} =  f_I(u_I,p,t)
-f_I(u_I,p,t) = f(exp(-At)u_I, p, t)
-```
-This new system is then solved with an explicit Runge-Kutta method.
-
-This type of problem is often encountered in semilinear parabolic PDE: heat diffusion, Schrödinger equation ...
-
-This method is only implemented for semilinear problem: `SplitODEProblem` when the first function `f1` is a `AbsractSciMLOperator` A.
-	
-The algorithm needs for A to have the following traits of the `AbsractSciMLOperator` implemented
-
-```julia
-LinearAlgebra.exp(A, t) = #...
-A(du, u, v, p, t) # for in-place problem
-A(u, v p, t) # for out-of-place problem 
-```
-
-Moreover, the resulting ```expA = exp(A, t)``` must also implement ```(expA)(u, v, p, t)``` and/or ```(expA)(du, u, v, p, t)```.
-The algorithm only works for constant `A` (does no depend on v, p, t).
-
-The algorithm will cache and resue the computed operator-exponential for a set of steps. 
-
-# Arguments
-- `dtmin::T`: the smallest step `dt` for which `exp(A*dt)` will be cached. Default is `1e-3`
-- `dtmax::T`: the largest step `dt` for which `exp(A*dt)` will be cached. Default is `1.0`
-
-The set of cached steps size `dt_for_expA_caching` will follow a geometric progression such that `first(dt_for_expA_caching) = dtmin` and `last(dt_for_expA_caching) = dtmax`.
-
-Time stepping can still happen outside the bonds but this will results in no cache (as `exp(A*h)` will be computed each step) degrading the performances. The time step can be clamped within the cache range through the args `clamp_lower_dt`  and `clamp_higher_dt`.
-
-# Optional Arguments
-- `nb_of_cache_step::Integer`: the number of steps. Default is `100`.
-- `tableau::ExplicitRKTableau`: the Runge-Kutta Tableau to use, default is `constructDormandPrince6()`.
-- `clamp_lower_dt::Bool`: weither to clamp proposed step to the smallest cached step in order to force the use of cached exponential, improving performance. 
-	This may prevent reaching the desired tolerance. Default is `false`.
-- `clamp_higher_dt::Bool`: weither to clamp proposed step to the largest cached step in order to force the use of cached exponential, improving performance. 
-	This can cause performance degredation if `integrator.dtmax` is too small. Default is `true`.
-- `use_ldiv::Bool`: weither, to use `ldiv(exp(A, t), v)` instead of caching `exp(A, -t)*v`. Reduce the memory usage but slightly less efficient. `ldiv` must be implemented. Only works for in-place problem. Default is `false`.
-
-Cached operator exponentials are stored in the alorithm meaning that in the following code:
-
-```julia
-rkip = RKIP()
-solve(ode_prob_1, rkip, t1)
-solve(ode_prob_2, rkip, t2)
-````
-
-will reuse the cache of the first solve. This can be useful when needed to solve several time the same problem but one must be sure that `A` does not change. 
-
-"""
-RKIP(dtmin::T=1e-3, dtmax::T=1.0; nb_of_cache_step::Int=100, tableau=constructDormandPrince6(T), clamp_lower_dt::Bool=false, clamp_higher_dt::Bool=true, use_ldiv=false) where {T} = RKIP_ALG{typeof(tableau),T,Vector{T}}(
-    tableau,
-    logrange(dtmin, dtmax, nb_of_cache_step),
-    clamp_lower_dt,
-    clamp_higher_dt,
-    use_ldiv,
-    nothing
-)
-
-function alg_cache(alg::RKIP_ALG, u::uType, rate_prototype, uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits, uprev, uprev2, f, t, dt, reltol, p, calck, iip) where {uType}
+function alg_cache(
+        alg::RKIP, u::uType, rate_prototype, uEltypeNoUnits, uBottomEltypeNoUnits,
+        tTypeNoUnits, uprev, uprev2, f, t, dt, reltol, p, calck, iip) where {uType}
     tmp = zero(u)
     utilde = zero(u)
     kk = [zero(u) for _ in 1:(alg.tableau.stages)]
 
-    Â = isa(f, SplitFunction) ? f.f1.f : throw(ArgumentError("RKIP is only implemented for semilinear problems"))
+    Â = isa(f, SplitFunction) ? f.f1.f :
+         throw(ArgumentError("RKIP is only implemented for semilinear problems"))
     opType = typeof(Â)
     expOpType = typeof(exp(Â, 1.0))
 
     if isnothing(alg.cache)
-
         is_cached = Vector{Bool}(undef, length(alg.dt_for_expÂ_caching))
         fill!(is_cached, false)
 
@@ -120,16 +134,22 @@ function alg_cache(alg::RKIP_ALG, u::uType, rate_prototype, uEltypeNoUnits, uBot
         c_unique = unique(c_extended) # in some tableau, there is duplicate: we only keep the unique value to save on caching time and memory
         c_index = [findfirst(==(c), c_unique) for c in c_extended] # index mapping
 
-        exp_cache = ExpCache{expOpType}(Array{expOpType,2}(undef, length(alg.dt_for_expÂ_caching), length(c_unique)), Vector{expOpType}(undef, length(c_unique)))
+        exp_cache = ExpCache{expOpType}(
+            Array{expOpType, 2}(undef, length(alg.dt_for_expÂ_caching), length(c_unique)),
+            Vector{expOpType}(undef, length(c_unique)))
 
         if !alg.use_ldiv
-            exp_cache = ExpCacheNoLdiv(exp_cache, ExpCache{expOpType}(Array{expOpType,2}(undef, length(alg.dt_for_expÂ_caching), length(c_unique)), Vector{expOpType}(undef, length(c_unique))))
+            exp_cache = ExpCacheNoLdiv(exp_cache,
+                ExpCache{expOpType}(
+                    Array{expOpType, 2}(
+                        undef, length(alg.dt_for_expÂ_caching), length(c_unique)),
+                    Vector{expOpType}(undef, length(c_unique))))
             expCacheType = ExpCacheNoLdiv{expOpType}
         else
             expCacheType = ExpCache{expOpType}
         end
 
-        alg.cache = RKIPCache{expOpType,expCacheType,tTypeNoUnits,opType,uType,iip}(
+        alg.cache = RKIPCache{expOpType, expCacheType, tTypeNoUnits, opType, uType, iip}(
             exp_cache,
             zero(tTypeNoUnits),
             is_cached,
@@ -139,8 +159,9 @@ function alg_cache(alg::RKIP_ALG, u::uType, rate_prototype, uEltypeNoUnits, uBot
             c_unique,
             c_index
         )
-    else # cache recycling 
-        alg.cache = RKIPCache{expOpType,typeof(alg.cache.exp_cache),tTypeNoUnits,opType,uType,iip}(
+    else # cache recycling
+        alg.cache = RKIPCache{
+            expOpType, typeof(alg.cache.exp_cache), tTypeNoUnits, opType, uType, iip}(
             alg.cache.exp_cache,
             alg.last_step,
             alg.cache.is_cached,
