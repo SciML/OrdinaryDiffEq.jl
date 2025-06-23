d7e8d8509edccfcc487950522fecb0f4a79a654b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-06-23 10:29:52 +0000

    Update lib/OrdinaryDiffEqCore/ext/OrdinaryDiffEqCoreMooncakeExt.jl
    Co-authored-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- lib/OrdinaryDiffEqCore/ext/OrdinaryDiffEqCoreMooncakeExt.jl
+++ lib/OrdinaryDiffEqCore/ext/OrdinaryDiffEqCoreMooncakeExt.jl
@@ -2,6 +2,6 @@ module OrdinaryDiffEqCoreMooncakeExt
 
 using OrdinaryDiffEqCore, Mooncake
 using Mooncake: @zero_adjoint, MinimalCtx
-@zero_adjoint MinimalCtx Tuple{typeof(OrdinaryDiffEqCore.ode_determine_initdt), Any, Any, Any, Any, Any, Any, Any, Any, Any}
+Mooncake.@zero_adjoint Mooncake.MinimalCtx Tuple{typeof(OrdinaryDiffEqCore.ode_determine_initdt), Any, Any, Any, Any, Any, Any, Any, Any, Any}
 
 end
