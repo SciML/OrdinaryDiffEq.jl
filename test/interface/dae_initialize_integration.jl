3348c1a965392e50d00a65ca872a0645de6d4e99
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-07-16 16:50:18 +0530

    test: make `reinit!` test independent of variable ordering

--- test/interface/dae_initialize_integration.jl
+++ test/interface/dae_initialize_integration.jl
@@ -83,13 +83,14 @@ sol = solve(prob, Rodas5P(), dt = 1e-10)
     @variables x(t) y(t)
     @parameters c1 c2
     @mtkbuild sys = System([D(x) ~ -c1 * x + c2 * y, D(y) ~ c1 * x - c2 * y], t)
-    prob = ODEProblem(sys, [1.0, 2.0], (0.0, 1.0), [c1 => 1.0, c2 => 2.0])
+    prob = ODEProblem(sys, [x => 1.0, y => 2.0], (0.0, 1.0), [c1 => 1.0, c2 => 2.0])
     @test prob.ps[Initial(x)] ≈ 1.0
     @test prob.ps[Initial(y)] ≈ 2.0
     integ = init(prob, Tsit5())
     @test integ.ps[Initial(x)] ≈ 1.0
     @test integ.ps[Initial(y)] ≈ 2.0
-    reinit!(integ, [2.0, 3.0])
+    new_u0 = ModelingToolkit.get_u0(sys, Dict(x => 2.0, y => 3.0))
+    reinit!(integ, new_u0)
     @test integ.ps[Initial(x)] ≈ 2.0
     @test integ.ps[Initial(y)] ≈ 3.0
     @test integ[x] ≈ 2.0
