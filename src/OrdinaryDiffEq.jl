e6caf438c4932f41daa6caed609f9d9eb23d7ef3
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-20 12:42:35 -0400

    fix a bunch

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -3,19 +3,18 @@ $(DocStringExtensions.README)
 """
 module OrdinaryDiffEq
 
-using Reexport
+import Reexport
+using Reexport: @reexport
 @reexport using DiffEqBase
+
+# Explicit imports for functions that are re-exported
+import DiffEqBase: DiffEqBase, init, solve, solve!, step!, addsteps!, savevalues!, terminate!
 
 import OrdinaryDiffEqCore: OrdinaryDiffEqCore,
                            trivial_limiter!, CompositeAlgorithm, alg_order,
                            ShampineCollocationInit, BrownFullBasicInit, NoInit,
                            set_new_W!, set_W_γdt!, get_W, isfirstcall, isfirststage,
                            isJcurrent, get_new_W_γdt_cutoff,
-                           DIRK, COEFFICIENT_MULTISTEP, NORDSIECK_MULTISTEP, GLM,
-                           MethodType, Divergence, VerySlowConvergence,
-                           SlowConvergence, Convergence, FastConvergence, NLStatus,
-                           TryAgain, AbstractNLSolverCache,
-                           AbstractNLSolverAlgorithm, AbstractNLSolver,
                            handle_discontinuities!, copyat_or_push!,
                            du_cache, full_cache, isfsal, ode_interpolant, u_cache,
                            AutoSwitch, has_discontinuity,
@@ -29,7 +28,7 @@ import OrdinaryDiffEqCore: OrdinaryDiffEqCore,
                            stepsize_controller!,
                            accept_step_controller, step_accept_controller!,
                            step_reject_controller!,
-                           DummyController, issplit, calculate_residuals,
+                           issplit, calculate_residuals,
                            calculate_residuals!,
                            nlsolve_f, unwrap_cache, ode_addsteps!, get_chunksize,
                            handle_callback_modifiers!,
@@ -38,41 +37,37 @@ import OrdinaryDiffEqCore: OrdinaryDiffEqCore,
                            isimplicit, initialize_d_discontinuities, isdtchangeable,
                            _searchsortedfirst,
                            _searchsortedlast,
-                           @unpack, ismultistep, DEFAULT_PRECS, isautoswitch,
+                           @unpack, ismultistep, isautoswitch,
                            get_chunksize_int,
                            _unwrap_val, alg_autodiff, concrete_jac, alg_difftype,
                            standardtag,
                            alg_extrapolates, alg_maximum_order, alg_adaptive_order,
-                           OrdinaryDiffEqCompositeAlgorithm, initialize_callbacks!,
-                           PredictiveController,
-                           get_differential_vars, alg_cache, AutoSwitchCache,
-                           InterpolationData,
-                           DEOptions, OrdinaryDiffEqAlgorithm, @cache, fsal_typeof,
-                           OrdinaryDiffEqCache,
-                           OrdinaryDiffEqAdaptiveAlgorithm, handle_dt!,
+                           initialize_callbacks!,
+                           get_differential_vars, alg_cache,
+                           @cache, fsal_typeof,
+                           handle_dt!,
                            ode_determine_initdt,
-                           loopheader!, OrdinaryDiffEqConstantCache, _loopfooter!,
+                           loopheader!, _loopfooter!,
                            isadaptive,
-                           OrdinaryDiffEqMutableCache, current_interpolant!,
+                           current_interpolant!,
                            is_mass_matrix_alg,
-                           False, True, _savevalues!, postamble!, recursivefill!,
-                           _change_t_via_interpolation!, ODEIntegrator, _ode_interpolant!,
+                           _savevalues!, postamble!, recursivefill!,
+                           _change_t_via_interpolation!, _ode_interpolant!,
                            current_interpolant, resize_nlsolver!, _ode_interpolant,
                            handle_tstop!, _postamble!, update_uprev!, resize_J_W!,
-                           DAEAlgorithm, get_fsalfirstlast, strip_cache,
-                           Sequential, BaseThreads, PolyesterThreads
+                           get_fsalfirstlast, strip_cache
 
 export CompositeAlgorithm, ShampineCollocationInit, BrownFullBasicInit, NoInit,
        AutoSwitch
 
 import OrdinaryDiffEqDifferentiation
-using OrdinaryDiffEqDifferentiation: _alg_autodiff, resize_grad_config!, dolinsolve,
-                                     wrapprecs, UJacobianWrapper, build_jac_config,
-                                     WOperator, FirstAutodiffJacError, calc_J!, calc_W!,
+using OrdinaryDiffEqDifferentiation: resize_grad_config!, dolinsolve,
+                                     wrapprecs, build_jac_config,
+                                     calc_J!, calc_W!,
                                      calc_J, calc_W, jacobian2W!, isnewton
 
 using OrdinaryDiffEqNonlinearSolve: NLNewton, NLAnderson, NLFunctional, nlsolvefail,
-                                    initial_η, NonlinearSolveAlg, compute_step!, NLSolver,
+                                    initial_η, NonlinearSolveAlg, compute_step!,
                                     nlsolve!, resize_jac_config!, anderson!, build_nlsolver,
                                     markfirststage!, anderson
 export NLNewton, NLAnderson, NLFunctional, NonlinearSolveAlg
@@ -152,7 +147,6 @@ using OrdinaryDiffEqSDIRK: ImplicitEuler, ImplicitMidpoint, Trapezoid, TRBDF2, S
                            Kvaerno5, KenCarp4, KenCarp47, KenCarp5, KenCarp58, ESDIRK54I8L2SA, SFSDIRK4,
                            SFSDIRK5, CFNLIRK3, SFSDIRK6, SFSDIRK7, SFSDIRK8,
                            ESDIRK436L2SA2, ESDIRK437L2SA, ESDIRK547L2SA2, ESDIRK659L2SA
-import OrdinaryDiffEqSDIRK: ImplicitEulerConstantCache, ImplicitEulerCache
 export ImplicitEuler, ImplicitMidpoint, Trapezoid, TRBDF2, SDIRK2, SDIRK22,
        Kvaerno3, KenCarp3, Cash4, Hairer4, Hairer42, SSPSDIRK2, Kvaerno4,
        Kvaerno5, KenCarp4, KenCarp47, KenCarp5, KenCarp58, ESDIRK54I8L2SA, SFSDIRK4,
@@ -167,9 +161,8 @@ export ABDF2, QNDF1, QBDF1, QNDF2, QBDF2, QNDF, QBDF, FBDF,
        SBDF2, SBDF3, SBDF4, MEBDF2, IMEXEuler, IMEXEulerARK,
        DImplicitEuler, DABDF2, DFBDF
 
-using OrdinaryDiffEqTsit5: Tsit5, AutoTsit5, Tsit5ConstantCache, Tsit5Cache
+using OrdinaryDiffEqTsit5: Tsit5, AutoTsit5
 export Tsit5, AutoTsit5
-import OrdinaryDiffEqTsit5: Tsit5ConstantCache, Tsit5Cache
 
 using OrdinaryDiffEqRosenbrock: Rosenbrock23, Rosenbrock32, RosShamp4, Veldd4, Velds4, GRK4T, GRK4A,
                                 Ros4LStab, ROS3P, Rodas3, Rodas23W, Rodas3P, Rodas4, Rodas42, Rodas4P, Rodas4P2,
@@ -184,7 +177,6 @@ export Rosenbrock23, Rosenbrock32, RosShamp4, Veldd4, Velds4, GRK4T, GRK4A,
        RosenbrockW6S4OS, ROS34PW1a, ROS34PW1b, ROS34PW2, ROS34PW3, ROS34PRw, ROS3PRL,
        ROS3PRL2, ROK4a,
        ROS2, ROS2PR, ROS2S, ROS3, ROS3PR, Scholz4_7
-import OrdinaryDiffEqRosenbrock: RosenbrockMutableCache
 
 using OrdinaryDiffEqDefault: DefaultODEAlgorithm
 export DefaultODEAlgorithm
@@ -201,14 +193,8 @@ export PDIRK44
 using OrdinaryDiffEqPRK: KuttaPRK2p5
 export KuttaPRK2p5
 
-using OrdinaryDiffEqHighOrderRK: TanYam7, DP8, PFRK87, TsitPap8, DP8ConstantCache, DP8Cache
 
-using OrdinaryDiffEqLowOrderRK: Euler, SplitEuler, Heun, Ralston, Midpoint, RK4,
-                                BS3, OwrenZen3, OwrenZen4, OwrenZen5, BS5,
-                                DP5, Anas5, RKO65, FRK65, RKM, MSRK5, MSRK6,
-                                PSRK4p7q6, PSRK3p5q4, PSRK3p6q5, Stepanov5, SIR54,
-                                Alshina2, Alshina3, Alshina6, AutoDP5,
-                                BS3Cache, BS3ConstantCache, RK4ConstantCache, RK4Cache
+using OrdinaryDiffEqLowOrderRK: AutoDP5
 
 using OrdinaryDiffEqFunctionMap: FunctionMap
 export FunctionMap
