d99392629957f5c08bec9be06c96d006e3dfcf2e
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-20 00:57:27 -0400

    Update test/interface/stiffness_detection_test.jl

--- test/interface/stiffness_detection_test.jl
+++ test/interface/stiffness_detection_test.jl
@@ -55,7 +55,7 @@ for (i, prob) in enumerate(probArr)
     sol = solve(prob, AutoVern7(Hairer42(); maxstiffstep = 4, maxnonstiffstep = 2),
         maxiters = 1000)
     @test length(sol.t) < 610
-    @test_broken SciMLBase.successful_retcode(sol)
+    @test_skip SciMLBase.successful_retcode(sol)
     @test is_switching_fb(sol)
     sol = solve(prob, AutoVern8(Rosenbrock23(); maxstiffstep = 4, maxnonstiffstep = 4),
         maxiters = 1000)
