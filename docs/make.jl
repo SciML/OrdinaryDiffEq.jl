11c6a6ae86075d9e67cf855e1c42d8498bf8b154
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-20 20:34:04 -0400

    Separate the pages.jl for the global docs build

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,9 @@ using Documenter, OrdinaryDiffEq
 cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
 cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
 
+# Keep pages.jl separate for the DiffEqDocs.jl build
+include("pages.jl")
+
 makedocs(sitename = "OrdinaryDiffEq.jl",
     authors = "Chris Rackauckas et al.",
     clean = true,
@@ -41,60 +44,7 @@ makedocs(sitename = "OrdinaryDiffEq.jl",
         canonical = "https://ordinarydiffeq.sciml.ai/stable/",
         size_threshold_ignore = [joinpath("semiimplicit", "Rosenbrock.md"),
             joinpath("massmatrixdae", "Rosenbrock.md")]),
-    pages = [
-        "OrdinaryDiffEq.jl: ODE solvers and utilities" => "index.md",
-        "Usage" => "usage.md",
-        "Explicit Solvers" => [
-            "explicit/Tsit5.md",
-            "explicit/Verner.md",
-            "explicit/AdamsBashforthMoulton.md",
-            "explicit/LowStorageRK.md",
-            "explicit/SSPRK.md",
-            "explicit/LowOrderRK.md",
-            "explicit/HighOrderRK.md",
-            "explicit/Feagin.md",
-            "explicit/PRK.md",
-            "explicit/QPRK.md",
-            "explicit/TaylorSeries.md",
-            "explicit/Extrapolation.md"
-        ],
-        "Semi-Implicit Solvers" => [
-            "semiimplicit/Rosenbrock.md",
-            "semiimplicit/StabilizedRK.md",
-            "semiimplicit/ExponentialRK.md"
-        ],
-        "Implicit Solvers" => [
-            "implicit/SDIRK.md",
-            "implicit/FIRK.md",
-            "implicit/BDF.md",
-            "implicit/Extrapolation.md",
-            "implicit/PDIRK.md",
-            "implicit/Nordsieck.md"
-        ],
-        "IMEX Solvers" => [
-            "imex/IMEXMultistep.md",
-            "imex/StabilizedIRK.md",
-            "imex/IMEXBDF.md"
-        ],
-        "Dynamical ODE Explicit Solvers" => [
-            "dynamicalodeexplicit/RKN.md",
-            "dynamicalodeexplicit/SymplecticRK.md"
-        ],
-        "Semilinear ODE Solvers" => [
-            "semilinear/ExponentialRK.md",
-            "semilinear/Linear.md"
-        ],
-        "Mass Matrix DAE Solvers" => [
-            "massmatrixdae/Rosenbrock.md",
-            "massmatrixdae/BDF.md"
-        ],
-        "Fully Implicit DAE Solvers" => [
-            "fullyimplicitdae/BDF.md"
-        ],
-        "Misc Solvers" => [
-            "misc.md"
-        ]
-    ])
+    pages = pages)
 
 deploydocs(repo = "github.com/SciML/OrdinaryDiffEq.jl";
     push_preview = true)
