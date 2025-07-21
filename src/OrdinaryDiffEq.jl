e6a8a8df6d3eab28afe80f73c040309fab3db515
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-21 09:52:50 -0400

    Try a few changes

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -149,7 +149,16 @@ using OrdinaryDiffEqPRK: KuttaPRK2p5
 export KuttaPRK2p5
 
 
-using OrdinaryDiffEqLowOrderRK: AutoDP5
+using OrdinaryDiffEqLowOrderRK: Euler, SplitEuler, Heun, Ralston, Midpoint, RK4,
+                                BS3, OwrenZen3, OwrenZen4, OwrenZen5, BS5,
+                                DP5, Anas5, RKO65, FRK65, RKM, MSRK5, MSRK6,
+                                PSRK4p7q6, PSRK3p5q4, PSRK3p6q5, Stepanov5, SIR54,
+                                Alshina2, Alshina3, Alshina6, AutoDP5
+export Euler, SplitEuler, Heun, Ralston, Midpoint, RK4,
+       BS3, OwrenZen3, OwrenZen4, OwrenZen5, BS5,
+       DP5, Anas5, RKO65, FRK65, RKM, MSRK5, MSRK6,
+       PSRK4p7q6, PSRK3p5q4, PSRK3p6q5, Stepanov5, SIR54,
+       Alshina2, Alshina3, Alshina6, AutoDP5
 
 using OrdinaryDiffEqFunctionMap: FunctionMap
 export FunctionMap
