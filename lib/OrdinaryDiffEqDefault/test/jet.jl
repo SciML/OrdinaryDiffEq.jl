24a095c28ffe2dea007274dd6b8b66779eab16f9
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-31 19:54:08 -0400

    No JET on pre

--- lib/OrdinaryDiffEqDefault/test/jet.jl
+++ lib/OrdinaryDiffEqDefault/test/jet.jl
@@ -1,7 +1,9 @@
 import OrdinaryDiffEqDefault
 using JET
 
-@testset "JET Tests" begin
-    test_package(
-        OrdinaryDiffEqDefault, target_defined_modules = true, mode = :typo)
+if isempty(VERSION.prerelease)
+    @testset "JET Tests" begin
+        test_package(
+            OrdinaryDiffEqDefault, target_defined_modules = true, mode = :typo)
+    end
 end
