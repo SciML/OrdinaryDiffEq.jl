6af5fbb224727da0697a8f463d81140307ab82b6
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-08-04 20:28:42 -0400

    Update OrdinaryDiffEqNonlinearSolve.jl

--- lib/OrdinaryDiffEqNonlinearSolve/src/OrdinaryDiffEqNonlinearSolve.jl
+++ lib/OrdinaryDiffEqNonlinearSolve/src/OrdinaryDiffEqNonlinearSolve.jl
@@ -18,7 +18,7 @@ using FastBroadcast: @..
 import FastClosures: @closure
 using LinearAlgebra: UniformScaling, UpperTriangular, givens, cond, dot, lmul!, axpy!
 import LinearAlgebra
-import ArrayInterface: ismutable, restructure
+import ArrayInterface: ArrayInterface, ismutable, restructure
 import LinearSolve: OperatorAssumptions
 import LinearSolve
 import ForwardDiff: pickchunksize
