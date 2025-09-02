dc4299be3556c8096372cf5ff3f0d25190ca0727
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-09-02 12:55:53 -0400

    Update OrdinaryDiffEq.jl

--- src/OrdinaryDiffEq.jl
+++ src/OrdinaryDiffEq.jl
@@ -168,7 +168,7 @@ using OrdinaryDiffEqRosenbrock: Rosenbrock23, Rosenbrock32, RosShamp4, Veldd4, V
                                 GRK4T, GRK4A,
                                 Ros4LStab, ROS3P, Rodas3, Rodas23W, Rodas3P, Rodas4,
                                 Rodas42, Rodas4P, Rodas4P2,
-                                Rodas5, Rodas5P, Rodas5Pe, Rodas5Pr, Rodas6P,
+                                Rodas5, Rodas5P, Rodas5Pe, Rodas5Pr,
                                 RosenbrockW6S4OS, ROS34PW1a, ROS34PW1b, ROS34PW2, ROS34PW3,
                                 ROS34PRw, ROS3PRL,
                                 ROS3PRL2, ROK4a,
