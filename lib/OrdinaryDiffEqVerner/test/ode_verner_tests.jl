fad691485e3160e736922da32b9872f545cee224
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-10-22 15:48:54 +0000

    Update lib/OrdinaryDiffEqVerner/test/ode_verner_tests.jl

--- lib/OrdinaryDiffEqVerner/test/ode_verner_tests.jl
+++ lib/OrdinaryDiffEqVerner/test/ode_verner_tests.jl
@@ -1,6 +1,5 @@
 using OrdinaryDiffEqVerner, OrdinaryDiffEqCore
 using DiffEqBase, DiffEqDevTools, LinearAlgebra, Random, Test, Plots
-import OrdinaryDiffEqLowStorageRK
 import ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinear, prob_ode_bigfloatlinear, prob_ode_bigfloat2Dlinear
 
 # Problem mappings
