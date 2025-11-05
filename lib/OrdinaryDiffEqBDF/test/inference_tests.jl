f5e5d1d14a3ae114dd199ecf8a6647efc04b3489
jClugstor <jadonclugston@gmail.com>
2025-11-05 12:04:20 -0500

    fix up some verbosity and linear verbosity

--- lib/OrdinaryDiffEqBDF/test/inference_tests.jl
+++ lib/OrdinaryDiffEqBDF/test/inference_tests.jl
@@ -1,23 +1,19 @@
 using OrdinaryDiffEqBDF, ADTypes, Test
 using NonlinearSolve: TrustRegion
+using SciMLLogging: Standard
 
 prob = ODEProblem((du, u, p, t) -> du .= u, zeros(1), (0.0, 1.0))
-nlalg = FBDF(
-    autodiff = false,
-    nlsolve = OrdinaryDiffEqBDF.NonlinearSolveAlg(TrustRegion(autodiff = AutoFiniteDiff()))
-)
+nlalg = FBDF(autodiff = false,
+    nlsolve = OrdinaryDiffEqBDF.NonlinearSolveAlg(TrustRegion(autodiff = AutoFiniteDiff())))
 basicalg = FBDF(autodiff = false)
 basicalgad = FBDF()
 
 nlsolver = @inferred OrdinaryDiffEqBDF.build_nlsolver(
     basicalg, prob.u0, prob.u0, prob.p, 0.0, 0.0, prob.f, prob.u0, Float64,
-    Float64, Float64, 0.0, 0.0, Val(true)
-)
+    Float64, Float64, 0.0, 0.0, Val(true), Standard())
 nlsolver = @inferred OrdinaryDiffEqBDF.build_nlsolver(
     nlalg, prob.u0, prob.u0, prob.p, 0.0, 0.0, prob.f, prob.u0, Float64,
-    Float64, Float64, 0.0, 0.0, Val(true)
-)
+    Float64, Float64, 0.0, 0.0, Val(true), Standard())
 nlsolver = @test_throws Any @inferred OrdinaryDiffEqBDF.build_nlsolver(
     basicalgad, prob.u0, prob.u0, prob.p, 0.0, 0.0, prob.f, prob.u0, Float64,
-    Float64, Float64, 0.0, 0.0, Val(true)
-)
+    Float64, Float64, 0.0, 0.0, Val(true), Standard())
