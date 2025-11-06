6c2922a14010610149fcc3544f3629cf0737376a
jClugstor <jadonclugston@gmail.com>
2025-11-05 21:53:18 -0500

    remove unused SciMLMessages

--- lib/OrdinaryDiffEqPRK/src/OrdinaryDiffEqPRK.jl
+++ lib/OrdinaryDiffEqPRK/src/OrdinaryDiffEqPRK.jl
@@ -4,7 +4,7 @@ import OrdinaryDiffEqCore: OrdinaryDiffEqAlgorithm, alg_order, OrdinaryDiffEqMut
                            OrdinaryDiffEqConstantCache, constvalue, @cache,
                            alg_cache, get_fsalfirstlast,
                            unwrap_alg, perform_step!, @threaded, initialize!, isthreaded,
-                           full_cache, generic_solver_docstring, @SciMLMessage
+                           full_cache, generic_solver_docstring
 import MuladdMacro: @muladd
 import FastBroadcast: @..
 using Polyester
--- lib/OrdinaryDiffEqQPRK/src/OrdinaryDiffEqQPRK.jl
+++ lib/OrdinaryDiffEqQPRK/src/OrdinaryDiffEqQPRK.jl
@@ -7,7 +7,7 @@ import OrdinaryDiffEqCore: OrdinaryDiffEqAdaptiveAlgorithm, OrdinaryDiffEqConsta
                            trivial_limiter!, alg_cache, alg_order, initialize!,
                            perform_step!, get_fsalfirstlast,
                            constvalue, calculate_residuals!, calculate_residuals,
-                           full_cache, @SciMLMessage
+                           full_cache
 using Static: False
 using MuladdMacro, FastBroadcast
 using RecursiveArrayTools: recursive_unitless_bottom_eltype, recursivefill!
