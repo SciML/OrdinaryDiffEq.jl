9f162fa3086e1e64e705208ed0d87e2f65d2bb23
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-11-06 05:50:00 -0500

    Delete lib/OrdinaryDiffEqSDIRK/src/sdirk_perform_step.jl

--- lib/OrdinaryDiffEqSDIRK/src/sdirk_perform_step.jl
+++
@@ -1,8 +0,0 @@
-# Legacy SDIRK perform_step implementations
-# 
-# These have been replaced by the unified tableau-based generic implementations in:
-# - generic_sdirk_perform_step.jl (contains the unified perform_step! functions)
-# - unified_sdirk_tableaus.jl (contains the unified tableau structures)
-#
-# All SDIRK methods now use the generic implementation with their respective tableaus,
-# eliminating code duplication while maintaining full backward compatibility and performance.
