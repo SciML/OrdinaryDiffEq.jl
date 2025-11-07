74e1ccbb87c99955fcc601e680ecf8a206b520de
jClugstor <jadonclugston@gmail.com>
2025-11-07 15:25:33 -0500

    add another dep path for oop

--- lib/OrdinaryDiffEqCore/src/deprecated.jl
+++ lib/OrdinaryDiffEqCore/src/deprecated.jl
@@ -6,3 +6,11 @@ function alg_cache(alg, u, rate_prototype, ::Type{uEltypeNoUnits},
     alg_cache(alg, u, rate_prototype, uEltypeNoUnits, uBottomEltypeNoUnits,
         tTypeNoUnits, uprev, uprev2, f, t, dt, reltol, p, calck, Val(true), ODEVerbosity())
 end
+
+function alg_cache(alg, u, rate_prototype, ::Type{uEltypeNoUnits},
+        ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+        dt, reltol, p, calck,
+        ::Val{false}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    alg_cache(alg, u, rate_prototype, uEltypeNoUnits, uBottomEltypeNoUnits,
+        tTypeNoUnits, uprev, uprev2, f, t, dt, reltol, p, calck, Val(false), ODEVerbosity())
+end
