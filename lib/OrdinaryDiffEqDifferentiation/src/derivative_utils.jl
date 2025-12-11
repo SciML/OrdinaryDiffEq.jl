652008c8d85a885e38c625405fdc001bee12eeb5
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-12-11 18:18:17 -0100

    Patch OrdinaryDiffEqDifferentiation for verbosity form change
    This should just be short term until https://github.com/SciML/OrdinaryDiffEq.jl/pull/2916 gets rid of the the interface break here.

--- lib/OrdinaryDiffEqDifferentiation/src/derivative_utils.jl
+++ lib/OrdinaryDiffEqDifferentiation/src/derivative_utils.jl
@@ -866,10 +866,10 @@ build_uf(alg, nf, t, p, ::Val{false}) = UDerivativeWrapper(nf, t, p)
 function LinearSolve.init_cacheval(
         alg::LinearSolve.DefaultLinearSolver, A::WOperator, b, u,
         Pl, Pr,
-        maxiters::Int, abstol, reltol, verbose::Bool,
+        maxiters::Int, abstol, reltol, verbose::Union{LinearVerbosity,Bool},
         assumptions::OperatorAssumptions)
     LinearSolve.init_cacheval(alg, A.J, b, u, Pl, Pr,
-        maxiters::Int, abstol, reltol, verbose::Bool,
+        maxiters::Int, abstol, reltol, verbose::Union{LinearVerbosity,Bool},
         assumptions::OperatorAssumptions)
 end
 
