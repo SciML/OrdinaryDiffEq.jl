a6551064e909c9769fb3da69559519883dc20f62
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-12-12 10:40:56 -0100

    other dispatch

--- lib/OrdinaryDiffEqDifferentiation/src/derivative_utils.jl
+++ lib/OrdinaryDiffEqDifferentiation/src/derivative_utils.jl
@@ -897,10 +897,10 @@ for alg in [LinearSolve.AppleAccelerateLUFactorization,
     LinearSolve.SparspakFactorization,
     LinearSolve.UMFPACKFactorization]
     @eval function LinearSolve.init_cacheval(alg::$alg, A::WOperator, b, u, Pl, Pr,
-            maxiters::Int, abstol, reltol, verbose::Bool,
+            maxiters::Int, abstol, reltol, verbose::Union{LinearVerbosity,Bool},
             assumptions::OperatorAssumptions)
         LinearSolve.init_cacheval(alg, A.J, b, u, Pl, Pr,
-            maxiters::Int, abstol, reltol, verbose::Bool,
+            maxiters::Int, abstol, reltol, verbose::Union{LinearVerbosity,Bool},
             assumptions::OperatorAssumptions)
     end
 end
