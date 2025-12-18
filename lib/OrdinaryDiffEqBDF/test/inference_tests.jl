19b960994ab184f866e753bff3eb2e927fd168d8
jClugstor <jadonclugston@gmail.com>
2025-12-18 09:44:23 -0500

    use ODEVerbosity in build_nlsolver for inference tests

--- lib/OrdinaryDiffEqBDF/test/inference_tests.jl
+++ lib/OrdinaryDiffEqBDF/test/inference_tests.jl
@@ -1,6 +1,7 @@
 using OrdinaryDiffEqBDF, ADTypes, Test
 using NonlinearSolve: TrustRegion
 import OrdinaryDiffEqCore.SciMLLogging as SciMLLogging
+using OrdinaryDiffEqCore: ODEVerbosity
 
 prob = ODEProblem((du, u, p, t) -> du .= u, zeros(1), (0.0, 1.0))
 nlalg = FBDF(autodiff = false,
@@ -10,10 +11,10 @@ basicalgad = FBDF()
 
 nlsolver = @inferred OrdinaryDiffEqBDF.build_nlsolver(
     basicalg, prob.u0, prob.u0, prob.p, 0.0, 0.0, prob.f, prob.u0, Float64,
-    Float64, Float64, 0.0, 0.0, Val(true), SciMLLogging.Standard())
+    Float64, Float64, 0.0, 0.0, Val(true), ODEVerbosity())
 nlsolver = @inferred OrdinaryDiffEqBDF.build_nlsolver(
     nlalg, prob.u0, prob.u0, prob.p, 0.0, 0.0, prob.f, prob.u0, Float64,
-    Float64, Float64, 0.0, 0.0, Val(true), SciMLLogging.Standard())
+    Float64, Float64, 0.0, 0.0, Val(true), ODEVerbosity())
 nlsolver = @test_throws Any @inferred OrdinaryDiffEqBDF.build_nlsolver(
     basicalgad, prob.u0, prob.u0, prob.p, 0.0, 0.0, prob.f, prob.u0, Float64,
-    Float64, Float64, 0.0, 0.0, Val(true), SciMLLogging.Standard())
+    Float64, Float64, 0.0, 0.0, Val(true), ODEVerbosity())
