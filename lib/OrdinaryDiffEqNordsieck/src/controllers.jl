c32b956dc1db5364bda49536aedd7af6a260fd31
termi-official <termi-official@users.noreply.github.com>
2026-01-05 17:47:20 +0100

    Update Nordsieck methods

--- lib/OrdinaryDiffEqNordsieck/src/controllers.jl
+++ lib/OrdinaryDiffEqNordsieck/src/controllers.jl
@@ -1,25 +1,22 @@
-struct DummyController <: AbstractController
-end
-
 # JVODE
 function stepsize_controller!(integrator, alg::JVODE)
     if iszero(integrator.EEst)
-        η = integrator.opts.qmax
+        η = alg.qmax
     else
         η = integrator.cache.η
-        integrator.qold = η
+        integrator.cache.ηold = η
     end
     return η
 end
 
 function step_accept_controller!(integrator, alg::JVODE, η)
     q = inv(η)
-    if q <= integrator.opts.qsteady_max && q >= integrator.opts.qsteady_min
+    if q <= alg.qsteady_max && q >= alg.qsteady_min
         q = one(q)
     end
     return integrator.dt / q  # dtnew
 end
 
 function step_reject_controller!(integrator, alg::JVODE)
-    return integrator.dt *= integrator.qold
+    return integrator.dt *= integrator.cache.η
 end
