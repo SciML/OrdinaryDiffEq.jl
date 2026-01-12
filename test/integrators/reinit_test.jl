d01f52c1eda1ca981d65fb3fee6fe702eb776dd3
jClugstor <jadonclugston@gmail.com>
2026-01-12 12:06:14 -0500

    add tests for kwargs

--- test/integrators/reinit_test.jl
+++ test/integrators/reinit_test.jl
@@ -114,3 +114,49 @@ end
     allocs = @allocated reinit!(integrator; reset_dt = false)
     @test allocs == 0
 end
+
+@testset "reinit with new tstops" begin
+    prob = prob_ode_2Dlinear
+    integrator = init(prob, Tsit5(); dt = 1 // 2^(4), tstops = [1 // 2])
+    solve!(integrator)
+
+    # Reinit with different tstops
+    new_tstops = [1 // 4, 3 // 4]
+    reinit!(integrator; tstops = new_tstops)
+    solve!(integrator)
+
+    # Check that solution includes the new tstops
+    @test 1 // 4 ∈ integrator.sol.t
+    @test 3 // 4 ∈ integrator.sol.t
+end
+
+@testset "reinit with new saveat" begin
+    prob = prob_ode_2Dlinear
+    integrator = init(prob, Tsit5(); dt = 1 // 2^(4), saveat = [1 // 4])
+    solve!(integrator)
+
+    # Reinit with different saveat
+    new_saveat = [1 // 8, 3 // 8, 5 // 8, 7 // 8]
+    reinit!(integrator; saveat = new_saveat)
+    solve!(integrator)
+
+    # Check that solution includes the new saveat points
+    @test 1 // 8 ∈ integrator.sol.t
+    @test 3 // 8 ∈ integrator.sol.t
+    @test 5 // 8 ∈ integrator.sol.t
+    @test 7 // 8 ∈ integrator.sol.t
+end
+
+@testset "reinit with new d_discontinuities" begin
+    prob = prob_ode_2Dlinear
+    integrator = init(prob, Tsit5(); dt = 1 // 2^(4), d_discontinuities = [1 // 2])
+    solve!(integrator)
+
+    # Reinit with different d_discontinuities
+    new_d_discontinuities = [1 // 3, 2 // 3]
+    reinit!(integrator; d_discontinuities = new_d_discontinuities)
+    solve!(integrator)
+
+    # The solver should handle the discontinuities correctly
+    @test integrator.sol.retcode == ReturnCode.Success
+end
