c9fcc86afcdf3f8655fff2a24c2d85566f3fb87d
termi-official <termi-official@users.noreply.github.com>
2026-02-11 19:36:05 +0100

    Extend inference test

--- test/regression/inference.jl
+++ test/regression/inference.jl
@@ -5,15 +5,9 @@ using Test
     prob = ODEProblemLibrary.prob_ode_linear
     prob2D = ODEProblemLibrary.prob_ode_2Dlinear
 
-    inferred = [BS3(), Tsit5(), RK4(), Vern6()]
+    inferred = [BS3(), Tsit5(), RK4(), Vern6(), SDIRK2(), TRBDF2(), KenCarp4(), Rosenbrock23(), Rodas4(), QNDF1(), QNDF()]
     for alg in inferred
         @inferred init(prob, alg)
         @inferred init(prob2D, alg)
     end
-
-    #notinferred = [SDIRK2(), TRBDF2(), KenCarp4(), Rosenbrock23(), Rodas4()]
-    #for alg in notinferred
-    #    @test_broken @inferred init(prob, alg)
-    #    @test_broken @inferred init(prob2D, alg)
-    #end
 end
