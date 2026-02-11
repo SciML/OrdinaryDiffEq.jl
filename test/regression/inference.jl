a54297bb4c5564cbef574f56b74ce77f4b52a552
termi-official <termi-official@users.noreply.github.com>
2026-02-11 20:52:57 +0100

    Revert inference tests

--- test/regression/inference.jl
+++ test/regression/inference.jl
@@ -5,9 +5,15 @@ using Test
     prob = ODEProblemLibrary.prob_ode_linear
     prob2D = ODEProblemLibrary.prob_ode_2Dlinear
 
-    inferred = [BS3(), Tsit5(), RK4(), Vern6(), SDIRK2(), TRBDF2(), KenCarp4(), Rosenbrock23(), Rodas4(), QNDF1(), QNDF()]
+    inferred = [BS3(), Tsit5(), RK4(), Vern6()]
     for alg in inferred
         @inferred init(prob, alg)
         @inferred init(prob2D, alg)
     end
+
+    #notinferred = [SDIRK2(), TRBDF2(), KenCarp4(), Rosenbrock23(), Rodas4()]
+    #for alg in notinferred
+    #    @test_broken @inferred init(prob, alg)
+    #    @test_broken @inferred init(prob2D, alg)
+    #end
 end
