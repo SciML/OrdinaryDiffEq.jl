78d51c813e4829501d2e335f9f0eb791bf763438
termi-official <termi-official@users.noreply.github.com>
2026-02-24 18:57:58 +0100

    Update test to new API

--- test/interface/controllers.jl
+++ test/interface/controllers.jl
@@ -28,8 +28,8 @@ sol = solve(prob, RDPK3SpFSAL49())
     # Exponential decay - the auto dt init may pick a very conservative initial dt
     prob_exp = ODEProblem((u, p, t) -> -u, 1.0, (0.0, 100.0))
 
-    # Custom qmax_first_step via New* controller API
-    ctrl = OrdinaryDiffEqCore.NewPIController(Tsit5(), qmax_first_step = 500)
+    # Custom qmax_first_step via controller API
+    ctrl = OrdinaryDiffEqCore.PIController(Tsit5(), qmax_first_step = 500)
     @test ctrl.qmax_first_step == 500.0
 
     # Verify that qmax_first_step=10000 still produces correct solutions
@@ -40,7 +40,7 @@ sol = solve(prob, RDPK3SpFSAL49())
 
         # With a very restrictive qmax_first_step (same as normal qmax),
         # the solution should still be correct
-        ctrl_restricted = OrdinaryDiffEqCore.NewPIController(alg, qmax_first_step = 10)
+        ctrl_restricted = OrdinaryDiffEqCore.PIController(alg, qmax_first_step = 10)
         sol_restricted = solve(prob_exp, alg, controller = ctrl_restricted)
         @test sol_restricted.retcode == ReturnCode.Success
         @test isapprox(sol_restricted[end], exp(-100.0), atol = 1.0e-6)
@@ -54,18 +54,18 @@ sol = solve(prob, RDPK3SpFSAL49())
     integrator.success_iter = 1
     @test OrdinaryDiffEqCore.get_current_qmax(integrator, 10.0) == 10.0
 
-    # Test get_current_qmax with New* controller (custom qmax_first_step)
-    ctrl_new = OrdinaryDiffEqCore.NewPIController(Tsit5(), qmax_first_step = 500)
+    # Test get_current_qmax with controller (custom qmax_first_step)
+    ctrl_new = OrdinaryDiffEqCore.PIController(Tsit5(), qmax_first_step = 500)
     integrator_new = init(prob_exp, Tsit5(), controller = ctrl_new)
     @test integrator_new.success_iter == 0
     @test OrdinaryDiffEqCore.get_current_qmax(integrator_new, 10.0) == 500.0
     integrator_new.success_iter = 1
     @test OrdinaryDiffEqCore.get_current_qmax(integrator_new, 10.0) == 10.0
 
-    # Test that all New* controllers have qmax_first_step field
-    ctrl_i = OrdinaryDiffEqCore.NewIController(Tsit5(), qmax_first_step = 200)
+    # Test that all controllers have qmax_first_step field
+    ctrl_i = OrdinaryDiffEqCore.IController(Tsit5(), qmax_first_step = 200)
     @test ctrl_i.qmax_first_step == 200.0
 
-    ctrl_pred = OrdinaryDiffEqCore.NewPredictiveController(Tsit5(), qmax_first_step = 300)
+    ctrl_pred = OrdinaryDiffEqCore.PredictiveController(Tsit5(), qmax_first_step = 300)
     @test ctrl_pred.qmax_first_step == 300.0
 end
