825f9e8df3cf14c9f8a65879339e870b2dbf8c93
Songchen Tan <i@tansongchen.com>
2026-03-06 14:39:29 -0500

    Remove nlf; reduce allocation; expand to order 2, 3, 4

--- lib/OrdinaryDiffEqTaylorSeriesImplicit/src/TaylorSeries_caches.jl
+++ lib/OrdinaryDiffEqTaylorSeriesImplicit/src/TaylorSeries_caches.jl
@@ -1,15 +1,9 @@
 import OrdinaryDiffEqTaylorSeries: build_jet, build_propagator
 
-abstract type ImplicitTaylorMutableCache <: OrdinaryDiffEqMutableCache end
-abstract type ImplicitTaylorConstantCache <: OrdinaryDiffEqConstantCache end
-function get_fsalfirstlast(cache::ImplicitTaylorMutableCache, u)
-    return (cache.fsalfirst, cache.fsalfirst)
-end
-
-@cache mutable struct ImplicitTaylor1Cache{
+@cache mutable struct ImplicitTaylorCache{
         T, uType, propagatorType, d_propagatorType, jacobianType, rateType, uNoUnitsType, F1, Tol, StepLimiter,
     } <:
-    ImplicitTaylorMutableCache
+    OrdinaryDiffEqMutableCache
     μ::T
     propagator::propagatorType
     d_propagator::d_propagatorType
@@ -31,14 +25,15 @@ end
 end
 
 function alg_cache(
-        alg::ImplicitTaylor1, u, rate_prototype, ::Type{uEltypeNoUnits},
+        alg::ImplicitTaylor, u, rate_prototype, ::Type{uEltypeNoUnits},
         ::Type{uBottomEltypeNoUnits},
         ::Type{tTypeNoUnits}, uprev, uprev2, f, t, dt, reltol, p, calck,
         ::Val{true}, verbose
     ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
-    propagator_all, d_propagator_all = build_propagator(f, p, Val(1), length(u))
-    propagator, _ = propagator_all
-    d_propagator, _ = d_propagator_all
+    propagator_all, d_propagator_all = build_propagator(f, p, alg.order, length(u))
+    # get the iip version
+    _, propagator = propagator_all
+    _, d_propagator = d_propagator_all
     fsalfirst = zero(rate_prototype)
     atmp = similar(u, uEltypeNoUnits)
     recursivefill!(atmp, false)
@@ -59,18 +54,22 @@ function alg_cache(
     iter = 10000
     status = Convergence
 
-    return ImplicitTaylor1Cache(
+    return ImplicitTaylorCache(
         alg.μ, propagator, d_propagator, u, uprev, uprev2, ut, J, utilde, tmp, atmp, fsalfirst, linsolve,
         κ, ηold, status, iter, alg.step_limiter!
     )
 end
 
-mutable struct ImplicitTaylor1ConstantCache{N} <: ImplicitTaylorConstantCache
+function get_fsalfirstlast(cache::ImplicitTaylorCache, u)
+    return (cache.fsalfirst, cache.fsalfirst)
+end
+
+mutable struct ImplicitTaylorConstantCache{N} <: OrdinaryDiffEqConstantCache
     nlsolver::N
 end
 
 function alg_cache(
-        alg::ImplicitTaylor1, u, rate_prototype, ::Type{uEltypeNoUnits},
+        alg::ImplicitTaylor, u, rate_prototype, ::Type{uEltypeNoUnits},
         ::Type{uBottomEltypeNoUnits},
         ::Type{tTypeNoUnits}, uprev, uprev2, f, t, dt, reltol, p, calck,
         ::Val{false}
@@ -80,11 +79,11 @@ function alg_cache(
         alg, u, uprev, p, t, dt, f, rate_prototype, uEltypeNoUnits,
         uBottomEltypeNoUnits, tTypeNoUnits, γ, c, Val(false)
     )
-    return ImplicitTaylor1ConstantCache(nlsolver)
+    return ImplicitTaylorConstantCache(nlsolver)
 end
 
 @inline function SciMLBase.get_tmp_cache(
-        integrator, alg::Union{ImplicitTaylor1},
+        integrator, alg::Union{ImplicitTaylor},
         cache::OrdinaryDiffEqMutableCache
     )
     return (cache.tmp, cache.atmp)
