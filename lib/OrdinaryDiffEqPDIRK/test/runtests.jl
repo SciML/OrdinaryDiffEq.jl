4d2139862432da1df5f68a2bb20ee4b9c8c0d55d
Oscar Smith <oscardssmith@gmail.com>
2026-03-22 00:36:22 -0400

    fix SDIRK tests

--- lib/OrdinaryDiffEqPDIRK/test/runtests.jl
+++ lib/OrdinaryDiffEqPDIRK/test/runtests.jl
@@ -6,3 +6,5 @@ const TEST_GROUP = get(ENV, "ODEDIFFEQ_TEST_GROUP", "ALL")
 if TEST_GROUP != "Core" && isempty(VERSION.prerelease)
     @time @safetestset "JET Tests" include("jet.jl")
 end
+
+@time @safetestset "Convergance Tests" include("pdirk_convergance_tests.jl")
