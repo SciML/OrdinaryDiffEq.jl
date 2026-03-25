74f4fbc315b4b5e49974d90bb922eb2dc325bf58
Harsh Singh <harsh@Harshs-MacBook-Air.local>
2026-03-25 13:52:00 +0530

    Fix ARS343 embedded error estimator (satisfy order conditions for adaptive stepping)

--- lib/OrdinaryDiffEqSDIRK/src/imex_tableaus.jl
+++ lib/OrdinaryDiffEqSDIRK/src/imex_tableaus.jl
@@ -305,14 +305,8 @@ function ARS343Tableau(T::Type{<:CompiledFloats}, T2::Type{<:CompiledFloats})
 
     c_vec = T2[zero(T2), c2, c3, c4]
 
-    btilde_vec = T[
-        zero(T), convert(T, 1.20849664917601), convert(T, -0.644363170684469),
-        convert(T, -0.564133478491541),
-    ]
-    ebtilde_vec = T[
-        zero(T), convert(T, 1.20849664917601), convert(T, -0.644363170684469),
-        convert(T, -0.564133478491541),
-    ]
+    btilde_vec = T[zero(T), γ, -2γ, γ]
+    ebtilde_vec = T[zero(T), γ, -2γ, γ]
 
     α_mat = zeros(T2, s, s)
 
@@ -760,8 +754,8 @@ function ARS343Tableau(T, T2)
 
     c_vec = T2[zero(T2), convert(T2, c2), convert(T2, c3), convert(T2, c4)]
 
-    btilde_vec = bi_vec .- T[zero(T), zero(T), zero(T), one(T)]
-    ebtilde_vec = be_vec .- T[zero(T), zero(T), zero(T), one(T)]
+    btilde_vec = T[zero(T), γ, -2γ, γ]
+    ebtilde_vec = T[zero(T), γ, -2γ, γ]
 
     α_mat = zeros(T2, s, s)
 
