SciML/OrdinaryDiffEq.jl

commit 023d7c3960382f47857e5efa09c0c02bcf9e7311

TSRKC3 method (#3420)
* rkc method fix

* tsrck3 method implementation

* rkc tests

* minor refactor

* md file update

* minor misprint fix

* refactor

* version rollback

* Apply suggestion from @ChrisRackauckas

* rkc tests update

* Apply suggestion from @ChrisRackauckas

---------

Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

lib/OrdinaryDiffEqStabilizedRK/src/rkc_caches.jl
@@ -315,3 +315,52 @@ function alg_cache(
     ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
     return SERK2ConstantCache(u)
 end
+
+mutable struct TSRKC3ConstantCache{zType, tTypeNoUnits} <: OrdinaryDiffEqConstantCache
+    #to match the types to call maxeig!
+    zprev::zType
+    tsw0::tTypeNoUnits
+    acoshtsw0::tTypeNoUnits
+end
+@cache struct TSRKC3Cache{uType, rateType, uNoUnitsType, C <: TSRKC3ConstantCache} <:
+    StabilizedRKMutableCache
+    u::uType
+    uprev::uType
+    gprev::uType
+    gprev2::uType
+    tmp::uType
+    atmp::uNoUnitsType
+    fsalfirst::rateType
+    k::rateType
+    constantcache::C
+end
+
+function alg_cache(
+        alg::TSRKC3, u, rate_prototype, ::Type{uEltypeNoUnits},
+        ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+        dt, reltol, p, calck,
+        ::Val{true}, verbose
+    ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    tsw0 = tTypeNoUnits(1.25)
+    acoshtsw0 = tTypeNoUnits(acosh(1.25))
+    constantcache = TSRKC3ConstantCache(u, tsw0, acoshtsw0)
+    gprev = zero(u)
+    gprev2 = zero(u)
+    tmp = zero(u)
+    atmp = similar(u, uEltypeNoUnits)
+    recursivefill!(atmp, false)
+    fsalfirst = zero(rate_prototype)
+    k = zero(rate_prototype)
+    return TSRKC3Cache(u, uprev, gprev, gprev2, tmp, atmp, fsalfirst, k, constantcache)
+end
+
+function alg_cache(
+        alg::TSRKC3, u, rate_prototype, ::Type{uEltypeNoUnits},
+        ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits}, uprev, uprev2, f, t,
+        dt, reltol, p, calck,
+        ::Val{false}, verbose
+    ) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
+    tsw0 = tTypeNoUnits(1.25)
+    acoshtsw0 = tTypeNoUnits(acosh(1.25))
+    return TSRKC3ConstantCache(u, tsw0, acoshtsw0)
+end

lib/OrdinaryDiffEqStabilizedRK/src/rkc_perform_step.jl
@@ -451,11 +451,8 @@ end
     (; t, dt, uprev, u, f, p, fsalfirst) = integrator
     alg = unwrap_alg(integrator, true)
     alg.eigen_est === nothing ? maxeig!(integrator, cache) : alg.eigen_est(integrator)
-    # The the number of degree for Chebyshev polynomial
-    #maxm = max(2,Int(floor(sqrt(integrator.opts.internalnorm(integrator.opts.reltol,t)/(10*eps(integrator.opts.internalnorm(uprev,t)))))))
-    maxm = 50
-    mdeg = 1 + Int(floor(sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)))
-    mdeg = (mdeg > maxm) ? maxm : mdeg
+    # The number of degree for Chebyshev polynomial
+    mdeg = floor(Int, sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)) + 1
 
     w0 = 1 + 2 / (13 * (mdeg^2))
     temp1 = w0^2 - 1
@@ -508,9 +505,11 @@ end
             d2z1 = d2z
         end
     end
+    integrator.fsallast = f(u, p, t + dt)
+    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
     # error estimate
     if integrator.opts.adaptive
-        tmp = 0.8 * (uprev - u) + 0.4 * dt * (fsalfirst + gprev)
+        tmp = 0.8 * (uprev - u) + 0.4 * dt * (fsalfirst + integrator.fsallast)
         atmp = calculate_residuals(
             tmp, uprev, u, integrator.opts.abstol,
             integrator.opts.reltol, integrator.opts.internalnorm, t
@@ -518,8 +517,7 @@ end
         integrator.EEst = integrator.opts.internalnorm(atmp, t)
     end
     integrator.k[1] = integrator.fsalfirst
-    integrator.k[2] = integrator.fsallast = f(u, p, t + dt)
-    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+    integrator.k[2] = integrator.fsallast
     integrator.u = u
 end
 
@@ -537,11 +535,8 @@ end
     (; k, tmp, gprev2, gprev, atmp) = cache
     alg = unwrap_alg(integrator, true)
     alg.eigen_est === nothing ? maxeig!(integrator, cache) : alg.eigen_est(integrator)
-    # The the number of degree for Chebyshev polynomial
-    #maxm = max(2,Int(floor(sqrt(integrator.opts.internalnorm(integrator.opts.reltol,t)/10eps(t)))))
-    maxm = 50
-    mdeg = 1 + Int(floor(sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)))
-    mdeg = (mdeg > maxm) ? maxm : mdeg
+    # The number of degree for Chebyshev polynomial
+    mdeg = floor(Int, sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)) + 1
 
     w0 = 1 + 2 / (13 * (mdeg^2))
     temp1 = w0^2 - 1
@@ -580,8 +575,8 @@ end
             dt * μs * (k - νs * fsalfirst)
         th = μ * th1 + ν * th2 + μs * (1 - νs)
         if (iter < mdeg)
-            gprev2 = gprev
-            gprev = u
+            @.. broadcast = false gprev2 = gprev
+            @.. broadcast = false gprev = u
             th2 = th1
             th1 = th
             b2 = b1
@@ -594,9 +589,11 @@ end
             d2z1 = d2z
         end
     end
+    f(integrator.fsallast, u, p, t + dt)
+    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
     # error estimate
     if integrator.opts.adaptive
-        @.. broadcast = false tmp = 0.8 * (uprev - u) + 0.4 * dt * (fsalfirst + gprev)
+        @.. broadcast = false tmp = 0.8 * (uprev - u) + 0.4 * dt * (fsalfirst + integrator.fsallast)
         calculate_residuals!(
             atmp, tmp, uprev, u, integrator.opts.abstol,
             integrator.opts.reltol, integrator.opts.internalnorm, t
@@ -604,8 +601,6 @@ end
         integrator.EEst = integrator.opts.internalnorm(atmp, t)
     end
     integrator.k[1] = integrator.fsalfirst
-    f(integrator.fsallast, u, p, t + dt)
-    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
     integrator.k[2] = integrator.fsallast
     integrator.u = u
 end
@@ -1063,5 +1058,267 @@ end
     end
     integrator.k[1] = integrator.fsalfirst
     integrator.k[2] = integrator.fsallast = k
+    integrator.u = u
+end
+
+function initialize!(integrator, cache::TSRKC3ConstantCache)
+    integrator.kshortsize = 2
+    integrator.k = typeof(integrator.k)(undef, integrator.kshortsize)
+    integrator.fsalfirst = integrator.f(integrator.uprev, integrator.p, integrator.t) # Pre-start fsal
+    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+
+    # Avoid undefined entries if k is an array of arrays
+    integrator.fsallast = zero(integrator.fsalfirst)
+    integrator.k[1] = integrator.fsalfirst
+    return integrator.k[2] = integrator.fsallast
+end
+
+@muladd function perform_step!(integrator, cache::TSRKC3ConstantCache, repeat_step = false)
+    (; t, tprev, dt, uprev, u, f, p, fsalfirst, uprev2) = integrator
+
+    q = (t - tprev) / dt
+    onemq = 1 - q
+    onepq = 1 + q
+    onepq2 = onepq * onepq
+
+    # The first and possibly second steps are calculated via the one-step RKC method.
+    rkcstep = q < 0.49
+
+    alg = unwrap_alg(integrator, true)
+    alg.eigen_est === nothing ? maxeig!(integrator, cache) : alg.eigen_est(integrator)
+    if (rkcstep)
+        mdeg = floor(Int, sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)) + 1
+        w0 = 1 + 2 / (13 * (mdeg^2))
+        temp1 = w0^2 - 1
+        temp2 = sqrt(temp1)
+        arg = mdeg * log(w0 + temp2)
+        w1 = (sinh(arg) * temp1) / (cosh(arg) * mdeg * temp2 - w0 * sinh(arg))
+        b1 = 1 / ((2 * w0)^2)
+        b = b1
+        b2 = b1
+    else
+        mdeg = floor(Int, sqrt(4 + 1.267029788142009 * abs(dt) * integrator.eigen_est * (onemq + sqrt(1 + q * (0.44256220745562963 + q))))) + 1
+        mdeg2 = mdeg^2
+        tsw0 = cache.tsw0
+        acoshtsw0 = cache.acoshtsw0
+        acoshtsw0dm = acoshtsw0 / mdeg
+        w0 = cosh(acoshtsw0dm)
+        w0sq = w0^2
+        w0sqm1 = w0sq - 1
+        dtsw0 = mdeg * sinh(acoshtsw0) / sqrt(w0sqm1)
+        d2tsw0 = (mdeg2 * tsw0 - w0 * dtsw0) / w0sqm1
+        d3tsw0 = ((1 + 2 * w0sq + mdeg2 * w0sqm1) * dtsw0 - 3 * mdeg2 * w0 * tsw0) / (w0sqm1^2)
+        w1 = (onemq * d2tsw0 + sqrt((onemq * d2tsw0)^2 + 4 * q * dtsw0 * d3tsw0)) / (2 * d3tsw0)
+
+        b1 = 0.25 * sinh((mdeg - 2) * acoshtsw0dm) / sinh((mdeg - 1) * acoshtsw0dm)
+        b = 15 / ((8 * w0)^2)
+        b2 = b1
+    end
+
+    # stage-1
+    gprev2 = uprev
+    μs = w1 * b1
+    gprev = uprev + dt * μs * fsalfirst
+    th2 = zero(eltype(u))
+    th1 = μs
+    z1 = w0
+    z2 = one(eltype(u))
+    dz1 = one(eltype(u))
+    dz2 = zero(eltype(u))
+    d2z1 = zero(eltype(u))
+    d2z2 = zero(eltype(u))
+    z = 2 * w0 * z1 - z2
+    dz = 2 * w0 * dz1 - dz2 + 2 * z1
+    d2z = 2 * w0 * d2z1 - d2z2 + 4 * dz1
+
+    # stage 2 - mdeg
+    for iter in 2:mdeg
+        νs = 1 - z1 * b1
+        μ = (2 * w0 * b) / b1
+        ν = -b / b2
+        μs = μ * w1 / w0
+        #using u as temporary storage
+        u = f(gprev, p, t + dt * th1)
+        OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+        u = μ * gprev + ν * gprev2 + (1 - μ - ν) * uprev + dt * μs * (u - νs * fsalfirst)
+        if (iter < mdeg)
+            gprev2 = gprev
+            gprev = u
+            th = μ * th1 + ν * th2 + μs * (1 - νs)
+            th2 = th1
+            th1 = th
+            b2 = b1
+            b1 = b
+            z2 = z1
+            z1 = z
+            dz2 = dz1
+            dz1 = dz
+            d2z2 = d2z1
+            d2z1 = d2z
+
+            z = 2 * w0 * z1 - z2
+            dz = 2 * w0 * dz1 - dz2 + 2 * z1
+            d2z = 2 * w0 * d2z1 - d2z2 + 4 * dz1
+
+            b = d2z / (dz^2)
+        end
+    end
+
+    if (!rkcstep)
+        a = onepq / (q * dtsw0 + w1 * d2tsw0)
+        g = (a * dtsw0 - 1) / q
+        b = a / (b * w1)
+        μ = 1 - g - b
+        u = μ * uprev + g * uprev2 + b * u
+    end
+
+    integrator.fsallast = f(u, p, t + dt)
+    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+
+    # error estimate
+    if integrator.opts.adaptive
+        if (rkcstep)
+            tmp = 1.2 * (uprev - u) + 0.6 * dt * (fsalfirst + integrator.fsallast)
+        else
+            tmp = 0.6 * (uprev / q - uprev2 / (q * onepq2) - u * (2 + q) / onepq2 + dt * integrator.fsallast / onepq)
+        end
+        atmp = calculate_residuals(
+            tmp, uprev, u, integrator.opts.abstol,
+            integrator.opts.reltol, integrator.opts.internalnorm, t
+        )
+        integrator.EEst = integrator.opts.internalnorm(atmp, t)
+    end
+    integrator.k[1] = integrator.fsalfirst
+    integrator.k[2] = integrator.fsallast
+    integrator.u = u
+end
+
+function initialize!(integrator, cache::TSRKC3Cache)
+    integrator.kshortsize = 2
+    resize!(integrator.k, integrator.kshortsize)
+    integrator.k[1] = integrator.fsalfirst
+    integrator.k[2] = integrator.fsallast
+    integrator.f(integrator.fsalfirst, integrator.uprev, integrator.p, integrator.t) # Pre-start fsal
+    return OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+end
+
+@muladd function perform_step!(integrator, cache::TSRKC3Cache, repeat_step = false)
+    (; t, tprev, dt, uprev, u, f, p, fsalfirst, uprev2) = integrator
+    (; k, tmp, gprev2, gprev, atmp, constantcache) = cache
+
+    q = (t - tprev) / dt
+    onemq = 1 - q
+    onepq = 1 + q
+    onepq2 = onepq * onepq
+    
+    # The first and possibly second steps are calculated via the one-step RKC method.
+    rkcstep = q < 0.49
+
+    alg = unwrap_alg(integrator, true)
+    alg.eigen_est === nothing ? maxeig!(integrator, cache) : alg.eigen_est(integrator)
+    if (rkcstep)
+        mdeg = floor(Int, sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)) + 1
+        w0 = 1 + 2 / (13 * (mdeg^2))
+        temp1 = w0^2 - 1
+        temp2 = sqrt(temp1)
+        arg = mdeg * log(w0 + temp2)
+        w1 = (sinh(arg) * temp1) / (cosh(arg) * mdeg * temp2 - w0 * sinh(arg))
+        b1 = 1 / ((2 * w0)^2)
+        b = b1
+        b2 = b1
+    else
+        mdeg = floor(Int, sqrt(4 + 1.267029788142009 * abs(dt) * integrator.eigen_est * (onemq + sqrt(1 + q * (0.44256220745562963 + q))))) + 1
+        mdeg2 = mdeg^2
+        tsw0 = constantcache.tsw0
+        acoshtsw0 = constantcache.acoshtsw0
+        acoshtsw0dm = acoshtsw0 / mdeg
+        w0 = cosh(acoshtsw0dm)
+        w0sq = w0^2
+        w0sqm1 = w0sq - 1
+        dtsw0 = mdeg * sinh(acoshtsw0) / sqrt(w0sqm1)
+        d2tsw0 = (mdeg2 * tsw0 - w0 * dtsw0) / w0sqm1
+        d3tsw0 = ((1 + 2 * w0sq + mdeg2 * w0sqm1) * dtsw0 - 3 * mdeg2 * w0 * tsw0) / (w0sqm1^2)
+        w1 = (onemq * d2tsw0 + sqrt((onemq * d2tsw0)^2 + 4 * q * dtsw0 * d3tsw0)) / (2 * d3tsw0)
+
+        b1 = 0.25 * sinh((mdeg - 2) * acoshtsw0dm) / sinh((mdeg - 1) * acoshtsw0dm)
+        b = 15 / ((8 * w0)^2)
+        b2 = b1
+    end
+
+    # stage-1
+    @.. broadcast = false gprev2 = uprev
+    μs = w1 * b1
+    @.. broadcast = false gprev = uprev + dt * μs * fsalfirst
+    th2 = zero(eltype(u))
+    th1 = μs
+    z1 = w0
+    z2 = one(eltype(u))
+    dz1 = one(eltype(u))
+    dz2 = zero(eltype(u))
+    d2z1 = zero(eltype(u))
+    d2z2 = zero(eltype(u))
+    z = 2 * w0 * z1 - z2
+    dz = 2 * w0 * dz1 - dz2 + 2 * z1
+    d2z = 2 * w0 * d2z1 - d2z2 + 4 * dz1
+
+    # stage 2 - mdeg
+    for iter in 2:mdeg
+        νs = 1 - z1 * b1
+        μ = (2 * w0 * b) / b1
+        ν = -b / b2
+        μs = μ * w1 / w0
+        f(k, gprev, p, t + dt * th1)
+        OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+        @.. broadcast = false u = μ * gprev + ν * gprev2 + (1 - μ - ν) * uprev +
+            dt * μs * (k - νs * fsalfirst)
+        if (iter < mdeg)
+            @.. broadcast = false gprev2 = gprev
+            @.. broadcast = false gprev = u
+            th = μ * th1 + ν * th2 + μs * (1 - νs)
+            th2 = th1
+            th1 = th
+            b2 = b1
+            b1 = b
+            z2 = z1
+            z1 = z
+            dz2 = dz1
+            dz1 = dz
+            d2z2 = d2z1
+            d2z1 = d2z
+
+            z = 2 * w0 * z1 - z2
+            dz = 2 * w0 * dz1 - dz2 + 2 * z1
+            d2z = 2 * w0 * d2z1 - d2z2 + 4 * dz1
+
+            b = d2z / (dz^2)
+        end
+    end
+
+    if (!rkcstep)
+        a = onepq / (q * dtsw0 + w1 * d2tsw0)
+        g = (a * dtsw0 - 1) / q
+        b = a / (b * w1)
+        μ = 1 - g - b
+        @.. broadcast = false u = μ * uprev + g * uprev2 + b * u
+    end
+
+    f(integrator.fsallast, u, p, t + dt)
+    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+
+    # error estimate
+    if integrator.opts.adaptive
+        if (rkcstep)
+            @.. broadcast = false tmp = 1.2 * (uprev - u) + 0.6 * dt * (fsalfirst + integrator.fsallast)
+        else
+            @.. broadcast = false tmp = 0.6 * (uprev / q - uprev2 / (q * onepq2) - u * (2 + q) / onepq2 + dt * integrator.fsallast / onepq)
+        end
+        calculate_residuals!(
+            atmp, tmp, uprev, u, integrator.opts.abstol,
+            integrator.opts.reltol, integrator.opts.internalnorm, t
+        )
+        integrator.EEst = integrator.opts.internalnorm(atmp, t)
+    end
+    integrator.k[1] = integrator.fsalfirst
+    integrator.k[2] = integrator.fsallast
     integrator.u = u
 end

lib/OrdinaryDiffEqStabilizedRK/src/OrdinaryDiffEqStabilizedRK.jl
@@ -1,7 +1,7 @@
 module OrdinaryDiffEqStabilizedRK
 
 import OrdinaryDiffEqCore: alg_order, alg_adaptive_order, calculate_residuals!,
-    beta2_default, beta1_default, gamma_default,
+    beta2_default, beta1_default, gamma_default, qmax_default, alg_extrapolates,
     fac_default_gamma, has_dtnew_modification,
     initialize!, perform_step!, unwrap_alg,
     calculate_residuals,
@@ -30,6 +30,6 @@ include("rkc_tableaus_rock2.jl")
 include("rkc_tableaus_eserk5.jl")
 include("rkc_tableaus_eserk4.jl")
 
-export ROCK2, ROCK4, RKC, ESERK4, ESERK5, SERK2
+export ROCK2, ROCK4, RKC, ESERK4, ESERK5, SERK2, TSRKC3
 
 end

lib/OrdinaryDiffEqStabilizedRK/src/alg_utils.jl
@@ -7,14 +7,20 @@ alg_order(alg::SERK2) = 2
 
 alg_order(alg::RKC) = 2
 
+alg_order(alg::TSRKC3) = 3
+
+alg_extrapolates(alg::TSRKC3) = true
+
 ispredictive(alg::Union{SERK2}) = alg.controller === :Predictive
-ispredictive(alg::Union{RKC}) = true
+ispredictive(alg::Union{RKC, TSRKC3}) = true
 
-alg_adaptive_order(alg::RKC) = 2
+alg_adaptive_order(alg::Union{RKC, TSRKC3}) = 2
 
-gamma_default(alg::RKC) = 8 // 10
+gamma_default(alg::Union{RKC, TSRKC3}) = 8 // 10
 
-fac_default_gamma(alg::Union{RKC, SERK2}) = true
+qmax_default(alg::TSRKC3) = 2
+
+fac_default_gamma(alg::Union{RKC, SERK2, TSRKC3}) = true
 has_dtnew_modification(alg::Union{ROCK2, ROCK4, SERK2, ESERK4, ESERK5}) = true
 
 function dtnew_modification(integrator, alg::ROCK2, dtnew)

lib/OrdinaryDiffEqStabilizedRK/src/algorithms.jl
@@ -61,7 +61,7 @@ end
 
 # SERK methods
 
-for Alg in [:ESERK4, :ESERK5, :RKC]
+for Alg in [:ESERK4, :ESERK5, :RKC, :TSRKC3]
     @eval begin
         struct $Alg{E} <: OrdinaryDiffEqAdaptiveAlgorithm
             eigen_est::E
@@ -159,3 +159,22 @@ struct SERK2{E} <: OrdinaryDiffEqAdaptiveAlgorithm
     eigen_est::E
 end
 SERK2(; controller = :PI, eigen_est = nothing) = SERK2(controller, eigen_est)
+
+@doc generic_solver_docstring(
+    """Third order method. Exhibits high stability for real eigenvalues.""",
+    "TSRKC3",
+    "Two-step Stabilized Explicit Method.",
+    """A. V. Moisa. Third order two-step Runge-Kutta-Chebyshev methods,
+    Journal of Computational and Applied Mathematics, 457, pp 116291, 2025. doi:
+    https://doi.org/10.1016/j.cam.2024.116291""",
+    """
+    - `eigen_est`: function of the form
+        `(integrator) -> integrator.eigen_est = upper_bound`,
+        where `upper_bound` is an estimated upper bound on the spectral radius of the Jacobian matrix.
+        If `eigen_est` is not provided, `upper_bound` will be estimated using the power iteration.
+    """,
+    """
+    eigen_est = nothing,
+    """
+)
+function TSRKC3 end

lib/OrdinaryDiffEqStabilizedRK/src/rkc_utils.jl
@@ -1,6 +1,6 @@
 # This function calculates the largest eigenvalue
 # (absolute value wise) by power iteration.
-const RKCAlgs = Union{RKC, ESERK4, ESERK5, SERK2, ROCK2, ROCK4}
+const RKCAlgs = Union{RKC, TSRKC3, ESERK4, ESERK5, SERK2, ROCK2, ROCK4}
 
 function maxeig!(integrator, cache::OrdinaryDiffEqConstantCache)
     isfirst = integrator.iter == 1 || integrator.u_modified

lib/OrdinaryDiffEqStabilizedRK/test/rkc_tests.jl
@@ -55,9 +55,26 @@ end
         sim = test_convergence(dts, prob, ROCK4(min_stages = 21))
         @test sim.𝒪est[:l∞] ≈ 4 atol = testTol
 
-        println("ROCKC")
-        sim = test_convergence(dts, prob, RKC())
+        println("RKC")
+        eigen_est = (integrator) -> integrator.eigen_est = 1 / integrator.dt
+        sim = test_convergence(dts, prob, RKC(eigen_est = eigen_est))
         @test sim.𝒪est[:l∞] ≈ 2 atol = testTol
+        eigen_est = (integrator) -> integrator.eigen_est = 100 / integrator.dt
+        sim = test_convergence(dts, prob, RKC(eigen_est = eigen_est))
+        @test sim.𝒪est[:l∞] ≈ 2 atol = testTol
+        eigen_est = (integrator) -> integrator.eigen_est = 10000 / integrator.dt
+        sim = test_convergence(dts, prob, RKC(eigen_est = eigen_est))
+        @test sim.𝒪est[:l∞] ≈ 2 atol = testTol
+        println("TSRKC3")
+        eigen_est = (integrator) -> integrator.eigen_est = 1 / integrator.dt
+        sim = test_convergence(dts, prob, TSRKC3(eigen_est = eigen_est))
+        @test sim.𝒪est[:l∞] ≈ 3 atol = testTol
+        eigen_est = (integrator) -> integrator.eigen_est = 100 / integrator.dt
+        sim = test_convergence(dts, prob, TSRKC3(eigen_est = eigen_est))
+        @test sim.𝒪est[:l∞] ≈ 3 atol = testTol
+        eigen_est = (integrator) -> integrator.eigen_est = 10000 / integrator.dt
+        sim = test_convergence(dts, prob, TSRKC3(eigen_est = eigen_est))
+        @test sim.𝒪est[:l∞] ≈ 3 atol = testTol
         println("SERK2")
         sim = test_convergence(dts, prob, SERK2())
         @test sim.𝒪est[:l∞] ≈ 2 atol = testTol
@@ -93,6 +110,7 @@ end
             ROCK2(), ROCK2(eigen_est = eigen_est),
             ROCK4(), ROCK4(eigen_est = eigen_est),
             RKC(), RKC(eigen_est = eigen_est),
+            TSRKC3(), TSRKC3(eigen_est = eigen_est),
             SERK2(), SERK2(eigen_est = eigen_est),
             ESERK4(), ESERK4(eigen_est = eigen_est),
             ESERK5(), ESERK5(eigen_est = eigen_est),
@@ -118,6 +136,7 @@ end
         ROCK2(), ROCK2(eigen_est = eigen_est),
         ROCK4(), ROCK4(eigen_est = eigen_est),
         RKC(), RKC(eigen_est = eigen_est),
+        TSRKC3(), TSRKC3(eigen_est = eigen_est),
         SERK2(), SERK2(eigen_est = eigen_est),
         ESERK4(), ESERK4(eigen_est = eigen_est),
         ESERK5(), ESERK5(eigen_est = eigen_est),

test/regression/iipvsoop_tests.jl
@@ -145,7 +145,7 @@ rosenbrock_algs = [
     @test sol_ip.t ≈ sol_scalar.t && sol_ip[1, :] ≈ sol_scalar.u
 end
 
-rkc_algs = [RKC(), ROCK2(), ROCK4(), SERK2()]
+rkc_algs = [RKC(), ROCK2(), ROCK4(), SERK2(), OrdinaryDiffEq.OrdinaryDiffEqStabilizedRK.TSRKC3()]
 
 @testset "Algorithm $(nameof(typeof(alg)))" for alg in rkc_algs
     println(nameof(typeof(alg)))

commit df9b7ef713e42ac55e3e50f76ba5b5278ccd0c2e

Fix OrdinaryDiffEqStabilizedRK construction under StaticArraysCore-only import
The package imports SVector from StaticArraysCore rather than full
StaticArrays, which only supplies the tuple-form constructor and
leaves out the varargs constructor and getindex. In the isolated QA
test environment (no transitive StaticArrays) this surfaced two
distinct failures:

1. SERK2, ESERK4, ESERK5 cache construction failed because the
   struct fields ms/Cᵤ/Cₑ were typed as SVector{N, Int} but the
   tableaus returned plain Vector{Int}, and no Vector -> SVector
   convert method was available.
2. ROCK2 and ROCK4 cache construction failed because the tableaus
   called SVector{N, Int}(args...) with 46 / 50 varargs, a
   constructor not defined in StaticArraysCore.
3. ROCK4 perform_step failed with CanonicalIndexError on
   SVector{6, Float64} because StaticArraysCore does not define
   getindex for SArray.

Switch the ms / fp1 / fp2 / Cᵤ / Cₑ struct fields to plain Vector
(they are built once per cache at solve time and indexed, so the
SVector specialization is not buying anything in the hot loop) and
drop the SVector wrapping from the ROCK2 / ROCK4 tableaus. Convert
the ROCK4 per-step tableau blocks from SVector{N, T} to NTuple{N, T}
to preserve the static-length, tuple-indexable shape without
requiring the full StaticArrays package.

Also fix a separate JET regression introduced in #3420 (TSRKC3):
dtsw0 and d2tsw0 are defined only in the non-rkcstep branch but
read unconditionally in a later if (!rkcstep) block; JET flagged
them as possibly-undefined. Initialize both to
zero(typeof(cache.tsw0)) (and the constantcache variant) before
the branch so the definitions dominate every use.

Fixes CI failures in the OrdinaryDiffEqStabilizedRK_QA sublibrary
job exposed by #3439 and the TSRKC3 JET failure introduced by #3420.

Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

lib/OrdinaryDiffEqStabilizedRK/Project.toml
@@ -1,7 +1,7 @@
 name = "OrdinaryDiffEqStabilizedRK"
 uuid = "358294b1-0aab-51c3-aafe-ad5ab194a2ad"
 authors = ["ParamThakkar123 <paramthakkar864@gmail.com>"]
-version = "1.11.0"
+version = "1.11.1"
 
 [deps]
 MuladdMacro = "46d2c3a1-f734-5fdb-9937-b9b9aeba4221"

lib/OrdinaryDiffEqStabilizedRK/src/rkc_caches.jl
@@ -2,9 +2,9 @@ abstract type StabilizedRKMutableCache <: OrdinaryDiffEqMutableCache end
 get_fsalfirstlast(cache::StabilizedRKMutableCache, u) = (cache.fsalfirst, cache.k)
 
 mutable struct ROCK2ConstantCache{T, T2, zType} <: OrdinaryDiffEqConstantCache
-    ms::SVector{46, Int}
-    fp1::SVector{46, T}
-    fp2::SVector{46, T}
+    ms::Vector{Int}
+    fp1::Vector{T}
+    fp2::Vector{T}
     recf::Vector{T2}
     zprev::zType
     mdeg::Int
@@ -57,7 +57,7 @@ function alg_cache(
 end
 
 mutable struct ROCK4ConstantCache{T, T2, T3, T4, zType} <: OrdinaryDiffEqConstantCache
-    ms::SVector{50, Int}
+    ms::Vector{Int}
     fpa::Vector{T}
     fpb::Vector{T2}
     fpbe::Vector{T3}
@@ -159,9 +159,9 @@ function alg_cache(
 end
 
 mutable struct ESERK4ConstantCache{T, zType} <: OrdinaryDiffEqConstantCache
-    ms::SVector{46, Int}
-    Cᵤ::SVector{4, Int}
-    Cₑ::SVector{4, Int}
+    ms::Vector{Int}
+    Cᵤ::Vector{Int}
+    Cₑ::Vector{Int}
     zprev::zType
     Bᵢ::Vector{T}
     mdeg::Int
@@ -213,9 +213,9 @@ function alg_cache(
 end
 
 mutable struct ESERK5ConstantCache{T, zType} <: OrdinaryDiffEqConstantCache
-    ms::SVector{49, Int}
-    Cᵤ::SVector{5, Int}
-    Cₑ::SVector{5, Int}
+    ms::Vector{Int}
+    Cᵤ::Vector{Int}
+    Cₑ::Vector{Int}
     zprev::zType
     Bᵢ::Vector{T}
     mdeg::Int
@@ -267,7 +267,7 @@ function alg_cache(
 end
 
 mutable struct SERK2ConstantCache{T, zType} <: OrdinaryDiffEqConstantCache
-    ms::SVector{11, Int}
+    ms::Vector{Int}
     zprev::zType
     Bᵢ::Vector{T}
     mdeg::Int

lib/OrdinaryDiffEqStabilizedRK/src/rkc_perform_step.jl
@@ -1086,6 +1086,8 @@ end
 
     alg = unwrap_alg(integrator, true)
     alg.eigen_est === nothing ? maxeig!(integrator, cache) : alg.eigen_est(integrator)
+    dtsw0 = zero(typeof(cache.tsw0))
+    d2tsw0 = zero(typeof(cache.tsw0))
     if (rkcstep)
         mdeg = floor(Int, sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)) + 1
         w0 = 1 + 2 / (13 * (mdeg^2))
@@ -1216,6 +1218,8 @@ end
 
     alg = unwrap_alg(integrator, true)
     alg.eigen_est === nothing ? maxeig!(integrator, cache) : alg.eigen_est(integrator)
+    dtsw0 = zero(typeof(constantcache.tsw0))
+    d2tsw0 = zero(typeof(constantcache.tsw0))
     if (rkcstep)
         mdeg = floor(Int, sqrt(1.54 * abs(dt) * integrator.eigen_est + 1)) + 1
         w0 = 1 + 2 / (13 * (mdeg^2))

lib/OrdinaryDiffEqStabilizedRK/src/rkc_tableaus_rock2.jl
@@ -1,12 +1,12 @@
 function ROCK2ConstantCache(T, T2, zprev)
-    ms = SVector{46, Int}(
+    ms = Int[
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
         20,
         22, 24, 26, 28, 30, 33, 36, 39, 43, 47, 51, 56, 61, 66, 72, 78,
         85, 93,
-        102, 112, 123, 135, 148, 163, 180, 198
-    )
-    fp1 = SVector{46, T}(
+        102, 112, 123, 135, 148, 163, 180, 198,
+    ]
+    fp1 = T[
         0.4102693550421609e+0, 0.3889624104727243e+0,
         0.3804692420283886e+0,
         0.3760815680865637e+0, 0.3735177579729938e+0,
@@ -37,9 +37,9 @@ function ROCK2ConstantCache(T, T2, zprev)
         0.3679181786833088e+0,
         0.3679192462983425e+0, 0.367920432307971e+0,
         0.3679216942157868e+0,
-        0.3679229127010114e+0
-    )
-    fp2 = SVector{46, T}(
+        0.3679229127010114e+0,
+    ]
+    fp2 = T[
         0.4495196112243335e+0, 0.4219428123056774e+0,
         0.4084335547255627e+0,
         0.4009301129475925e+0, 0.3963598727888637e+0,
@@ -70,8 +70,8 @@ function ROCK2ConstantCache(T, T2, zprev)
         0.3843392605995229e+0,
         0.3843359163858929e+0, 0.3843331309176606e+0,
         0.3843309056586355e+0,
-        0.3843292556220249e+0
-    )
+        0.3843292556220249e+0,
+    ]
     recf = [
         0.1794612899156781,
         0.09326607661089206,

lib/OrdinaryDiffEqStabilizedRK/src/rkc_tableaus_rock4.jl
@@ -1,11 +1,11 @@
 function ROCK4ConstantCache(T, T2, zprev)
-    ms = SVector{50, Int}(
+    ms = Int[
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
         20,
         22, 24, 26, 28, 30, 32, 34, 36, 38, 41, 44, 47, 50, 53, 56, 59,
         63, 67, 71, 76, 81, 86,
-        92, 98, 105, 112, 120, 129, 138, 148
-    )
+        92, 98, 105, 112, 120, 129, 138, 148,
+    ]
     fpa = [
         (
             -0.149352078672699, 0.629768962985252,
@@ -4945,9 +4945,9 @@ function ROCK4ConstantCache(T, T2, zprev)
         0.9136748776971255,
     ]
 
-    _fpa = map(x -> SVector{6, T}(x), fpa)
-    _fpb = map(x -> SVector{4, T}(x), fpb)
-    _fpbe = map(x -> SVector{5, T}(x), fpbe)
+    _fpa = map(x -> NTuple{6, T}(x), fpa)
+    _fpb = map(x -> NTuple{4, T}(x), fpb)
+    _fpbe = map(x -> NTuple{5, T}(x), fpbe)
     return ROCK4ConstantCache{eltype(_fpa), eltype(_fpb), eltype(_fpbe), T2, typeof(zprev)}(
         ms,
         _fpa,