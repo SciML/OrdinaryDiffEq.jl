bce08bfb832dd9084b2b72b56b5df2fca50a74fc
Your Name <you@example.com>
2026-05-19 01:22:22 +0530

    Keep legacy ImplicitEulerCache structs for OrdinaryDiffEqBDF compat
    IE itself routes through ESDIRKIMEXCache via _PureSDIRKAlg alg_cache;
    the legacy struct + perform_step methods are retained because BDF's
    ABDF2 constructs and dispatches on ImplicitEulerCache directly.

--- lib/OrdinaryDiffEqSDIRK/src/sdirk_perform_step.jl
+++ lib/OrdinaryDiffEqSDIRK/src/sdirk_perform_step.jl
@@ -19,3 +19,126 @@ function initialize!(integrator, cache::SDIRKMutableCache)
     return OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
 end
 
+function calculate_error_estimate!(
+        integrator, cache::ImplicitEulerConstantCache, ::Nothing, t
+    )
+    (; uprev, u, dt) = integrator
+    if integrator.opts.adaptive && integrator.success_iter > 0
+        # local truncation error (LTE) bound by dt^2/2*max|y''(t)|
+        # use 2nd divided differences (DD) a la SPICE and Shampine
+        uprev2 = integrator.uprev2
+        tprev = integrator.tprev
+        dt1 = dt * (t + dt - tprev)
+        dt2 = (t - tprev) * (t + dt - tprev)
+        c = 7 / 12
+        r = c * dt^2
+        tmp = r *
+            integrator.opts.internalnorm.((u - uprev) / dt1 - (uprev - uprev2) / dt2, t)
+        atmp = calculate_residuals(
+            tmp, uprev, u, integrator.opts.abstol,
+            integrator.opts.reltol, integrator.opts.internalnorm, t
+        )
+        OrdinaryDiffEqCore.set_EEst!(integrator, integrator.opts.internalnorm(atmp, t))
+    else
+        OrdinaryDiffEqCore.set_EEst!(integrator, 1)
+    end
+end
+
+@muladd function perform_step!(
+        integrator, cache::ImplicitEulerConstantCache,
+        repeat_step = false
+    )
+    (; t, dt, uprev, u, f, p) = integrator
+    nlsolver = cache.nlsolver
+    alg = unwrap_alg(integrator, true)
+    markfirststage!(nlsolver)
+
+    # initial guess
+    if alg.extrapolant == :linear
+        nlsolver.z = dt * integrator.fsalfirst
+    else # :constant
+        nlsolver.z = zero(u)
+    end
+
+    nlsolver.tmp = uprev
+    nlsolver.γ = 1
+    z = nlsolve!(nlsolver, integrator, cache, repeat_step)
+    nlsolvefail(nlsolver) && return
+    u = nlsolver.tmp + z
+    integrator.u = u
+
+    calculate_error_estimate!(integrator, cache, nothing, t)
+
+    integrator.fsallast = f(u, p, t + dt)
+
+    if integrator.opts.adaptive && integrator.differential_vars !== nothing
+        atmp = @. ifelse(!integrator.differential_vars, integrator.fsallast, false) ./
+            integrator.opts.abstol
+        OrdinaryDiffEqCore.set_EEst!(integrator, OrdinaryDiffEqCore.get_EEst(integrator) + (integrator.opts.internalnorm(atmp, t)))
+    end
+
+    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+    integrator.k[1] = integrator.fsalfirst
+    integrator.k[2] = integrator.fsallast
+end
+
+function calculate_error_estimate!(
+        integrator, cache::ImplicitEulerCache, ::Nothing, t
+    )
+    (; uprev, u, dt) = integrator
+    (; atmp, nlsolver) = cache
+    (; tmp) = nlsolver
+    if integrator.opts.adaptive && integrator.success_iter > 0
+        uprev2 = integrator.uprev2
+        tprev = integrator.tprev
+        dt1 = dt * (t + dt - tprev)
+        dt2 = (t - tprev) * (t + dt - tprev)
+        c = 7 / 12
+        r = c * dt^2
+        @.. broadcast = false tmp = r * integrator.opts.internalnorm(
+            (u - uprev) / dt1 - (uprev - uprev2) / dt2, t
+        )
+        calculate_residuals!(
+            atmp, tmp, uprev, u, integrator.opts.abstol,
+            integrator.opts.reltol, integrator.opts.internalnorm, t
+        )
+        OrdinaryDiffEqCore.set_EEst!(integrator, integrator.opts.internalnorm(atmp, t))
+    else
+        OrdinaryDiffEqCore.set_EEst!(integrator, 1)
+    end
+end
+
+@muladd function perform_step!(integrator, cache::ImplicitEulerCache, repeat_step = false)
+    (; t, dt, uprev, u, f, p) = integrator
+    (; atmp, nlsolver, step_limiter!) = cache
+    (; z, tmp) = nlsolver
+    alg = unwrap_alg(integrator, true)
+    markfirststage!(nlsolver)
+
+    # initial guess
+    if alg.extrapolant == :linear
+        @.. broadcast = false z = dt * integrator.fsalfirst
+    else # :constant
+        z .= zero(eltype(u))
+    end
+
+    nlsolver.tmp .= uprev
+    nlsolver.γ = 1
+    z = nlsolve!(nlsolver, integrator, cache, repeat_step)
+    nlsolvefail(nlsolver) && return
+    @.. broadcast = false u = uprev + z
+    integrator.u = u
+
+    step_limiter!(u, integrator, p, t + dt)
+
+    calculate_error_estimate!(integrator, cache, nothing, t)
+
+    OrdinaryDiffEqCore.increment_nf!(integrator.stats, 1)
+    f(integrator.fsallast, u, p, t + dt)
+
+    if integrator.opts.adaptive && integrator.differential_vars !== nothing
+        @.. broadcast = false atmp = ifelse(cache.algebraic_vars, integrator.fsallast, false) /
+            integrator.opts.abstol
+        OrdinaryDiffEqCore.set_EEst!(integrator, OrdinaryDiffEqCore.get_EEst(integrator) + (integrator.opts.internalnorm(atmp, t)))
+    end
+end
