f5d04b7b534e59bf00483a2102b676523c006e84
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-02 08:34:21 -0400

    Re-clean OrdinaryDiffEqLinear QA ignore list to genuine residual
    The OrdinaryDiffEqCore/DiffEqBase solver-author API is now declared public
    on this branch, so those names are dropped from the ExplicitImports ignore
    lists. Only three genuine non-public externals remain:
    AbstractSciMLOperator (SciMLOperators), _vec (SciMLBase), and alloc_mem
    (ExponentialUtilities).
    
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- lib/OrdinaryDiffEqLinear/test/qa/qa.jl
+++ lib/OrdinaryDiffEqLinear/test/qa/qa.jl
@@ -4,31 +4,21 @@ run_qa(
     OrdinaryDiffEqLinear;
     explicit_imports = true,
     ei_kwargs = (;
-        # All residual names below are non-public internals of upstream packages
-        # that have no public-API replacement yet; they are tightly listed (not
-        # blanket ei_broken) and feed the next make-public round (see SciML/OrdinaryDiffEq.jl#3776).
+        # Residual non-public names below have no public-API replacement yet.
+        # The OrdinaryDiffEqCore/DiffEqBase solver-author API is now declared
+        # public on this branch, so those names are no longer ignored here.
         all_explicit_imports_are_public = (;
             ignore = (
-                # OrdinaryDiffEqCore internal solver-interface API (no public surface yet)
-                Symbol("@cache"), :OrdinaryDiffEqAdaptiveAlgorithm,
-                :OrdinaryDiffEqAlgorithm, :OrdinaryDiffEqConstantCache,
-                :OrdinaryDiffEqLinearExponentialAlgorithm, :OrdinaryDiffEqMutableCache,
-                :alg_cache, :alg_extrapolates, :dt_required, :generic_solver_docstring,
-                :get_fsalfirstlast, :isdtchangeable, :perform_step!, :unwrap_alg,
-                # DiffEqBase internal (owner of calculate_residuals!), non-public
-                :calculate_residuals!,
-                # SciMLBase internal (owner of _vec), non-public
-                :_vec,
                 # SciMLOperators internal abstract type, non-public
                 :AbstractSciMLOperator,
+                # SciMLBase internal (owner of _vec), non-public
+                :_vec,
             ),
         ),
         all_qualified_accesses_are_public = (;
             ignore = (
-                :alloc_mem,          # ExponentialUtilities internal cache allocator
-                :increment_nf!,      # OrdinaryDiffEqCore internal stats mutator
-                :set_EEst!,          # OrdinaryDiffEqCore internal error-estimate setter
-                :prepare_alg,        # DiffEqBase internal algorithm-preparation hook
+                # ExponentialUtilities internal cache allocator, non-public
+                :alloc_mem,
             ),
         ),
     ),
