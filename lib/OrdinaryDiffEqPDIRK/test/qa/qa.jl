530a0eef28eb0440849bf3315252a91188cae370
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-05 09:22:24 -0400

    Harden PDIRK public API QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- lib/OrdinaryDiffEqPDIRK/test/qa/qa.jl
+++ lib/OrdinaryDiffEqPDIRK/test/qa/qa.jl
@@ -2,9 +2,6 @@ using SciMLTesting, OrdinaryDiffEqPDIRK, Test
 
 run_qa(
     OrdinaryDiffEqPDIRK;
-    # No docs/ tree here; the umbrella manual renders this package's API.
-    api_docs_kwargs = (; rendered = false),
-    reexports_allow = union(public_api_names(SciMLBase), (:SciMLBase,)),
     explicit_imports = true,
     ei_kwargs = (;
         all_explicit_imports_are_public = (;
