a576cf441379919ab276449b35fc5935c2c58084
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-05 00:02:35 -0400

    Remove PRK blanket SciMLBase reexport
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- lib/OrdinaryDiffEqPRK/src/OrdinaryDiffEqPRK.jl
+++ lib/OrdinaryDiffEqPRK/src/OrdinaryDiffEqPRK.jl
@@ -3,16 +3,11 @@ module OrdinaryDiffEqPRK
 import OrdinaryDiffEqCore: OrdinaryDiffEqAlgorithm, OrdinaryDiffEqMutableCache,
     OrdinaryDiffEqConstantCache, constvalue, @cache,
     alg_cache, get_fsalfirstlast,
-    unwrap_alg, perform_step!, @threaded, isthreaded,
-    generic_solver_docstring
+    unwrap_alg, perform_step!, @threaded, isthreaded
 import SciMLBase: alg_order, full_cache
 import DiffEqBase: initialize!
 import MuladdMacro: @muladd
 import FastBroadcast: @..
-
-using Reexport: Reexport, @reexport
-using SciMLBase: SciMLBase
-@reexport using SciMLBase
 
 include("algorithms.jl")
 include("alg_utils.jl")
