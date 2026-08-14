214df7a0d5cb772a332dc18389d818e98046b89a
Christopher Rackauckas <accounts@chrisrackauckas.com>
2026-08-14 08:16:52 +0000

    Delete lib/DiffEqBase/test/check_error.jl

--- lib/DiffEqBase/test/check_error.jl
+++
@@ -1,7 +0,0 @@
-using DiffEqBase, SciMLBase, Test
-
-if isdefined(SciMLBase, :report_integrator_failure)
-    @test any(m -> m.module === DiffEqBase, methods(SciMLBase.report_integrator_failure))
-else
-    @test !isdefined(SciMLBase, :report_integrator_failure)
-end
