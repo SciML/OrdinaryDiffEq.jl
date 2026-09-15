17cc2e25b314ac65eb01b1f7e0f4bdcbe4e3152e
Henry <henry@la-privat.com>
2026-09-15 08:23:43 +0200

    Added SharpFineRKN6 cache and tests.

--- lib/OrdinaryDiffEqRKN/src/OrdinaryDiffEqRKN.jl
+++ lib/OrdinaryDiffEqRKN/src/OrdinaryDiffEqRKN.jl
@@ -32,8 +32,8 @@ include("interp_func.jl")
 include("interpolants.jl")
 include("rkn_perform_step.jl")
 
-export Nystrom4, FineRKN4, FineRKN5, Nystrom4VelocityIndependent,
-    Nystrom5VelocityIndependent,
+export Nystrom4, FineRKN4, FineRKN5, SharpFineRKN6,
+    Nystrom4VelocityIndependent, Nystrom5VelocityIndependent,
     IRKN3, IRKN4, DPRKN4, DPRKN5, DPRKN6, DPRKN6FM, DPRKN8, DPRKN12, ERKN4, ERKN5, ERKN7,
     RKN4
 
